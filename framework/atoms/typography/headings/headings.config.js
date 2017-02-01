1d1ff4d66cdfea523692bc4c7f2e2acdbcb3f4cd
Yannick Huard <huard.yan@gmail.com>
2017-02-01 12:12:09 +0100

    feat(typography-headings): add headings component

---
+++ framework/atoms/typography/headings/headings.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  title: 'Headings',
+  label: 'Headings',
+  status: 'ready',
+  collated: true,
+  collator(markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<p>\n${markup}\n</p>\n<!-- End: @${item.handle} -->\n`;
+  },
+  default: 'h1',
+  variants: [{
+    name: 'h1',
+    label: 'h1',
+    context: {
+      modifier: 'h1',
+    },
+  }, {
+    name: 'h2',
+    label: 'h2',
+    context: {
+      modifier: 'h2',
+    },
+  }, {
+    name: 'h3',
+    label: 'h3',
+    context: {
+      modifier: 'h3',
+    },
+  }, {
+    name: 'h4',
+    label: 'h4',
+    context: {
+      modifier: 'h4',
+    },
+  }, {
+    name: 'h5',
+    label: 'h5',
+    context: {
+      modifier: 'h5',
+    },
+  }, {
+    name: 'h6',
+    label: 'h6',
+    context: {
+      modifier: 'h6',
+    },
+  }],
+};
