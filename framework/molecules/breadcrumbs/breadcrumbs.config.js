a42ef7da848126be935cde347d3573dce67a1c3e
Kalin Chernev <kalin.chernev@gmail.com>
2017-02-01 15:15:12 +0100

    feat(breadcrumb): include pattern

---
+++ framework/molecules/breadcrumbs/breadcrumbs.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Breadcrumbs',
+  label: 'Breadcrumbs',
+  status: 'ready',
+  variants: [{
+    name: 'collapsible',
+    label: 'Collapsible',
+    context: {
+      modifier: 'collapsible',
+    },
+  }],
+};
