4766dbe3701bba5a2f76f8ad77c66ddba1a1d7ad
wesley deglise <wesley.deglise@gmail.com>
2017-02-02 13:26:25 +0100

    feat(timeline): init component (#17)
    * feat(timeline): init component
    
    * feat(timeline): add missing package.json
    
    * feat(timeline): add script and classify as molecule
    
    * fix(timelines): rename timeline-> timelines
    
    * style(typo): add missing "s"

---
+++ framework/molecules/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,8 @@
+/**
+ * Breadcrumb related behaviors.
+ */
+
+ // eslint-disable-next-line import/prefer-default-export
+export function breadcrumbs() {
+  console.error('This script needs to be rewritten without jQuery');
+}
