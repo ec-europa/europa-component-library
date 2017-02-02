a2d95fe817fc39a1fe6fbb95437dd817796f3571
Yannick Huard <huard.yan@gmail.com>
2017-02-02 14:31:14 +0100

    feat(fields): provide fields component

---
+++ framework/atoms/fields/fields.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Fields',
+  label: 'Fields',
+  status: 'ready',
+  collated: false,
+  variants: [{
+    name: 'default',
+    label: 'Default',
+  }, {
+    name: 'aligned',
+    label: 'Aligned',
+  }],
+};
