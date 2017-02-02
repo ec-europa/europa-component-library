eed7f83fe63b637953db01d299098035fa6a7e71
Kalin Chernev <kalinchernev@users.noreply.github.com>
2017-02-02 16:02:54 +0100

    feat(context-navs): include pattern (#22)
    * feat(context-navs): include pattern
    
    * refactor(context-navs): correct package.json file
    
    * refactor(context-navs): remove unnecessary file

---
+++ framework/molecules/context-navs/context-navs.js
@@ -0,0 +1,8 @@
+/**
+ * Contextual navigation scripts
+ */
+
+// eslint-disable-next-line import/prefer-default-export
+export function contextualNav() {
+  console.error('This script needs to be rewritten without jQuery');
+}
