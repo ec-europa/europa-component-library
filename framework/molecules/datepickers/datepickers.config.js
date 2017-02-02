9048bb88088c22d32e41d16107a40dc1593fccd6
Kalin Chernev <kalin.chernev@gmail.com>
2017-02-02 14:24:25 +0100

    feat(datepickers): include pattern

---
+++ framework/molecules/datepickers/datepickers.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Datepickers',
+  label: 'Datepickers',
+  status: 'wip',
+  collated: true,
+  context: {
+    _demo: {
+      scripts: 'Europa.datepicker();',
+    },
+  },
+};
