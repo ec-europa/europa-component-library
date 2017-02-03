9d426b6e0951263a0e715444081609c4619d858b
Yannick Huard <huard.yan@gmail.com>
2017-02-03 20:51:45 +0100

    feat(templating): propose a new way to create templates

--- framework/organisms/page-headers/informational/informational.config.js
+++ framework/organisms/page-headers/informational/informational.config.js
@@ -2,4 +2,19 @@ module.exports = {
   title: 'Informational',
   label: 'Informational',
   status: 'wip',
+  context: {
+    data: {
+      breadcrumb: {
+        segments: [{
+          target: '#',
+          title: 'European Commission',
+          isInternal: true,
+        }, {
+          target: '#',
+          title: 'Announcements',
+          isInternal: false,
+        }],
+      },
+    },
+  },
 };
