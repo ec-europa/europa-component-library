22c83c86fa3a9914b291e599ab118af171e3c11a
Kalin Chernev <kalin.chernev@gmail.com>
2017-02-03 10:39:27 +0100

    Updates

---
+++ components/raw/molecule-filters/filters.js
@@ -0,0 +1,8 @@
+/**
+ * Exposed filter related behaviors.
+ */
+
+// eslint-disable-next-line import/prefer-default-export
+export function filters() {
+  console.error('This script needs to be rewritten without jQuery');
+}
