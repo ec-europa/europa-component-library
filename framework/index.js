134a2829b60cfd85f18417691d6991144b7e897c
Yannick Huard <mail@yannickhuard.com>
2017-02-03 10:36:12 +0100

    feat(filters): add filters component (#28)
    * feat(filters): add filters component
    
    * feat(filters): update dependencies

--- framework/index.js
+++ framework/index.js
@@ -4,4 +4,5 @@ export * from './molecules/breadcrumbs/breadcrumbs';
 export * from './molecules/context-navs/context-navs';
 export * from './molecules/datepickers/datepickers';
 export * from './molecules/files/files';
+export * from './molecules/filters/filters';
 export * from './molecules/timelines/timelines';
