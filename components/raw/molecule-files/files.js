c888daff77344c3b13e0558b1b275191cdb9c746
Yannick Huard <huard.yan@gmail.com>
2017-02-03 09:56:54 +0100

    Updates

---
+++ components/raw/molecule-files/files.js
@@ -0,0 +1,5 @@
+document
+  .querySelectorAll('.file__translations-control')
+  .forEach(button => button.addEventListener('click', (event) => {
+    event.target.classList.toggle('is-collapsed');
+  }));
