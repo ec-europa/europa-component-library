7a678b8248494c8e3eb6693da8f32320c6d1256e
Yannick Huard <mail@yannickhuard.com>
2017-02-06 08:12:20 +0100

    feat(templating): propose a new way to create templates (#35)

--- framework/molecules/breadcrumbs/breadcrumbs.config.js
+++ framework/molecules/breadcrumbs/breadcrumbs.config.js
@@ -2,10 +2,22 @@ module.exports = {
   title: 'Breadcrumbs',
   label: 'Breadcrumbs',
   status: 'wip',
+  context: {
+    segments: [{
+      target: '#',
+      title: 'European Commission',
+      isInternal: true,
+    }, {
+      target: '#',
+      title: 'Priorities',
+      isInternal: false,
+    }, {
+      target: '#',
+      title: 'Jobs, Growth and Investment',
+      isInternal: false,
+    }],
+  },
   variants: [{
-    name: 'default',
-    label: 'Default',
-  }, {
     name: 'collapsible',
     label: 'Collapsible',
     context: {
