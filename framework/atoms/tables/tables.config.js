86368fb0b7c820d0c50e77cb988802623fd25d73
weslito <wesley.deglise@gmail.com>
2017-02-06 11:03:58 +0100

    feat(tables): init component

---
+++ framework/atoms/tables/tables.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Tables',
+  label: 'Tables',
+  status: 'wip',
+  collated: false,
+  preview: '@preview-center-transparent',
+  variants: [{
+    name: 'default',
+    label: 'Default',
+    context: {
+      modifier: '',
+    },
+  }],
+};
