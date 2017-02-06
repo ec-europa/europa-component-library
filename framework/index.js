643a83185a47dddd3c5e4448124c7e0c0ab76470
Kalin Chernev <kalin.chernev@gmail.com>
2017-02-06 11:40:32 +0100

    feat(inpage-navs): include inpage-navs

--- framework/index.js
+++ framework/index.js
@@ -6,5 +6,6 @@ export * from './molecules/context-navs/context-navs';
 export * from './molecules/datepickers/datepickers';
 export * from './molecules/files/files';
 export * from './molecules/filters/filters';
+export * from './molecules/inpage-navs/inpage-navs';
 export * from './molecules/timelines/timelines';
 export * from './organisms/profile-topbars/profile-topbars';
---
+++ framework/molecules/inpage-navs/README.md
@@ -0,0 +1,4 @@
+# Inpage navigations
+
+Floating menu containing links to headings in a long page. Note: Style guide implementation is not complete. TODO: Refactor Drupal js to be technology agnostic.
+
