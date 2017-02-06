a2e317f989dfbf4790ceede620800e7944d93dfa
Kalin Chernev <kalinchernev@users.noreply.github.com>
2017-02-06 12:05:51 +0100

    feat(inpage-navs): include inpage-navs (#44)
    * feat(inpage-navs): include inpage-navs
    
    * refactor(inpage-navs): remove js inclusion

---
+++ framework/molecules/inpage-navs/inpage-navs.js
@@ -0,0 +1,8 @@
+/**
+ * Helper methods for the inpage navigation component.
+ */
+
+// eslint-disable-next-line import/prefer-default-export
+export function inpageNav() {
+  console.error('This script needs to be rewritten without jQuery');
+}
