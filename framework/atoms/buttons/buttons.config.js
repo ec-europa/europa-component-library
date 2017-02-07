8b1ad97f8be7855d57968bfe7cb4dcd30a266025
Yannick Huard <mail@yannickhuard.com>
2017-02-07 08:38:57 +0100

    feat(buttons): turn buttons variants into collections and variants (#51)
    * feat(buttons): turn buttons variants into collections and variants
    
    * feat(buttons): add button groups component

--- framework/atoms/buttons/buttons.config.js
+++ framework/atoms/buttons/buttons.config.js
@@ -1,44 +1,6 @@
 module.exports = {
   title: 'Buttons',
   label: 'Buttons',
-  status: 'wip',
-  collated: false,
+  prefix: 'atom-buttons',
   preview: '@preview-center-transparent',
-  variants: [{
-    name: 'default',
-    label: 'Default',
-    context: {
-      modifier: 'default',
-    },
-  }, {
-    name: 'primary',
-    label: 'Primary',
-    context: {
-      modifier: 'primary',
-    },
-  }, {
-    name: 'secondary',
-    label: 'Secondary',
-    context: {
-      modifier: 'secondary',
-    },
-  }, {
-    name: 'ctn',
-    label: 'Call to action',
-    context: {
-      modifier: 'ctn',
-    },
-  }, {
-    name: 'ctn--border',
-    label: 'Call to action with border',
-    context: {
-      modifier: 'ctn--border',
-    },
-  }, {
-    name: 'menu',
-    label: 'Menu',
-    context: {
-      modifier: 'menu',
-    },
-  }],
 };
