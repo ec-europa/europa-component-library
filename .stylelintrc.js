7e4f4e51a82c921fa6c11940d350526fcd6259fe
Yannick Huard <huard.yan@gmail.com>
2017-02-08 10:20:41 +0100

    chore(cs): activate sass guidelines

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: [
-    // 'stylelint-config-sass-guidelines',
+    'stylelint-config-sass-guidelines',
     'stylelint-config-standard'
   ],
   plugins: ['stylelint-selector-bem-pattern'],
