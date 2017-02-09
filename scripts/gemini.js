630991f1b065162826cf8ec5152c3d3e506f4e28
Yannick Huard <huard.yan@gmail.com>
2017-02-09 12:15:46 +0100

    chore(visual-regression): update process

--- scripts/gemini.js
+++ scripts/gemini.js
@@ -3,29 +3,49 @@ const path = require('path');
 const express = require('express');
 const serveStatic = require('serve-static');
 
+// Check if './dist' is present
+
 const gemini = new Gemini(path.resolve(__dirname, '../.gemini.conf.js'));
 
 // Start local server
 const app = express();
-
 app.use(serveStatic(path.resolve(__dirname, '../dist')));
-const server = app.listen(3000, () => {
+
+const server = require('http').createServer(app);
+
+function closeServer(code) {
+  console.info('Closing server...');
+  server.close(() => {
+    console.info('Server closed.');
+    process.exit(code);
+  });
+}
+
+server.listen(3000, () => {
   if (process.env.GEMINI_TARGET === 'test') {
     // Run the tests
     gemini.test('./test/visual/tests', {
-      reporters: ['flat'],
+      reporters: ['flat', 'html'],
+    }).then(({ errored, failed }) => {
+      const exitCode = errored || failed ? 1 : 0;
+      closeServer(exitCode);
+    }).catch(() => {
+      closeServer(1);
     });
   } else if (process.env.GEMINI_TARGET === 'update') {
     // Run the tests
     gemini.update('./test/visual/tests', {
       reporters: ['flat'],
+    }).then(({ errored, failed }) => {
+      const exitCode = errored || failed ? 1 : 0;
+      closeServer(exitCode);
+    }).catch(() => {
+      closeServer(1);
     });
   }
 });
 
-gemini.on(gemini.events.END_RUNNER, () => {
-  console.info('Closing server...');
-  server.close(() => {
-    console.info('Server closed.');
-  });
+// ctrl-c to trigger
+process.on('SIGINT', () => {
+  closeServer(1);
 });
