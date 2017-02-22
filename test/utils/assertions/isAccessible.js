b2c8d377ab18a8a4603be801d8af012708da33ec
Yannick Huard <mail@yannickhuard.com>
2017-02-22 17:42:38 +0100

    feat(header): add visual regression and a11y tests
    a11y check reports contrast issue, but it could be linked to https://github.com/dequelabs/axe-core/issues/273

--- test/utils/assertions/isAccessible.js
+++ test/utils/assertions/isAccessible.js
@@ -2,8 +2,26 @@
 
 const chai = require('chai');
 
+function formatErrors(violations) {
+  let message = '';
+  violations.forEach((violation) => {
+    message += `- [${violation.impact}] ${violation.description}: `;
+    violation.nodes.forEach((node) => {
+      message += `-- [${node.impact}] ${node.target}: `;
+      node.any.forEach((err) => {
+        message += `--- [${err.impact}] ${err.message}`;
+      });
+    });
+  });
+
+  return message;
+}
+
 module.exports = function isAccessible() {
   const { result, err } = this._obj;
   new chai.Assertion(err).to.be.null;
-  new chai.Assertion(result.violations).to.be.empty;
+
+  this.assert(result.violations.length === 0,
+    `expected a11y report to be empty but instead got: ${formatErrors(result.violations)}`,
+    'expected a11y report to contain errors');
 };
