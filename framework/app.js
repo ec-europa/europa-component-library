c26c56f39062343a1c768f1ff74650a1678231e8
Yannick Huard <mail@yannickhuard.com>
2017-03-07 16:10:01 +0100

    fix(components): fix broken components

--- framework/app.js
+++ framework/app.js
@@ -4,6 +4,10 @@
 import './index';
 import './index.scss';
 
+// Example
+// In production, it would be: import '@ec-europa/ecl-banners';
+import './components/banners/';
+
 if (module.hot) {
   module.hot.accept();
 }
