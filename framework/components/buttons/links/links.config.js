f5fa59c6e005843d9ee18f6ec4c7bf0966e78fa8
Romain Emery <emery.romain@gmail.com>
2017-03-15 10:20:37 +0100

    refactor(buttons): improve display of button links

--- framework/components/buttons/links/links.config.js
+++ framework/components/buttons/links/links.config.js
@@ -1,27 +1,19 @@
-const buttonVariants = require('../variants.json');
-
-const variants = [];
-
-buttonVariants.forEach((v) => {
-  variants.push(Object.assign({}, v, {
-    context: Object.assign({}, v.context, {
-      label: 'Link button',
-      to: '#',
-    }),
-  }));
-
-  variants.push(Object.assign({}, v, {
-    context: Object.assign({}, v.context, {
-      label: 'External link button',
-      to: 'http://example.com',
-    }),
-  }));
-});
+const variants = require('../variants.json');
 
 module.exports = {
   title: 'Links',
   label: 'Links',
   status: 'wip',
   collated: true,
+  collator(markup, item) {
+    return `
+      <!-- Start: @${item.handle} -->\n
+      <div style="margin-bottom: 1rem;">\n
+        <a href="#" class="ecl-button ecl-button--${item.context.modifier}">Link button</a>\n
+        <a href="http://example.com" class="ecl-button ecl-button--${item.context.modifier}">External link button</a>\n
+      </div>\n
+      <!-- End: @${item.handle} -->\n
+    `;
+  },
   variants,
 };
