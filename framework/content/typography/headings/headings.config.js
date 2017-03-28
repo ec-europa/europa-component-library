3667ac1d6a3618c806fb6fac0fa83ccf35f994f5
Yannick Huard <mail@yannickhuard.com>
2017-03-28 15:33:13 +0200

    Upgrade headings

--- framework/content/typography/headings/headings.config.js
+++ framework/content/typography/headings/headings.config.js
@@ -1,47 +1,56 @@
 module.exports = {
   title: 'Headings',
   label: 'Headings',
-  status: 'planned',
+  status: 'ready',
   collated: true,
-  collator(markup, item) {
-    return `<!-- Start: @${item.handle} -->\n<p>\n${markup}\n</p>\n<!-- End: @${item.handle} -->\n`;
-  },
   default: 'h1',
   variants: [{
     name: 'h1',
     label: 'h1',
     context: {
-      modifier: 'h1',
+      element: 'h1',
+      class: 'ecl-h1',
+      text: 'Heading <h1> with class .ecl-h1',
     },
   }, {
     name: 'h2',
     label: 'h2',
     context: {
-      modifier: 'h2',
+      element: 'h2',
+      class: 'ecl-h2',
+      text: 'Heading <h2> with class .ecl-h2',
     },
   }, {
     name: 'h3',
     label: 'h3',
     context: {
-      modifier: 'h3',
+      element: 'h3',
+      class: 'ecl-h3',
+      text: 'Heading <h3> with class .ecl-h3',
     },
   }, {
     name: 'h4',
     label: 'h4',
     context: {
-      modifier: 'h4',
+      element: 'h4',
+      class: 'ecl-h4',
+      text: 'Heading <h4> with class .ecl-h4',
     },
   }, {
     name: 'h5',
     label: 'h5',
     context: {
-      modifier: 'h5',
+      element: 'h5',
+      class: 'ecl-h5',
+      text: 'Heading <h5> with class .ecl-h5',
     },
   }, {
     name: 'h6',
     label: 'h6',
     context: {
-      modifier: 'h6',
+      element: 'h6',
+      class: 'ecl-h6',
+      text: 'Heading <h6> with class .ecl-h6',
     },
   }],
 };
