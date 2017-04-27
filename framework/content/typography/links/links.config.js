f1b51d18fd58d3bdb86e70fa63506dad1f4aa410
Voidtek <vdacosta@voidtek.com>
2017-04-27 16:44:35 +0200

    NEPT-976: Content Links

--- framework/content/typography/links/links.config.js
+++ framework/content/typography/links/links.config.js
@@ -1,5 +1,32 @@
 module.exports = {
   title: 'Links',
   label: 'Links',
-  status: 'planned',
+  status: 'wip',
+  collated: true,
+  default: 'default',
+  variants: [{
+    name: 'default',
+    label: 'default',
+    context: {
+      href: '/url',
+      class: '',
+      text: 'Link element',
+    },
+  }, {
+    name: 'external',
+    label: 'external',
+    context: {
+      href: 'http://',
+      class: 'is-external',
+      text: 'External link',
+    },
+  }, {
+    name: 'smaller',
+    label: 'smaller',
+    context: {
+      href: '#',
+      class: 'smaller',
+      text: 'Smaller text link',
+    },
+  }],
 };
