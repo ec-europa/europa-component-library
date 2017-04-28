0a9bd031de01b9595ec696e8168b5b700d073af9
Voidtek <vdacosta@voidtek.com>
2017-04-28 10:25:05 +0200

    NEPT-976: Content Lists - context

--- framework/content/typography/lists/lists.config.js
+++ framework/content/typography/lists/lists.config.js
@@ -1,5 +1,36 @@
 module.exports = {
   title: 'Lists',
   label: 'Lists',
-  status: 'planned',
+  status: 'ready',
+  collated: true,
+  default: 'ul_ul',
+  variants: [{
+    name: 'ul_ul',
+    label: 'Lists: ul > li > ul > li',
+    context: {
+      element_l1: 'ul',
+      element_l2: 'ul',
+    },
+  }, {
+    name: 'ul_ol',
+    label: 'Lists: ul > li > ol > li',
+    context: {
+      element_l1: 'ul',
+      element_l2: 'ol',
+    },
+  }, {
+    name: 'ol_ul',
+    label: 'Lists: ol > li > ul > li',
+    context: {
+      element_l1: 'ol',
+      element_l2: 'ul',
+    },
+  }, {
+    name: 'ol_ol',
+    label: 'Lists: ol > li > ol > li',
+    context: {
+      element_l1: 'ol',
+      element_l2: 'ol',
+    },
+  }],
 };
