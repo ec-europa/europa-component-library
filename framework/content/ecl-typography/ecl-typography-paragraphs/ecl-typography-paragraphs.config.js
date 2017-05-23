19237a083b1d0626b3099682bece62cb42592092
Romain Emery <emery.romain@gmail.com>
2017-05-23 10:38:00 +0200

    refactor(content): update paragraph

--- framework/content/ecl-typography/ecl-typography-paragraphs/ecl-typography-paragraphs.config.js
+++ framework/content/ecl-typography/ecl-typography-paragraphs/ecl-typography-paragraphs.config.js
@@ -1,22 +1,46 @@
 module.exports = {
   title: 'Paragraphs',
   label: 'Paragraphs',
-  status: 'planned',
-  collated: false,
-  default: 'intro',
+  status: 'ready',
+  collated: true,
+  collator(markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<h3 class="ecl-h3">${item.label}</h3>\n${markup}\n<!-- End: @${item.handle} -->\n`;
+  },
+  default: 'l',
   variants: [
     {
-      name: 'intro',
-      label: 'Intro',
+      name: 'l',
+      label: 'Leading paragraph',
       context: {
-        modifier: 'paragraph--intro',
+        modifier: 'ecl-p ecl-p--l',
       },
     },
     {
-      name: 'small',
-      label: 'Small body text',
+      name: 'm',
+      label: 'Medium paragraph',
       context: {
-        modifier: 'paragraph--small',
+        modifier: 'ecl-p ecl-p--m',
+      },
+    },
+    {
+      name: 's',
+      label: 'Small paragraph',
+      context: {
+        modifier: 'ecl-p ecl-p--s',
+      },
+    },
+    {
+      name: 'xs',
+      label: 'Extra small paragraph',
+      context: {
+        modifier: 'ecl-p ecl-p--xs',
+      },
+    },
+    {
+      name: 'xxs',
+      label: 'Extra extra small paragraph',
+      context: {
+        modifier: 'ecl-p ecl-p--xxs',
       },
     },
   ],
