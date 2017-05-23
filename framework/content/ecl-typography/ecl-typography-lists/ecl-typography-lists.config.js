077f80145b2553d0efbf72b0def5eb8c5f5836aa
Romain Emery <emery.romain@gmail.com>
2017-05-23 13:27:22 +0200

    refactor(content): add lists

--- framework/content/ecl-typography/ecl-typography-lists/ecl-typography-lists.config.js
+++ framework/content/ecl-typography/ecl-typography-lists/ecl-typography-lists.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   title: 'Lists',
   label: 'Lists',
-  status: 'planned',
+  status: 'ready',
+  collated: true,
+  collator(markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<h3 class="ecl-h ecl-h--h3">${item.label}</h3>\n${markup}\n<!-- End: @${item.handle} -->\n`;
+  },
+  variants: [
+    {
+      name: 'unordered',
+      label: 'Unordered list',
+    },
+    {
+      name: 'ordered',
+      label: 'Ordered list',
+    },
+  ],
+  default: 'unordered',
 };
