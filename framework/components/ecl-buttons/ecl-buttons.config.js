6d05e8d1ec06b9fa16bb42698fb94f9824e2d2ec
Yannick Huard <mail@yannickhuard.com>
2017-05-24 13:44:45 +0200

    refactor(buttons): refactor buttons’ markup and styling - INNO-307

--- framework/components/ecl-buttons/ecl-buttons.config.js
+++ framework/components/ecl-buttons/ecl-buttons.config.js
@@ -1,7 +1,19 @@
+const variants = require('./variants.json');
+
 module.exports = {
   title: 'Buttons',
   label: 'Buttons',
   preview: '@preview-center-transparent',
   tags: ['atom'],
-  status: 'broken',
+  status: 'wip',
+  collated: true,
+  collator(markup, item) {
+    return `
+      <!-- Start: @${item.handle} -->\n
+      <h4>${item.label}\n</h4>\n
+      ${markup}\n
+      <!-- End: @${item.handle} -->\n
+    `;
+  },
+  variants,
 };
