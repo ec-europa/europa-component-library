476e1fccbbcdfc805cb0b1b97a573f44b50b9f03
Romain Emery <emery.romain@gmail.com>
2017-05-29 14:24:04 +0200

    refactor(tables): update tables

--- framework/content/ecl-tables/ecl-tables.config.js
+++ framework/content/ecl-tables/ecl-tables.config.js
@@ -1,6 +1,47 @@
 module.exports = {
   title: 'Tables',
   label: 'Tables',
-  status: 'planned',
-  tags: ['atom'],
+  status: 'ready',
+  collator(markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<h3 class="ecl-heading ecl-heading--h3">${item.label}</h3>\n${markup}\n<!-- End: @${item.handle} -->\n`;
+  },
+  variants: [
+    {
+      name: 'default',
+      label: 'Default table',
+      context: {
+        _demo: {
+          scripts: 'Europa.eclTables();',
+        },
+      },
+    },
+    {
+      name: 'empty',
+      label: 'Table with empty heading',
+      context: {
+        _demo: {
+          scripts: 'Europa.eclTables();',
+        },
+      },
+    },
+    {
+      name: 'colspan',
+      label: 'Table with colspan',
+      context: {
+        _demo: {
+          scripts: 'Europa.eclTables();',
+        },
+      },
+    },
+    {
+      name: 'colspan-empty',
+      label: 'Table with colspan and empty heading',
+      context: {
+        _demo: {
+          scripts: 'Europa.eclTables();',
+        },
+      },
+    },
+  ],
+  default: 'default',
 };
