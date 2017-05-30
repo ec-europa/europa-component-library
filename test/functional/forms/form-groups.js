0210571c837eb0ea7b604d0676fe72800b60d609
Yannick Huard <mail@yannickhuard.com>
2017-05-30 15:37:58 +0200

    feat(text-input): provide ecl-text-input class - INNO-520 (#150)
    * Import basic styles for text inputs
    
    * Finalize component
    
    * Generate tests
    
    * Fix form-group and help-block
    
    * Fix dependencies
    
    * Add tests for all 3 new components
    
    * Update site-headers screenshots

---
+++ test/functional/forms/form-groups.js
@@ -0,0 +1,31 @@
+describe('forms-form-group', () => {
+  before(() => {
+    // Set viewport size
+    browser.setViewportSize({
+      width: 1400,
+      height: 600,
+    });
+
+    browser.pause(1000);
+
+    // Go to url
+    browser.url(`ecl-forms-form-groups.html`);
+    // Make sure the browser has finished painting
+    browser.pause(1000);
+    // Inject axe-core (for accessibility tests)
+    browser.injectAxeCore();
+  });
+
+  // Normal state
+  it('should match the reference screenshot', () => {
+    const screenshots = browser.checkDocument({
+      name: `forms/form-groups`,
+    });
+    expect(screenshots).to.matchReference();
+  });
+
+  it('should be accessible', () => {
+    const a11yReport = browser.runAxeCore('ecl-form-group').value;
+    expect(a11yReport).to.be.accessible;
+  });
+});
