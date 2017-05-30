b7fff39975de62e2c9f6726168a9c93ac0698730
Kalin Chernev <kalin.chernev@gmail.com>
2017-05-30 10:35:30 +0200

    feat(lang-select-sites): refactor

---
+++ test/functional/ecl-lang-select-sites.js
@@ -0,0 +1,27 @@
+describe('ecl-lang-select-sites', () => {
+  before(() => {
+    // Set viewport size
+    browser.setViewportSize({
+      width: 300,
+      height: 90,
+    });
+
+    browser.pause(1000);
+
+    // Go to url
+    browser.url('ecl-lang-select-sites.html');
+
+    // Make sure the browser has finished painting
+    browser.pause(1000);
+  });
+
+  // Normal state
+  context('with plain state', () => {
+    it('should match the reference screenshot', () => {
+      const screenshots = browser.checkDocument({
+        name: 'ecl-lang-select-sites',
+      });
+      expect(screenshots).to.matchReference();
+    });
+  });
+});
