3a928605d1c9776769118ff32a36c9579155b994
Yannick Huard <mail@yannickhuard.com>
2017-05-30 07:52:38 +0200

    Update refs

--- test/functional/footers.js
+++ test/functional/footers.js
@@ -1,11 +1,11 @@
-const variants = ["default", "custom"];
+const variants = ['default', 'custom'];
 
-describe("footers", () => {
+describe('footers', () => {
   before(() => {
     // Set viewport size
     browser.setViewportSize({
       width: 1400,
-      height: 600
+      height: 600,
     });
 
     browser.pause(1000);
@@ -23,16 +23,16 @@ describe("footers", () => {
       });
 
       // Normal state
-      context("with plain state", () => {
-        it("should match the reference screenshot", () => {
+      context('with plain state', () => {
+        it('should match the reference screenshot', () => {
           const screenshots = browser.checkDocument({
-            name: `footers/${variant}`
+            name: `footers/${variant}`,
           });
           expect(screenshots).to.matchReference();
         });
 
-        it("should be accessible", () => {
-          const a11yReport = browser.runAxeCore("ecl-footer").value;
+        it('should be accessible', () => {
+          const a11yReport = browser.runAxeCore('ecl-footer').value;
           expect(a11yReport).to.be.accessible;
         });
       });
