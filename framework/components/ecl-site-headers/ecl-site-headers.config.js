c825049a8408f085fff309092b73935ecf524dc9
Yannick Huard <mail@yannickhuard.com>
2017-05-31 11:08:49 +0200

    Correct statuses

--- framework/components/ecl-site-headers/ecl-site-headers.config.js
+++ framework/components/ecl-site-headers/ecl-site-headers.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Site headers',
   label: 'Site headers',
   tags: ['organism'],
-  status: 'ready',
+  status: 'wip',
   context: {
     global: {
       language: 'en',
