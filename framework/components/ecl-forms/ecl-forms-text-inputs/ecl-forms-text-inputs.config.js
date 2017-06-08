ec9cb153c223bcc4d4ab5eeb0dbb699ff069d9a5
Yannick Huard <mail@yannickhuard.com>
2017-06-08 09:04:31 +0200

    Refactor @ec-europa/ecl-forms-text-inputs template

--- framework/components/ecl-forms/ecl-forms-text-inputs/ecl-forms-text-inputs.config.js
+++ framework/components/ecl-forms/ecl-forms-text-inputs/ecl-forms-text-inputs.config.js
@@ -2,4 +2,47 @@ module.exports = {
   title: 'Text inputs',
   label: 'Text inputs',
   status: 'ready',
+  collated: true,
+  collator(markup, item) {
+    return `
+      <!-- Start: @${item.handle} -->\n
+      <label for="${item.context.id}">${item.label}\n</label>\n
+      ${markup}\n
+      <!-- End: @${item.handle} -->\n
+    `;
+  },
+  variants: [
+    {
+      name: 'default',
+      label: 'Normal input',
+      context: {
+        id: 'example-input-id-1',
+        extraAttributes: [
+          { name: 'placeholder', value: 'Some placeholder text.' },
+        ],
+      },
+    },
+    {
+      name: 'disabled',
+      label: 'Disabled input',
+      context: {
+        id: 'example-input-id-2',
+        isDisabled: true,
+        extraAttributes: [
+          { name: 'placeholder', value: 'Some placeholder text.' },
+        ],
+      },
+    },
+    {
+      name: 'with-error',
+      label: 'Input with error',
+      context: {
+        id: 'example-input-id-3',
+        hasError: true,
+        extraAttributes: [
+          { name: 'placeholder', value: 'Some placeholder text.' },
+        ],
+      },
+    },
+  ],
 };
