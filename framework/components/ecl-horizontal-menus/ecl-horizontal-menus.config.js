5b202c6c2fea1b5416b30fba3d961da3e6d57895
Romain Emery <emery.romain@gmail.com>
2017-06-12 15:35:42 +0200

    feat(horizontal-menu): init component

---
+++ framework/components/ecl-horizontal-menus/ecl-horizontal-menus.config.js
@@ -0,0 +1,11 @@
+const navigation = 'test';
+
+module.exports = {
+  title: 'Horizontal menus',
+  label: 'Horizontal menus',
+  status: 'ready',
+  tags: ['organism'],
+  context: {
+    navigationTree: navigation,
+  },
+};
