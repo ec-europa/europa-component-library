002bf277e9d6982290de2fbd0f2bb26dda88e62e
Kalin Chernev <kalin.chernev@gmail.com>
2017-06-15 13:43:36 +0200

    feat(datepickers): add icon

--- framework/index.js
+++ framework/index.js
@@ -1,7 +1,6 @@
 // Export
 
 export * from './components/ecl-context-navs/context-navs';
-export * from './components/ecl-datepickers/datepickers';
 export * from './components/ecl-expandables/expandables';
 export * from './components/ecl-files/files';
 export * from './components/ecl-filters/filters';
