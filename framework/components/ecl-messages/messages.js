0b88553990147c0886d796bd6cfba6c84ebd25c0
Romain Emery <emery.romain@gmail.com>
2017-06-20 14:15:17 +0200

    refactor(messages): fix javascript

--- framework/components/ecl-messages/messages.js
+++ framework/components/ecl-messages/messages.js
@@ -9,7 +9,7 @@ function dismissMessage(message) {
 
 // Helper method to automatically attach the event listener to all the messages on page load
 export function initMessages() {
-  const selectorClass = 'ecl-message__close';
+  const selectorClass = 'ecl-message__dismiss';
 
   const messages = Array.prototype.slice.call(
     document.getElementsByClassName(selectorClass)
