379eb0d465ab0c9e68a120300045311b5520aee3
Yannick Huard <mail@yannickhuard.com>
2017-06-21 16:35:23 +0200

    Refactor template and update refs

--- framework/components/ecl-accordions/ecl-accordions.config.js
+++ framework/components/ecl-accordions/ecl-accordions.config.js
@@ -9,5 +9,27 @@ module.exports = {
         ECL.accordions();
       });`,
     },
+    panels: [
+      {
+        id: 'pan1',
+        isExpanded: true,
+        heading: {
+          title:
+            'Jobs, Growth, Investment and Competitiveness with an additional quite long string',
+          icon: 'growth',
+        },
+        body:
+          '<p>Bullfights. Bull hockey. Do you like this? The bull is stabbed, prodded, beaten. The bull is wounded. The bull is tired before the matador ever steps into the ring. Now, is that victory? Of course it is. Wanna know the secret to winning? Creative sportsmanship. In other words, one has to rig the game.</p>',
+      },
+      {
+        id: 'pan2',
+        heading: {
+          title: 'Energy Union',
+          icon: 'energy',
+        },
+        body:
+          '<p>Bullfights. Bull hockey. Do you like this? The bull is stabbed, prodded, beaten. The bull is wounded. The bull is tired before the matador ever steps into the ring. Now, is that victory? Of course it is. Wanna know the secret to winning? Creative sportsmanship. In other words, one has to rig the game.</p>',
+      },
+    ],
   },
 };
