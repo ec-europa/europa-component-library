732f7a004ebabd7f046e3f5f688297bbaf3743a4
Yannick Huard <mail@yannickhuard.com>
2017-06-21 21:25:45 +0200

    Update tests

--- framework/components/ecl-forms/ecl-forms-labels/ecl-forms-labels.config.js
+++ framework/components/ecl-forms/ecl-forms-labels/ecl-forms-labels.config.js
@@ -2,4 +2,20 @@ module.exports = {
   title: 'Labels',
   label: 'Labels',
   status: 'ready',
+  collated: true,
+  variants: [
+    {
+      name: 'default',
+      context: {
+        label: 'Some label',
+      },
+    },
+    {
+      name: 'disabled',
+      context: {
+        label: 'Some disabled label',
+        isDisabled: true,
+      },
+    },
+  ],
 };
--- test/functional/forms/labels.js
+++ test/functional/forms/labels.js
@@ -1,31 +1,38 @@
+const variants = ['default', 'disabled'];
+
 describe('forms-labels', () => {
   before(() => {
     // Set viewport size
     browser.setViewportSize({
-      width: 1400,
+      width: 800,
       height: 600,
     });
 
     browser.pause(1000);
-
-    // Go to url
-    browser.url(`ecl-forms-labels.html`);
-    // Make sure the browser has finished painting
-    browser.pause(1000);
-    // Inject axe-core (for accessibility tests)
-    browser.injectAxeCore();
   });
 
-  // Normal state
-  it('should match the reference screenshot', () => {
-    const screenshots = browser.checkDocument({
-      name: `forms/labels`,
+  variants.forEach(variant => {
+    describe(`--${variant}`, () => {
+      before(() => {
+        // Go to url
+        browser.url(`ecl-forms-labels--${variant}.html`);
+        // Make sure the browser has finished painting
+        browser.pause(1000);
+        // Inject axe-core (for accessibility tests)
+        browser.injectAxeCore();
+      });
+
+      it('should match the reference screenshot', () => {
+        const screenshots = browser.checkDocument({
+          name: `forms/labels/${variant}`,
+        });
+        expect(screenshots).to.matchReference();
+      });
+
+      it('should be accessible', () => {
+        const a11yReport = browser.runAxeCore('ecl-form-label').value;
+        expect(a11yReport).to.be.accessible;
+      });
     });
-    expect(screenshots).to.matchReference();
-  });
-
-  it('should be accessible', () => {
-    const a11yReport = browser.runAxeCore('ecl-form-label').value;
-    expect(a11yReport).to.be.accessible;
   });
 });
