f72c641a6b2dcc28f3ec7d2ad962dc13a4ea62b0
Romain Emery <emery.romain@gmail.com>
2017-06-28 08:54:22 +0200

    feat(dropdown): markup and logic improvements

--- framework/components/ecl-link-blocks/ecl-link-blocks.config.js
+++ framework/components/ecl-link-blocks/ecl-link-blocks.config.js
@@ -3,21 +3,40 @@ module.exports = {
   label: 'Link blocks',
   status: 'ready',
   tags: ['molecule'],
-  context: {
-    linkBlockTitle: 'Block of links',
-    links: [
-      {
-        target: '#',
-        title: 'European Commission',
-      },
-      {
-        target: '#',
-        title: 'Priorities',
-      },
-      {
-        target: '#',
-        title: 'Jobs, Growth and Investment',
-      },
-    ],
+  collated: true,
+  collator(markup, item) {
+    return `
+      <!-- Start: @${item.handle} -->\n
+      <h3 class="ecl-heading ecl-heading--h3">${item.label}\n</h3>\n
+      ${markup}\n
+      <!-- End: @${item.handle} -->\n
+    `;
   },
+  default: 'standalone',
+  variants: [
+    {
+      name: 'standalone',
+      label: 'Standalone block of links',
+      context: {
+        links: [
+          { href: '#', label: 'European Commission' },
+          { href: '#', label: 'Priorities' },
+          { href: '#', label: 'Jobs, Growth and Investment' },
+        ],
+      },
+    },
+    {
+      name: 'wrapper',
+      label: 'Block of links with wrapper',
+      context: {
+        wrapper: true,
+        title: 'More information',
+        links: [
+          { href: '#', label: 'European Commission' },
+          { href: '#', label: 'Priorities' },
+          { href: '#', label: 'Jobs, Growth and Investment' },
+        ],
+      },
+    },
+  ],
 };
