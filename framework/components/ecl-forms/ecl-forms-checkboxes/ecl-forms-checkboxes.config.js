5b81cd32d0d03527e53a0d5473b4feff04e10401
Romain Emery <emery.romain@gmail.com>
2017-06-29 07:45:23 +0200

    refactor(checkboxes): upgrade component

--- framework/components/ecl-forms/ecl-forms-checkboxes/ecl-forms-checkboxes.config.js
+++ framework/components/ecl-forms/ecl-forms-checkboxes/ecl-forms-checkboxes.config.js
@@ -1,5 +1,44 @@
 module.exports = {
   title: 'Checkboxes',
   label: 'Checkboxes',
-  status: 'planned',
+  status: 'ready',
+  collated: true,
+  collator(markup, item) {
+    return `
+      <!-- Start: @${item.handle} -->\n
+      <div class="ecl-u-mv-s">\n
+      ${markup}\n
+      </div>\n
+      <!-- End: @${item.handle} -->\n
+    `;
+  },
+  default: 'default',
+  variants: [
+    {
+      name: 'default',
+      context: {
+        label: 'Normal checkbox',
+        id: 'example-checkbox-id-1',
+        name: 'example-checkbox-name-1',
+      },
+    },
+    {
+      name: 'disabled',
+      context: {
+        label: 'Disabled checkbox',
+        id: 'example-checkbox-id-2',
+        name: 'example-checkbox-name-2',
+        isDisabled: true,
+      },
+    },
+    {
+      name: 'with-error',
+      context: {
+        label: 'Checkbox with error',
+        id: 'example-checkbox-id-3',
+        name: 'example-checkbox-name-3',
+        hasError: true,
+      },
+    },
+  ],
 };
