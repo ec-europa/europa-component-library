38e592417ed0296afb3ac6510f76447b83611b3d
Yannick Huard <mail@yannickhuard.com>
2017-06-29 16:02:11 +0200

    Refactor radios

--- framework/components/ecl-forms/ecl-forms-radios/ecl-forms-radios.config.js
+++ framework/components/ecl-forms/ecl-forms-radios/ecl-forms-radios.config.js
@@ -2,27 +2,46 @@ module.exports = {
   title: 'Radios',
   label: 'Radios',
   status: 'ready',
-  context: {
-    name: 'radio-example',
-    id: 'example-1',
-    desc: 'Small description',
-    options: [
-      {
+  collated: true,
+  variants: [
+    {
+      name: 'default',
+      context: {
         id: 'opt-1',
+        name: 'opt-1',
         value: 'opt-1',
-        label: 'option 1',
+        label: 'Default',
       },
-      {
+    },
+    {
+      name: 'checked',
+      context: {
         id: 'opt-2',
+        name: 'opt-2',
         value: 'opt-2',
-        label: 'option 2',
+        label: 'Checked option',
         checked: true,
       },
-      {
+    },
+    {
+      name: 'disabled',
+      context: {
         id: 'opt-3',
+        name: 'opt-3',
         value: 'opt-3',
-        label: 'option 3',
+        label: 'Disabled option',
+        isDisabled: true,
       },
-    ],
-  },
+    },
+    {
+      name: 'error',
+      context: {
+        id: 'opt-4',
+        name: 'opt-4',
+        value: 'opt-4',
+        label: 'Option with error',
+        hasError: true,
+      },
+    },
+  ],
 };
--- test/functional/forms/radios.js
+++ test/functional/forms/radios.js
@@ -1,31 +1,33 @@
-describe('forms-radios', () => {
-  before(() => {
-    // Set viewport size
-    browser.setViewportSize({
-      width: 800,
-      height: 400,
+const variants = ['default', 'disabled', 'error'];
+
+variants.forEach(variant => {
+  describe(`ecl-forms-radios--${variant}`, () => {
+    before(() => {
+      // Set viewport size
+      browser.setViewportSize({
+        width: 600,
+        height: 300,
+      });
+      // Go to url
+      browser.url(`ecl-forms-radios--${variant}.html`);
+
+      // Make sure the browser has finished painting
+      browser.pause(1000);
+      // Inject axe-core (for accessibility tests)
+      browser.injectAxeCore();
     });
 
-    browser.pause(1000);
-
-    // Go to url
-    browser.url(`ecl-forms-radios.html`);
-    // Make sure the browser has finished painting
-    browser.pause(1000);
-    // Inject axe-core (for accessibility tests)
-    browser.injectAxeCore();
-  });
-
-  // Normal state
-  it('should match the reference screenshot', () => {
-    const screenshots = browser.checkDocument({
-      name: `forms/radios`,
+    // Normal state
+    it('should match the reference screenshot', () => {
+      const screenshots = browser.checkDocument({
+        name: `forms/radios/${variant}`,
+      });
+      expect(screenshots).to.matchReference();
     });
-    expect(screenshots).to.matchReference();
-  });
 
-  it('should be accessible', () => {
-    const a11yReport = browser.runAxeCore('ecl-radio').value;
-    expect(a11yReport).to.be.accessible;
+    it('should be accessible', () => {
+      const a11yReport = browser.runAxeCore('ecl-radio').value;
+      expect(a11yReport).to.be.accessible;
+    });
   });
 });
