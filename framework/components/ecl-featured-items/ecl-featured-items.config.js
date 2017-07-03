1bc963e3a8626f4df10e0607f11c2a098b23a3a2
Romain Emery <emery.romain@gmail.com>
2017-07-03 15:42:38 +0200

    refactor(featured-items): add screenshots

--- framework/components/ecl-featured-items/ecl-featured-items.config.js
+++ framework/components/ecl-featured-items/ecl-featured-items.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
     },
     {
-      name: 'translations',
+      name: 'extended',
       label: 'Featured item extended',
       context: {
         variant: 'extended',
