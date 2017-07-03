a8f938ff21345e067ce15020e37ef67183499fce
Yannick Huard <mail@yannickhuard.com>
2017-07-03 15:36:27 +0200

    feat(radios): upgrade radios - INNO-631 (#223)
    * feat(radios): upgrade radios - INNO-631
    
    * Add surrounding role="radiogroup"
    
    * Fix a11y
    
    * Fix display
    
    * Refactor radios

--- framework/components/ecl-forms/ecl-forms-radios/ecl-forms-radios.config.js
+++ framework/components/ecl-forms/ecl-forms-radios/ecl-forms-radios.config.js
@@ -1,5 +1,47 @@
 module.exports = {
   title: 'Radios',
   label: 'Radios',
-  status: 'planned',
+  status: 'ready',
+  collated: true,
+  variants: [
+    {
+      name: 'default',
+      context: {
+        id: 'opt-1',
+        name: 'opt-1',
+        value: 'opt-1',
+        label: 'Default',
+      },
+    },
+    {
+      name: 'checked',
+      context: {
+        id: 'opt-2',
+        name: 'opt-2',
+        value: 'opt-2',
+        label: 'Checked option',
+        checked: true,
+      },
+    },
+    {
+      name: 'disabled',
+      context: {
+        id: 'opt-3',
+        name: 'opt-3',
+        value: 'opt-3',
+        label: 'Disabled option',
+        isDisabled: true,
+      },
+    },
+    {
+      name: 'error',
+      context: {
+        id: 'opt-4',
+        name: 'opt-4',
+        value: 'opt-4',
+        label: 'Option with error',
+        hasError: true,
+      },
+    },
+  ],
 };
