082c70ca9f7cc2982cb338f4c3f99b9d2d4c439c
degliwe <wesley.deglise@gmail.com>
2017-07-05 13:52:18 +0200

    feat(site-switcher): upgrade component, add documentation and tests

--- framework/components/ecl-site-switchers/ecl-site-switchers.config.js
+++ framework/components/ecl-site-switchers/ecl-site-switchers.config.js
@@ -1,22 +1,34 @@
 module.exports = {
   title: 'Site switchers',
   label: 'Site switchers',
-  status: 'planned',
+  status: 'ready',
   preview: '@preview-center-transparent',
   tags: ['atom'],
   variants: [
     {
+      name: 'default',
+      label: 'Default',
+      context: {
+        political: 'Commission and its priorities',
+        info: 'Policies, information and services',
+      },
+    },
+    {
       name: 'header',
       label: 'Header',
       context: {
-        modifier: 'site-switcher--header',
+        modifier: 'ecl-site-switcher--header',
+        political: 'Commission and its priorities',
+        info: 'Policies, information and services',
       },
     },
     {
       name: 'footer',
       label: 'Footer',
       context: {
-        modifier: 'site-switcher--footer',
+        modifier: 'ecl-site-switcher--footer',
+        political: 'Commission and its priorities',
+        info: 'Policies, information and services',
       },
     },
   ],
