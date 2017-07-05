8facd94f3c3e8752e71597cb79a650f2d25d53e6
Kalin Chernev <kalin.chernev@gmail.com>
2017-07-05 16:58:14 +0200

    feat(gallery): refactor the template

--- framework/components/ecl-galleries/ecl-galleries.config.js
+++ framework/components/ecl-galleries/ecl-galleries.config.js
@@ -4,6 +4,124 @@ module.exports = {
   status: 'ready',
   tags: ['organism'],
   context: {
-    media: [],
+    data: {
+      rows: [
+        [
+          {
+            classes: 'ecl-col-md-4 has-icon',
+            image: {
+              src: 'http://placehold.it/380x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '380' },
+                { name: 'height', value: '185' },
+              ],
+            },
+            caption: 'Example image caption',
+            icon: 'ecl-icon--camera',
+          },
+          {
+            classes: 'ecl-col-md-3 has-icon',
+            image: {
+              src: 'http://placehold.it/285x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '285' },
+                { name: 'height', value: '185' },
+              ],
+            },
+            icon: 'ecl-icon--audio',
+          },
+          {
+            classes: 'ecl-col-md-2 has-icon',
+            image: {
+              src: 'http://placehold.it/190x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '190' },
+                { name: 'height', value: '185' },
+              ],
+            },
+            caption: 'Example image caption',
+            icon: 'ecl-icon--camera',
+          },
+          {
+            classes: 'ecl-col-md-3',
+            image: {
+              src: 'http://placehold.it/285x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '285' },
+                { name: 'height', value: '185' },
+              ],
+            },
+          },
+        ],
+        [
+          {
+            classes: 'ecl-col-md-2',
+            image: {
+              src: 'http://placehold.it/190x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '190' },
+                { name: 'height', value: '185' },
+              ],
+            },
+            caption: 'Example image caption',
+          },
+          {
+            classes: 'ecl-col-md-3',
+            image: {
+              src: 'http://placehold.it/285x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '285' },
+                { name: 'height', value: '185' },
+              ],
+            },
+          },
+          {
+            classes: 'ecl-col-md-3',
+            image: {
+              src: 'http://placehold.it/285x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '285' },
+                { name: 'height', value: '185' },
+              ],
+            },
+          },
+          {
+            classes: 'ecl-col-md-4',
+            image: {
+              src: 'http://placehold.it/380x185',
+              alt: 'Example alt text',
+              extraClass: 'img-responsive',
+              extraAttributes: [
+                { name: 'typeof', value: 'foaf:Image' },
+                { name: 'width', value: '380' },
+                { name: 'height', value: '185' },
+              ],
+            },
+            caption: 'Example image caption',
+          },
+        ],
+      ],
+    },
   },
 };
