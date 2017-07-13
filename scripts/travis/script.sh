a06f05ec4df07566e5b580cb47bd325c5976b5cb
Yannick Huard <mail@yannickhuard.com>
2017-07-13 13:54:46 +0200

    decouple scripts

---
+++ scripts/travis/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ev
+
+if [ "${TRAVIS_BRANCH}" = "master" -a "${TRAVIS_PULL_REQUEST}" = "false" ] || [ "${TEST_SUITE}" != "test:functional" ] || [ "$(yarn lerna updated -- --since master)" ] ; then
+  npm run $TEST_SUITE;
+fi
