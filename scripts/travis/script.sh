36d4cd59c831e70a6bc6c5a88e2b65ac61680cfb
Yannick Huard <mail@yannickhuard.com>
2017-07-13 14:00:07 +0200

    fix scripts

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-if [ "${TRAVIS_BRANCH}" = "master" -a "${TRAVIS_PULL_REQUEST}" = "false" ] || [ "${TEST_SUITE}" != "test:functional" ] || [ "$(yarn lerna updated -- --since master)" ] ; then
-  npm run $TEST_SUITE;
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] || [ "${TEST_SUITE}" != "test:functional" ] || [ "$(yarn lerna updated -- --since master)" ] ; then
+  npm run "$TEST_SUITE";
 fi
