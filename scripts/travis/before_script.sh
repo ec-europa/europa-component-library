d3f25f6222a5155862bdce7992a66591dd13fada
Yannick Huard <mail@yannickhuard.com>
2017-07-13 14:24:24 +0200

    test fix

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-if [ "${TEST_SUITE}" = "test:functional" ] && { [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] || [ "$(yarn lerna updated -- --since master)" ] ; } ; then
+if [ "${TEST_SUITE}" = "test:functional" ] && { [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] || yarn lerna updated -- --since master ; } ; then
   npm rebuild node-sass;
   npm run dist;
 fi
