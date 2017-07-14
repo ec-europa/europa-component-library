9eca47f15a63675e81c044555f978314557e3333
Kalin Chernev <kalin.chernev@gmail.com>
2017-07-14 15:07:47 +0200

    feat(social-icons): add general tests

--- framework/components/ecl-social-icons/test/spec/social-icons.js
+++ framework/components/ecl-social-icons/test/spec/social-icons.js
@@ -1,36 +1,29 @@
-describe("social-icons", () => {
+describe('social-icons', () => {
   before(() => {
     // Set viewport size
     browser.setViewportSize({
       width: 1400,
-      height: 5700
+      height: 5700,
     });
 
     browser.pause(1000);
   });
 
-  describe("social-icons", () => {
+  describe('social-icons', () => {
     before(() => {
       // Go to url
-      browser.url("ecl-social-icons.html");
+      browser.url('ecl-social-icons.html');
       // Make sure the browser has finished painting
       browser.pause(1000);
-      // Inject axe-core (for accessibility tests)
-      browser.injectAxeCore();
     });
 
     // Normal state
-    context("in overall demonstration", () => {
-      it("should match the reference screenshot", () => {
+    context('in overall demonstration', () => {
+      it('should match the reference screenshot', () => {
         const screenshots = browser.checkDocument({
-          name: "social-icons"
+          name: 'social-icons',
         });
         expect(screenshots).to.matchReference();
-      });
-
-      it("should be accessible", () => {
-        const a11yReport = browser.runAxeCore("ecl-container").value;
-        expect(a11yReport).to.be.accessible;
       });
     });
   });
