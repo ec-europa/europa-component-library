2301b0614526a6749ec99521c176fe90694758b4
Romain Emery <emery.romain@gmail.com>
2017-07-18 09:38:56 +0200

    refactor(inpage-nav): upgrade component - INNO-670 (#280)
    * refactor(inpage-nav): upgrade component
    
    * refactor(inpage-nav): upgrade component
    
    * refactor(inpage-nav): upgrade component
    
    * refactor(inpage-nav): upgrade component
    
    * refactor(inpage-nav): add test file
    
    * refactor(inpage-nav): fix dependencies
    
    * refactor(inpage-nav): update readme
    
    * refactor(inpage-nav): update javascript management
    
    * refactor(inpage-nav): small fixes after QA

--- framework/components/ecl-inpage-navs/inpage-navs.js
+++
@@ -1,8 +0,0 @@
-/**
- * Helper methods for the inpage navigation component.
- */
-
-// eslint-disable-next-line import/prefer-default-export
-export function inpageNav() {
-  console.error('This script needs to be rewritten without jQuery');
-}
