cb7da9af5af554f4f119c699f09107d38954d204
Romain Emery <emery.romain@gmail.com>
2017-07-18 08:03:22 +0200

    feat(tests): add visual tests for content - INNO-649 (#289)
    * feat(tests): add visual tests for content
    
    * feat(tests): revert yarn.lock

--- framework/content/ecl-images/ecl-images.config.js
+++ framework/content/ecl-images/ecl-images.config.js
@@ -4,7 +4,7 @@ module.exports = {
   status: 'ready',
   order: 4,
   context: {
-    src: 'https://unsplash.it/800/600',
+    src: 'http://lorempixel.com/800/600/nature/8',
     alt: 'This is a test image',
   },
 };
