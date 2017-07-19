497ebb9e451990f477d23b59be933c20b2fcfc97
Kalin Chernev <kalin.chernev@gmail.com>
2017-07-19 15:22:56 +0200

    Updates

--- components/raw/ecl-filters/filters.js
+++
@@ -1,8 +0,0 @@
-/**
- * Exposed filter related behaviors.
- */
-
-// eslint-disable-next-line import/prefer-default-export
-export function filters() {
-  console.error('This script needs to be rewritten without jQuery');
-}
