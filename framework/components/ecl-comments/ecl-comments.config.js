6456a6a385fc8d015ea9d6aa9ac6f2232c980750
Yannick Huard <mail@yannickhuard.com>
2017-07-24 11:49:01 +0200

    feat(comments): upgrade comments component - INNO-706 (#320)
    * feat(comments): upgrade comments component - INNO-706
    
    * Add tests

--- framework/components/ecl-comments/ecl-comments.config.js
+++ framework/components/ecl-comments/ecl-comments.config.js
@@ -1,7 +1,38 @@
 module.exports = {
   title: 'Comments',
   label: 'Comments',
-  status: 'planned',
+  status: 'ready',
   preview: '@preview-center-transparent',
   tags: ['molecule'],
+  context: {
+    image: {
+      src: 'https://unsplash.it/300/400?image=1005',
+      alt: "Martin's photo",
+    },
+    author: 'Martin (Not verified)',
+    date: '26 March 2015',
+    title: {
+      href: '#',
+      text: 'Nice article',
+    },
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorem eius enim eos error esse fugiat incidunt iste laudantium nemo, numquam quam quasi repudiandae sapiente soluta sunt tempore ullam voluptatem.',
+    operations: {
+      delete: {
+        href: '#',
+        title: 'Delete',
+        visible: true,
+      },
+      edit: {
+        href: '#',
+        title: 'Edit',
+        visible: true,
+      },
+      reply: {
+        href: '#',
+        title: 'Reply',
+        visible: true,
+      },
+    },
+  },
 };
