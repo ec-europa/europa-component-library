1251511dd514f1342a1880730ddc107458e01ce7
Yannick Huard <mail@yannickhuard.com>
2017-07-25 12:07:45 +0200

    feat(lang-select-pages): upgrade component - INNO-672

--- framework/components/ecl-lang-select-pages/README.md
+++ framework/components/ecl-lang-select-pages/README.md
@@ -1,7 +1,16 @@
-# Language select (Page)
+# Language selector | Page level
 
-Language selector for a page content.
+## Why and how to use this component
 
-# Language select (Dropdown)
+The language selector offers the alternative language versions of the page's main content.
 
-Demonstrates the language switcher for mobile and small viewports.
+The current language is indicated and the user can easily select a different language. It appears below the breadcrumb and above the page header.
+
+## When to use this component
+
+- on every page for which the user’s preferred language is not available
+
+## Do not use this component
+
+- if the site is only in one language, the language selector must not be implemented at the page level
+- if the site is translated, implementation of the corporate language selector is optional (in case of technical complexity)
--- framework/components/ecl-lang-select-pages/ecl-lang-select-pages.config.js
+++ framework/components/ecl-lang-select-pages/ecl-lang-select-pages.config.js
@@ -1,19 +1,94 @@
+const data = {
+  unavailable: 'français',
+  options: [
+    {
+      href: '/en',
+      hreflang: 'en',
+      label: 'English',
+      lang: 'en',
+    },
+    {
+      href: '/nl',
+      hreflang: 'nl',
+      label: 'Nederlands',
+      lang: 'nl',
+      is_selected: true,
+    },
+    {
+      href: '/hr',
+      hreflang: 'hr',
+      label: 'hrvatski',
+      lang: 'hr',
+    },
+    {
+      href: '/it',
+      hreflang: 'it',
+      label: 'italiano',
+      lang: 'it',
+    },
+    {
+      href: '/lv',
+      hreflang: 'lv',
+      label: 'latviesu',
+      lang: 'lv',
+    },
+    {
+      href: '/hu',
+      hreflang: 'hu',
+      label: 'magyar',
+      lang: 'hu',
+    },
+  ],
+};
+
 module.exports = {
   title: 'Language select (Page)',
   label: 'Language select (Page)',
   tags: ['molecule'],
+  status: 'ready',
+  collated: true,
+  collator(markup, item) {
+    return `
+      <!-- Start: @${item.handle} -->\n
+      <h3 class="ecl-heading ecl-heading--h3">${item.label}\n</h3>\n
+      ${markup}\n
+      <!-- End: @${item.handle} -->\n
+    `;
+  },
   variants: [
     {
       name: 'default',
       title: 'Language select (Page) - Default',
       label: 'Default',
-      status: 'planned',
+      context: Object.assign({}, data, {
+        variant: 'default',
+      }),
+    },
+    {
+      name: 'default-primary',
+      title: 'Language select (Page) - Default primary',
+      label: 'Default primary',
+      context: Object.assign({}, data, {
+        variant: 'default',
+        theme: 'primary',
+      }),
     },
     {
       name: 'dropdown',
       title: 'Language select (Page) - Dropdown',
       label: 'Dropdown',
-      status: 'planned',
+      context: Object.assign({}, data, {
+        variant: 'dropdown',
+      }),
+    },
+    {
+      name: 'dropdown-primary',
+      title: 'Language select (Page) - Dropdown primary',
+      label: 'Dropdown primary',
+      context: Object.assign({}, data, {
+        variant: 'dropdown',
+        theme: 'primary',
+      }),
     },
   ],
 };
