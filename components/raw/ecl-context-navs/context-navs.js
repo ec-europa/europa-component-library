4636f2a70bf11abb8ac00b2b5521eb4b0715dec3
Yannick Huard <mail@yannickhuard.com>
2017-08-01 10:57:25 +0200

    Updates

--- components/raw/ecl-context-navs/context-navs.js
+++
@@ -1,8 +0,0 @@
-/**
- * Contextual navigation scripts
- */
-
-// eslint-disable-next-line import/prefer-default-export
-export function contextualNav() {
-  console.error('This script needs to be rewritten without jQuery');
-}
