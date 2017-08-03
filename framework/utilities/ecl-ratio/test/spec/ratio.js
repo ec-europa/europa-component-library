32d64d92d5b6b2a71f6fce4e2a08a807bc23d9e7
Romain Emery <emery.romain@gmail.com>
2017-08-03 14:54:05 +0200

    refactor(arial): arial everywhere

---
+++ framework/utilities/ecl-ratio/test/spec/ratio.js
@@ -0,0 +1,25 @@
+describe('ratio', () => {
+  before(() => {
+    // Set viewport size
+    browser.setViewportSize({
+      width: 1400,
+      height: 800,
+    });
+
+    browser.pause(1000);
+
+    browser.url(`ecl-ratio.html`);
+    // Make sure the browser has finished painting
+    browser.pause(1000);
+    // Inject axe-core (for accessibility tests)
+    browser.injectAxeCore();
+  });
+
+  // Normal state
+  it('should match the reference screenshot', () => {
+    const screenshots = browser.checkDocument({
+      name: 'ratio',
+    });
+    expect(screenshots).to.matchReference();
+  });
+});
