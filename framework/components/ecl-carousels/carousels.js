454366500a8c54d1368ab487401725b6684d9f29
Kalin Chernev <kalin.chernev@gmail.com>
2017-08-03 16:53:47 +0200

    feat(carousel): add prev and next fns

--- framework/components/ecl-carousels/carousels.js
+++ framework/components/ecl-carousels/carousels.js
@@ -1,26 +1,44 @@
-import { queryAll } from '@ec-europa/ecl-base/helpers/dom';
+import {
+  addClass,
+  removeClass,
+  queryAll,
+} from '@ec-europa/ecl-base/helpers/dom';
 
 /**
  * @param {object} options Object containing configuration overrides
  */
-export const carousels = (
-  {
-    selectorId: selectorId = 'ecl-carousel',
-    showNavigation: showNavigation = true,
-    animate: animate = true,
-    startAnimated: startAnimated = true,
-  } = {}
-) => {
+export const carousels = ({ selectorId: selectorId = 'ecl-carousel' } = {}) => {
   // SUPPORTS
   if (!('querySelector' in document) || !('addEventListener' in window)) {
     return null;
   }
 
-  // SETUP
+  let currentSlide = 0;
   const carousel = document.getElementById(selectorId);
+  // We need to invoke the function here in order to use the `controls`.
+  // The controls MUST be added via JavaScript, as they are useless in `no-js`.
+  // eslint-disable-next-line no-use-before-define
+  addCarouselControls(carousel);
+  const slides = queryAll('.ecl-carousel__item', carousel);
+  const controls = carousel.querySelector('.ecl-carousel__controls');
+  const prevButton = controls.querySelector('.ecl-carousel__btn--previous');
+  const nextButton = controls.querySelector('.ecl-carousel__btn--next');
 
-  // Carousel navigation is useful only when JS is enabled.
-  function addNavigation() {
+  function goToSlide(n) {
+    removeClass(slides[currentSlide], 'ecl-carousel__item--showing');
+    currentSlide = (n + slides.length) % slides.length;
+    addClass(slides[currentSlide], 'ecl-carousel__item--showing');
+  }
+
+  function previousSlide() {
+    goToSlide(currentSlide - 1);
+  }
+
+  function nextSlide() {
+    goToSlide(currentSlide + 1);
+  }
+
+  function addCarouselControls(targetCarousel) {
     const navControls = document.createElement('ul');
 
     navControls.className = 'ecl-carousel__controls ecl-list--unstyled';
@@ -35,18 +53,16 @@ export const carousels = (
 
     navControls
       .querySelector('.ecl-carousel__btn--previous', '.ecl-carousel__controls')
-      .addEventListener('click', () => {
-        console.log('prev clicked');
-      });
+      .addEventListener('click', previousSlide);
 
     navControls
       .querySelector('.ecl-carousel__btn--next', '.ecl-carousel__controls')
-      .addEventListener('click', () => {
-        console.log('next clicked');
-      });
+      .addEventListener('click', nextSlide);
 
-    document.querySelector('.ecl-carousel__list').append(navControls);
+    targetCarousel.querySelector('.ecl-carousel__list').append(navControls);
   }
+
+  // SETUP
 
   // BIND EVENTS
   function bindEvents(elements) {}
@@ -58,9 +74,7 @@ export const carousels = (
   function destroy() {}
 
   // INIT
-  function init() {
-    addNavigation();
-  }
+  function init() {}
 
   init();
 
