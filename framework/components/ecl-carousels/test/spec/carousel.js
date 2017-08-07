54030315aaabd9329c2bf2d36baf3bea373260cc
Kalin Chernev <kalin.chernev@gmail.com>
2017-08-07 10:46:08 +0200

    feat(carousel): add tests

---
+++ framework/components/ecl-carousels/test/spec/carousel.js
@@ -0,0 +1,57 @@
+const componentUrl = 'ecl-carousels.html';
+
+describe('carousels', () => {
+  context('mobile', () => {
+    before(() => {
+      // Set viewport size
+      browser.setViewportSize({
+        width: 500,
+        height: 1000,
+      });
+
+      // Reload
+      browser.url(componentUrl);
+      browser.pause(4000);
+      browser.injectAxeCore();
+    });
+
+    it('should match the reference screenshot', () => {
+      const screenshots = browser.checkDocument({
+        name: 'mobile',
+      });
+      expect(screenshots).to.matchReference();
+    });
+
+    it('should be accessible', () => {
+      const a11yReport = browser.runAxeCore('ecl-carousel').value;
+      expect(a11yReport).to.be.accessible;
+    });
+  });
+
+  context('desktop', () => {
+    before(() => {
+      // Set viewport size
+      browser.setViewportSize({
+        width: 1400,
+        height: 800,
+      });
+
+      // Reload
+      browser.url(componentUrl);
+      browser.pause(4000);
+      browser.injectAxeCore();
+    });
+
+    it('should match the reference screenshot', () => {
+      const screenshots = browser.checkDocument({
+        name: 'desktop',
+      });
+      expect(screenshots).to.matchReference();
+    });
+
+    it('should be accessible', () => {
+      const a11yReport = browser.runAxeCore('ecl-carousel').value;
+      expect(a11yReport).to.be.accessible;
+    });
+  });
+});
