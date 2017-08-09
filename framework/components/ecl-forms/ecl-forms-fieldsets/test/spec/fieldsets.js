009c27d4e73993c37d7bfbc0128b7ab89df8da99
Yannick Huard <mail@yannickhuard.com>
2017-08-09 09:22:18 +0200

    Provide empty demo

--- framework/components/ecl-forms/ecl-forms-fieldsets/test/spec/fieldsets.js
+++ framework/components/ecl-forms/ecl-forms-fieldsets/test/spec/fieldsets.js
@@ -1,29 +1,3 @@
 describe('forms-fieldset', () => {
-  before(() => {
-    // Set viewport size
-    browser.setViewportSize({
-      width: 1400,
-      height: 600,
-    });
-
-    // Go to url
-    browser.url(`ecl-forms-fieldsets.html`);
-    // Make sure the browser has finished painting
-    browser.pause(1000);
-    // Inject axe-core (for accessibility tests)
-    browser.injectAxeCore();
-  });
-
-  // Normal state
-  it('should match the reference screenshot', () => {
-    const screenshots = browser.checkDocument({
-      name: `forms/fieldsets`,
-    });
-    expect(screenshots).to.matchReference();
-  });
-
-  it('should be accessible', () => {
-    const a11yReport = browser.runAxeCore('ecl-fieldset').value;
-    expect(a11yReport).to.be.accessible;
-  });
+  /* No test provided */
 });
--- framework/components/ecl-forms/ecl-forms-forms/test/spec/forms.js
+++ framework/components/ecl-forms/ecl-forms-forms/test/spec/forms.js
@@ -1,29 +1,3 @@
 describe('forms-form', () => {
-  before(() => {
-    // Set viewport size
-    browser.setViewportSize({
-      width: 1400,
-      height: 600,
-    });
-
-    // Go to url
-    browser.url(`ecl-forms-forms.html`);
-    // Make sure the browser has finished painting
-    browser.pause(1000);
-    // Inject axe-core (for accessibility tests)
-    browser.injectAxeCore();
-  });
-
-  // Normal state
-  it('should match the reference screenshot', () => {
-    const screenshots = browser.checkDocument({
-      name: `forms/forms`,
-    });
-    expect(screenshots).to.matchReference();
-  });
-
-  it('should be accessible', () => {
-    const a11yReport = browser.runAxeCore('ecl-form').value;
-    expect(a11yReport).to.be.accessible;
-  });
+  /* No test provided */
 });
