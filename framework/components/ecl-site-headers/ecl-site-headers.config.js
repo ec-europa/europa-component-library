ba252db389794805d72ceb8168f3396fbc026482
Romain Emery <emery.romain@gmail.com>
2017-08-17 13:57:17 +0200

    refactor(site-header): add composition

--- framework/components/ecl-site-headers/ecl-site-headers.config.js
+++ framework/components/ecl-site-headers/ecl-site-headers.config.js
@@ -6,6 +6,10 @@ module.exports = {
   variants: [
     {
       name: 'default',
+      context: {
+        political: { href: '#', label: 'Commission and its priorities' },
+        info: { href: '#', label: 'Policies, information and services' },
+      },
     },
     {
       name: 'homepage',
