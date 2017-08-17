e2df1c4fb6c2b79439c2856a4168ac0b735084ab
Romain Emery <emery.romain@gmail.com>
2017-08-17 14:47:40 +0200

    refactor(site-header): add composition

--- framework/components/ecl-site-headers/ecl-site-headers.config.js
+++ framework/components/ecl-site-headers/ecl-site-headers.config.js
@@ -1,23 +1,16 @@
 module.exports = {
   title: 'Site headers',
   label: 'Site headers',
-  tags: ['organism'],
   status: 'ready',
-  variants: [
-    {
-      name: 'default',
-      context: {
-        political: { href: '#', label: 'Commission and its priorities' },
-        info: { href: '#', label: 'Policies, information and services' },
-      },
-    },
-    {
-      name: 'homepage',
-      context: {
-        global: {
-          language: 'en',
-        },
-      },
-    },
-  ],
+  tags: ['organism'],
+  context: {
+    political: { href: '#', label: 'Commission and its priorities' },
+    info: { href: '#', label: 'Policies, information and services' },
+    title: 'User menu',
+    links: [
+      { href: '#', label: 'My workbench' },
+      { href: '#', label: 'My account' },
+      { href: '#', label: 'Log out' }
+    ]
+  },
 };
