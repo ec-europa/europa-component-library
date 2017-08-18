adff86b26f5e77b1b9dbb96786e6d0b2bf6f6392
Yannick Huard <mail@yannickhuard.com>
2017-08-18 10:14:48 +0200

    feat(page-menu): provide page with menu template - INNO-749 (#379)
    * feat(page-menu): provide page with menu template - INNO-749
    
    * Update dependencies
    
    * Fix padding
    
    * Fix expandable look
    
    * Finalize PR
    
    * Fixes
    
    * Remove overflow property

--- framework/templates/ecl-pages/test/spec/pages.js
+++ framework/templates/ecl-pages/test/spec/pages.js
@@ -1,3 +1,5 @@
+const variants = ['simple', 'with-menu'];
+
 describe('pages', () => {
   before(() => {
     // Set viewport size
@@ -5,26 +7,33 @@ describe('pages', () => {
       width: 1400,
       height: 600,
     });
-
-    browser.url(`ecl-pages.html`);
-
-    // Inject axe-core (for accessibility tests)
-    browser.injectAxeCore();
-
-    // Make sure the browser has finished painting
-    browser.pause(1000);
   });
 
-  // Normal state
-  it('should match the reference screenshot', () => {
-    const screenshots = browser.checkDocument({
-      name: 'pages',
+  variants.forEach(variant => {
+    describe(`--${variant}`, () => {
+      before(() => {
+        // Set viewport size
+        browser.url(`ecl-pages--${variant}.html`);
+
+        // Inject axe-core (for accessibility tests)
+        browser.injectAxeCore();
+
+        // Make sure the browser has finished painting
+        browser.pause(1000);
+      });
+
+      // Normal state
+      it('should match the reference screenshot', () => {
+        const screenshots = browser.checkDocument({
+          name: `pages/${variant}`,
+        });
+        expect(screenshots).to.matchReference();
+      });
+
+      it('should be accessible', () => {
+        const a11yReport = browser.runAxeCore('ecl-container').value;
+        expect(a11yReport).to.be.accessible;
+      });
     });
-    expect(screenshots).to.matchReference();
-  });
-
-  it('should be accessible', () => {
-    const a11yReport = browser.runAxeCore('ecl-container').value;
-    expect(a11yReport).to.be.accessible;
   });
 });
