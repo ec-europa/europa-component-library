f8f802859d3644bee0c473e8900869b993ed2874
Romain Emery <emery.romain@gmail.com>
2017-08-23 08:54:06 +0200

    refactor(utilities): add prefix - INNO-692 (#406)
    * refactor(utilities): add prefix
    
    * refactor(utilities): add prefix
    
    * refactor(utilities): add prefix
    
    * refactor(utilities): add prefix
    
    * refactor(utilities): fix regression
    
    * refactor(utilities): update screenshots
    
    * Fix dependency

---
+++ framework/utilities/ecl-u-z-index/test/spec/z-index.js
@@ -0,0 +1,29 @@
+describe('z-index', () => {
+  before(() => {
+    // Set viewport size
+    browser.setViewportSize({
+      width: 800,
+      height: 600,
+    });
+
+    browser.pause(1000);
+
+    // Go to url
+    browser.url('ecl-u-z-index.html');
+
+    // Make sure the browser has finished painting
+    browser.pause(1000);
+    // Inject axe-core (for accessibility tests)
+    browser.injectAxeCore();
+  });
+
+  // Normal state
+  context('with plain state', () => {
+    it('should match the reference screenshot', () => {
+      const screenshots = browser.checkDocument({
+        name: 'z-index',
+      });
+      expect(screenshots).to.matchReference();
+    });
+  });
+});
