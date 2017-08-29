0d46e3d758a1331cc9c0563daf3bb66fa1da7297
Yannick Huard <mail@yannickhuard.com>
2017-08-29 21:44:09 +0200

    Start fixing tests

--- framework/components/ecl-meta/test/spec/metas.js
+++ framework/components/ecl-meta/test/spec/metas.js
@@ -1,33 +1,39 @@
 const variants = ['default', 'header'];
 
-variants.forEach(variant => {
-  describe(`ecl-meta--${variant}`, () => {
-    before(() => {
-      // Set viewport size
-      browser.setViewportSize({
-        width: 1400,
-        height: 600,
-      });
-      // Go to url
-      browser.url(`ecl-meta--${variant}.html`);
-
-      // Make sure the browser has finished painting
-      browser.pause(1000);
-      // Inject axe-core (for accessibility tests)
-      browser.injectAxeCore();
+describe('ecl-meta', () => {
+  before(() => {
+    // Set viewport size
+    browser.setViewportSize({
+      width: 1400,
+      height: 600,
     });
+  });
 
-    // Normal state
-    it('should match the reference screenshot', () => {
-      const screenshots = browser.checkDocument({
-        name: `meta/${variant}`,
+  variants.forEach(variant => {
+    describe(`--${variant}`, () => {
+      before(() => {
+        // Go to url
+        browser.url(`ecl-meta--${variant}.html`);
+
+        // Inject axe-core (for accessibility tests)
+        browser.injectAxeCore();
+
+        // Make sure the browser has finished painting
+        browser.pause(1000);
       });
-      expect(screenshots).to.matchReference();
-    });
 
-    it('should be accessible', () => {
-      const a11yReport = browser.runAxeCore('ecl-meta').value;
-      expect(a11yReport).to.be.accessible;
+      // Normal state
+      it('should match the reference screenshot', () => {
+        const screenshots = browser.checkDocument({
+          name: `meta/${variant}`,
+        });
+        expect(screenshots).to.matchReference();
+      });
+
+      it('should be accessible', () => {
+        const a11yReport = browser.runAxeCore('ecl-meta').value;
+        expect(a11yReport).to.be.accessible;
+      });
     });
   });
 });
