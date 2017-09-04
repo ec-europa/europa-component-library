6d850ffeb1b2868f7c7986f701a9fe2fde864c82
Romain Emery <emery.romain@gmail.com>
2017-09-04 08:22:53 +0200

    feat(social-media-links): init component

---
+++ framework/components/ecl-social-media-links/test/spec/social-media-links.js
@@ -0,0 +1,30 @@
+describe('social-media-links', () => {
+  before(() => {
+    // Set viewport size
+    browser.setViewportSize({
+      width: 800,
+      height: 800,
+    });
+
+    browser.pause(500);
+  });
+
+  describe('social-media-links', () => {
+    before(() => {
+      // Go to url
+      browser.url('ecl-social-media-links.html');
+      // Make sure the browser has finished painting
+      browser.pause(500);
+    });
+
+    // Normal state
+    context('in overall demonstration', () => {
+      it('should match the reference screenshot', () => {
+        const screenshots = browser.checkDocument({
+          name: 'social-media-links',
+        });
+        expect(screenshots).to.matchReference();
+      });
+    });
+  });
+});
