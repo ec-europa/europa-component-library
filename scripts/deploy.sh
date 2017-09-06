ba3219cd46645e28e99f14141aed8830cafffcd6
Yannick Huard <mail@yannickhuard.com>
2017-09-06 09:58:04 +0200

    Debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 # Exit the script on any command with non 0 return code
 set -e
 
+# Echo every command being executed
+set -x
+
 # Go to project root
 cd "$(dirname "$0")"
 cd ..
@@ -14,7 +17,6 @@ fi
 
 # Deploy
 ./node_modules/.bin/gh-pages \
-  --silent \
   --repo https://${GH_TOKEN}@github.com/ec-europa/europa-component-library.git \
   --dist dist
   --message "Update GitHub Pages"
