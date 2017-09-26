1178cd075703a5df629ceccf365af46aad425fcf
Romain Emery <emery.romain@gmail.com>
2017-09-26 10:31:56 +0200

    refactor(table): upgrade component

--- framework/components/ecl-tables/ecl-tables.config.js
+++ framework/components/ecl-tables/ecl-tables.config.js
@@ -2,14 +2,41 @@ module.exports = {
   title: 'Tables',
   label: 'Tables',
   status: 'ready',
-  collator(markup, item) {
-    return `<!-- Start: @${item.handle} -->\n<h3 class="ecl-heading ecl-heading--h3">${item.label}</h3>\n${markup}\n<!-- End: @${item.handle} -->\n`;
-  },
   variants: [
     {
       name: 'default',
       label: 'Default table',
       context: {
+        headers: [
+          [
+            { label: 'Name' },
+            { label: 'Registration date' },
+            { label: 'Email' },
+          ],
+        ],
+        rows: [
+          [
+            { label: 'John Doe' },
+            { label: '01/01/2016' },
+            {
+              label: '<a href="mailto:john.doe@mail.com">john.doe@mail.com</a>',
+            },
+          ],
+          [
+            { label: 'Jane Doe' },
+            { label: '06/12/2016' },
+            {
+              label: '<a href="mailto:jane.doe@mail.com">jane.doe@mail.com</a>',
+            },
+          ],
+          [
+            { label: 'Jack Doe' },
+            { label: '03/05/2017' },
+            {
+              label: '<a href="mailto:jack.doe@mail.com">jack.doe@mail.com</a>',
+            },
+          ],
+        ],
         _demo: {
           scripts: 'ECL.eclTables();',
         },
@@ -19,6 +46,32 @@ module.exports = {
       name: 'empty',
       label: 'Table with empty heading',
       context: {
+        headers: [
+          [{ label: '' }, { label: 'Registration date' }, { label: 'Email' }],
+        ],
+        rows: [
+          [
+            { label: 'John Doe' },
+            { label: '01/01/2016' },
+            {
+              label: '<a href="mailto:john.doe@mail.com">john.doe@mail.com</a>',
+            },
+          ],
+          [
+            { label: 'Jane Doe' },
+            { label: '06/12/2016' },
+            {
+              label: '<a href="mailto:jane.doe@mail.com">jane.doe@mail.com</a>',
+            },
+          ],
+          [
+            { label: 'Jack Doe' },
+            { label: '03/05/2017' },
+            {
+              label: '<a href="mailto:jack.doe@mail.com">jack.doe@mail.com</a>',
+            },
+          ],
+        ],
         _demo: {
           scripts: 'ECL.eclTables();',
         },
@@ -28,6 +81,41 @@ module.exports = {
       name: 'colspan',
       label: 'Table with colspan',
       context: {
+        headers: [
+          [
+            { label: 'Name', attributes: 'rowspan="2"' },
+            { label: 'Registration date', attributes: 'rowspan="2"' },
+            { label: 'Language', attributes: 'colspan="3"' },
+          ],
+          [
+            { label: 'English' },
+            { label: 'French' },
+            { label: 'German' },
+          ],
+        ],
+        rows: [
+          [
+            { label: 'John Doe' },
+            { label: '01/01/2016' },
+            { label: 'yes'},
+            { label: 'no'},
+            { label: 'no'},
+          ],
+          [
+            { label: 'Jane Doe' },
+            { label: '06/12/2016' },
+            { label: 'no'},
+            { label: 'yes'},
+            { label: 'yes'},
+          ],
+          [
+            { label: 'Jack Doe' },
+            { label: '03/05/2017' },
+            { label: 'yes'},
+            { label: 'no'},
+            { label: 'no'},
+          ],
+        ],
         _demo: {
           scripts: 'ECL.eclTables();',
         },
@@ -37,6 +125,41 @@ module.exports = {
       name: 'colspan-empty',
       label: 'Table with colspan and empty heading',
       context: {
+        headers: [
+          [
+            { label: '', attributes: 'rowspan="2"' },
+            { label: 'Registration date', attributes: 'rowspan="2"' },
+            { label: 'Language', attributes: 'colspan="3"' },
+          ],
+          [
+            { label: 'English' },
+            { label: 'French' },
+            { label: 'German' },
+          ],
+        ],
+        rows: [
+          [
+            { label: 'John Doe' },
+            { label: '01/01/2016' },
+            { label: 'yes'},
+            { label: 'no'},
+            { label: 'no'},
+          ],
+          [
+            { label: 'Jane Doe' },
+            { label: '06/12/2016' },
+            { label: 'no'},
+            { label: 'yes'},
+            { label: 'yes'},
+          ],
+          [
+            { label: 'Jack Doe' },
+            { label: '03/05/2017' },
+            { label: 'yes'},
+            { label: 'no'},
+            { label: 'no'},
+          ],
+        ],
         _demo: {
           scripts: 'ECL.eclTables();',
         },
