f45a32031570e9c19c7a9efa0dfbd27db4bd5f77
Yannick Huard <mail@yannickhuard.com>
2017-09-29 14:10:44 +0200

    chore(maintenance): update dependencies and add missing screenshots - INNO-822

--- framework/components/ecl-tags/test/spec/tags.js
+++ framework/components/ecl-tags/test/spec/tags.js
@@ -1,33 +1,39 @@
 const variants = ['default', 'facet', 'facet-close'];
 
-variants.forEach(variant => {
-  describe(`ecl-tags--${variant}`, () => {
-    before(() => {
-      // Set viewport size
-      browser.setViewportSize({
-        width: 600,
-        height: 300,
-      });
-      // Go to url
-      browser.url(`ecl-tags--${variant}.html`);
-
-      // Make sure the browser has finished painting
-      browser.pause(500);
-      // Inject axe-core (for accessibility tests)
-      browser.injectAxeCore();
+describe(`ecl-tags`, () => {
+  before(() => {
+    // Set viewport size
+    browser.setViewportSize({
+      width: 600,
+      height: 300,
     });
+  });
 
-    // Normal state
-    it('should match the reference screenshot', () => {
-      const screenshots = browser.checkDocument({
-        name: `tags/${variant}`,
+  variants.forEach(variant => {
+    describe(`--${variant}`, () => {
+      before(() => {
+        // Go to url
+        browser.url(`ecl-tags--${variant}.html`);
+
+        // Inject axe-core (for accessibility tests)
+        browser.injectAxeCore();
+
+        // Make sure the browser has finished painting
+        browser.pause(500);
       });
-      expect(screenshots).to.matchReference();
-    });
 
-    it('should be accessible', () => {
-      const a11yReport = browser.runAxeCore('ecl-tag').value;
-      expect(a11yReport).to.be.accessible;
+      // Normal state
+      it('should match the reference screenshot', () => {
+        const screenshots = browser.checkDocument({
+          name: `tags/${variant}`,
+        });
+        expect(screenshots).to.matchReference();
+      });
+
+      it('should be accessible', () => {
+        const a11yReport = browser.runAxeCore('ecl-tag').value;
+        expect(a11yReport).to.be.accessible;
+      });
     });
   });
 });
