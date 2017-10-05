342883dd24ce3d472e23410905dfe7fd7be894dc
Romain Emery <emery.romain@gmail.com>
2017-10-05 14:40:33 +0200

    refactor(page-header): use composition for metas - INNO-668 (#466)
    * refactor(page-header): use composition for metas
    
    * refactor(page-header): update screenshots
    
    * refactor(page-header): fix pages

--- framework/components/ecl-page-headers/ecl-page-headers.config.js
+++ framework/components/ecl-page-headers/ecl-page-headers.config.js
@@ -19,12 +19,7 @@ module.exports = {
         introduction:
           'EU economy, finance and the euro, and practical information for EU businesses and entrepreneurs on product safety, environmental regulations, trade with non-EU countries and competition rules.',
         paragraph_class: 'ecl-paragraph ecl-paragraph--l',
-        meta: {
-          type: 'News article',
-          date: '6 July 2015',
-          timestamp: '2015-07-06T17:44:28+02:00',
-          location: 'Brussels',
-        },
+        metas: ['News article', '6 July 2015', 'Brussels'],
       },
     },
     {
