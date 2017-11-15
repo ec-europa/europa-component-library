407d926df240f82acb2af5738769dfbe373faf8f
Kalin Chernev <kalin.chernev@gmail.com>
2017-11-15 17:14:49 +0100

    Fix prettier

--- framework/components/ecl-social-icons/ecl-social-icons.config.js
+++ framework/components/ecl-social-icons/ecl-social-icons.config.js
@@ -12,16 +12,19 @@ module.exports = {
         <div class="ecl-container">
           <div class="ecl-row">
             <div class="ecl-col">
-              <a href="#" class="ecl-link ecl-icon-social ecl-icon-social--${item
-                .context.variant}">${item.label}</a>
+              <a href="#" class="ecl-link ecl-icon-social ecl-icon-social--${
+                item.context.variant
+              }">${item.label}</a>
             </div>
             <div class="ecl-col">
-              <span class="ecl-icon-social ecl-icon-social--${item.context
-                .variant}">${item.label}</span>
+              <span class="ecl-icon-social ecl-icon-social--${
+                item.context.variant
+              }">${item.label}</span>
             </div>
             <div class="ecl-col">
-              <span class="ecl-icon-social ecl-icon-social--smaller ecl-icon-social--${item
-                .context.variant}">${item.label}</span>
+              <span class="ecl-icon-social ecl-icon-social--smaller ecl-icon-social--${
+                item.context.variant
+              }">${item.label}</span>
             </div>
           </div>
       </div>
