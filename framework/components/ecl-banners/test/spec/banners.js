4431dd2ff2a88731a09b27b293480182096ff4e3
Romain Emery <emery.romain@gmail.com>
2017-11-15 14:37:44 +0100

    fix(banners): fix tests

--- framework/components/ecl-banners/test/spec/banners.js
+++ framework/components/ecl-banners/test/spec/banners.js
@@ -1,39 +1,30 @@
-const variants = ['quote'];
-
-describe('ecl-banners', () => {
+describe('banners', () => {
   before(() => {
     // Set viewport size
     browser.setViewportSize({
       width: 1400,
       height: 600,
     });
+
+    browser.url(`ecl-banners.html`);
+
+    // Inject axe-core (for accessibility tests)
+    browser.injectAxeCore();
+
+    // Make sure the browser has finished painting
+    browser.pause(500);
   });
 
-  variants.forEach(variant => {
-    describe(`--${variant}`, () => {
-      before(() => {
-        // Go to url
-        browser.url(`ecl-banners--${variant}.html`);
-
-        // Inject axe-core (for accessibility tests)
-        browser.injectAxeCore();
-
-        // Make sure the browser has finished painting
-        browser.pause(500);
-      });
-
-      // Normal state
-      it('should match the reference screenshot', () => {
-        const screenshots = browser.checkDocument({
-          name: `banners/${variant}`,
-        });
-        expect(screenshots).to.matchReference();
-      });
-
-      it('should be accessible', () => {
-        const a11yReport = browser.runAxeCore('ecl-banner').value;
-        expect(a11yReport).to.be.accessible;
-      });
+  // Normal state
+  it('should match the reference screenshot', () => {
+    const screenshots = browser.checkDocument({
+      name: 'banners',
     });
+    expect(screenshots).to.matchReference();
+  });
+
+  it('should be accessible', () => {
+    const a11yReport = browser.runAxeCore('ecl-banner').value;
+    expect(a11yReport).to.be.accessible;
   });
 });
