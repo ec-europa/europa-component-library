e78335e5fd1bf635ef44f6c13357474572d4e895
Yannick Huard <mail@yannickhuard.com>
2017-12-04 09:42:12 +0100

    Fix all linting errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,21 @@
+const defaultSassRules = require('stylelint-config-sass-guidelines').rules;
+
 module.exports = {
-  extends: ['@ec-europa/stylelint-config-ecl'],
+  extends: ['@ec-europa/stylelint-config-ecl', 'stylelint-config-prettier'],
+  rules: {
+    'at-rule-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'selector-combinator-space-before': null,
+    'selector-descendant-combinator-no-non-space': null,
+    'order/order': [
+      defaultSassRules['order/order'][0].concat([
+        {
+          type: 'at-rule',
+          name: 'include',
+          parameter: 'ecl-media-breakpoint-up',
+          hasBlock: true,
+        },
+      ]),
+    ],
+  },
 };
