02c98808907b8257af919fac4904365a5cc73bd1
Yannick Huard <mail@yannickhuard.com>
2017-12-04 11:44:40 +0100

    Fix "ecl-media-breakpoint-up" order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,3 @@
-const defaultSassRules = require('stylelint-config-sass-guidelines').rules;
-
 module.exports = {
   extends: ['@ec-europa/stylelint-config-ecl', 'stylelint-config-prettier'],
   rules: {
@@ -8,15 +6,5 @@ module.exports = {
     'declaration-empty-line-before': null,
     'selector-combinator-space-before': null,
     'selector-descendant-combinator-no-non-space': null,
-    'order/order': [
-      defaultSassRules['order/order'][0].concat([
-        {
-          type: 'at-rule',
-          name: 'include',
-          parameter: 'ecl-media-breakpoint-up',
-          hasBlock: true,
-        },
-      ]),
-    ],
   },
 };
