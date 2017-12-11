d47649d84b935e49e7f9cdeeee8ee87394e0be0a
Kalin Chernev <kalin.chernev@gmail.com>
2017-12-11 14:01:47 +0100

    Demonstrate difference in tables

--- framework/components/ecl-tables/ecl-tables.config.js
+++ framework/components/ecl-tables/ecl-tables.config.js
@@ -1,166 +1,18 @@
+const variants = require('./variants');
+
 module.exports = {
   title: 'Tables',
   label: 'Tables',
   status: 'ready',
-  variants: [
-    {
-      name: 'default',
-      label: 'Default table',
-      context: {
-        extra_classes: 'ecl-table--responsive',
-        headers: [
-          [
-            { label: 'Name' },
-            { label: 'Registration date' },
-            { label: 'Email' },
-          ],
-        ],
-        rows: [
-          [
-            { label: 'John Doe' },
-            { label: '01/01/2016' },
-            {
-              label: '<a href="mailto:john.doe@mail.com">john.doe@mail.com</a>',
-            },
-          ],
-          [
-            { label: 'Jane Doe' },
-            { label: '06/12/2016' },
-            {
-              label: '<a href="mailto:jane.doe@mail.com">jane.doe@mail.com</a>',
-            },
-          ],
-          [
-            { label: 'Jack Doe' },
-            { label: '03/05/2017' },
-            {
-              label: '<a href="mailto:jack.doe@mail.com">jack.doe@mail.com</a>',
-            },
-          ],
-        ],
-        _demo: {
-          scripts: 'ECL.eclTables();',
-        },
-      },
-    },
-    {
-      name: 'empty',
-      label: 'Table with empty heading',
-      context: {
-        extra_classes: 'ecl-table--responsive',
-        headers: [
-          [{ label: '' }, { label: 'Registration date' }, { label: 'Email' }],
-        ],
-        rows: [
-          [
-            { label: 'John Doe' },
-            { label: '01/01/2016' },
-            {
-              label: '<a href="mailto:john.doe@mail.com">john.doe@mail.com</a>',
-            },
-          ],
-          [
-            { label: 'Jane Doe' },
-            { label: '06/12/2016' },
-            {
-              label: '<a href="mailto:jane.doe@mail.com">jane.doe@mail.com</a>',
-            },
-          ],
-          [
-            { label: 'Jack Doe' },
-            { label: '03/05/2017' },
-            {
-              label: '<a href="mailto:jack.doe@mail.com">jack.doe@mail.com</a>',
-            },
-          ],
-        ],
-        _demo: {
-          scripts: 'ECL.eclTables();',
-        },
-      },
-    },
-    {
-      name: 'colspan',
-      label: 'Table with colspan',
-      context: {
-        extra_classes: 'ecl-table--responsive',
-        headers: [
-          [
-            { label: 'Name', attributes: 'rowspan="2"' },
-            { label: 'Registration date', attributes: 'rowspan="2"' },
-            { label: 'Language', attributes: 'colspan="3"' },
-          ],
-          [{ label: 'English' }, { label: 'French' }, { label: 'German' }],
-        ],
-        rows: [
-          [
-            { label: 'John Doe' },
-            { label: '01/01/2016' },
-            { label: 'yes' },
-            { label: 'no' },
-            { label: 'no' },
-          ],
-          [
-            { label: 'Jane Doe' },
-            { label: '06/12/2016' },
-            { label: 'no' },
-            { label: 'yes' },
-            { label: 'yes' },
-          ],
-          [
-            { label: 'Jack Doe' },
-            { label: '03/05/2017' },
-            { label: 'yes' },
-            { label: 'no' },
-            { label: 'no' },
-          ],
-        ],
-        _demo: {
-          scripts: 'ECL.eclTables();',
-        },
-      },
-    },
-    {
-      name: 'colspan-empty',
-      label: 'Table with colspan and empty heading',
-      context: {
-        extra_classes: 'ecl-table--responsive',
-        headers: [
-          [
-            { label: '', attributes: 'rowspan="2"' },
-            { label: 'Registration date', attributes: 'rowspan="2"' },
-            { label: 'Language', attributes: 'colspan="3"' },
-          ],
-          [{ label: 'English' }, { label: 'French' }, { label: 'German' }],
-        ],
-        rows: [
-          [
-            { label: 'John Doe' },
-            { label: '01/01/2016' },
-            { label: 'yes' },
-            { label: 'no' },
-            { label: 'no' },
-          ],
-          [
-            { label: 'Jane Doe' },
-            { label: '06/12/2016' },
-            { label: 'no' },
-            { label: 'yes' },
-            { label: 'yes' },
-          ],
-          [
-            { label: 'Jack Doe' },
-            { label: '03/05/2017' },
-            { label: 'yes' },
-            { label: 'no' },
-            { label: 'no' },
-          ],
-        ],
-        _demo: {
-          scripts: 'ECL.eclTables();',
-        },
-      },
-    },
-  ],
-  default: 'default',
+  collated: true,
+  preview: '@preview-tables',
+  collator(markup, item) {
+    return `
+        <!-- Start: @${item.handle} -->\n
+        <h3 class="ecl-heading ecl-heading--h3">${item.label}\n</h3>\n
+        ${markup}\n
+        <!-- End: @${item.handle} -->\n
+      `;
+  },
+  variants,
 };
