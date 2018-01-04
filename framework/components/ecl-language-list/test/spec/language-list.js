1ff3b2d1b25318d75dd4c51779addebd0f797fa2
Radu Chiriac <contact@raduchiriac.me>
2018-01-04 17:42:15 +0100

    Remove and reuse ecl-splash-page template

--- framework/components/ecl-language-list/test/spec/language-list.js
+++ framework/components/ecl-language-list/test/spec/language-list.js
@@ -1,37 +1,30 @@
-describe('language-lists', () => {
+describe('splash-pages', () => {
   before(() => {
     // Set viewport size
     browser.setViewportSize({
-      width: 800,
-      height: 400,
+      width: 1400,
+      height: 600,
     });
 
+    browser.url(`ecl-splash-pages.html`);
+
+    // Inject axe-core (for accessibility tests)
+    browser.injectAxeCore();
+
+    // Make sure the browser has finished painting
     browser.pause(500);
   });
 
-  describe(`language-list`, () => {
-    before(() => {
-      // Go to url
-      browser.url(`ecl-language-list.html`);
-      // Make sure the browser has finished painting
-      browser.pause(500);
-      // Inject axe-core (for accessibility tests)
-      browser.injectAxeCore();
+  // Normal state
+  it('should match the reference screenshot', () => {
+    const screenshots = browser.checkDocument({
+      name: 'splash-pages',
     });
+    expect(screenshots).to.matchReference();
+  });
 
-    // Normal state
-    context('with plain state', () => {
-      it('should match the reference screenshot', () => {
-        const screenshots = browser.checkDocument({
-          name: `language-list`,
-        });
-        expect(screenshots).to.matchReference();
-      });
-
-      it('should be accessible', () => {
-        const a11yReport = browser.runAxeCore('ecl-language-list').value;
-        expect(a11yReport).to.be.accessible;
-      });
-    });
+  it('should be accessible', () => {
+    const a11yReport = browser.runAxeCore('ecl-container').value;
+    expect(a11yReport).to.be.accessible;
   });
 });
