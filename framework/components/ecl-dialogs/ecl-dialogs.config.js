489794b7d0a99f24329d1556a9f9238befcfd47f
Radu Chiriac <contact@raduchiriac.me>
2018-01-05 15:53:51 +0100

    Add a new variant

--- framework/components/ecl-dialogs/ecl-dialogs.config.js
+++ framework/components/ecl-dialogs/ecl-dialogs.config.js
@@ -1,40 +1,60 @@
+const context = {
+  _demo: {
+    scripts: `
+      document.addEventListener('DOMContentLoaded', function () {
+        // This is only for demo purposes to facilitate end-users.
+
+        // Create a link.
+        var link = document.createElement('a');
+        var text = document.createTextNode('Open dialog');
+
+        // Include textual content.
+        link.appendChild(text);
+        link.title = "Click to test the modal";
+
+        // Add necessary demo attributes.
+        link.setAttribute('href', '#dialog');
+        link.setAttribute('class', 'ecl-link');
+        link.setAttribute('data-ecl-dialog', 'ecl-dialog');
+
+        // Show the link
+        document.body.appendChild(link);
+
+        ECL.dialogs();
+      });
+    `,
+  },
+  dialog_description: {
+    value: 'Example description',
+  },
+  dismiss: {
+    label: 'Dismiss this dialog window',
+  },
+};
+
 module.exports = {
   title: 'Dialogs',
   label: 'Dialogs',
+  collated: false,
   preview: '@preview-center-transparent',
   status: 'ready',
   tags: ['molecule'],
-  context: {
-    _demo: {
-      scripts: `
-        document.addEventListener('DOMContentLoaded', function () {
-          // This is only for demo purposes to facilitate end-users.
-
-          // Create a link.
-          var link = document.createElement('a');
-          var text = document.createTextNode('Open dialog');
-
-          // Include textual content.
-          link.appendChild(text);
-          link.title = "Click to test the modal";
-
-          // Add necessary demo attributes.
-          link.setAttribute('href', '#dialog');
-          link.setAttribute('class', 'ecl-link');
-          link.setAttribute('data-ecl-dialog', 'ecl-dialog');
-
-          // Show the link
-          document.body.appendChild(link);
-
-          ECL.dialogs();
-        });
-      `,
+  default: 'default',
+  variants: [
+    {
+      name: 'default',
+      label: 'Normal',
+      context: Object.assign({}, context, {
+        variant: 'default',
+      }),
     },
-    dialog_description: {
-      value: 'Example description',
+    {
+      name: 'wide',
+      label: 'Wide',
+      context: Object.assign({}, context, {
+        variant: 'wide',
+        theme: 'wide',
+      }),
     },
-    dismiss: {
-      label: 'Dismiss this dialog window',
-    },
-  },
+  ],
 };
