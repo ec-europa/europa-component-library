e746efdd3703c78f4ba03b8960c40f48ee2f158f
Romain Emery <emery.romain@gmail.com>
2018-02-26 11:21:19 +0100

    feat(eu-menu): add eu menu

--- packages/styleguides/eu/ecl-builder.config.js
+++ packages/styleguides/eu/ecl-builder.config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       entry: path.resolve(
         __dirname,
-        '../../../src/flavors/eu/presets/eu-preset-base/index.js'
+        '../../../src/flavors/eu/presets/eu-preset-full/index.js'
       ),
       dest: path.resolve(__dirname, outputFolder, 'scripts/europa.js'),
       options: {
@@ -21,7 +21,7 @@ module.exports = {
     {
       entry: path.resolve(
         __dirname,
-        '../../../src/flavors/eu/presets/eu-preset-base/index.scss'
+        '../../../src/flavors/eu/presets/eu-preset-full/index.scss'
       ),
       dest: path.resolve(__dirname, outputFolder, 'styles/europa.css'),
       options: {
