0cd60684d31cd442189eb7c344437f1ad0a4fbfd
Kalin Chernev <kalin.chernev@gmail.com>
2018-02-27 08:21:46 +0100

    Remove unnecessary npx in command

--- scripts/styleguide.sh
+++ scripts/styleguide.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cd packages/styleguides/docs
-npx fractal build && npx cross-env NODE_ENV=production
+npx fractal build && cross-env NODE_ENV=production
 
 cd ../ec
-npx fractal build && npx cross-env NODE_ENV=production npm run build
+npx fractal build && cross-env NODE_ENV=production npm run build
 
 cd ../eu
-npx fractal build && npx cross-env NODE_ENV=production npm run build
+npx fractal build && cross-env NODE_ENV=production npm run build
