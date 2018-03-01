5fb365eadcd9aa8059fec0e80886eb0fb74dba69
Romain Emery <emery.romain@gmail.com>
2018-03-01 09:30:25 +0100

    feat(eu-header): add eu header - INNO-927 (#574)
    * feat(eu-header): add eu header
    
    * feat(eu-header): update header
    
    * feat(eu-header): fix size
    
    * feat(eu-header): make language optional
    
    * feat(eu-header): update logos
    
    * feat(eu-header): fix width

---
+++ src/flavors/eu/components/eu-site-headers/eu-site-headers.config.js
@@ -0,0 +1,38 @@
+const languages = require('../eu-language-list/languages.json');
+
+module.exports = {
+  title: 'Site headers',
+  label: 'Site headers',
+  status: 'ready',
+  tags: ['organism'],
+  context: {
+    lang: 'en',
+    site_switcher: {
+      political: { href: '#', label: 'Commission and its priorities' },
+      info: {
+        href: '#',
+        label: 'Policies, information and services',
+        is_active: true,
+      },
+    },
+    user_menu: {
+      title: 'User menu',
+      links: [
+        { href: '#', label: 'My workbench' },
+        { href: '#', label: 'My account' },
+        { href: '#', label: 'Log out' },
+      ],
+    },
+    languages,
+    _demo: {
+      scripts: `
+        document.addEventListener('DOMContentLoaded', function () {
+          ECL.dialogs({
+            dialogOverlayId: 'ecl-overlay-language-list',
+            triggerElementsSelector: '#ecl-lang-select-sites__overlay'
+          });
+        });
+      `,
+    },
+  },
+};
