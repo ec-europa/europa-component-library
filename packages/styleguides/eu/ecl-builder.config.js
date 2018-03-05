dd40e8c4f24c3a434a8a123c54274efe42f76dc1
Yannick Huard <mail@yannickhuard.com>
2018-03-05 17:03:55 +0100

    Build all presets

--- packages/styleguides/eu/ecl-builder.config.js
+++ packages/styleguides/eu/ecl-builder.config.js
@@ -16,6 +16,17 @@ module.exports = {
         moduleName: 'ECL',
       },
     },
+    {
+      entry: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/presets/ecl-preset-corporate-eu/index.js'
+      ),
+      dest: path.resolve(__dirname, outputFolder, 'scripts/eu-corporate.js'),
+      options: {
+        sourceMap: isProd ? false : 'inline',
+        moduleName: 'ECL',
+      },
+    },
   ],
   styles: [
     {
@@ -28,12 +39,81 @@ module.exports = {
         sourceMap: isProd ? 'file' : true,
       },
     },
+    {
+      entry: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/presets/ecl-preset-base-eu/index.scss'
+      ),
+      dest: path.resolve(__dirname, outputFolder, 'styles/europa-base.css'),
+      options: {
+        sourceMap: isProd ? 'file' : true,
+      },
+    },
+    {
+      entry: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/presets/ecl-preset-editor-eu/index.scss'
+      ),
+      dest: path.resolve(__dirname, outputFolder, 'styles/europa-editor.css'),
+      options: {
+        sourceMap: isProd ? 'file' : true,
+      },
+    },
+    {
+      entry: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/presets/ecl-preset-corporate-eu/index.scss'
+      ),
+      dest: path.resolve(
+        __dirname,
+        outputFolder,
+        'styles/europa-corporate.css'
+      ),
+      options: {
+        sourceMap: isProd ? 'file' : true,
+      },
+    },
   ],
   copy: [
     {
       from: path.resolve(
         __dirname,
-        '../../../src/flavors/eu/components/ecl-logos-eu/images'
+        '../../../src/flavors/eu/components/ecl-forms-eu/ecl-forms-checkboxes-eu/images'
+      ),
+      to: path.resolve(__dirname, outputFolder, 'images'),
+    },
+    {
+      from: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/components/ecl-forms-eu/ecl-forms-feedback-messages-eu/images'
+      ),
+      to: path.resolve(__dirname, outputFolder, 'images'),
+    },
+    {
+      from: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/components/ecl-forms-eu/ecl-forms-radios-eu/images'
+      ),
+      to: path.resolve(__dirname, outputFolder, 'images'),
+    },
+    {
+      from: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/components/ecl-forms-eu/ecl-forms-selects-eu/images'
+      ),
+      to: path.resolve(__dirname, outputFolder, 'images'),
+    },
+    {
+      from: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/components/ecl-social-icons-eu/images'
+      ),
+      to: path.resolve(__dirname, outputFolder, 'images'),
+    },
+    {
+      from: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/components/ecl-messages-eu/images'
       ),
       to: path.resolve(__dirname, outputFolder, 'images'),
     },
@@ -43,6 +123,17 @@ module.exports = {
         '../../../src/generic/content/ecl-icons/fonts'
       ),
       to: path.resolve(__dirname, outputFolder, 'fonts'),
+    },
+    {
+      from: path.resolve(
+        __dirname,
+        '../../../src/flavors/eu/components/ecl-logos-eu/images'
+      ),
+      to: path.resolve(__dirname, outputFolder, 'images'),
+    },
+    {
+      from: path.resolve(__dirname, '../../../src/flavors/eu/images'),
+      to: path.resolve(__dirname, outputFolder, 'images'),
     },
   ],
 };
