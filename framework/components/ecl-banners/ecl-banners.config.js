1a71cadf1f3c547e61e8b2fd3b14e36a2e8caab3
Kalin Chernev <kalin.chernev@gmail.com>
2018-03-09 10:35:00 +0100

    No new lines, updated tests

--- framework/components/ecl-banners/ecl-banners.config.js
+++ framework/components/ecl-banners/ecl-banners.config.js
@@ -5,11 +5,7 @@ module.exports = {
   tags: ['molecule'],
   context: {
     quote: {
-      body: `
-        Nullam eget gravida sapien. Curabitur nec ligula varius tellus blandit auctor in non felis.
-        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-        Pellentesque in volutpat eros, ut fringilla tortor.
-      `,
+      body: `Nullam eget gravida sapien. Curabitur nec ligula varius tellus blandit auctor in non felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque in volutpat eros, ut fringilla tortor.`,
       author: 'President Juncker',
     },
   },
