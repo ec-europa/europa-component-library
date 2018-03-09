13801553850a8e465d22a93eabefb00de060485e
Yannick Huard <mail@yannickhuard.com>
2018-03-09 13:24:47 +0100

    feat(test): only check components that have changed from PR's target branch - no issue (#591)
    * feat(test): only check components that have changed from PR's target branch
    
    * Wrap test runner
    
    * Change console output
    
    * Guess PR
    
    * test
    
    * correctly use head
    
    * try fetch all
    
    * git pull all
    
    * pull no tags
    
    * checkout all the branches?
    
    * only fetch branch when needed
    
    * no condition on test (now)
    
    * forgot await...
    
    * Clean up test, cache 'lerna updated' results
    
    * test new interface
    
    * fix bash script
    
    * update condition
    
    * remove test data
    
    * Update test script

---
+++ scripts/test-functional.js
@@ -0,0 +1,66 @@
+/**
+ * Run: "yarn test:functional"
+ * Accepted parameters:
+ * --ignoreCache              - Ignore cache, rebuilt the updated packages list (default: false)
+ * --cacheResults             - If false, the cache file won't be updated (default: true)
+ * --since [branch]           - Force comparison with the branch
+ * --spec [path to spec file] - Run a specific test file
+ */
+
+const path = require('path');
+const { Launcher } = require('webdriverio');
+const { getTestSpecs } = require('./utils/getTestSpecs');
+const minimist = require('minimist');
+
+const testFunctional = async ({
+  ignoreCache = false,
+  cacheResults = true,
+  since = '',
+  spec = '',
+} = {}) => {
+  let specs = [];
+
+  if (!spec) {
+    // Load specs
+    specs = await getTestSpecs({
+      ignoreCache,
+      cacheResults,
+      since,
+    });
+
+    if (specs.length === 0) {
+      console.log('No test will be run');
+      return process.exit(0);
+    }
+
+    console.log('The following tests will be run', specs);
+  } else {
+    console.log('The following test will be run', spec);
+  }
+
+  // Configure launcher
+  const wdio = new Launcher(
+    path.resolve(__dirname, '../test/functional/wdio.conf.js'),
+    {
+      specs,
+      spec,
+    }
+  );
+
+  // Start tests
+  return wdio.run().then(
+    // On success
+    code => {
+      console.log('Process exited with code', code);
+      process.exit(code);
+    },
+    // On error
+    error => {
+      console.error('Launcher failed to start the test', error.stacktrace);
+      process.exit(1);
+    }
+  );
+};
+
+const argv = minimist(process.argv.slice(2));
+testFunctional(argv);
--- scripts/travis/before_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-if [ "${TEST_SUITE}" = "test:functional" ] && { [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] || yarn lerna updated -- --since master ; } ; then
-  npm rebuild node-sass;
-  npm run dist;
-fi
