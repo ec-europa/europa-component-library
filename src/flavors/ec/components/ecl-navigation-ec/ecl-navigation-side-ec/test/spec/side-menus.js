383d22af25e43d54b82d9d9a4834ab9d23b55367
Romain Emery <emery.romain@gmail.com>
2018-03-12 15:03:46 +0100

    feat(side-navigation): fix test

--- src/flavors/ec/components/ecl-navigation-ec/ecl-navigation-side-ec/test/spec/side-menus.js
+++ src/flavors/ec/components/ecl-navigation-ec/ecl-navigation-side-ec/test/spec/side-menus.js
@@ -9,7 +9,7 @@ describe('ecl-navigation-side', () => {
     browser.pause(1000);
 
     // Go to the URL of the component
-    browser.goToComponent('ecl-navigation-side-eu');
+    browser.goToComponent('ecl-navigation-side-ec');
 
     // Make sure the browser has finished painting
     browser.pause(1000);
