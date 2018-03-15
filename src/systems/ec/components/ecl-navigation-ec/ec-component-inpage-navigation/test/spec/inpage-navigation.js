6e2bc6e52415c8fd7b33e805dd226f8ebeadab0d
Yannick Huard <mail@yannickhuard.com>
2018-03-15 22:12:51 +0100

    rename navigation-side -> side-navigation and inpage

---
+++ src/systems/ec/components/ecl-navigation-ec/ec-component-inpage-navigation/test/spec/inpage-navigation.js
@@ -0,0 +1,26 @@
+describe('inpage navigation', () => {
+  before(() => {
+    browser.setViewportSize({
+      width: 1400,
+      height: 600,
+    });
+    browser.goToComponent('ec-component-inpage-navigation');
+    browser.pause(500);
+    browser.injectAxeCore();
+  });
+
+  // Normal state
+  context('with plain state', () => {
+    it('should match the reference screenshot', () => {
+      const screenshots = browser.checkDocument({
+        name: `ecl-inpage-navigation`,
+      });
+      expect(screenshots).to.matchReference();
+    });
+
+    it('should be accessible', () => {
+      const a11yReport = browser.runAxeCore('ecl-inpage-navigation').value;
+      expect(a11yReport).to.be.accessible;
+    });
+  });
+});
