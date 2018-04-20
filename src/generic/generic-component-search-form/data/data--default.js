55582efa13c8390ae49e382f875e09a03d7b2d5e
Radu Chiriac <contact@raduchiriac.me>
2018-04-20 15:12:16 +0200

    Add better social-icons context importing

---
+++ src/generic/generic-component-search-form/data/data--default.js
@@ -0,0 +1,13 @@
+module.exports = {
+  aria_label: 'Search this website',
+  button: {
+    label: 'Search',
+  },
+  input: {
+    id: 'corporate-search',
+    extra_attributes: [
+      { name: 'size', value: '30' },
+      { name: 'maxlength', value: '128' },
+    ],
+  },
+};
---
+++ src/generic/generic-component-search-form/data/data--internal.js
@@ -0,0 +1,10 @@
+module.exports = {
+  variant: 'internal',
+  aria_label: 'Search this website',
+  button: {
+    label: 'Search',
+  },
+  input: {
+    id: 'internal-search',
+  },
+};
