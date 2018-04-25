ffbceaf8ab04a3dc5d90ea6afa16689e5f625713
Radu Chiriac <contact@raduchiriac.me>
2018-04-25 13:17:15 +0200

    Recover 'screen_reader_description' key

--- src/generic/generic-component-message/data/demo--info.js
+++ src/generic/generic-component-message/data/demo--info.js
@@ -1,5 +1,6 @@
 module.exports = {
   dismiss: true,
+  screen_reader_description: 'Informative message',
   title: 'Some info title',
   messages: [
     'Lorem ipsum lor sit amet, consectetur adipi',
--- src/generic/generic-component-message/data/demo--warning.js
+++ src/generic/generic-component-message/data/demo--warning.js
@@ -1,6 +1,7 @@
 module.exports = {
   modifier: 'warning',
   dismiss: true,
+  screen_reader_description: 'Warning message',
   title: 'Some warning title',
   messages: [
     'Lorem ipsum lor sit amet, consectetur adipi',
