21df5c7c63f4c8d88f3eba56cb51fc030e9f5e87
Radu Chiriac <contact@raduchiriac.me>
2018-04-25 20:54:52 +0200

    refactor(config): harmonize config.js, pt.4 - INNO-1026 (#673)
    * Move context for field
    
    * Move context for file
    
    * Move context for field, file, featured-item, dropdown, dialog
    
    * Move context for datepicker

--- src/systems/eu/eu-component/eu-component-dialog/eu-component-dialog.config.js
+++ src/systems/eu/eu-component/eu-component-dialog/eu-component-dialog.config.js
@@ -1,36 +1,4 @@
-const context = {
-  _demo: {
-    scripts: `
-      document.addEventListener('DOMContentLoaded', function () {
-        // This is only for demo purposes to facilitate end-users.
-
-        // Create a link.
-        var link = document.createElement('a');
-        var text = document.createTextNode('Open dialog');
-
-        // Include textual content.
-        link.appendChild(text);
-        link.title = "Click to test the modal";
-
-        // Add necessary demo attributes.
-        link.setAttribute('href', '#dialog');
-        link.setAttribute('class', 'ecl-link');
-        link.setAttribute('data-ecl-dialog', 'ecl-dialog');
-
-        // Show the link
-        document.body.appendChild(link);
-
-        ECL.dialogs();
-      });
-    `,
-  },
-  dialog_description: {
-    value: 'Example description',
-  },
-  dismiss: {
-    label: 'Dismiss this dialog window',
-  },
-};
+const context = require('@ecl/generic-component-dialog/data/demo');
 
 module.exports = {
   title: 'Dialogs',
