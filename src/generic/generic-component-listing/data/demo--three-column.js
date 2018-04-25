06faf951c4dc1aa8ad52fb795f20bc77d3adf6d8
Radu Chiriac <contact@raduchiriac.me>
2018-04-25 10:49:01 +0200

    Swap data for component-listing demo

--- src/generic/generic-component-listing/data/demo--three-column.js
+++ src/generic/generic-component-listing/data/demo--three-column.js
@@ -2,33 +2,70 @@ module.exports = {
   variant: 'three-columns',
   items: [
     {
-      metas: ['Director-General'],
-      screen_reader: 'List item: Director-General John Doe',
-      title: 'John Doe',
-      href: '../../example.html#listing-thumb-1',
+      screen_reader: 'List item: Business, Economy, Euro',
+      title: 'Business, Economy, Euro',
+      href: '../../example.html#listing-three-columns-1',
+      detail:
+        'EU economy, the euro, and practical information for EU businesses and entrepreneurs.',
+    },
+    {
+      screen_reader: 'List item: About the European Union',
+      title: 'About the European Union',
+      href: '../../example.html#listing-three-columns-2',
+      detail: 'The EU and its institutions, how to visit and work at the EU.',
+    },
+    {
+      screen_reader: 'List item: Live, Work, Travel in EU',
+      title: 'Live, Work, Travel in EU',
+      href: '../../example.html#listing-three-columns-3',
+      detail:
+        'Advice on living, working or travelling in the EU, on visas and immigration for non-EU citizens, European culture.',
+    },
+    {
+      screen_reader: 'List item: Funding, Tenders',
+      title: 'Funding, Tenders',
+      href: '../../example.html#listing-three-columns-4',
+      detail: 'EU funding, grants, tenders, and how to apply.',
+    },
+    {
+      screen_reader: 'List item: Law',
+      title: 'Law',
+      href: '../../example.html#listing-three-columns-5',
+      detail:
+        'EU law and judgments, how EU law is applied, public consultations, data protection, infringements, fraud, serious crime.',
+    },
+    {
+      screen_reader: 'List item: Research and innovation',
+      title: 'Research and innovation',
+      href: '../../example.html#listing-three-columns-6',
+      detail:
+        'Research funding, partners, results, and EU action to promote innovation.',
+    },
+    {
+      variant: 'highlight',
+      screen_reader: 'List item: State of the European Union 2017',
+      title: 'State of the European Union 2017',
       primary_image: {
-        src: '../../assets/demo_images/default_profile_image.png',
-        alt: 'Director-General John Doe',
+        src: '../../assets/demo_images/nature-demo-1.jpg',
+        alt: 'State of the European Union 2017',
       },
     },
     {
-      metas: ['Deputy Director-General'],
-      screen_reader: 'List item: Deputy Director-General Jane Doe',
-      title: 'Jane Doe',
-      href: '../../example.html#listing-thumb-2',
+      variant: 'highlight',
+      screen_reader: 'List item: White paper on the future of Europe',
+      title: 'White paper on the future of Europe',
       primary_image: {
-        src: '../../assets/demo_images/default_profile_image.png',
-        alt: 'Deputy Director-General Jane Doe',
+        src: '../../assets/demo_images/nature-demo-2.jpg',
+        alt: 'White paper on the future of Europe',
       },
     },
     {
-      metas: ['Acting Deputy Director-General'],
-      screen_reader: 'List item: Acting Deputy Director-General Jack Doe',
-      title: 'Jack Doe',
-      href: '../../example.html#listing-thumb-3',
+      variant: 'highlight',
+      screen_reader: 'List item: EU solidarity',
+      title: 'EU solidarity',
       primary_image: {
-        src: '../../assets/demo_images/default_profile_image.png',
-        alt: 'Acting Deputy Director-General Jack Doe',
+        src: '../../assets/demo_images/nature-demo-3.jpg',
+        alt: 'EU solidarity',
       },
     },
   ],
