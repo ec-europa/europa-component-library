02d452312035ed1ab096d247943a76ac12ccea5d
Yannick Huard <mail@yannickhuard.com>
2018-04-26 08:38:03 +0200

    fix(ecl-editor): fix ecl-editor exports - no issue (#675)
    * fix(ecl-editor): fix ecl-editor exports - no issue
    
    * fix imports and mixin name
    
    * fix site-switcher
    
    * update screenshots
    
    * fix tests

--- src/generic/generic-component-table/data/demo--empty-enhanced.js
+++ src/generic/generic-component-table/data/demo--empty-enhanced.js
@@ -1,34 +1,32 @@
 module.exports = {
   extra_classes: 'ecl-table--responsive',
   headers: [
-    [
-      { label: '', attributes: 'rowspan="2"' },
-      { label: 'Registration date', attributes: 'rowspan="2"' },
-      { label: 'Language', attributes: 'colspan="3"' },
-    ],
-    [{ label: 'English' }, { label: 'French' }, { label: 'German' }],
+    [{ label: '' }, { label: 'Registration date' }, { label: 'Email' }],
   ],
   rows: [
     [
       { label: 'John Doe' },
       { label: '01/01/2016' },
-      { label: 'yes' },
-      { label: 'no' },
-      { label: 'no' },
+      {
+        label:
+          '<a class="ecl-link" href="mailto:john.doe@mail.com">john.doe@mail.com</a>',
+      },
     ],
     [
       { label: 'Jane Doe' },
       { label: '06/12/2016' },
-      { label: 'no' },
-      { label: 'yes' },
-      { label: 'yes' },
+      {
+        label:
+          '<a class="ecl-link" href="mailto:jane.doe@mail.com">jane.doe@mail.com</a>',
+      },
     ],
     [
       { label: 'Jack Doe' },
       { label: '03/05/2017' },
-      { label: 'yes' },
-      { label: 'no' },
-      { label: 'no' },
+      {
+        label:
+          '<a class="ecl-link" href="mailto:jack.doe@mail.com">jack.doe@mail.com</a>',
+      },
     ],
   ],
 };
