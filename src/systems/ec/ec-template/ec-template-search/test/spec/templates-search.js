d696302e26dde7366fa7c24312bf8526ecccfd59
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-04 14:42:58 +0200

    fix(templates-search): correct usage of flex utility class - noissue

--- src/systems/ec/ec-template/ec-template-search/test/spec/templates-search.js
+++ src/systems/ec/ec-template/ec-template-search/test/spec/templates-search.js
@@ -1,4 +1,4 @@
-describe('templates-search', () => {
+describe('templates-search in desktop', () => {
   before(() => {
     browser.setViewportSize({
       width: 1400,
@@ -15,7 +15,35 @@ describe('templates-search', () => {
   // Normal state
   it('should match the reference screenshot', () => {
     const screenshots = browser.checkDocument({
-      name: 'templates-search',
+      name: 'templates-search/desktop',
+    });
+    expect(screenshots).to.matchReference();
+  });
+
+  it('should be accessible', () => {
+    const a11yReport = browser.runAxeCore('ecl-row').value;
+    expect(a11yReport).to.be.accessible;
+  });
+});
+
+describe('templates-search in mobile', () => {
+  before(() => {
+    browser.setViewportSize({
+      width: 920,
+      height: 920,
+    });
+
+    browser.goToComponent('ec-template-search');
+
+    browser.injectAxeCore();
+
+    browser.pause(500);
+  });
+
+  // Normal state
+  it('should match the reference screenshot', () => {
+    const screenshots = browser.checkDocument({
+      name: 'templates-search/mobile',
     });
     expect(screenshots).to.matchReference();
   });
