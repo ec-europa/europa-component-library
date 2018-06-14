44612a205bbf714ab77a612379e93b37ae11f11d
Romain Emery <emery.romain@gmail.com>
2018-06-14 14:41:04 +0200

    feat(breadcrumb): add collapse behavior

--- src/systems/ec/ec-component/ec-component-breadcrumb/ec-component-breadcrumb.js
+++ src/systems/ec/ec-component/ec-component-breadcrumb/ec-component-breadcrumb.js
@@ -1,3 +1,4 @@
+import debounce from 'lodash.debounce';
 import { queryAll } from '@ecl/generic-base/helpers/dom';
 
 /**
@@ -5,7 +6,10 @@ import { queryAll } from '@ecl/generic-base/helpers/dom';
  */
 export const initBreadcrumb = ({
   breadcrumbSelector: breadcrumbSelector = '.ecl-breadcrumb',
-  expandSelector: expandSelector = '.ecl-breadcrumb__expand',
+  expandButtonSelector: expandButtonSelector = '.ecl-breadcrumb__expand-btn',
+  segmentSelector: segmentSelector = '.ecl-breadcrumb__segment',
+  segmentVisibleSelector: segmentVisibleSelector = '.ecl-breadcrumb__segment:not(.ecl-breadcrumb__segment--first):not(.ecl-breadcrumb__segment--ellipsis):not(.ecl-breadcrumb__segment--last):not([aria-hidden="true"])',
+  ellipsisSelector: ellipsisSelector = '.ecl-breadcrumb__segment--ellipsis',
 } = {}) => {
   if (
     !('querySelector' in document) ||
@@ -15,34 +19,113 @@ export const initBreadcrumb = ({
     return null;
 
   // ACTIONS
+  function displayEllipsis(breadcrumbContainer) {
+    const breadcrumbEllipsis = queryAll(ellipsisSelector, breadcrumbContainer);
+    breadcrumbEllipsis.forEach(ellipsis => {
+      ellipsis.setAttribute('aria-hidden', 'false');
+    });
+  }
+
+  function breadcrumbIsTooLarge(breadcrumbContainer) {
+    // get wrapper width
+    const wrapperWidth = breadcrumbContainer.offsetWidth;
+
+    // get displayed segments width
+    const breadcrumbSegments = queryAll(segmentSelector, breadcrumbContainer);
+    let segmentWidth = 0;
+    breadcrumbSegments.forEach(breadcrumbSegment => {
+      segmentWidth += breadcrumbSegment.offsetWidth;
+    });
+
+    return segmentWidth > wrapperWidth;
+  }
+
+  function hideSegment(breadcrumbContainer) {
+    // hide segments if needed
+    const breadcrumbVisibleSegments = queryAll(
+      segmentVisibleSelector,
+      breadcrumbContainer
+    );
+    if (breadcrumbVisibleSegments.length > 0) {
+      breadcrumbVisibleSegments[0].setAttribute('aria-hidden', 'true');
+    }
+
+    // check if there is another segment to be hidden
+    if (breadcrumbIsTooLarge(breadcrumbContainer)) {
+      hideSegment(breadcrumbContainer);
+    }
+  }
 
   // EVENTS
-  function eventExpandClick(e) {
+  function eventExpandClick(e, breadcrumbContainer) {
     e.preventDefault();
+    // display all segments
+    const breadcrumbSegments = queryAll(segmentSelector, breadcrumbContainer);
+    breadcrumbSegments.forEach(breadcrumbSegment => {
+      breadcrumbSegment.setAttribute('aria-hidden', 'false');
+    });
+
+    // hide ellipsis once expanded
     const target = e.currentTarget;
     target.parentElement.setAttribute('aria-hidden', 'true');
   }
 
-  // BIND EVENTS
-  function bindBreadcrumbEvents(breadcrumbContainer) {
-    const expands = queryAll(expandSelector, breadcrumbContainer);
-    // bind all items in the list click and keydown events
-    expands.forEach(expand => {
-      expand.addEventListener('click', eventExpandClick);
-    });
-  }
-
-  // UNBIND EVENTS
-  function unbindBreadcrumbEvents(breadcrumbContainer) {
-    const expands = queryAll(expandSelector, breadcrumbContainer);
-    // bind all items in the list click and keydown events
-    expands.forEach(expand => {
-      expand.removeEventListener('click', eventExpandClick);
-    });
+  function eventResize(breadcrumbContainer) {
+    if (breadcrumbIsTooLarge(breadcrumbContainer)) {
+      hideSegment(breadcrumbContainer);
+      displayEllipsis(breadcrumbContainer);
+    }
   }
 
   // SETUP
   const breadcrumbContainers = queryAll(breadcrumbSelector);
+
+  // BIND EVENTS
+  function bindBreadcrumbEvents(breadcrumbContainer) {
+    const expands = queryAll(expandButtonSelector, breadcrumbContainer);
+
+    // bind click event for expand
+    expands.forEach(expand => {
+      expand.addEventListener('click', e =>
+        eventExpandClick(e, breadcrumbContainer)
+      );
+    });
+
+    // bind resize event to check breadcrumb width
+    window.addEventListener(
+      'resize',
+      debounce(
+        () => {
+          breadcrumbContainers.forEach(eventResize);
+        },
+        100,
+        { maxWait: 300 }
+      )
+    );
+  }
+
+  // UNBIND EVENTS
+  function unbindBreadcrumbEvents(breadcrumbContainer) {
+    const expands = queryAll(expandButtonSelector, breadcrumbContainer);
+    // unbind click event for expand
+    expands.forEach(expand => {
+      expand.removeEventListener('click', e =>
+        eventExpandClick(e, breadcrumbContainer)
+      );
+    });
+
+    // unbind resize event to check breadcrumb width
+    window.removeEventListener(
+      'resize',
+      debounce(
+        () => {
+          breadcrumbContainers.forEach(eventResize);
+        },
+        100,
+        { maxWait: 300 }
+      )
+    );
+  }
 
   // DESTROY
   function destroy() {
@@ -58,6 +141,9 @@ export const initBreadcrumb = ({
     if (breadcrumbContainers.length) {
       breadcrumbContainers.forEach(breadcrumbContainer => {
         bindBreadcrumbEvents(breadcrumbContainer);
+
+        // trigger resize event to init collapse
+        eventResize(breadcrumbContainer);
       });
     }
   }
