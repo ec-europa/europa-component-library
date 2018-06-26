cf8c6c04d9e4c75d5c2fae019c89b440ae0e6ecb
Radu Chiriac <contact@raduchiriac.me>
2018-06-26 15:36:12 +0200

    Add new variant for list-item

--- src/generic/generic-component-block-list/data/demo.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  href: '../../example.html#1',
-  title: 'Home',
-  description:
-    'Irure id id sint ex occaecat aute exercitation adipisicing pariatur nisi tempor adipisicing.',
-};
---
+++ src/generic/generic-component-list-item/data/demo--block.js
@@ -0,0 +1,6 @@
+module.exports = {
+  variant: 'block',
+  href: '../../example.html#block-item-1',
+  title: 'EU law',
+  detail: 'Dolore duis elit in id voluptate nisi commodo quis.',
+};
