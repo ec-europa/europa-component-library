8723d8361b16c8317c68819630ce5b11bffa8bfa
Radu Chiriac <contact@raduchiriac.me>
2018-06-29 11:08:33 +0200

    Translate languages not titles

--- src/generic/generic-component-file/data/demo--translations.js
+++ src/generic/generic-component-file/data/demo--translations.js
@@ -6,11 +6,12 @@ module.exports = {
   button_label: 'Download',
   icon: 'file',
   translations: [
-    { title: 'Titre du fichier', meta: '228.84 kB - PDF - 4 pages' },
-    { title: 'Dateititel', meta: '232.12 kB - PDF - 4 pages' },
-    { title: 'Файл Заглавие', meta: '257.54 kB - PDF - 4 pages' },
+    { title: 'español', meta: '228.84 kB - PDF - 4 pages' },
+    { title: 'German', meta: '232.12 kB - PDF - 4 pages' },
+    { title: 'dansk', meta: '257.54 kB - PDF - 4 pages' },
+    { title: 'Български', meta: '211.54 kB - PDF - 4 pages' },
   ],
-  translations_label: 'Available languages (3)',
+  translations_label: 'Available languages (4)',
   translations_tooltip: 'Click to see translations',
   _demo: {
     scripts: `document.addEventListener('DOMContentLoaded', function () {
