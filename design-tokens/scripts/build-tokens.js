412cc6f12282db43d1b3bd088f20cef54aad4c44
Yannick Huard <mail@yannickhuard.com>
2018-07-11 12:27:30 +0200

    more complex demo

--- design-tokens/scripts/build-tokens.js
+++ design-tokens/scripts/build-tokens.js
@@ -3,15 +3,24 @@ const path = require('path');
 const mkdirp = require('mkdirp');
 const theo = require('theo');
 
-const input = path.join(__dirname, '../tokens/index.yml');
-const writeDir = path.join(__dirname, '../dist');
+const inputs = [
+  {
+    file: path.join(__dirname, '../themes/default/index.yml'),
+    theme: 'default',
+  },
+  {
+    file: path.join(__dirname, '../themes/theme1/index.yml'),
+    theme: 'theme1',
+  },
+];
 
+const writeDir = path.join(__dirname, '../dist');
 mkdirp.sync(writeDir);
 
 // Based on https://github.com/salesforce-ux/theo/blob/master/lib/formats/default.scss.hbs
 theo.registerFormat(
   'eclScss',
-  `/* ECL Tokens */
+  `/* ECL Variables */
 
 {{#each props as |prop|}}
   {{#if prop.comment~}}
@@ -23,21 +32,23 @@ theo.registerFormat(
 `
 );
 
-theo
-  .convert({
-    transform: {
-      type: 'web',
-      file: input,
-    },
-    format: {
-      type: 'eclScss',
-    },
-  })
-  .then(result => {
-    fs.writeFile(
-      path.join(writeDir, 'index.scss'),
-      result,
-      error => error && console.log('Error writing file: ', error)
-    );
-  })
-  .catch(error => error && console.log('Error converting tokens: ', error));
+inputs.forEach(({ file, theme }) => {
+  theo
+    .convert({
+      transform: {
+        type: 'web',
+        file,
+      },
+      format: {
+        type: 'eclScss',
+      },
+    })
+    .then(result => {
+      fs.writeFile(
+        path.join(writeDir, `${theme}.scss`),
+        result,
+        error => error && console.log('Error writing file: ', error)
+      );
+    })
+    .catch(error => error && console.log('Error converting tokens: ', error));
+});
