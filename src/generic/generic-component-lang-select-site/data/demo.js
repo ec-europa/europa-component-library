2791e4bef4365213404497230f6f0483f83cd447
Romain Emery <emery.romain@gmail.com>
2018-07-17 15:08:58 +0200

    refactor(icons): lang-select-site

--- src/generic/generic-component-lang-select-site/data/demo.js
+++ src/generic/generic-component-lang-select-site/data/demo.js
@@ -1,5 +1,14 @@
-module.exports = {
-  language: 'English',
-  href: '../../example.html#',
-  code: 'en',
+module.exports = function context(system) {
+  const c = {
+    language: 'English',
+    href: '../../example.html#',
+    code: 'en',
+    icon: {
+      icon_path: `../../${system}-preset-website/images/icons/symbol-defs.svg`,
+      icon: 'language',
+      size: 'l',
+    },
+  };
+
+  return c;
 };
