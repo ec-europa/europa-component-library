50938a7287a7bda84d8c18df5f1efceacf8f0443
Romain Emery <emery.romain@gmail.com>
2018-07-17 12:58:08 +0200

    refactor(icons): file

--- src/generic/generic-component-file/data/demo--translations.js
+++ src/generic/generic-component-file/data/demo--translations.js
@@ -1,21 +1,29 @@
-module.exports = {
-  variant: 'translation',
-  title: 'File title example',
-  language: 'English',
-  meta: '213.25 kB - PDF - 4 pages',
-  button_label: 'Download',
-  icon: 'file',
-  translations: [
-    { title: 'español', meta: '228.84 kB - PDF - 4 pages' },
-    { title: 'German', meta: '232.12 kB - PDF - 4 pages' },
-    { title: 'dansk', meta: '257.54 kB - PDF - 4 pages' },
-    { title: 'Български', meta: '211.54 kB - PDF - 4 pages' },
-  ],
-  translations_label: 'Available languages (4)',
-  translations_tooltip: 'Click to see translations',
-  _demo: {
-    scripts: `document.addEventListener('DOMContentLoaded', function () {
-      ECL.initExpandables('#translations-expand-button');
-    });`,
-  },
+module.exports = function context(system) {
+  const c = {
+    variant: 'translation',
+    title: 'File title example',
+    language: 'English',
+    meta: '213.25 kB - PDF - 4 pages',
+    button_label: 'Download',
+    icon: {
+      icon_path: `../../${system}-preset-website/images/icons/symbol-defs.svg`,
+      icon: 'file',
+      size: 'xxl',
+    },
+    translations: [
+      { title: 'español', meta: '228.84 kB - PDF - 4 pages' },
+      { title: 'German', meta: '232.12 kB - PDF - 4 pages' },
+      { title: 'dansk', meta: '257.54 kB - PDF - 4 pages' },
+      { title: 'Български', meta: '211.54 kB - PDF - 4 pages' },
+    ],
+    translations_label: 'Available languages (4)',
+    translations_tooltip: 'Click to see translations',
+    _demo: {
+      scripts: `document.addEventListener('DOMContentLoaded', function () {
+        ECL.initExpandables('#translations-expand-button');
+      });`,
+    },
+  };
+
+  return c;
 };
