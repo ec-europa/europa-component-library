8b54d8782b9b4feac9a2fbd16e92982f724ddb6d
Romain Emery <emery.romain@gmail.com>
2018-07-23 10:28:50 +0200

    refactor(icons): breadcrumb

--- src/generic/generic-component-breadcrumb/data/demo.js
+++ src/generic/generic-component-breadcrumb/data/demo.js
@@ -1,29 +1,38 @@
-module.exports = {
-  segments: [
-    {
-      href: '../../example.html#',
-      label: 'Home',
+module.exports = function context(system) {
+  const c = {
+    segments: [
+      {
+        href: '../../example.html#',
+        label: 'Home',
+      },
+      {
+        href: '../../example.html#',
+        label: 'Priorities',
+      },
+      {
+        href: '../../example.html#',
+        label: 'Category',
+      },
+      {
+        href: '../../example.html#',
+        label: 'Jobs, Growth and Investment',
+      },
+      {
+        label: 'Page title',
+      },
+    ],
+    icon: {
+      icon_path: `../../${system}-preset-website/images/icons/symbol-defs.svg`,
+      icon: 'right',
+      size: 'xxs',
     },
-    {
-      href: '../../example.html#',
-      label: 'Priorities',
+    _demo: {
+      scripts: `
+        document.addEventListener('DOMContentLoaded', function () { 
+          ECL.initBreadcrumb();
+        });`,
     },
-    {
-      href: '../../example.html#',
-      label: 'Category',
-    },
-    {
-      href: '../../example.html#',
-      label: 'Jobs, Growth and Investment',
-    },
-    {
-      label: 'Page title',
-    },
-  ],
-  _demo: {
-    scripts: `
-      document.addEventListener('DOMContentLoaded', function () { 
-        ECL.initBreadcrumb();
-      });`,
-  },
+  };
+
+  return c;
 };
