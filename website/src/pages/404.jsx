162016771c32fd43c7ff0821eac05936f4fb90d1
Yannick Huard <mail@yannickhuard.com>
2018-07-30 16:08:40 +0200

    make splash page independent from side nav

--- website/src/pages/404.jsx
+++ website/src/pages/404.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 const PageNotFound = () => (
   <main id="main-content" tabIndex="-1">
+    <Helmet title="404 - Page not found" />
     <div className="ecl-container ecl-u-pv-xl">
       <div className="ecl-row ecl-u-mt-xl ecl-editor">
         <h1>Page not found</h1>
