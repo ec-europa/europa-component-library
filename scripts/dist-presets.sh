be2ae2db3959ded0924f164a51f22eb912c5bd46
Yannick Huard <mail@yannickhuard.com>
2018-07-30 11:46:44 +0200

    fix script

--- scripts/dist-presets.sh
+++ scripts/dist-presets.sh
@@ -21,8 +21,8 @@ cp -r ./src/systems/ec/ec-preset/ec-preset-corporate/dist ./dist/packages/ec-pre
 cp -r ./src/systems/ec/ec-preset/ec-preset-full/dist ./dist/packages/ec-preset-full
 cp -r ./src/systems/ec/ec-preset/ec-preset-website/dist ./dist/packages/ec-preset-website
 cp -r ./src/systems/ec/ec-preset/ec-preset-webtools/dist ./dist/packages/ec-preset-webtools
-cp -r ./src/systems/eu/eu-preset/eu-preset-base/dist ./dist/packages/eu-preset-base
-cp -r ./src/systems/eu/eu-preset/eu-preset-corporate/dist ./dist/packages/eu-preset-corporate
-cp -r ./src/systems/eu/eu-preset/eu-preset-full/dist ./dist/packages/eu-preset-full
-cp -r ./src/systems/eu/eu-preset/eu-preset-website/dist ./dist/packages/eu-preset-website
-cp -r ./src/systems/eu/eu-preset/eu-preset-webtools/dist ./dist/packages/eu-preset-webtools
+cp -r ./src/systems/eu/vanilla-components/eu-preset/eu-preset-base/dist ./dist/packages/eu-preset-base
+cp -r ./src/systems/eu/vanilla-components/eu-preset/eu-preset-corporate/dist ./dist/packages/eu-preset-corporate
+cp -r ./src/systems/eu/vanilla-components/eu-preset/eu-preset-full/dist ./dist/packages/eu-preset-full
+cp -r ./src/systems/eu/vanilla-components/eu-preset/eu-preset-website/dist ./dist/packages/eu-preset-website
+cp -r ./src/systems/eu/vanilla-components/eu-preset/eu-preset-webtools/dist ./dist/packages/eu-preset-webtools
