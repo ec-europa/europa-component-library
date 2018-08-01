cad8b5f2f02ba1387994319f08f3271df425519e
Yannick Huard <mail@yannickhuard.com>
2018-08-01 14:38:25 +0200

    write content in markdown

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Document from './index.md';
 import LogoLink from '../components/LogoLink';
 import ECLogo from '@ecl/ec-preset-website/dist/images/logo/logo--en.svg';
 import EULogo from '@ecl/eu-preset-website/dist/images/logo/logo--en.svg';
@@ -8,18 +9,7 @@ const HomePage = () => (
   <main id="main-content" tabIndex="-1" className="tmp-splash-page">
     <div className="custom-container">
       <div className="ecl-editor tmp-splash-page-editor">
-        <h1>Component Library</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis
-          metus non lectus dignissim, ac viverra elit convallis. Pellentesque
-          tempus purus id facilisis lacinia.
-        </p>
-        <p>
-          Nulla consequat suscipit commodo. Fusce sollicitudin orci id volutpat
-          tempor. Duis vestibulum, diam vel gravida dapibus, odio velit eleifend
-          felis, id elementum dolor nisi id arcu.
-        </p>
-        <p>Select between the available libraries:</p>
+        <Document />
       </div>
       <div className="custom-row tmp-splash-page-actions-row">
         <div className="custom-col-4/4 custom-col-md-4/8 custom-col-xl-6/12 ecl-u-d-flex">
