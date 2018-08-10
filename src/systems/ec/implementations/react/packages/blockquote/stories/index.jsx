08adaea98bf96a2370ce3dfb58c539197b85a128
Yannick Huard <mail@yannickhuard.com>
2018-08-10 14:58:40 +0200

    share context

--- src/systems/ec/implementations/react/packages/blockquote/stories/index.jsx
+++ src/systems/ec/implementations/react/packages/blockquote/stories/index.jsx
@@ -2,18 +2,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs/react';
-import Blockquote from '../Blockquote';
+import demoContent from '@ecl/ec-specs-blockquote/demo/data';
 
+import Blockquote from '../Blockquote';
 import './index.scss';
 
 storiesOf('Blockquote', module)
   .addDecorator(withKnobs)
   .add('interactive', () => (
     <Blockquote
-      author={text('Author', 'President Juncker')}
-      citation={text(
-        'Citation',
-        'An interconnected grid will help deliver the ultimate goal of the Energy Union, to ensure affordable, secure and sustainable energy, and also growth across the EU.'
-      )}
+      author={text('Author', demoContent.author)}
+      citation={text('Citation', demoContent.citation)}
     />
   ));
--- src/systems/ec/specs/components/blockquote/demo/data.js
+++ src/systems/ec/specs/components/blockquote/demo/data.js
@@ -1,5 +1,6 @@
+// Simple content for demo
 module.exports = {
-  body:
+  citation:
     'An interconnected grid will help deliver the ultimate goal of the Energy Union, to ensure affordable, secure and sustainable energy, and also growth across the EU.',
   author: 'President Juncker',
 };
