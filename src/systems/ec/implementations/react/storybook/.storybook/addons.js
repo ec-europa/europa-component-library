5a7a5c921f2978a40e612f1b9ccf45f7bbb8686f
Yannick Huard <mail@yannickhuard.com>
2018-08-15 12:50:45 +0200

    upgrade to storybook 4 (alpha)

--- src/systems/ec/implementations/react/storybook/.storybook/addons.js
+++ src/systems/ec/implementations/react/storybook/.storybook/addons.js
@@ -1,4 +1,5 @@
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-knobs/register';
 import '@storybook/addon-options/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-viewport/register';
+import '@storybook/addon-a11y/register';
