8de7b6abcb59ca9174a344d7336addba9780fda8
Romain Emery <emery.romain@gmail.com>
2018-08-28 09:42:38 +0200

    fix(logo): remove mute logo - INNO-1185 (#786)
    * remove mute logo
    
    * fix css
    
    * update status

--- src/systems/ec/implementations/react/packages/logo/stories/index.jsx
+++ src/systems/ec/implementations/react/packages/logo/stories/index.jsx
@@ -3,20 +3,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs/react';
 
-import demoContentHorizontal from '@ecl/ec-specs-logo/demo/data--horizontal';
-import demoContentMute from '@ecl/ec-specs-logo/demo/data--mute';
+import demoContent from '@ecl/ec-specs-logo/demo/data';
 
 import Logo from '../Logo';
 import './index.scss';
 
 storiesOf('Logo', module)
   .addDecorator(withKnobs)
-  .add('horizontal', () => (
-    <Logo
-      variant="horizontal"
-      title={text('Title', demoContentHorizontal.title)}
-    />
-  ))
-  .add('mute', () => (
-    <Logo variant="mute" title={text('Title', demoContentMute.title)} />
-  ));
+  .add('default', () => <Logo title={text('Title', demoContent.title)} />);
