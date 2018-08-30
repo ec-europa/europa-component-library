0fef74f919d46e3c35ff9bafe9cfa648586619ac
Romain Emery <emery.romain@gmail.com>
2018-08-30 08:39:08 +0200

    refactor(icon): add svg4everybody

--- src/website/src/App.jsx
+++ src/website/src/App.jsx
@@ -8,6 +8,9 @@ import 'normalize.css/normalize.css';
 import './styles/app.scss';
 import 'prismjs/themes/prism.css';
 
+// Global scripts
+import svg4everybody from 'svg4everybody/dist/svg4everybody.min';
+
 // Static routes
 import HomePage from './routes/index';
 import Example from './routes/example';
@@ -15,22 +18,30 @@ import PageNotFound from './routes/404';
 import ECRoutes from './routes/ec';
 import EURoutes from './routes/eu';
 
-const App = () => (
-  <Router basename={`${process.env.PUBLIC_URL}/`}>
-    <Fragment>
-      <Helmet
-        titleTemplate="%s - ECL 2.0"
-        defaultTitle="Europa Component Library"
-      />
-      <Switch>
-        <Route exact strict path="/" component={HomePage} />
-        <Route strict path="/example" component={Example} />
-        <Route path="/ec/" strict component={ECRoutes} />
-        <Route path="/eu/" strict component={EURoutes} />
-        <Route component={PageNotFound} />
-      </Switch>
-    </Fragment>
-  </Router>
-);
+class App extends React.Component {
+  componentDidMount() {
+    svg4everybody();
+  }
+
+  render() {
+    return (
+      <Router basename={`${process.env.PUBLIC_URL}/`}>
+        <Fragment>
+          <Helmet
+            titleTemplate="%s - ECL 2.0"
+            defaultTitle="Europa Component Library"
+          />
+          <Switch>
+            <Route exact strict path="/" component={HomePage} />
+            <Route strict path="/example" component={Example} />
+            <Route path="/ec/" strict component={ECRoutes} />
+            <Route path="/eu/" strict component={EURoutes} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </Fragment>
+      </Router>
+    );
+  }
+}
 
 export default hot(module)(App);
