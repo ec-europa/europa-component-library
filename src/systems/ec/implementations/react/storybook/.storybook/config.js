96382e90f888202bc06066d15fe313f02ff069ec
Yannick Huard <mail@yannickhuard.com>
2018-09-03 11:57:42 +0200

    try to import svg4everybody  in storybook

--- src/systems/ec/implementations/react/storybook/.storybook/config.js
+++ src/systems/ec/implementations/react/storybook/.storybook/config.js
@@ -1,6 +1,9 @@
 import { configure, addDecorator } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
 import { checkA11y } from '@storybook/addon-a11y';
+import svg4everybody from 'svg4everybody/dist/svg4everybody.min';
+
+svg4everybody();
 
 setOptions({
   name: 'ECL v2 - EC',
