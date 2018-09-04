17fd5029b8ea55c2ad02188cc43c76943110bf5c
Yannick Huard <mail@yannickhuard.com>
2018-09-04 11:25:27 +0200

    feat(design-tokens): only rely on design tokens for colors and spacing - INNO-1209

--- src/systems/eu/themes/default/scripts/build-tokens.js
+++ src/systems/eu/themes/default/scripts/build-tokens.js
@@ -1,65 +1,94 @@
-const fs = require('fs');
-const path = require('path');
-const mkdirp = require('mkdirp');
 const theo = require('theo');
+const path = require('path');
 
-const inputFile = path.join(__dirname, '../index.yml');
-const outputFile = 'index.scss';
-const outputJson = 'tokens.json';
+const buildTokens = require('./util');
 
-const writeDir = path.join(__dirname, '../exports');
-mkdirp.sync(writeDir);
+// Formats
+const scss = require('./formats/scss');
+const scssMap = require('./formats/scss-map');
 
-// Based on https://github.com/salesforce-ux/theo/blob/master/lib/formats/default.scss.hbs
+// Every SCSS variables
+theo.registerFormat('ecl.scss', scss);
+buildTokens({
+  input: path.join(__dirname, '../index.yml'),
+  output: path.join(__dirname, '../exports/all.scss'),
+  type: 'web',
+  format: 'ecl.scss',
+});
+
+// Spacings SCSS map
+const spacingPrefix = 'SPACING_';
 theo.registerFormat(
-  'eclScss',
-  `/* ECL Variables */
-
-{{#each props as |prop|}}
-  {{#if prop.comment~}}
-  // {{{trim prop.comment}}}
-  {{/if~}}
-    $ecl-{{kebabcase prop.name}}: {{#eq prop.type "string"}}"{{/eq}}{{{prop.value}}}{{#eq prop.type "string"}}"{{/eq}} !default;
-
-  {{/each}}
-`
+  'spacing.scss.map',
+  scssMap({
+    mapName: '$ecl-spacing',
+    keyName: prop =>
+      prop
+        .get('name')
+        .substr(spacingPrefix.length)
+        .toLowerCase()
+        .replace(/_/g, '-'),
+    filter: prop => prop.get('name').indexOf(spacingPrefix) === 0,
+  })
 );
 
-theo
-  .convert({
-    transform: {
-      type: 'web',
-      file: inputFile,
-    },
-    format: {
-      type: 'eclScss',
-    },
-  })
-  .then(result => {
-    fs.writeFile(
-      path.join(writeDir, outputFile),
-      result,
-      error => error && console.log('Error writing file: ', error)
-    );
-  })
-  .catch(error => error && console.log('Error converting tokens: ', error));
+buildTokens({
+  input: path.join(__dirname, '../aliases/spacing.yml'),
+  output: path.join(__dirname, '../exports/spacing.scss'),
+  type: 'web',
+  format: 'spacing.scss.map',
+});
 
-// Export raw JSON
-theo
-  .convert({
-    transform: {
-      type: 'raw',
-      file: inputFile,
-    },
-    format: {
-      type: 'raw.json',
-    },
+// Colors SCSS map
+const colorPrefix = 'COLOR_';
+theo.registerFormat(
+  'colors.scss.map',
+  scssMap({
+    mapName: '$ecl-colors',
+    keyName: prop =>
+      prop
+        .get('name')
+        .substr(colorPrefix.length)
+        .toLowerCase()
+        .replace(/_/g, '-'),
+    filter: prop => prop.get('name').indexOf(colorPrefix) === 0,
   })
-  .then(result => {
-    fs.writeFile(
-      path.join(writeDir, outputJson),
-      result,
-      error => error && console.log('Error writing file: ', error)
-    );
+);
+
+buildTokens({
+  input: path.join(__dirname, '../aliases/colors.yml'),
+  output: path.join(__dirname, '../exports/colors.scss'),
+  type: 'web',
+  format: 'colors.scss.map',
+});
+
+// Background colors SCSS map
+const bgColorPrefix = 'BG_COLOR_';
+theo.registerFormat(
+  'background-colors.scss.map',
+  scssMap({
+    mapName: '$ecl-colors-bg',
+    keyName: prop =>
+      prop
+        .get('name')
+        .substr(bgColorPrefix.length)
+        .toLowerCase()
+        .replace(/_/g, '-'),
+    filter: prop => prop.get('name').indexOf(bgColorPrefix) === 0,
   })
-  .catch(error => error && console.log('Error converting tokens: ', error));
+);
+
+buildTokens({
+  input: path.join(__dirname, '../aliases/colors-bg.yml'),
+  output: path.join(__dirname, '../exports/colors-bg.scss'),
+  type: 'web',
+  format: 'background-colors.scss.map',
+});
+
+// JSON Tokens
+buildTokens({
+  input: path.join(__dirname, '../index.yml'),
+  output: path.join(__dirname, '../exports/tokens.json'),
+  type: 'raw',
+  format: 'raw.json',
+});
