ed7dd8c1d21c143abee1e7e2c0d7b5a15d160f4e
Romain Emery <emery.romain@gmail.com>
2018-09-05 10:50:24 +0200

    add icon

--- src/systems/ec/specs/components/hero-banner/demo/data--primary.js
+++ src/systems/ec/specs/components/hero-banner/demo/data--primary.js
@@ -1,4 +1,6 @@
 // Simple content for demo
+const svgSprite = require('@ecl/ec-resources/dist/icons.svg');
+
 module.exports = {
   title: 'EU Budget for the future (primary)',
   description:
@@ -6,6 +8,11 @@ module.exports = {
   button: {
     variant: 'call',
     label: 'Subscribe',
+    icon: {
+      icon: 'Icon_Corner-arrow-right',
+      iconPath: svgSprite,
+      size: 'xs',
+    },
   },
   centered: true,
 };
