5e7c11fa75aa3578cf3f008e67bdcfa11853219b
Romain Emery <emery.romain@gmail.com>
2018-09-06 08:24:07 +0200

    disable hack

--- src/website/src/App.jsx
+++ src/website/src/App.jsx
@@ -38,11 +38,11 @@ const customComponents = {
 
 class App extends React.Component {
   componentDidMount() {
-    svg4everybody({
+    svg4everybody(/* {
       validate(src) {
         return src && src.indexOf('#') !== 0;
       },
-    });
+    } */);
   }
 
   render() {
