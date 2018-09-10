a30cf351fc93d3360db13dca5f8b8c748fac4492
Romain Emery <emery.romain@gmail.com>
2018-09-10 14:34:12 +0200

    add eu logo

---
+++ src/systems/eu/implementations/react/packages/logo/Logo.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const Logo = ({ title, alt, language, className, ...props }) => {
+  const classNames = classnames(className, {
+    'ecl-logo': true,
+  });
+
+  /* eslint-disable-next-line */
+  const path = require(`@ecl/eu-resources/logo/logo--${language}.svg`);
+
+  return (
+    <img {...props} title={title} className={classNames} src={path} alt={alt} />
+  );
+};
+
+Logo.propTypes = {
+  title: PropTypes.string,
+  alt: PropTypes.string,
+  language: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Logo.defaultProps = {
+  title: '',
+  alt: '',
+  language: 'en',
+  className: '',
+};
+
+export default Logo;
