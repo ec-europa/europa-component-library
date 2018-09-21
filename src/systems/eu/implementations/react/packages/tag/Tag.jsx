60914c71098a8aee3ef71a49e426d17f597291ef
Yannick Huard <mail@yannickhuard.com>
2018-09-21 10:55:12 +0200

    finalize tag component (ec + eu)

--- src/systems/eu/implementations/react/packages/tag/Tag.jsx
+++ src/systems/eu/implementations/react/packages/tag/Tag.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import Icon from '@ecl/ec-react-component-icon';
+import Icon from '@ecl/eu-react-component-icon';
 
 const Tag = ({ label, variant, href, className, ...props }) => {
   if (!label) return null;
