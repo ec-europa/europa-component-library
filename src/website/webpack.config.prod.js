5ddbfbad4442a09036ffd10d55485bc5bf9cd261
Yannick Huard <mail@yannickhuard.com>
2018-09-24 08:49:54 +0200

    test

--- src/website/webpack.config.prod.js
+++ src/website/webpack.config.prod.js
@@ -51,7 +51,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     // `web` extension prefixes have been added for better support
     // for React Native Web.
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
+    extensions: ['.mjs', '.js', '.json', '.jsx'],
   },
   module: {
     // strictExportPresence makes missing exports an error instead of warning
