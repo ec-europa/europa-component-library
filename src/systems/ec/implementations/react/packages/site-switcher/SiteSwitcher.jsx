3d65f1acbfcdfd6a93ecbb9f3f520fed895dfdf0
wesley <wesley.deglise@gmail.com>
2018-09-25 09:07:22 +0200

    fix(site-switcher): make better use of design props

--- src/systems/ec/implementations/react/packages/site-switcher/SiteSwitcher.jsx
+++ src/systems/ec/implementations/react/packages/site-switcher/SiteSwitcher.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
@@ -58,7 +57,7 @@ SiteSwitcher.defaultProps = {
     href: '',
     isSelected: false,
   },
-  variant: 'header',
+  variant: '',
   className: '',
 };
 
