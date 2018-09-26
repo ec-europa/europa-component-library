f605c379f47d42777781afdd441fb44394913f90
Romain Emery <emery.romain@gmail.com>
2018-09-26 08:02:52 +0200

    update component

--- src/systems/eu/implementations/react/packages/card/Card.jsx
+++ src/systems/eu/implementations/react/packages/card/Card.jsx
@@ -2,16 +2,66 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const Card = ({ image, className, ...props }) => {
+import Link from '@ecl/eu-react-component-link/Link';
+
+const Card = ({
+  image,
+  meta,
+  title,
+  description,
+  links,
+  className,
+  ...props
+}) => {
   const classNames = classnames(className, 'ecl-card', {});
+
+  // Image
+  let imageMarkup = '';
+  if (image && image.src) {
+    imageMarkup = (
+      <img className="ecl-card__image" alt={image.alt} src={image.src} />
+    );
+  }
+
+  // Meta
+  let metaMarkup = '';
+  if (meta) {
+    metaMarkup = <div className="ecl-card__meta">{meta}</div>;
+  }
+
+  // Title
+  let titleMarkup = '';
+  if (title) {
+    if (title.href) {
+      titleMarkup = <Link {...title} />;
+    } else {
+      titleMarkup = <div className="ecl-card__title">{title.label}</div>;
+    }
+  }
+
+  // Description
+  let descriptionMarkup = '';
+  if (description) {
+    descriptionMarkup = (
+      <div className="ecl-card__description">{description}</div>
+    );
+  }
+
+  // Links
+  let linksMarkup = '';
+  if (links) {
+    linksMarkup = links.map(link => <Link {...link} />);
+  }
 
   return (
     <div {...props} className={classNames}>
-      <img className="ecl-card__image" alt={image.alt} src={image.src} />
-      <div className="ecl-card__meta">meta</div>
-      <div className="ecl-card__title">title</div>
-      <div className="ecl-card__description">description</div>
-      <div className="ecl-card__label">labels</div>
+      {imageMarkup}
+      {metaMarkup}
+      {titleMarkup}
+      {descriptionMarkup}
+      {linksMarkup}
+      <div className="ecl-card__actions">action</div>
+      <div className="ecl-card__labels">labels</div>
     </div>
   );
 };
@@ -21,11 +71,27 @@ Card.propTypes = {
     alt: PropTypes.string,
     src: PropTypes.string,
   }),
+  meta: PropTypes.string,
+  title: PropTypes.shape({
+    label: PropTypes.string,
+    href: PropTypes.string,
+  }),
+  description: PropTypes.string,
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      href: PropTypes.string,
+    })
+  ),
   className: PropTypes.string,
 };
 
 Card.defaultProps = {
   image: {},
+  meta: '',
+  title: {},
+  description: '',
+  links: [],
   className: '',
 };
 
--- src/systems/eu/specs/components/card/demo/data.js
+++ src/systems/eu/specs/components/card/demo/data.js
@@ -5,4 +5,16 @@ module.exports = {
     src:
       'https://ec.europa.eu/education/sites/education/files/jean-monnet-gs-banner.jpg',
   },
+  meta: 'META1 | META2',
+  title: {
+    label: 'Better regulation',
+    href: '/example',
+  },
+  description:
+    "Transparently designing and evaluating evidence-based EU legislation, backed by citizens' views.",
+  links: [
+    { label: 'link 1', href: '/example' },
+    { label: 'link 2', href: '/example' },
+    { label: 'link 3', href: '/example' },
+  ],
 };
