c4f5c95091d204d2d175a4bd8c62ecc41770e385
Yannick Huard <mail@yannickhuard.com>
2018-10-02 12:34:25 +0200

    update form components

--- src/systems/generic/implementations/vanilla/generic-component-form-help-block/data/demo--default.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  content: 'This is some placeholder help text.',
-};
--- src/systems/generic/implementations/vanilla/generic-component-form-text-input/data/demo--with-error.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  id: 'example-input-id-3',
-  has_error: true,
-  extra_attributes: [{ name: 'placeholder', value: 'Some placeholder text.' }],
-};
