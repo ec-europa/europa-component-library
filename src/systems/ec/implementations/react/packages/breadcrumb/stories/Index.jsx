a7399bd6c86194f696d9975f9c67b05efe19954c
Yannick Huard <mail@yannickhuard.com>
2018-10-04 07:55:13 +0200

    fix(storybook): fix storybook on IE11 - INNO-1243 (#833)
    * feat(storyshots): integrate storyshots - INNO-1226
    
    * generate screenshots
    
    * fix snapshots
    
    * test build on drone
    
    * update storybook
    
    * update deps
    
    * update deps
    
    * try IE fix
    
    * fix(storybook): fix storybook on IE11 - no issue
    
    * fix deprecation warnings

--- src/systems/ec/implementations/react/packages/breadcrumb/stories/Index.jsx
+++ src/systems/ec/implementations/react/packages/breadcrumb/stories/Index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import simpleContent from '@ecl/ec-specs-breadcrumb/demo/data-simple';
 import demoContent from '@ecl/ec-specs-breadcrumb/demo/data';
