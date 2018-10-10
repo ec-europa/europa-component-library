bedbf8b9e9e986f9ab17d7237c39f8d51552ceab
Yannick Huard <mail@yannickhuard.com>
2018-10-10 10:47:53 +0200

    remove style tab

--- src/systems/ec/specs/components/media-container/config.js
+++ src/systems/ec/specs/components/media-container/config.js
@@ -1,4 +1,3 @@
-import style from './docs/style.md';
 import code from './docs/code.mdx';
 import usage from './docs/usage.md';
 
@@ -17,11 +16,6 @@ export default {
       name: 'Showcase',
       component: code,
       url: 'showcase',
-    },
-    {
-      name: 'Style',
-      component: style,
-      url: 'style',
     },
   ],
   defaultTab: 'usage',
