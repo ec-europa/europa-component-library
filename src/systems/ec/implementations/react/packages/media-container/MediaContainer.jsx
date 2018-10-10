bd3c4270c72a0d1468e3e5ccb1910339f2daab47
wesley deglise <wesley.deglise@gmail.com>
2018-10-10 11:41:54 +0200

    feat(media-container): bootstrap component - INNO-1153 (#823)
    # Specs
    https://webgate.ec.europa.eu/CITnet/confluence/display/NEXTEUROPA/Media+container+Design+Specifications

---
+++ src/systems/ec/implementations/react/packages/media-container/MediaContainer.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const MediaContainer = ({
+  image,
+  alt,
+  sources,
+  tracks,
+  description,
+  className,
+  ...props
+}) => {
+  const classNames = classnames(className, 'ecl-media-container');
+
+  return (
+    <figure {...props} className={classNames}>
+      {Array.isArray(sources) && sources.length !== 0 ? (
+        /* eslint-disable-next-line jsx-a11y/media-has-caption */
+        <video
+          className="ecl-media-container__media"
+          controls="controls"
+          poster={image}
+          alt={alt}
+        >
+          {sources.map(source => (
+            <source {...source} key={source.src} />
+          ))}
+
+          {tracks.map(track => (
+            <track {...track} src={track.src} />
+          ))}
+        </video>
+      ) : (
+        <img className="ecl-media-container__media" src={image} alt={alt} />
+      )}
+      {description && (
+        <figcaption className="ecl-media-container__caption">
+          {description}
+        </figcaption>
+      )}
+    </figure>
+  );
+};
+
+MediaContainer.propTypes = {
+  alt: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  sources: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+      type: PropTypes.string,
+    })
+  ),
+  tracks: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+      kind: PropTypes.string,
+      srcLang: PropTypes.string,
+      label: PropTypes.string,
+    })
+  ),
+  className: PropTypes.string,
+};
+
+MediaContainer.defaultProps = {
+  alt: '',
+  description: '',
+  image: '',
+  className: '',
+  sources: [],
+  tracks: [],
+};
+
+export default MediaContainer;
