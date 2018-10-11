f92e0c118ce234dfd2212a60b8de6ad5a30d5cdb
Yannick Huard <mail@yannickhuard.com>
2018-10-11 15:58:28 +0200

    chore(alpha0): prepare alpha 0 release (bis) - INNO-1249 (#875)
    Packages in 2.0.0-alpha.1 have been published to npm

--- src/systems/generic/implementations/vanilla/generic-component-file/data/demo--images.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  variant: 'image',
-  src: 'https://placehold.it/450x250',
-  alt: 'placeholder image',
-  caption:
-    'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
-};
--- src/systems/generic/implementations/vanilla/generic-component-message/data/demo--success.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  modifier: 'success',
-  dismiss: true,
-  screen_reader_description: 'Success message',
-  title: 'Some success title',
-  messages: ['Lorem ipsum lor sit amet, consectetur adipi'],
-};
