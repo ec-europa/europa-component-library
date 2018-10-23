a2d68fccef9d2e7a4b26b827727244c56fbc5748
Romain Emery <emery.romain@gmail.com>
2018-10-23 13:48:36 +0200

    update component

--- src/systems/ec/implementations/react/packages/pager/Pager.jsx
+++ src/systems/ec/implementations/react/packages/pager/Pager.jsx
@@ -4,23 +4,40 @@ import classnames from 'classnames';
 
 import Link from '@ecl/ec-react-component-link/Link';
 
-const Pager = ({ links, className, ...props }) => {
+const Pager = ({ label, items, className, ...props }) => {
   const classNames = classnames(className, 'ecl-pager');
 
   return (
-    <nav {...props} className={classNames}>
-      Pager
+    <nav {...props} className={classNames} aria-label={label}>
+      <ul className="ecl-pager__list">
+        {items.map(item => (
+          <li key={item.label} className="ecl-pager__item">
+            {item.link && (
+              <Link
+                {...item.link}
+                className={classnames(item.link.className, 'ecl-pager__link')}
+                aria-label={item.link.label}
+              />
+            )}
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
 
 Pager.propTypes = {
-  links: PropTypes.arrayOf(PropTypes.shape(Link.propTypes)),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      link: PropTypes.shape(Link.propTypes),
+    })
+  ),
   className: PropTypes.string,
 };
 
 Pager.defaultProps = {
-  links: [],
+  items: [],
   className: '',
 };
 
--- src/systems/ec/specs/components/pager/demo/data.js
+++ src/systems/ec/specs/components/pager/demo/data.js
@@ -1,49 +1,75 @@
 // Simple content for demo
 module.exports = {
-  links: [
+  label: 'Pagination',
+  items: [
     {
-      href: '/example',
-      label: 'Previous',
+      label: 'Go to previous page',
+      link: {
+        href: '/example',
+        label: 'Previous',
+      },
     },
     {
-      href: '/example',
-      label: '1',
+      label: 'Go to page 1',
+      link: {
+        href: '/example',
+        label: '1',
+      },
     },
     {
-      href: '/example',
-      label: '...',
+      label: '…',
     },
     {
-      href: '/example',
-      label: '24',
+      label: 'Go to page 24',
+      link: {
+        href: '/example',
+        label: '24',
+      },
     },
     {
-      href: '/example',
-      label: '25',
+      label: 'Go to page 25',
+      link: {
+        href: '/example',
+        label: '25',
+      },
     },
     {
-      href: '/example',
-      label: '26',
+      label: 'Go to page 26',
+      link: {
+        href: '/example',
+        label: '26',
+      },
     },
     {
-      href: '/example',
-      label: '27',
+      label: 'Go to page 27',
+      link: {
+        href: '/example',
+        label: '27',
+      },
     },
     {
-      href: '/example',
-      label: '28',
+      label: 'Go to page 28',
+      link: {
+        href: '/example',
+        label: '28',
+      },
     },
     {
-      href: '/example',
-      label: '...',
+      label: '…',
     },
     {
-      href: '/example',
-      label: '110',
+      label: 'Go to page 110',
+      link: {
+        href: '/example',
+        label: '110',
+      },
     },
     {
-      href: '/example',
-      label: 'Next',
+      label: 'Go to next page',
+      link: {
+        href: '/example',
+        label: 'Next',
+      },
     },
   ],
 };
