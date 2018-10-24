8c773f2c0aeb3158ba7846cfbd0aa8de6789f9ec
Yannick Huard <mail@yannickhuard.com>
2018-10-24 15:13:32 +0200

    chore(react-dependencies): harmonize react packages' dependencies - INNO-1268

--- src/systems/ec/implementations/react/packages/campaign-page/examples/Default.jsx
+++ src/systems/ec/implementations/react/packages/campaign-page/examples/Default.jsx
@@ -1,22 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint "import/no-extraneous-dependencies": ["error", { "devDependencies": true } ] */
 import React from 'react';
-
-import siteHeaderContent from '@ecl/ec-specs-site-header/demo/data';
-import heroBannerContent from '@ecl/ec-specs-hero-banner/demo/data--image';
-import mediaContainerContent from '@ecl/ec-specs-media-container/demo/data--video';
-import cardContent from '@ecl/ec-specs-card/demo/data--card-event';
-import blockquoteContent from '@ecl/ec-specs-blockquote/demo/data';
-import footerContent from '@ecl/ec-specs-footer/demo/data';
-
+import data from '@ecl/ec-specs-campaign-page/demo/data';
 import CampaignPage from '../CampaignPage';
 
-export default () => (
-  <CampaignPage
-    siteHeader={siteHeaderContent}
-    heroBanner={heroBannerContent}
-    card={cardContent}
-    footer={footerContent}
-    mediaContainer={mediaContainerContent}
-    blockquote={blockquoteContent}
-  />
-);
+export default () => <CampaignPage {...data} />;
