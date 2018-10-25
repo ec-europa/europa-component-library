196b6e95d8d4a91a47e05da194af9fcc99d5f5c6
Romain Emery <emery.romain@gmail.com>
2018-10-25 15:08:15 +0200

    add headings

--- src/systems/ec/implementations/react/packages/typography/stories/Index.jsx
+++ src/systems/ec/implementations/react/packages/typography/stories/Index.jsx
@@ -1,21 +1,41 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select, text } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
+import demoContentHeading from '@ecl/ec-specs-typography/demo/data--heading';
 import demoContentParagraph from '@ecl/ec-specs-typography/demo/data--paragraph';
-
-const paragraphClasses = {
-  Lead: 'ecl-u-type-paragraph-lead',
-  Medium: 'ecl-u-type-paragraph-m',
-  Small: 'ecl-u-type-paragraph-s',
-  'Extra small': 'ecl-u-type-paragraph-xs',
-};
 
 storiesOf('Typography', module)
   .addDecorator(withKnobs)
-  .add('paragraph', () => (
-    <p className={select('Size', paragraphClasses, 'ecl-u-type-paragraph-m')}>
-      {text('Content', demoContentParagraph.content)}
-    </p>
-  ));
+  .add('paragraph', () => {
+    const content = text('Content', demoContentParagraph.content);
+
+    return (
+      <Fragment>
+        <h2 className="ecl-u-type-heading-2">Lead paragraph</h2>
+        <p className="ecl-u-type-paragraph-lead">{content}</p>
+
+        <h2 className="ecl-u-type-heading-2">Medium paragraph</h2>
+        <p className="ecl-u-type-paragraph-m">{content}</p>
+
+        <h2 className="ecl-u-type-heading-2">Small paragraph</h2>
+        <p className="ecl-u-type-paragraph-s">{content}</p>
+
+        <h2 className="ecl-u-type-heading-2">Extra small paragraph</h2>
+        <p className="ecl-u-type-paragraph-xs">{content}</p>
+      </Fragment>
+    );
+  })
+  .add('heading', () => {
+    const content = text('Content', demoContentHeading.content);
+
+    return (
+      <Fragment>
+        <h1 className="ecl-u-type-heading-1">H1. {content}</h1>
+        <h2 className="ecl-u-type-heading-2">H2. {content}</h2>
+        <h3 className="ecl-u-type-heading-3">H3. {content}</h3>
+        <h4 className="ecl-u-type-heading-4">H4. {content}</h4>
+      </Fragment>
+    );
+  });
