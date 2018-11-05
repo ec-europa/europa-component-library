b870f9782d39fbbb2200a6c7f451362bb1c6e905
planctus <davidepuntoferrante@gmail.com>
2018-11-05 11:38:54 +0100

    feat(social-media): Add modifier for the social media share version (webotools)

--- src/systems/ec/implementations/react/packages/social-media-follow/stories/Index.jsx
+++ src/systems/ec/implementations/react/packages/social-media-follow/stories/Index.jsx
@@ -7,18 +7,27 @@ import demoContent from '@ecl/ec-specs-social-media-follow/demo/data';
 
 import SocialMediaFollow from '../SocialMediaFollow';
 
+import '@ecl/ec-specs-social-media-follow/demo/demo.css';
+
 storiesOf('SocialMediaFollow', module)
   .addDecorator(withKnobs)
   .add('horizontal', () => (
     <SocialMediaFollow
-      {...demoContent}
-      description={text('Description', demoContent.description)}
+      {...demoContent.default_data}
+      description={text('Description', demoContent.default_data.description)}
     />
   ))
   .add('vertical', () => (
     <SocialMediaFollow
-      {...demoContent}
+      {...demoContent.default_data}
       variant="vertical"
-      description={text('Description', demoContent.description)}
+      description={text('Description', demoContent.default_data.description)}
+    />
+  ))
+  .add('webtools', () => (
+    <SocialMediaFollow
+      {...demoContent.webtools}
+      variant="webtools"
+      description={text('Description', demoContent.webtools.description)}
     />
   ));
--- src/systems/ec/specs/components/social-media-follow/demo/data.js
+++ src/systems/ec/specs/components/social-media-follow/demo/data.js
@@ -1,76 +1,173 @@
 // Simple content for demo
 module.exports = {
-  description:
-    'Follow the latest progress and learn more about getting involved.',
-  links: [
-    {
-      href: '/example',
-      label: 'Twitter',
-      variant: 'standalone',
-      iconPosition: 'before',
-      icon: [
-        {
-          shape: 'twitter',
-          size: 'xl',
-        },
-        {
-          shape: 'twitter_hover',
-          size: 'xl',
-        },
-      ],
-    },
-    {
-      href: '/example',
-      label: 'Facebook',
-      variant: 'standalone',
-      iconPosition: 'before',
-      icon: [
-        {
-          shape: 'facebook',
-          size: 'xl',
-        },
-        {
-          shape: 'facebook_hover',
-          size: 'xl',
-        },
-      ],
-    },
-    {
-      href: '/example',
-      label: 'Google+',
-      variant: 'standalone',
-      iconPosition: 'before',
-      icon: [
-        {
-          shape: 'gplus',
-          size: 'xl',
-        },
-        {
-          shape: 'gplus_hover',
-          size: 'xl',
-        },
-      ],
-    },
-    {
-      href: '/example',
-      label: 'Linkedin',
-      variant: 'standalone',
-      iconPosition: 'before',
-      icon: [
-        {
-          shape: 'linkedin',
-          size: 'xl',
-        },
-        {
-          shape: 'linkedin_hover',
-          size: 'xl',
-        },
-      ],
-    },
-    {
-      href: '/example',
-      label: 'Other social networks',
-      variant: 'standalone',
-    },
-  ],
+  default_data: {
+    description:
+      'Follow the latest progress and learn more about getting involved.',
+    links: [
+      {
+        href: '/example',
+        label: 'Twitter',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'twitter',
+            size: 'xl',
+          },
+          {
+            shape: 'twitter_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Facebook',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'facebook',
+            size: 'xl',
+          },
+          {
+            shape: 'facebook_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Google+',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'gplus',
+            size: 'xl',
+          },
+          {
+            shape: 'gplus_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Linkedin',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'linkedin',
+            size: 'xl',
+          },
+          {
+            shape: 'linkedin_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Other social networks',
+        variant: 'standalone',
+      },
+    ],
+  },
+  webtools: {
+    description: '',
+    links: [
+      {
+        href: '/example',
+        label: 'Twitter',
+        className: 'ecl-social-media-follow__link--twitter',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'twitter',
+            size: 'xl',
+          },
+          {
+            shape: 'twitter_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Facebook',
+        className: 'ecl-social-media-follow__link--facebook',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'facebook',
+            size: 'xl',
+          },
+          {
+            shape: 'facebook_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Linkedin',
+        className: 'ecl-social-media-follow__link--linkedin',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'linkedin',
+            size: 'xl',
+          },
+          {
+            shape: 'linkedin_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Google+',
+        className: 'ecl-social-media-follow__link--google',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'gplus',
+            size: 'xl',
+          },
+          {
+            shape: 'gplus_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'E-mail',
+        className: 'ecl-social-media-follow__link--email',
+        variant: 'standalone',
+        iconPosition: 'before',
+        icon: [
+          {
+            shape: 'email',
+            size: 'xl',
+          },
+          {
+            shape: 'email_hover',
+            size: 'xl',
+          },
+        ],
+      },
+      {
+        href: '/example',
+        label: 'Other social networks',
+        variant: 'standalone',
+      },
+    ],
+  },
 };
