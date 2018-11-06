c34ae95b6a673e27af3be29bff647ca823cd10b0
emeryro <emery.romain@gmail.com>
2018-11-06 11:05:21 +0100

    fix path

--- src/systems/ec/implementations/react/packages/pagination/stories/Index.jsx
+++ src/systems/ec/implementations/react/packages/pagination/stories/Index.jsx
@@ -7,6 +7,6 @@ import demoContent from '@ecl/ec-specs-pagination/demo/data';
 
 import Pagination from '../Pagination';
 
-storiesOf('Pagination', module)
+storiesOf('Navigation/Pagination', module)
   .addDecorator(withKnobs)
   .add('default', () => <Pagination {...demoContent} />);
