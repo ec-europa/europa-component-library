86ea6953fe94fa583cbe7276ecacdf82d6c0c0a7
emeryro <emery.romain@gmail.com>
2018-11-27 09:56:46 +0100

    add js

--- src/systems/eu/implementations/react/packages/message/Message.jsx
+++ src/systems/eu/implementations/react/packages/message/Message.jsx
@@ -2,39 +2,63 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import VanillaMessage from '@ecl/eu-component-message/eu-component-message';
+
 import Button from '@ecl/eu-react-component-button/Button';
 import Icon from '@ecl/eu-react-component-icon/Icon';
 
-const Message = ({
-  variant,
-  icon,
-  title,
-  description,
-  close,
-  className,
-  ...props
-}) => {
-  const classNames = classnames(className, 'ecl-message', {
-    [`ecl-message--${variant}`]: variant,
-  });
+export default class Message extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div {...props} role="alert" className={classNames}>
-      <Icon
-        {...icon}
-        className={classnames(icon.className, 'ecl-message__icon')}
-      />
-      <div className="ecl-message__content">
-        <Button
-          {...close}
-          className={classnames(close.className, 'ecl-message__close')}
+    this.message = null;
+    this.messageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.message = new VanillaMessage(this.messageRef.current);
+    this.message.init();
+  }
+
+  componentWillUnmount() {
+    if (this.message) this.message.destroy();
+  }
+
+  render() {
+    const {
+      variant,
+      icon,
+      title,
+      description,
+      close,
+      className,
+      ...props
+    } = this.props;
+
+    const classNames = classnames(className, 'ecl-message', {
+      [`ecl-message--${variant}`]: variant,
+    });
+
+    return (
+      <div {...props} role="alert" className={classNames} ref={this.messageRef}>
+        <Icon
+          {...icon}
+          className={classnames(icon.className, 'ecl-message__icon')}
         />
-        <div className="ecl-message__title">{title}</div>
-        <p className="ecl-message__description">{description}</p>
+        <div className="ecl-message__content">
+          <Button
+            {...close}
+            type="button"
+            className={classnames(close.className, 'ecl-message__close')}
+            data-ecl-message-close
+          />
+          <div className="ecl-message__title">{title}</div>
+          <p className="ecl-message__description">{description}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Message.propTypes = {
   variant: PropTypes.string,
@@ -53,5 +77,3 @@ Message.defaultProps = {
   close: '',
   className: '',
 };
-
-export default Message;
