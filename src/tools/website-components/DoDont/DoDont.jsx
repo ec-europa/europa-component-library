58c3d2a2e1abff6839602d691aed8efb5db82d4d
Romain Emery <emery.romain@gmail.com>
2018-11-28 15:30:01 +0100

    update website

--- src/tools/website-components/DoDont/DoDont.jsx
+++ src/tools/website-components/DoDont/DoDont.jsx
@@ -5,63 +5,68 @@ import PropTypes from 'prop-types';
 import iconSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import styles from './DoDont.scss';
 
-const DoDont = ({
-  doTitle,
-  doImage,
-  doDescription,
-  dontTitle,
-  dontImage,
-  dontDescription,
-}) => (
+const DoDont = ({ itemDo, itemDont }) => (
   <div className={styles.dodont}>
     <div className={styles.do}>
       <div className={styles.image}>
-        <img src={doImage} alt={doTitle} />
+        <img src={itemDo.image} alt={itemDo.alt} />
       </div>
       <div className={styles.container}>
         <div className={styles.title}>
           <svg className={styles.title__icon}>
             <use xlinkHref={`${iconSprite}#notifications--success`} />
           </svg>
-          {doTitle}
+          {itemDo.title}
         </div>
-        <div className={styles.description}>{doDescription}</div>
+        <div className={styles.description}>{itemDo.description}</div>
       </div>
     </div>
 
     <div className={styles.dont}>
       <div className={styles.image}>
-        <img src={dontImage} alt={dontTitle} />
+        <img src={itemDont.image} alt={itemDont.alt} />
       </div>
       <div className={styles.container}>
         <div className={styles.title}>
           <svg className={styles.title__icon}>
             <use xlinkHref={`${iconSprite}#notifications--error`} />
           </svg>
-          {dontTitle}
+          {itemDont.title}
         </div>
-        <div className={styles.description}>{dontDescription}</div>
+        <div className={styles.description}>{itemDont.description}</div>
       </div>
     </div>
   </div>
 );
 
 DoDont.propTypes = {
-  doTitle: PropTypes.string,
-  doImage: PropTypes.string,
-  doDescription: PropTypes.string,
-  dontTitle: PropTypes.string,
-  dontImage: PropTypes.string,
-  dontDescription: PropTypes.string,
+  itemDo: PropTypes.shape({
+    title: PropTypes.string,
+    image: PropTypes.string,
+    alt: PropTypes.string,
+    description: PropTypes.string,
+  }),
+  itemDont: PropTypes.shape({
+    title: PropTypes.string,
+    image: PropTypes.string,
+    alt: PropTypes.string,
+    description: PropTypes.string,
+  }),
 };
 
 DoDont.defaultProps = {
-  doTitle: 'Do',
-  doImage: '',
-  doDescription: '',
-  dontTitle: "Don't",
-  dontImage: '',
-  dontDescription: '',
+  itemDo: {
+    title: 'Do',
+    image: '',
+    alt: '',
+    description: '',
+  },
+  itemDont: {
+    title: "Don't",
+    image: '',
+    alt: '',
+    description: '',
+  },
 };
 
 export default DoDont;
