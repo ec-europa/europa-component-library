3d796b1803937dd843090436ac31b6280516fbf3
wesley deglise <wesley.deglise@gmail.com>
2018-11-30 12:01:59 +0100

    fix(basic-page): adapt template content (#966)
    fix(basic-page): adapt template content - INNO-1296

--- src/systems/ec/ec-template/ec-template-page/ec-template-page.config.js
+++ src/systems/ec/ec-template/ec-template-page/ec-template-page.config.js
@@ -30,11 +30,9 @@ module.exports = {
             },
             { label: 'Business, Economy, Euro' },
           ],
-          identity: 'Digital single market',
           title: 'Business, Economy, Euro',
           introduction:
             'EU economy, finance and the euro, and practical information for EU businesses and entrepreneurs on product safety, environmental regulations, trade with non-EU countries and competition rules.',
-          metas: ['News article', '6 July 2015', 'Brussels'],
         },
         _demo: {
           scripts: `document.addEventListener('DOMContentLoaded', function () {
