f2f02f4444ff922318b8f4f10469a1298ed0c509
planctus <davidepuntoferrante@gmail.com>
2018-12-11 16:04:32 +0100

    feat(social-media-share): Adding the new component to EU system as well

---
+++ src/systems/eu/specs/components/social-media-share/demo/data.js
@@ -0,0 +1,96 @@
+// Simple content for demo
+module.exports = {
+  description: 'Share this page',
+  links: [
+    {
+      href: '/example',
+      label: 'Twitter',
+      className: 'ecl-social-media-share__link--twitter',
+      variant: 'standalone',
+      iconPosition: 'before',
+      icon: [
+        {
+          shape: 'twitter',
+          size: 'xl',
+        },
+        {
+          shape: 'twitter_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      href: '/example',
+      label: 'Facebook',
+      className: 'ecl-social-media-share__link--facebook',
+      variant: 'standalone',
+      iconPosition: 'before',
+      icon: [
+        {
+          shape: 'facebook',
+          size: 'xl',
+        },
+        {
+          shape: 'facebook_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      href: '/example',
+      label: 'Linkedin',
+      className: 'ecl-social-media-share__link--linkedin',
+      variant: 'standalone',
+      iconPosition: 'before',
+      icon: [
+        {
+          shape: 'linkedin',
+          size: 'xl',
+        },
+        {
+          shape: 'linkedin_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      href: '/example',
+      label: 'Google+',
+      className: 'ecl-social-media-share__link--google',
+      variant: 'standalone',
+      iconPosition: 'before',
+      icon: [
+        {
+          shape: 'gplus',
+          size: 'xl',
+        },
+        {
+          shape: 'gplus_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      href: '/example',
+      label: 'E-mail',
+      className: 'ecl-social-media-share__link--email',
+      variant: 'standalone',
+      iconPosition: 'before',
+      icon: [
+        {
+          shape: 'email',
+          size: 'xl',
+        },
+        {
+          shape: 'email_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      href: '/example',
+      label: 'Other social networks',
+      variant: 'standalone',
+    },
+  ],
+};
