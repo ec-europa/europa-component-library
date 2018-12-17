c1d8d91c9c7b2cb55d9d010db7dc1bb9e6ef1f4f
emeryro <emery.romain@gmail.com>
2018-12-17 14:32:44 +0100

    add select

--- src/systems/eu/implementations/react/packages/select/stories/Index.jsx
+++ src/systems/eu/implementations/react/packages/select/stories/Index.jsx
@@ -1,26 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean, number, text } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import { configureA11y } from '@storybook/addon-a11y';
 
-import demoContentDefault from '@ecl/ec-specs-text-area/demo/data--default';
+import demoContent from '@ecl/eu-specs-select/demo/data';
 
-import TextArea from '../TextArea';
+import Select from '../Select';
 
 // Disable label check
 configureA11y({
   rules: [{ id: 'label', enabled: false }],
 });
 
-storiesOf('Forms/TextArea', module)
+storiesOf('Forms/Select', module)
   .addDecorator(withKnobs)
   .add('default', () => (
-    <TextArea
-      id="example"
-      label={text('Label', demoContentDefault.label)}
-      placeholder={text('Placeholder', demoContentDefault.placeholder)}
-      rows={number('Rows', demoContentDefault.rows)}
+    <Select
+      id="select-id"
+      options={demoContent.options}
+      label={text('Label', demoContent.label)}
       invalid={boolean('Invalid', false)}
       invalidText={text('invalidText', 'Error')}
       helperText={text('helperText', 'Help message')}
