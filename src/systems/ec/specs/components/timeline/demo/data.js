7a80717df10527f73ba0df2710a94e143fa9fe56
Romain Emery <emery.romain@gmail.com>
2018-12-20 08:39:55 +0100

    feat(timeline): apply new specs - INNO-1215 (#948)
    * init component
    
    * fix prettier
    
    * fix linter
    
    * fix version
    
    * fix js
    
    * small fixes
    
    * fix button type

---
+++ src/systems/ec/specs/components/timeline/demo/data.js
@@ -0,0 +1,44 @@
+// Simple content for demo
+module.exports = {
+  button: {
+    label: 'Show all timeline',
+    icon: {
+      shape: 'ui--corner-arrow',
+      size: 'xs',
+      transform: 'rotate-180',
+    },
+    variant: 'call',
+    type: 'button',
+  },
+  items: [
+    {
+      label: '2018',
+      content:
+        'The EU in brief, institutions and bodies, countries, symbols, history, facts and figures',
+    },
+    {
+      label: '2017',
+      content: 'Information on agriculture, business, culture, health, etc.',
+    },
+    {
+      label: '2016',
+      content:
+        'Information on your rights to live, work, travel and study in another EU country, including access to healthcare and consumer rights',
+    },
+    {
+      label: '2015',
+      content:
+        'Information on taxes, customs, importing and exporting goods, financial support for businesses',
+    },
+    {
+      label: '2014',
+      content:
+        'Find EU treaties, legislation, case-law, how EU law is made and applied',
+    },
+    {
+      label: '2013',
+      content:
+        'Find official documents, publications, statistics, open data and more resources',
+    },
+  ],
+};
