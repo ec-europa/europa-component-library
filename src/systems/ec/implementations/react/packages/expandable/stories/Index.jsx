8dfc2ddcfb07ad3e72d5c2ba2709c20bf0043e31
Romain Emery <emery.romain@gmail.com>
2019-01-18 09:58:38 +0100

    feat(expandable): add expandable - INNO-1288 (#973)
    * add expandable
    
    * fix playground
    
    * make expandable a component
    
    * fix website
    
    * remove links
    
    * cleanup
    
    * small fixes
    
    * remove id
    
    * make package private

---
+++ src/systems/ec/implementations/react/packages/expandable/stories/Index.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import StoryWrapper from '@ecl/story-wrapper';
+
+import demoContent from '@ecl/ec-specs-expandable/demo/data';
+
+import VanillaExpandable from '@ecl/ec-component-expandable';
+
+import Expandable from '@ecl/ec-react-component-expandable/Expandable';
+
+storiesOf('Expandable', module)
+  .addDecorator(withKnobs)
+  .addDecorator(story => (
+    <StoryWrapper
+      afterMount={() => {
+        const element = document.querySelector('[data-ecl-expandable]');
+        const vanillaExpandable = new VanillaExpandable(element);
+        vanillaExpandable.init();
+
+        // Return new context
+        return { vanillaExpandable };
+      }}
+      beforeUnmount={context => {
+        if (context.vanillaExpandable) {
+          context.vanillaExpandable.destroy();
+        }
+      }}
+    >
+      {story()}
+    </StoryWrapper>
+  ))
+  .add('default', () => (
+    <Expandable
+      button={demoContent.button}
+      labelExpanded={demoContent.labelExpanded}
+      labelCollapsed={demoContent.labelCollapsed}
+      id={demoContent.id}
+    >
+      <p className="ecl-u-type-paragraph-m">{demoContent.content}</p>
+    </Expandable>
+  ));
