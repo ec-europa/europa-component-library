ecdc5a5092205473046963b997d6c9a2ce03818f
Yannick Huard <mail@yannickhuard.com>
2019-01-25 13:01:04 +0100

    use React lazy and Suspense instead of react-loadable

--- src/website/src/Index.jsx
+++ src/website/src/Index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
-import Loadable from 'react-loadable';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
-  Loadable.preloadReady().then(() => {
-    hydrate(<App />, rootElement);
-  });
+  hydrate(<App />, rootElement);
 } else {
   render(<App />, rootElement);
 }
