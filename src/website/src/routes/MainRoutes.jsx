5afc8605663870f7239860880b0cf3b0eb611607
Yannick Huard <mail@yannickhuard.com>
2019-01-28 09:51:14 +0100

    chore: update dependencies - no issue (#995)
    * chore: update dependencies
    
    * use prettier instead of js-beautify
    
    * use React lazy and Suspense instead of react-loadable
    
    * restore js-beautify
    
    * update deps, unleash the bots
    
    * update deps
    
    * moar goodness

--- src/website/src/routes/MainRoutes.jsx
+++ src/website/src/routes/MainRoutes.jsx
@@ -1,38 +1,23 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Loadable from 'react-loadable';
 
 import HomePage from './HomePage';
 import Example from './Example';
 import PageNotFound from './404';
 
-const ECRoutes = Loadable({
-  loader: () => import('./Ec'),
-  loading: () => null,
-});
+const ECRoutes = lazy(() => import('./Ec'));
+const EURoutes = lazy(() => import('./Eu'));
 
-const EURoutes = Loadable({
-  loader: () => import('./Eu'),
-  loading: () => null,
-});
-
-class MainRoutes extends React.Component {
-  componentDidMount() {
-    // ECRoutes.preload();
-    // EURoutes.preload();
-  }
-
-  render() {
-    return (
-      <Switch>
-        <Route exact strict path="/" component={HomePage} />
-        <Route strict path="/example" component={Example} />
-        <Route path="/ec/" strict component={ECRoutes} />
-        <Route path="/eu/" strict component={EURoutes} />
-        <Route component={PageNotFound} />
-      </Switch>
-    );
-  }
-}
+const MainRoutes = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Switch>
+      <Route exact strict path="/" component={HomePage} />
+      <Route strict path="/example" component={Example} />
+      <Route path="/ec/" strict component={ECRoutes} />
+      <Route path="/eu/" strict component={EURoutes} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </Suspense>
+);
 
 export default MainRoutes;
