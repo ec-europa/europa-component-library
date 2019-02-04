c7d3cec81efbc607cb98f79ce0dccf250b2a70e8
planctus <davidepuntoferrante@gmail.com>
2019-02-04 16:30:55 +0100

    feat(checkbox) - Fixing multiple lines labels, restoring knowbs on default demo

--- src/systems/ec/implementations/react/packages/checkbox/stories/Index.jsx
+++ src/systems/ec/implementations/react/packages/checkbox/stories/Index.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import demo from '@ecl/ec-specs-checkbox/demo/data';
 import Checkbox from '../Checkbox';
 
 storiesOf('Forms/Checkbox', module)
-  .add('default', () => <Checkbox {...demo.default} />)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    const label = text('Label', demo.default.label);
+    const helperText = text('helperText', demo.default.helperText);
+    const id = text('id', demo.default.id);
+
+    return <Checkbox label={label} id={id} helperText={helperText} />;
+  })
   .add('disabled', () => <Checkbox {...demo.disabled} />)
   .add('invalid', () => <Checkbox {...demo.invalid} />)
   .add('withoutLabel', () => <Checkbox {...demo.withoutLabel} />);
