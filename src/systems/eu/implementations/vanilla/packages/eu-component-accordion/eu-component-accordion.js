f9657227544675c20497a050414a531cd1ffb609
Yannick Huard <mail@yannickhuard.com>
2019-02-14 09:20:42 +0100

    fix eu

--- src/systems/eu/implementations/vanilla/packages/eu-component-accordion/eu-component-accordion.js
+++ src/systems/eu/implementations/vanilla/packages/eu-component-accordion/eu-component-accordion.js
@@ -1,6 +1,6 @@
 import { queryOne, queryAll } from '@ecl/eu-base/helpers/dom';
 
-class Accordion {
+export class Accordion {
   constructor(
     element,
     {
--- src/systems/eu/implementations/vanilla/packages/eu-component-contextual-navigation/eu-component-contextual-navigation.js
+++ src/systems/eu/implementations/vanilla/packages/eu-component-contextual-navigation/eu-component-contextual-navigation.js
@@ -1,6 +1,6 @@
 import { queryOne } from '@ecl/eu-base/helpers/dom';
 
-class ContextualNavigation {
+export class ContextualNavigation {
   constructor(
     element,
     {
