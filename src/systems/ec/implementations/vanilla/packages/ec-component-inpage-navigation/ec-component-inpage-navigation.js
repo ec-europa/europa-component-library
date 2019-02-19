5354edbf1d613a8651f486c123882d3eb108b742
planctus <davidepuntoferrante@gmail.com>
2019-02-19 11:38:10 +0100

    feat(inpage) - Dynamic inpage, enhanced demo

--- src/systems/ec/implementations/vanilla/packages/ec-component-inpage-navigation/ec-component-inpage-navigation.js
+++ src/systems/ec/implementations/vanilla/packages/ec-component-inpage-navigation/ec-component-inpage-navigation.js
@@ -48,6 +48,7 @@ class InpageNavigation {
     this.handleClickOnToggler = this.handleClickOnToggler.bind(this);
     this.handleClickOnLink = this.handleClickOnLink.bind(this);
     this.initScrollSpy = this.initScrollSpy.bind(this);
+    this.initObserver = this.initObserver.bind(this);
   }
 
   // ACTIONS
@@ -63,7 +64,7 @@ class InpageNavigation {
 
   initScrollSpy(inpageNavElement) {
     const toggleClass = this.spyActiveContainer;
-    const navigationTitle = document.querySelector(this.spyTrigger);
+    const navigationTitle = queryOne(this.spyTrigger);
     this.gumshoe.init({
       selector: this.spySelector,
       activeClass: this.spyClass,
@@ -84,6 +85,76 @@ class InpageNavigation {
     this.gumshoe.destroy();
   }
 
+  initObserver() {
+    const self = this;
+    this.observer = new MutationObserver(function observe(mutationsList) {
+      const body = queryOne('.ecl-col-md-9');
+      const currentInpage = queryOne('[data-ecl-inpage-navigation-list]');
+      let restart = false;
+
+      mutationsList.forEach(mutation => {
+        // Exclude the changes we perform.
+        if (
+          !mutation.target.className.includes(
+            'ecl-inpage-navigation__trigger-current'
+          )
+        ) {
+          // Added nodes.
+          if (mutation.addedNodes.length > 0) {
+            mutation.addedNodes.forEach(addedNode => {
+              if (addedNode.tagName === 'H2' && addedNode.id) {
+                const H2s = queryAll('h2[id]', body);
+                const addedNodeIndex = H2s.findIndex(
+                  H2 => H2.id === addedNode.id
+                );
+                const element = currentInpage.childNodes[
+                  addedNodeIndex - 1
+                ].cloneNode(true);
+                element.childNodes[0].innerText = addedNode.textContent;
+                element.childNodes[0].href = `#${addedNode.id}`;
+                currentInpage.childNodes[addedNodeIndex - 1].after(element);
+                restart = true;
+              }
+            });
+          }
+          // Removed nodes.
+          if (mutation.removedNodes.length > 0) {
+            mutation.removedNodes.forEach(removedNode => {
+              if (removedNode.tagName === 'H2' && removedNode.id) {
+                currentInpage.childNodes.forEach(item => {
+                  if (item.childNodes[0].href.includes(removedNode.id)) {
+                    // Remove the element from the inpage.
+                    currentInpage.removeChild(item);
+                    restart = true;
+                  }
+                });
+              }
+            });
+          }
+
+          if (restart) {
+            self.destroy();
+            self.init();
+            return;
+          }
+
+          self.initScrollSpy(currentInpage);
+        }
+      });
+    });
+
+    this.observer.observe(document, {
+      subtree: true,
+      childList: true,
+    });
+  }
+
+  destroyObserver() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
   // Init
   init() {
     const toggleElement = queryOne(this.toggleSelector, this.element);
@@ -91,6 +162,7 @@ class InpageNavigation {
 
     this.initSticky(this.element);
     this.initScrollSpy(this.element);
+    this.initObserver();
 
     if (this.attachClickListener && toggleElement) {
       toggleElement.addEventListener('click', this.handleClickOnToggler);
@@ -142,6 +214,7 @@ class InpageNavigation {
     }
     this.destroyScrollSpy();
     this.destroySticky();
+    this.destroyObserver();
   }
 }
 
