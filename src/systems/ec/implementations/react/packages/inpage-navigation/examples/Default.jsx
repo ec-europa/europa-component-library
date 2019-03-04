5630230b13d04665f594ecdde79ae41cf7c5f68d
planctus <davidepuntoferrante@gmail.com>
2019-03-04 11:38:51 +0100

    feat(inpage) - Enhancing the demo in the website

--- src/systems/ec/implementations/react/packages/inpage-navigation/examples/Default.jsx
+++ src/systems/ec/implementations/react/packages/inpage-navigation/examples/Default.jsx
@@ -4,11 +4,25 @@ import StoryWrapper from '@ecl/story-wrapper';
 import VanillaInpageNavigation from '@ecl/ec-component-inpage-navigation';
 import demoContent from '@ecl/ec-specs-inpage-navigation/demo/data';
 import loremIpsum from 'lorem-ipsum';
+import Breadcrumb, { BreadcrumbItem } from '@ecl/ec-react-component-breadcrumb';
+import Footer from '@ecl/ec-react-component-footer';
+import SiteHeader from '@ecl/ec-react-component-site-header';
+import PageHeader from '@ecl/ec-react-component-page-header';
+import breadcrumbContent from '@ecl/ec-specs-breadcrumb/demo/data-simple';
+import demoContentEn from '@ecl/ec-specs-site-header/demo/data--en';
+import demoContentCorporate from '@ecl/ec-specs-footer/demo/data--corporate';
 import InpageNavigation from '../src/InpageNavigation';
 
 export default () => {
   const demoText = loremIpsum({ count: 25 });
-
+  const { items, ...breadcrumbProps } = breadcrumbContent;
+  const breadcrumb = (
+    <Breadcrumb {...breadcrumbProps}>
+      {items.map(item => (
+        <BreadcrumbItem {...item} key={item.label} />
+      ))}
+    </Breadcrumb>
+  );
   return (
     <React.Fragment>
       <StoryWrapper
@@ -27,7 +41,8 @@ export default () => {
           }
         }}
       />
-
+      <SiteHeader {...demoContentEn} />
+      <PageHeader breadcrumb={breadcrumb} title="A demo page" />
       <div className="ecl-container">
         <div className="ecl-row ecl-u-mt-l">
           <div className="ecl-col-md-3">
@@ -54,6 +69,7 @@ export default () => {
           </div>
         </div>
       </div>
+      <Footer {...demoContentCorporate} />
     </React.Fragment>
   );
 };
