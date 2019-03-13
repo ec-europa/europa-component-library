95d62e2a4c8d9d4680259b993373a0f507f73f42
Yannick Huard <mail@yannickhuard.com>
2019-03-13 18:39:47 +0100

    make compilation less verbose

--- src/systems/ec/implementations/react/storybook/.storybook/webpack.config.js
+++ src/systems/ec/implementations/react/storybook/.storybook/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const includePaths = [
   path.resolve(__dirname, '../../../../../../../node_modules'),
@@ -17,6 +18,19 @@ module.exports = ({ config: defaultConfig, mode }) => {
   defaultConfig.module.rules[0].use[0].options.plugins.push(
     '@babel/plugin-proposal-export-default-from'
   );
+
+  // Make it less verbose
+  if (mode === 'PRODUCTION') {
+    // Remove ProgressPlugin (4th plugin)
+    const plugin = defaultConfig.plugins.splice(3, 1);
+
+    if (!(plugin[0] instanceof webpack.ProgressPlugin)) {
+      console.error(
+        'Error: 4th plugin is not ProgressPlugin.\nCheck src/systems/ec/implementations/react/storybook/.storybook/webpack.config.js'
+      );
+      return process.exit(1);
+    }
+  }
 
   return defaultConfig;
 };
