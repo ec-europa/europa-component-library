a26f9b5440dc40636fdae14ac10df5f6d08dae90
Kalin Chernev <kalin.chernev@gmail.com>
2019-04-05 12:22:31 +0200

    Refactoring and adding capabilities

---
+++ src/systems/ec/implementations/react/storybook/test/lib/image-snapshot.js
@@ -0,0 +1,95 @@
+import 'regenerator-runtime/runtime';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import { logger } from '@storybook/node-logger';
+import { Builder } from 'selenium-webdriver';
+
+import constructUrl from './url';
+
+expect.extend({ toMatchImageSnapshot });
+
+const defaultConfig = {
+  storybookUrl: 'http://localhost:6008',
+  capability: {},
+};
+
+const imageSnapshot = (customConfig = {}) => {
+  const { storybookUrl, capability } = {
+    ...defaultConfig,
+    ...customConfig,
+  };
+
+  let browser;
+
+  const testFn = async ({ context }) => {
+    let image;
+    const { kind, framework, name } = context;
+
+    if (framework === 'rn') {
+      // Skip tests since we de not support RN image snapshots.
+      logger.error(
+        "It seems you are running imageSnapshot on RN app and it's not supported. Skipping test."
+      );
+
+      throw new Error('Unsupported framework.');
+    }
+
+    if (!browser) {
+      logger.error(
+        `Error when generating image snapshot for test ${kind} - ${name} : there was no browser.`
+      );
+
+      throw new Error('Missing browser');
+    }
+
+    const url = constructUrl(storybookUrl, kind, name);
+
+    expect.assertions(1);
+
+    try {
+      await browser.get(url);
+      image = await browser.takeScreenshot();
+    } catch (error) {
+      logger.error(
+        `Error when connecting to ${url}, did you start or build the storybook first? A storybook instance should be running or a static version should be built when using image snapshot feature.`,
+        error
+      );
+
+      throw error;
+    }
+
+    expect(image).toMatchImageSnapshot({ context, url });
+  };
+
+  testFn.beforeEach = () => {
+    browser.pause(500);
+  };
+
+  testFn.afterAll = () => {
+    browser.quit();
+  };
+
+  testFn.beforeAll = async () => {
+    const {
+      SAUCE_USERNAME: username,
+      SAUCE_ACCESS_KEY: accessKey,
+    } = process.env;
+
+    browser = new Builder()
+      .withCapabilities({
+        username,
+        accessKey,
+        // name: componentName,
+        maxDuration: 3600,
+        idleTimeout: 1000,
+        ...capability,
+      })
+      .usingServer(
+        `http://${username}:${accessKey}@ondemand.saucelabs.com:80/wd/hub`
+      )
+      .build();
+  };
+
+  return testFn;
+};
+
+export default imageSnapshot;
--- src/systems/ec/implementations/react/storybook/test/visual.test.js
+++ src/systems/ec/implementations/react/storybook/test/visual.test.js
@@ -6,54 +6,50 @@ import finalhandler from 'finalhandler';
 import http from 'http';
 import serveStatic from 'serve-static';
 
-import imageSnapshot from '../lib/image-snapshot';
+import getCapabilities from './lib/capabilities';
+import imageSnapshot from './lib/image-snapshot';
 
-const pathToStorybookStatic = path.resolve(__dirname, '../build');
 const port = 6008;
+const capabilities = getCapabilities();
+const pathToStorybookStatic = path.resolve(__dirname, '../build');
 
 if (!fs.existsSync(pathToStorybookStatic)) {
   logger.error(
     'You are running image snapshots without having the static build of storybook. Please run "yarn build" before running tests.'
   );
-} else {
+  throw new Error('Missing build folder.');
+}
+
+let server = null;
+
+beforeAll(() => {
+  logger.info('Start server.');
+
+  server = http.createServer((req, res) => {
+    const serve = serveStatic(pathToStorybookStatic, {
+      index: ['index.html', 'index.htm'],
+    });
+
+    serve(req, res, finalhandler(req, res));
+  });
+
+  server.listen(port);
+});
+
+afterAll(() => {
+  server.close();
+  logger.info('Stop server.');
+});
+
+capabilities.forEach(capability => {
   const visualTest = {
     framework: 'react',
     suite: 'ECL - Visual Tests',
     test: imageSnapshot({
       storybookUrl: `http://localhost:${port}`,
-      getGotoOptions: () => ({
-        waitUntil: 'networkidle0',
-      }),
-      getMatchOptions: () => ({
-        failureThreshold: 0,
-        failureThresholdType: 'pixel',
-      }),
+      capability,
     }),
   };
 
-  let server = null;
-
-  beforeAll(() => {
-    logger.info('Start server');
-
-    // Create server
-    server = http.createServer((req, res) => {
-      // Serve up public/ftp folder
-      const serve = serveStatic(pathToStorybookStatic, {
-        index: ['index.html', 'index.htm'],
-      });
-
-      serve(req, res, finalhandler(req, res));
-    });
-
-    // Listen
-    server.listen(port);
-  });
-
-  afterAll(() => {
-    server.close();
-    logger.info('End');
-  });
-
   initStoryshots(visualTest);
-}
+});
