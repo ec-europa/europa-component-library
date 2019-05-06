b0700131d5b8b2f0245e2d071f40a52721a990f1
Yannick Huard <mail@yannickhuard.com>
2019-05-06 14:56:10 +0200

    test different approach

--- src/systems/eu/implementations/react/storybook/.storybook/ECL.jsx
+++ src/systems/eu/implementations/react/storybook/.storybook/ECL.jsx
@@ -2,3 +2,25 @@ import 'iframe-resizer/js/iframeResizer.contentWindow.min.js';
 import svg4everybody from 'svg4everybody/dist/svg4everybody.min';
 
 svg4everybody();
+
+window.onload = function() {
+  // Replace buttons' labels
+  var gridButton = document.querySelector('[title="Toggle background grid"]');
+  if (gridButton) {
+    gridButton.innerText = 'Grid';
+  }
+
+  var viewportButton = document.querySelector(
+    '[title="Change the size of the preview"]'
+  );
+  if (viewportButton) {
+    viewportButton.innerText = 'Viewport';
+  }
+
+  var colorButton = document.querySelector(
+    '[title="Color Blindness Emulation"]'
+  );
+  if (colorButton) {
+    colorButton.innerText = 'Color Filter';
+  }
+};
