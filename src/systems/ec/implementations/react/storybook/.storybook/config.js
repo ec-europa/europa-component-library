7ddbfd2cc5dbfe618ac0864baadf249b8b4aeb38
Yannick Huard <mail@yannickhuard.com>
2019-05-07 08:37:34 +0200

    chore: update dependencies - INNO-1474 (#1108)
    * chore: update dependencies - no issue
    
    * change storybook's defaults
    
    * inject CSS reset to spot leaks
    
    * inject stylesheets dynamically
    
    * fix iframes
    
    * add "responsive" viewport
    
    * add 1366x768 and 1920x1080 viewports
    
    * update dependencies
    
    * polyfill NodeList.forEach
    
    * try to patch storybook
    
    * remove useless files
    
    * update dependencies
    
    * change buttons labels
    
    * test different approach
    
    * patch labels
    
    * smoother first rendering

--- src/systems/ec/implementations/react/storybook/.storybook/config.js
+++ src/systems/ec/implementations/react/storybook/.storybook/config.js
@@ -2,9 +2,13 @@ import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withOptions } from '@storybook/addon-options';
 import { create } from '@storybook/theming';
 import { withA11y } from '@storybook/addon-a11y';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { withCssResources } from '@storybook/addon-cssresources';
+
 import './ECL';
 
 addDecorator(withA11y);
+addDecorator(withCssResources);
 
 addParameters({
   a11y: {
@@ -14,6 +18,37 @@ addParameters({
       restoreScroll: true,
     },
   },
+  cssresources: [
+    {
+      id: 'ecl-screen',
+      code: `<link rel="stylesheet" type="text/css" href="/storybook/ec/styles/ecl-ec-preset-website.css" />`,
+      picked: true,
+    },
+    {
+      id: 'ecl-print',
+      code: `<link rel="stylesheet" type="text/css" href="/storybook/ec/styles/ecl-ec-preset-website-print.css" />`,
+      picked: false,
+    },
+    {
+      id: 'test-fake-global-rules',
+      code: `
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html {
+  color: red;
+  font-family: serif;
+  font-weight: 700;
+  line-height: 2;
+}
+</style>`,
+      picked: false,
+    },
+  ],
   options: {
     theme: create({
       base: 'light',
@@ -25,6 +60,35 @@ addParameters({
       brandImage: null,
     }),
     sidebarAnimations: false,
+    panelPosition: 'right',
+  },
+  viewport: {
+    defaultViewport: 'iphone6',
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      '1366x768': {
+        name: '1366x768',
+        styles: {
+          width: '1366px',
+          height: '768px',
+        },
+        type: 'desktop',
+      },
+      '1920x1080': {
+        name: '1920x1080',
+        styles: {
+          width: '1920px',
+          height: '1080px',
+        },
+      },
+      responsive: {
+        name: 'Responsive',
+        styles: {
+          width: '100%',
+          height: '100%',
+        },
+      },
+    },
   },
 });
 
