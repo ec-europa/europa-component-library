97efe68e6423dcfcd86640bb494475c818e63703
Yannick Huard <mail@yannickhuard.com>
2019-05-27 11:56:40 +0200

    do's and don'ts

---
+++ src/tools/website-components/DoDont/Caution.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import iconSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
+import styles from './DoDont.scss';
+
+const Caution = ({ image, alt, title, description }) => (
+  <div className={styles.caution}>
+    <div className={styles['image-container']}>
+      <img src={image} alt={alt} className={styles.image} />
+    </div>
+    <div className={styles.separator} />
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <svg
+          focusable="false"
+          aria-hidden="true"
+          className={styles.title__icon}
+        >
+          <use xlinkHref={`${iconSprite}#notifications--warning`} />
+        </svg>
+        {title}
+      </div>
+      <div className={styles.description}>{description}</div>
+    </div>
+  </div>
+);
+
+Caution.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  alt: PropTypes.string,
+  description: PropTypes.string,
+};
+
+Caution.defaultProps = {
+  title: 'Caution',
+  image: '',
+  alt: '',
+  description: '',
+};
+
+export default Caution;
