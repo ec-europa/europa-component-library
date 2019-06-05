befee7e77cfba179c488a8f4596408bcf6304f85
Yannick Huard <mail@yannickhuard.com>
2019-06-05 10:27:52 +0200

    chore: replace thumbnails - INNO-1503 (#1145)
    * chore: replace thumbnails - INNO-1503
    
    * cleaning
    
    * make button primary

--- src/tools/website-components/Thumbnail/Thumbnail.jsx
+++ src/tools/website-components/Thumbnail/Thumbnail.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 
 import Link from '../Link/Link';
 import styles from './Thumbnail.scss';
 
-const Thumbnail = ({ image, link, title, zoom }) => (
+const Thumbnail = ({ image, link, title }) => (
   <Link className={styles.thumbnail} to={link}>
     <span className={styles.title}>{title}</span>
-    <img
-      src={image}
-      alt={`Thumbnail of ${title}`}
-      className={classnames(styles.image, {
-        [`${styles[`zoom${zoom}`]}`]: zoom,
-      })}
-    />
+    <div className={styles.imageContainer}>
+      <div className={styles.flexboxCentering}>
+        <img
+          src={image}
+          alt={`Thumbnail of ${title}`}
+          className={styles.image}
+        />
+      </div>
+    </div>
   </Link>
 );
 
@@ -23,11 +24,6 @@ Thumbnail.propTypes = {
   image: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  zoom: PropTypes.string,
-};
-
-Thumbnail.defaultProps = {
-  zoom: '100',
 };
 
 export default Thumbnail;
