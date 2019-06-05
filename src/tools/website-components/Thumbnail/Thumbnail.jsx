2b1e308ae074ef817d4ed77baccb71167e1f8e85
Yannick Huard <mail@yannickhuard.com>
2019-06-05 09:11:52 +0200

    cleaning

--- src/tools/website-components/Thumbnail/Thumbnail.jsx
+++ src/tools/website-components/Thumbnail/Thumbnail.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 
 import Link from '../Link/Link';
 import styles from './Thumbnail.scss';
 
-const Thumbnail = ({ image, link, title, zoom }) => (
+const Thumbnail = ({ image, link, title }) => (
   <Link className={styles.thumbnail} to={link}>
     <span className={styles.title}>{title}</span>
     <div className={styles.imageContainer}>
@@ -14,9 +13,7 @@ const Thumbnail = ({ image, link, title, zoom }) => (
         <img
           src={image}
           alt={`Thumbnail of ${title}`}
-          className={classnames(styles.image, {
-            [`${styles[`zoom${zoom}`]}`]: zoom,
-          })}
+          className={styles.image}
         />
       </div>
     </div>
@@ -27,11 +24,6 @@ Thumbnail.propTypes = {
   image: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  zoom: PropTypes.string,
-};
-
-Thumbnail.defaultProps = {
-  zoom: '100',
 };
 
 export default Thumbnail;
