b981115719d04080964add909a9dc86dbc33cb02
Romain Emery <emery.romain@gmail.com>
2019-06-11 15:35:38 +0200

    update content items

--- src/systems/ec/implementations/react/templates/compositions/content-item/examples/Default.jsx
+++ src/systems/ec/implementations/react/templates/compositions/content-item/examples/Default.jsx
@@ -3,34 +3,42 @@ import Icon from '@ecl/ec-react-component-icon';
 import { UnorderedList, UnorderedListItem } from '@ecl/ec-react-component-list';
 
 export default () => (
-  <article className="ecl-u-d-flex ecl-u-border-bottom ecl-u-border-color-grey-15">
-    <div>
+  <article className="ecl-u-d-flex ecl-u-border-bottom ecl-u-border-color-grey-15 ecl-u-pv-m">
+    <div className="ecl-u-flex-grow-1">
       <div className="ecl-u-type-s ecl-u-type-color-grey-75">
         NEWS ARTICLE | 17 October 2019
       </div>
-      <div className="ecl-u-type-prolonged-m ecl-u-type-bold">
+      <div className="ecl-u-type-prolonged-m ecl-u-type-bold ecl-u-mt-xs">
         <a href="/example" className="ecl-link ecl-link--standalone">
           Article title
         </a>
       </div>
-      <p className="ecl-u-type-paragraph ecl-u-type-color-grey-100">
+      <p className="ecl-u-type-paragraph ecl-u-type-color-grey-100 ecl-u-mt-xs">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut ex
         tristique, dignissim sem ac, bibendum est. Sed vehicula lorem non nunc
         tincidunt hendrerit. Nunc tristique ante et fringilla fermentum.
       </p>
 
-      <UnorderedList variant="no-bullet">
-        <UnorderedListItem className="ecl-u-d-inline-flex ecl-u-align-items-center ecl-u-type-color-grey-75">
+      <UnorderedList variant="no-bullet" className="ecl-u-mt-m">
+        <UnorderedListItem className="ecl-u-d-flex ecl-u-align-items-center ecl-u-type-color-grey-75">
           <Icon shape="general--location" size="m" />
-          <span className="ecl-u-type-s">Brussels, Belgium</span>
+          <span className="ecl-u-type-s ecl-u-ml-s">Brussels, Belgium</span>
         </UnorderedListItem>
-        <UnorderedListItem className="ecl-u-d-inline-flex ecl-u-align-items-center ecl-u-type-color-grey-75">
+        <UnorderedListItem className="ecl-u-d-flex ecl-u-align-items-center ecl-u-type-color-grey-75">
           <Icon shape="general--livestreaming" size="m" />
-          <span className="ecl-u-type-s">Live stream available</span>
+          <span className="ecl-u-type-s ecl-u-ml-s">Live stream available</span>
         </UnorderedListItem>
       </UnorderedList>
     </div>
 
-    <img src="/example-image.jpg" alt="example" width="120" height="80" />
+    <div
+      className="ecl-u-ratio-3-2 ecl-u-flex-shrink-0 ecl-u-ml-xl"
+      style={{
+        backgroundImage: 'url("/example-image.jpg")',
+        backgroundSize: 'contain',
+        height: '5rem',
+        width: '7.5rem',
+      }}
+    />
   </article>
 );
