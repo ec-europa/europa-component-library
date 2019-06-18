03b9e10e3d0e1fa3c39f38b3314fd00bc2162a10
Romain Emery <emery.romain@gmail.com>
2019-06-18 09:22:00 +0200

    fix(banner): replace button by a link - INNO-1535 (#1163)
    * replace button
    
    * fix color

--- src/systems/ec/specs/components/page-banner/demo/data--image-shade.js
+++ src/systems/ec/specs/components/page-banner/demo/data--image-shade.js
@@ -3,9 +3,9 @@ module.exports = {
   variant: 'image-shade',
   title: 'EU Budget for the future',
   baseline: 'Innovation, economy, environment and geopolitics',
-  button: {
-    variant: 'call',
+  link: {
     label: 'Subscribe',
+    href: '/example',
     icon: {
       shape: 'ui--corner-arrow',
       transform: 'rotate-90',
