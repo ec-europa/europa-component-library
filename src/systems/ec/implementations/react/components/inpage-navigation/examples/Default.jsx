de999b6e14e4227efb4bac6ca3235a875cb1141e
Yannick Huard <mail@yannickhuard.com>
2019-06-19 09:19:41 +0200

    fix demos

--- src/systems/ec/implementations/react/components/inpage-navigation/examples/Default.jsx
+++ src/systems/ec/implementations/react/components/inpage-navigation/examples/Default.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
-import StoryWrapper from '@ecl/story-wrapper';
+import React, { Fragment } from 'react';
 import VanillaInpageNavigation from '@ecl/ec-component-inpage-navigation';
 import demoContent from '@ecl/ec-specs-inpage-navigation/demo/data';
 import { loremIpsum } from 'lorem-ipsum';
@@ -13,63 +12,65 @@ import demoContentEn from '@ecl/ec-specs-site-header/demo/data--en';
 import demoContentCorporate from '@ecl/ec-specs-footer/demo/data--corporate';
 import InpageNavigation from '../src/InpageNavigation';
 
-export default () => {
-  const demoText = loremIpsum({ count: 25 });
-  const { items, ...breadcrumbProps } = breadcrumbContent;
-  const breadcrumb = (
-    <Breadcrumb {...breadcrumbProps}>
-      {items.map(item => (
-        <BreadcrumbItem {...item} key={item.label} />
-      ))}
-    </Breadcrumb>
-  );
-  return (
-    <React.Fragment>
-      <StoryWrapper
-        afterMount={() => {
-          const element = document.querySelector(
-            '[data-ecl-inpage-navigation]'
-          );
-          const vanillaInpageNavigation = new VanillaInpageNavigation(element);
-          vanillaInpageNavigation.init();
-          // Return new context
-          return { vanillaInpageNavigation };
-        }}
-        beforeUnmount={context => {
-          if (context.vanillaInpageNavigation) {
-            context.vanillaInpageNavigation.destroy();
-          }
-        }}
-      />
-      <SiteHeader {...demoContentEn} />
-      <PageHeader breadcrumb={breadcrumb} title="A demo page" />
-      <div className="ecl-container">
-        <div className="ecl-row ecl-u-mt-l">
-          <div className="ecl-col-md-3">
-            <InpageNavigation {...demoContent} />
-          </div>
+export default class DefaultExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.vanillaInpageNavigation = null;
+  }
 
-          <div className="ecl-col-md-9">
-            <h2 className="ecl-u-type-heading-2" id="inline-nav-1">
-              Heading 1
-            </h2>
-            <p className="ecl-u-type-paragraph-m">{demoText}</p>
-            <h2 className="ecl-u-type-heading-2" id="inline-nav-2">
-              Heading 2
-            </h2>
-            <p className="ecl-u-type-paragraph-m">{demoText}</p>
-            <h2 className="ecl-u-type-heading-2" id="inline-nav-3">
-              Heading 3
-            </h2>
-            <p className="ecl-u-type-paragraph-m">{demoText}</p>
-            <h2 className="ecl-u-type-heading-2" id="inline-nav-4">
-              Heading 4
-            </h2>
-            <p className="ecl-u-type-paragraph-m">{demoText}</p>
+  componentDidMount() {
+    const element = document.querySelector('[data-ecl-inpage-navigation]');
+    this.vanillaInpageNavigation = new VanillaInpageNavigation(element);
+    this.vanillaInpageNavigation.init();
+  }
+
+  componentWillUnmount() {
+    if (this.vanillaInpageNavigation) {
+      this.vanillaInpageNavigation.destroy();
+    }
+  }
+
+  render() {
+    const demoText = loremIpsum({ count: 25 });
+    const { items, ...breadcrumbProps } = breadcrumbContent;
+    const breadcrumb = (
+      <Breadcrumb {...breadcrumbProps}>
+        {items.map(item => (
+          <BreadcrumbItem {...item} key={item.label} />
+        ))}
+      </Breadcrumb>
+    );
+    return (
+      <Fragment>
+        <SiteHeader {...demoContentEn} />
+        <PageHeader breadcrumb={breadcrumb} title="A demo page" />
+        <div className="ecl-container">
+          <div className="ecl-row ecl-u-mt-l">
+            <div className="ecl-col-md-3">
+              <InpageNavigation {...demoContent} />
+            </div>
+            <div className="ecl-col-md-9">
+              <h2 className="ecl-u-type-heading-2" id="inline-nav-1">
+                Heading 1
+              </h2>
+              <p className="ecl-u-type-paragraph-m">{demoText}</p>
+              <h2 className="ecl-u-type-heading-2" id="inline-nav-2">
+                Heading 2
+              </h2>
+              <p className="ecl-u-type-paragraph-m">{demoText}</p>
+              <h2 className="ecl-u-type-heading-2" id="inline-nav-3">
+                Heading 3
+              </h2>
+              <p className="ecl-u-type-paragraph-m">{demoText}</p>
+              <h2 className="ecl-u-type-heading-2" id="inline-nav-4">
+                Heading 4
+              </h2>
+              <p className="ecl-u-type-paragraph-m">{demoText}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <Footer {...demoContentCorporate} />
-    </React.Fragment>
-  );
-};
+        <Footer {...demoContentCorporate} />
+      </Fragment>
+    );
+  }
+}
