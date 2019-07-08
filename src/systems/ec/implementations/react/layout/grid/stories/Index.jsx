51d6e945cb7c411d8e4161c5109957077a95cbaa
Yannick Huard <mail@yannickhuard.com>
2019-07-08 09:17:28 +0200

    add more examples

--- src/systems/ec/implementations/react/layout/grid/stories/Index.jsx
+++ src/systems/ec/implementations/react/layout/grid/stories/Index.jsx
@@ -1,7 +1,73 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+
+// eslint-disable-next-line react/prop-types
+const Demo = ({ children }) => (
+  <div
+    style={{
+      position: 'relative',
+      minHeight: '100vh',
+      backgroundColor: '#FCF4FB',
+    }}
+  >
+    {children}
+  </div>
+);
+
+const Background = () => (
+  <div
+    className="ecl-container"
+    style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    }}
+  >
+    <div
+      className="ecl-row"
+      style={{
+        height: '100%',
+      }}
+    >
+      {new Array(12).fill().map((v, i) => (
+        <div className="ecl-col-1" key={`bg-${i}`}>
+          <div
+            style={{
+              backgroundColor: '#E9E3FC',
+              height: '100%',
+              width: '100%',
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+// eslint-disable-next-line react/prop-types
+const Box = ({ padding, className }) => (
+  <div className={className}>
+    <div
+      style={{
+        backgroundColor: '#6D9DC7',
+        color: '#fff',
+        fontFamily: 'Arial, sans-serif',
+        fontSize: '1rem',
+        padding: padding || '0.75rem',
+      }}
+    >
+      {className
+        .split(' ')
+        .map(str => `.${str}`)
+        .join(' ')}
+    </div>
+  </div>
+);
 
 storiesOf('Layout|Grid', module)
   .addParameters({
@@ -10,129 +76,82 @@ storiesOf('Layout|Grid', module)
     },
   })
   .addDecorator(withKnobs)
-  .add('custom', () => {
+  .add('1-2-3-4-6-12', () => {
     return (
-      <div style={{ position: 'relative', minHeight: '100vh' }}>
-        <div
-          className="ecl-container"
-          style={{
-            backgroundColor: '#FCF4FB',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-          }}
-        >
-          <div
-            className="ecl-row"
-            style={{
-              height: '100%',
-            }}
-          >
-            {new Array(12).fill().map(() => (
-              <div className="ecl-col-1">
-                <div
-                  style={{
-                    backgroundColor: '#E9E3FC',
-                    height: '100%',
-                    width: '100%',
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
+      <Demo>
+        <Background />
         <div className="ecl-container">
-          <div className="ecl-row">
-            {new Array(12).fill().map(() => (
-              <div className="ecl-col-1">
-                <div
-                  style={{
-                    backgroundColor: '#6DC79F',
-                    color: '#fff',
-                    padding: '0.25rem',
-                  }}
-                >
-                  .ecl-col-1
-                </div>
-              </div>
+          <div className="ecl-row ecl-u-pt-m">
+            {new Array(12).fill().map((v, i) => (
+              <Box className="ecl-col-1" padding="0.25rem" key={`b1-${i}`} />
             ))}
           </div>
-          <div className="ecl-row" style={{ marginTop: '1rem' }}>
-            {new Array(6).fill().map(() => (
-              <div className="ecl-col-2">
-                <div
-                  style={{
-                    backgroundColor: '#6DC79F',
-                    color: '#fff',
-                    padding: '0.5rem',
-                  }}
-                >
-                  .ecl-col-2
-                </div>
-              </div>
+          <div className="ecl-row ecl-u-mt-m">
+            {new Array(6).fill().map((v, i) => (
+              <Box className="ecl-col-2" key={`b2-${i}`} />
             ))}
           </div>
-          <div className="ecl-row" style={{ marginTop: '1rem' }}>
-            {new Array(4).fill().map(() => (
-              <div className="ecl-col-3">
-                <div
-                  style={{
-                    backgroundColor: '#6DC79F',
-                    color: '#fff',
-                    padding: '0.5rem',
-                  }}
-                >
-                  .ecl-col-3
-                </div>
-              </div>
+          <div className="ecl-row ecl-u-mt-m">
+            {new Array(4).fill().map((v, i) => (
+              <Box className="ecl-col-3" key={`b3-${i}`} />
             ))}
           </div>
-          <div className="ecl-row" style={{ marginTop: '1rem' }}>
-            {new Array(3).fill().map(() => (
-              <div className="ecl-col-4">
-                <div
-                  style={{
-                    backgroundColor: '#6DC79F',
-                    color: '#fff',
-                    padding: '0.5rem',
-                  }}
-                >
-                  .ecl-col-4
-                </div>
-              </div>
+          <div className="ecl-row ecl-u-mt-m">
+            {new Array(3).fill().map((v, i) => (
+              <Box className="ecl-col-4" key={`b4-${i}`} />
             ))}
           </div>
-          <div className="ecl-row" style={{ marginTop: '1rem' }}>
-            {new Array(2).fill().map(() => (
-              <div className="ecl-col-6">
-                <div
-                  style={{
-                    backgroundColor: '#6DC79F',
-                    color: '#fff',
-                    padding: '0.5rem',
-                  }}
-                >
-                  .ecl-col-6
-                </div>
-              </div>
+          <div className="ecl-row ecl-u-mt-m">
+            {new Array(2).fill().map((v, i) => (
+              <Box className="ecl-col-6" key={`b5-${i}`} />
             ))}
           </div>
-          <div className="ecl-row" style={{ marginTop: '1rem' }}>
-            <div className="ecl-col-12">
-              <div
-                style={{
-                  backgroundColor: '#6DC79F',
-                  color: '#fff',
-                  padding: '0.5rem',
-                }}
-              >
-                .ecl-col-12
-              </div>
-            </div>
+          <div className="ecl-row ecl-u-mt-m">
+            <Box className="ecl-col-12" />
           </div>
         </div>
-      </div>
+      </Demo>
+    );
+  })
+  .add('6-4-2', () => {
+    return (
+      <Demo>
+        <Background />
+        <div className="ecl-container">
+          <div className="ecl-row ecl-u-pt-m">
+            <Box className="ecl-col-6" />
+            <Box className="ecl-col-4" />
+            <Box className="ecl-col-2" />
+          </div>
+        </div>
+      </Demo>
+    );
+  })
+  .add('3-3-6', () => {
+    return (
+      <Demo>
+        <Background />
+        <div className="ecl-container">
+          <div className="ecl-row ecl-u-pt-m">
+            <Box className="ecl-col-3" />
+            <Box className="ecl-col-3" />
+            <Box className="ecl-col-6 " />
+          </div>
+        </div>
+      </Demo>
+    );
+  })
+  .add('2-8-2', () => {
+    return (
+      <Demo>
+        <Background />
+        <div className="ecl-container">
+          <div className="ecl-row ecl-u-pt-m">
+            <Box className="ecl-col-2" />
+            <Box className="ecl-col-8" />
+            <Box className="ecl-col-2" />
+          </div>
+        </div>
+      </Demo>
     );
   });
