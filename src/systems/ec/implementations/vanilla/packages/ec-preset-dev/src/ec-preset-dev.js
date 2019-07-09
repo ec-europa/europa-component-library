8931f0940f5bd5bb36823b434e48c82fb9964491
emeryro <emery.romain@gmail.com>
2019-07-09 15:20:52 +0200

    split accordion

--- src/systems/ec/implementations/vanilla/packages/ec-preset-dev/src/ec-preset-dev.js
+++ src/systems/ec/implementations/vanilla/packages/ec-preset-dev/src/ec-preset-dev.js
@@ -1,6 +1,7 @@
 // Export components
 
 export * from '@ecl/ec-component-accordion';
+export * from '@ecl/ec-component-accordion2';
 export * from '@ecl/ec-component-breadcrumb';
 // export * from '@ecl/ec-component-carousel';
 export * from '@ecl/ec-component-contextual-navigation';
