bd57210cbf170d1dbc2cea0e546207f59fe11ed4
Romain Emery <emery.romain@gmail.com>
2019-07-10 12:44:54 +0200

    Update src/systems/eu/implementations/react/components/accordion/stories/Index.jsx
    Co-Authored-By: Yannick Huard <mail@yannickhuard.com>

--- src/systems/eu/implementations/react/components/accordion/stories/Index.jsx
+++ src/systems/eu/implementations/react/components/accordion/stories/Index.jsx
@@ -45,7 +45,7 @@ storiesOf('Components|Accordion', module)
     cssresources: [
       {
         id: 'ecl-legacy-screen',
-        code: `<link rel="stylesheet" type="text/css" href="./styles/ecl-ec-preset-legacy-website.css" />`,
+        code: `<link rel="stylesheet" type="text/css" href="./styles/ecl-eu-preset-legacy-website.css" />`,
         picked: true,
       },
       {
