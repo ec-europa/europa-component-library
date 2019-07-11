a02879f810de68180e076f3850c2696e603616d4
emeryro <emery.romain@gmail.com>
2019-07-11 07:44:05 +0200

    split list

---
+++ src/systems/ec/implementations/react/components/list-description/examples/Description.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import demoDescription from '@ecl/ec-specs-list-description/demo/data';
+import DescriptionListWithData from '../src/DescriptionListWithData';
+
+export default () => <DescriptionListWithData {...demoDescription} />;
--- src/systems/ec/implementations/react/components/list/stories/Index.jsx
+++ src/systems/ec/implementations/react/components/list/stories/Index.jsx
@@ -5,11 +5,9 @@ import Ordered from '../examples/Ordered';
 import Unordered from '../examples/Unordered';
 import UnorderedWithoutBullet from '../examples/UnorderedWithoutBullet';
 import UnorderedWithDivider from '../examples/UnorderedWithDivider';
-import Description from '../examples/Description';
 
 storiesOf('Components|List', module)
   .add('ordered', Ordered)
   .add('unordered', Unordered)
   .add('without bullet', UnorderedWithoutBullet)
-  .add('with divider', UnorderedWithDivider)
-  .add('description', Description);
+  .add('with divider', UnorderedWithDivider);
