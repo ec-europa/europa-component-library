04c6c9b7dddbd531d1b632d656407cbef1fef400
emeryro <emery.romain@gmail.com>
2019-07-11 09:21:02 +0200

    split ordered list

--- src/systems/ec/implementations/react/components/list/stories/Index.jsx
+++ src/systems/ec/implementations/react/components/list/stories/Index.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/react';
 
-import Ordered from '../examples/Ordered';
 import Unordered from '../examples/Unordered';
 import UnorderedWithoutBullet from '../examples/UnorderedWithoutBullet';
 import UnorderedWithDivider from '../examples/UnorderedWithDivider';
 
 storiesOf('Components|List', module)
-  .add('ordered', Ordered)
   .add('unordered', Unordered)
   .add('without bullet', UnorderedWithoutBullet)
   .add('with divider', UnorderedWithDivider);
