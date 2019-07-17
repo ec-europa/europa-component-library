25d16f743519918c45b59c1f8c566c9816c7586b
Yannick Huard <mail@yannickhuard.com>
2019-07-17 15:36:37 +0200

    EU version

---
+++ src/systems/eu/implementations/react/components/skip-link/src/SkipLink.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+export const SkipLink = ({ href, label, className, ...props }) => {
+  const classNames = classnames(className, 'ecl-skip-link');
+
+  return (
+    <a {...props} href={href} className={classNames}>
+      {label}
+    </a>
+  );
+};
+
+SkipLink.propTypes = {
+  href: PropTypes.string,
+  label: PropTypes.node,
+  className: PropTypes.string,
+};
+
+SkipLink.defaultProps = {
+  href: '#',
+  label: '',
+  className: '',
+};
+
+export default SkipLink;
