c490175de219f04ed70c389ef7be587e19722e64
planctus <davidepuntoferrante@gmail.com>
2019-07-18 10:25:28 +0200

    feat(checkbox) - Updating dependencies

--- src/systems/ec/implementations/react/components/checkbox/Checkbox.jsx
+++ src/systems/ec/implementations/react/components/checkbox/Checkbox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import Icon from '@ecl/ec-react-component-icon/Icon';
+import Icon from '@ecl/ec-react-component-icon';
 
 const Checkbox = ({
   className,
