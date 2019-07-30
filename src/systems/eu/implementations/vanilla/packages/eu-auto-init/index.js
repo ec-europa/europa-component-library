1ff8800c11bf42a574abd12f523533c477a240e6
Yannick Huard <mail@yannickhuard.com>
2019-07-30 11:06:35 +0200

    eu auto init

---
+++ src/systems/eu/implementations/vanilla/packages/eu-auto-init/index.js
@@ -0,0 +1,44 @@
+import { queryAll } from '@ecl/eu-base/helpers/dom';
+
+export const autoInit = ({ root: root = document, ...options } = {}) => {
+  if (!ECL) {
+    throw new TypeError('Called autoInit but ECL is not present');
+  }
+
+  const components = [];
+
+  const nodes = queryAll('[data-ecl-auto-init]', root);
+  nodes
+    .filter(node => node.getAttribute('data-ecl-auto-initialized') !== 'true')
+    .forEach(node => {
+      const componentType = node.getAttribute('data-ecl-auto-init');
+
+      if (!componentType) {
+        throw new TypeError(
+          `(ecl-auto-init) ${componentType} data-ecl-auto-init is empty`
+        );
+      }
+
+      const ctor = ECL[componentType];
+      if (typeof ctor !== 'function') {
+        throw new TypeError(
+          `(ecl-auto-init) Could not find '${componentType}'`
+        );
+      }
+
+      if (typeof ctor.autoInit !== 'function') {
+        throw new TypeError(
+          `(ecl-auto-init) Could not find autoInit for  '${componentType}'`
+        );
+      }
+
+      const component = ctor.autoInit(node, options);
+      components.push(component);
+
+      node.setAttribute('data-ecl-auto-initialized', 'true');
+    });
+
+  return components;
+};
+
+export default autoInit;
