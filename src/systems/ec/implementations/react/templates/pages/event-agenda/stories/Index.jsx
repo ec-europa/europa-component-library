4f3ee20bebdebda2a324aa76caba875fb19979e4
emeryro <emery.romain@gmail.com>
2019-08-14 09:12:39 +0200

    init

---
+++ src/systems/ec/implementations/react/templates/pages/event-agenda/stories/Index.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import StoryWrapper from '@ecl/story-wrapper';
+import createFocusTrap from 'focus-trap';
+
+import EventAgendaPageExample from '../examples/Default';
+import EventAgendaPageEcl from '../examples/PureEcl';
+
+storiesOf('Templates|Pages/Event agenda', module)
+  .addDecorator(story => {
+    function toggleOverlay(e) {
+      e.preventDefault();
+
+      if (this.languageListOverlay.hasAttribute('hidden')) {
+        this.languageListOverlay.removeAttribute('hidden');
+        e.currentTarget.setAttribute('aria-expanded', true);
+        this.focusTrap.activate();
+      } else {
+        this.languageListOverlay.setAttribute('hidden', true);
+        e.currentTarget.setAttribute('aria-expanded', false);
+        this.focusTrap.deactivate();
+      }
+    }
+
+    return (
+      <StoryWrapper
+        afterMount={() => {
+          // Get language list
+          const languageListOverlay = document.querySelector(
+            '[data-ecl-language-list-overlay]'
+          );
+          const languageSelector = document.querySelector(
+            '[data-ecl-language-selector]'
+          );
+          const close = document.querySelector(
+            '[data-ecl-language-list-close]'
+          );
+
+          // Create focus trap
+          const focusTrap = createFocusTrap(languageListOverlay, {
+            escapeDeactivates: false,
+          });
+
+          languageSelector.addEventListener(
+            'click',
+            toggleOverlay.bind({ focusTrap, languageListOverlay })
+          );
+          close.addEventListener(
+            'click',
+            toggleOverlay.bind({ focusTrap, languageListOverlay })
+          );
+
+          // Return new context
+          return { languageSelector, close };
+        }}
+        beforeUnmount={context => {
+          if (context.languageSelector) {
+            context.languageSelector.removeEventListener(
+              'click',
+              toggleOverlay
+            );
+          }
+        }}
+      >
+        {story()}
+      </StoryWrapper>
+    );
+  })
+  .add('Default', EventAgendaPageExample)
+  .add('Pure ECL', EventAgendaPageEcl);
