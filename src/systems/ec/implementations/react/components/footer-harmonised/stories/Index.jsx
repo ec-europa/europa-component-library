24848550d145d5bc9929b15337e4625700b25d42
Romain Emery <emery.romain@gmail.com>
2019-10-02 15:48:54 +0200

    feat(harmonised footer): add component - INNO-1820 (#1318)
    * add harmonised footer
    
    * fix sizes
    
    * fix css

---
+++ src/systems/ec/implementations/react/components/footer-harmonised/stories/Index.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+import demoContentGroup1 from '@ecl/ec-specs-footer-harmonised/demo/data--group1';
+import demoContentGroup2 from '@ecl/ec-specs-footer-harmonised/demo/data--group2';
+
+import FooterHarmonised from '../src/FooterHarmonised';
+
+storiesOf('Components|Footers/Harmonised', module)
+  .addDecorator(withKnobs)
+  .add('group 1', () => (
+    <FooterHarmonised
+      {...demoContentGroup1}
+      className="ecl-footer-harmonised--group1"
+    />
+  ))
+  .add('group 2', () => (
+    <FooterHarmonised
+      {...demoContentGroup2}
+      className="ecl-footer-harmonised--group2"
+    />
+  ));
