045efa8b85dad8a8cf5fc0dee466f67d286c2f5d
Yannick Huard <mail@yannickhuard.com>
2019-10-09 14:34:20 +0200

    update core template

--- src/website/src/website-components/Link/Link.jsx
+++ src/website/src/website-components/Link/Link.jsx
@@ -6,16 +6,35 @@ import { Link } from 'react-router-dom';
 
 import styles from './Link.scss';
 
-const StyledLink = ({ className, ...props }) => (
-  <Link {...props} className={classnames(className, styles.link)} />
-);
+const StyledLink = ({ className, to, children, ...props }) => {
+  if (!to) {
+    return null;
+  }
+
+  if (to.indexOf('http') === 0) {
+    return (
+      <a {...props} href={to} className={classnames(className, styles.link)}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <Link {...props} to={to} className={classnames(className, styles.link)}>
+      {children}
+    </Link>
+  );
+};
 
 StyledLink.propTypes = {
   className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  to: PropTypes.string,
 };
 
 StyledLink.defaultProps = {
   className: '',
+  to: '',
 };
 
 export default StyledLink;
