e93de3523b9ec13667ba52dcabe943540a3adc10
emeryro <emery.romain@gmail.com>
2019-10-09 11:14:06 +0200

    init

---
+++ src/systems/ec/implementations/react/components/site-header-harmonised/src/SiteHeaderHarmonised.jsx
@@ -0,0 +1,232 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import Icon from '@ecl/ec-react-component-icon';
+import Link from '@ecl/ec-react-component-link';
+import SearchForm from '@ecl/ec-react-component-search-form';
+import { LanguageListOverlay } from '@ecl/ec-react-component-language-list';
+
+const SiteHeaderHarmonised = ({
+  logo,
+  logged,
+  loginToggle,
+  loginBox,
+  languageSelector,
+  searchToggle,
+  searchForm,
+  bannerTop,
+  banner,
+  className,
+  ...props
+}) => {
+  const classNames = classnames(className, 'ecl-site-header-harmonised');
+
+  // Logo props
+  const {
+    className: logoClassName,
+    href: logoHref,
+    alt: logoAlt,
+    title: logoTitle,
+    language: logoLanguage,
+    ...logoProps
+  } = logo;
+
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  const logoSrc = require(`@ecl/ec-resources-logo/logo--${logoLanguage}.svg`);
+
+  const loginLabel = logged
+    ? loginToggle.labelLogged
+    : loginToggle.labelNotLogged;
+  const loginHref = logged ? loginToggle.hrefLogged : loginToggle.hrefNotLogged;
+  const loginIcon = logged ? 'general--logged-in' : 'general--log-in';
+
+  return (
+    <header {...props} className={classNames}>
+      <div className="ecl-site-header-harmonised__container ecl-container">
+        <div className="ecl-site-header-harmonised__top">
+          <a
+            className="ecl-link ecl-link--standalone ecl-site-header-harmonised__logo-link"
+            href={logoHref}
+            aria-label={logoTitle}
+          >
+            <img
+              {...logoProps}
+              alt={logoAlt}
+              title={logoTitle}
+              className={classnames(
+                logoClassName,
+                'ecl-site-header-harmonised__logo-image'
+              )}
+              src={logoSrc}
+            />
+          </a>
+          <div className="ecl-site-header-harmonised__action">
+            {!!(loginToggle && loginBox) && (
+              <div className="ecl-site-header-harmonised__login-container">
+                <a
+                  className="ecl-link ecl-link--standalone ecl-site-header-harmonised__login-toggle"
+                  href={loginHref}
+                  data-ecl-login-toggle
+                  aria-controls={loginBox.id}
+                  aria-expanded="false"
+                >
+                  <Icon shape={loginIcon} size="s" />
+                  {loginLabel}
+                </a>
+                <div
+                  id={loginBox.id}
+                  className="ecl-site-header-harmonised__login-box"
+                  data-ecl-login-box
+                >
+                  <p className="ecl-site-header-harmonised__login-description">
+                    {loginBox.description}
+                  </p>
+                  <Link
+                    label={loginBox.label}
+                    href={loginBox.href}
+                    variant="standalone"
+                  />
+                </div>
+              </div>
+            )}
+            {languageSelector && (
+              <a
+                className="ecl-link ecl-link--standalone ecl-site-header-harmonised__language-selector"
+                href={languageSelector.href}
+                data-ecl-language-selector
+              >
+                <span className="ecl-site-header-harmonised__language-icon">
+                  <Icon shape="general--language" size="s" />
+                  <span className="ecl-site-header-harmonised__language-code">
+                    {languageSelector.code}
+                  </span>
+                </span>
+                {languageSelector.label}
+              </a>
+            )}
+            {!!(searchToggle && searchForm) && (
+              <div className="ecl-site-header-harmonised__search-container">
+                <a
+                  className="ecl-link ecl-link--standalone ecl-site-header-harmonised__search-toggle"
+                  href={searchToggle.href}
+                  data-ecl-search-toggle
+                  aria-controls={searchForm.id}
+                  aria-expanded="false"
+                >
+                  <Icon shape="general--search" size="s" />
+                  {searchToggle.label}
+                </a>
+                <SearchForm
+                  {...searchForm}
+                  className="ecl-site-header-harmonised__search"
+                  id={searchForm.id}
+                  data-ecl-search-form
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {bannerTop && (
+        <div className="ecl-site-header-harmonised__banner-top">
+          <div className="ecl-container">{bannerTop}</div>
+        </div>
+      )}
+      {banner && (
+        <div className="ecl-site-header-harmonised__banner">
+          <div className="ecl-container">{banner}</div>
+        </div>
+      )}
+
+      {!!(languageSelector && languageSelector.overlay) && (
+        <LanguageListOverlay {...languageSelector.overlay} hidden />
+      )}
+    </header>
+  );
+};
+
+SiteHeaderHarmonised.propTypes = {
+  logo: PropTypes.shape({
+    title: PropTypes.string,
+    alt: PropTypes.string,
+    language: PropTypes.string,
+    href: PropTypes.string,
+    className: PropTypes.string,
+  }),
+  logged: PropTypes.bool,
+  loginToggle: PropTypes.shape({
+    labelNotLogged: PropTypes.string,
+    hrefNotLogged: PropTypes.string,
+    labelLogged: PropTypes.string,
+    hrefLogged: PropTypes.string,
+  }),
+  loginBox: PropTypes.shape({
+    id: PropTypes.string,
+    description: PropTypes.string,
+    label: PropTypes.string,
+    href: PropTypes.string,
+  }),
+  languageSelector: PropTypes.shape({
+    href: PropTypes.string,
+    label: PropTypes.string,
+    code: PropTypes.string,
+    overlay: PropTypes.object,
+  }),
+  searchToggle: PropTypes.shape({
+    label: PropTypes.string,
+    href: PropTypes.string,
+  }),
+  searchForm: PropTypes.shape({
+    id: PropTypes.string,
+    textInputId: PropTypes.string,
+    inputLabel: PropTypes.string,
+    buttonLabel: PropTypes.string,
+  }),
+  bannerTop: PropTypes.string,
+  banner: PropTypes.string,
+  className: PropTypes.string,
+};
+
+SiteHeaderHarmonised.defaultProps = {
+  logo: {
+    title: '',
+    alt: '',
+    language: 'en',
+    href: '#',
+  },
+  logged: false,
+  loginToggle: {
+    labelNotLogged: '',
+    hrefNotLogged: '#',
+    labelLogged: '',
+    hrefLogged: '#',
+  },
+  loginBox: {
+    id: '',
+    description: '',
+    label: '',
+    href: '#',
+  },
+  languageSelector: {
+    href: '#',
+    label: '',
+    code: '',
+  },
+  searchToggle: {
+    label: '',
+    href: '',
+  },
+  searchForm: {
+    id: '',
+    textInputId: '',
+    inputLabel: '',
+    buttonLabel: '',
+  },
+  bannerTop: '',
+  banner: '',
+  className: '',
+};
+
+export default SiteHeaderHarmonised;
---
+++ src/systems/ec/implementations/vanilla/packages/ec-component-site-header-harmonised/ec-component-site-header-harmonised.js
@@ -0,0 +1,185 @@
+import { queryOne } from '@ecl/ec-base/helpers/dom';
+import createFocusTrap from 'focus-trap';
+
+export class SiteHeaderHarmonised {
+  static autoInit(root, { SITE_HEADER_CORE: defaultOptions = {} } = {}) {
+    const siteHeaderHarmonised = new SiteHeaderHarmonised(root, defaultOptions);
+    siteHeaderHarmonised.init();
+    // eslint-disable-next-line no-param-reassign
+    root.ECLSiteHeaderHarmonised = siteHeaderHarmonised;
+    return siteHeaderHarmonised;
+  }
+
+  constructor(
+    element,
+    {
+      languageLinkSelector = '[data-ecl-language-selector]',
+      languageListOverlaySelector = '[data-ecl-language-list-overlay]',
+      closeOverlaySelector = '[data-ecl-language-list-close]',
+      searchToggleSelector = '[data-ecl-search-toggle]',
+      searchFormSelector = '[data-ecl-search-form]',
+      loginToggleSelector = '[data-ecl-login-toggle]',
+      loginBoxSelector = '[data-ecl-login-box]',
+    } = {}
+  ) {
+    // Check element
+    if (!element || element.nodeType !== Node.ELEMENT_NODE) {
+      throw new TypeError(
+        'DOM element should be given to initialize this widget.'
+      );
+    }
+
+    this.element = element;
+
+    // Options
+    this.languageLinkSelector = languageLinkSelector;
+    this.languageListOverlaySelector = languageListOverlaySelector;
+    this.closeOverlaySelector = closeOverlaySelector;
+    this.searchToggleSelector = searchToggleSelector;
+    this.searchFormSelector = searchFormSelector;
+    this.loginToggleSelector = loginToggleSelector;
+    this.loginBoxSelector = loginBoxSelector;
+
+    // Private variables
+    this.languageSelector = null;
+    this.languageListOverlay = null;
+    this.close = null;
+    this.focusTrap = null;
+    this.searchToggle = null;
+    this.searchForm = null;
+    this.loginToggle = null;
+    this.loginBox = null;
+
+    // Bind `this` for use in callbacks
+    this.openOverlay = this.openOverlay.bind(this);
+    this.closeOverlay = this.closeOverlay.bind(this);
+    this.toggleOverlay = this.toggleOverlay.bind(this);
+    this.toggleSearch = this.toggleSearch.bind(this);
+    this.toggleLogin = this.toggleLogin.bind(this);
+  }
+
+  init() {
+    // Language list management
+    this.languageSelector = queryOne(this.languageLinkSelector);
+    this.languageListOverlay = queryOne(this.languageListOverlaySelector);
+    this.close = queryOne(this.closeOverlaySelector);
+
+    // Create focus trap
+    this.focusTrap = createFocusTrap(this.languageListOverlay, {
+      onDeactivate: this.closeOverlay,
+    });
+
+    this.languageSelector.addEventListener('click', this.toggleOverlay);
+    this.close.addEventListener('click', this.toggleOverlay);
+
+    // Search form management
+    this.searchToggle = queryOne(this.searchToggleSelector);
+    this.searchForm = queryOne(this.searchFormSelector);
+
+    this.searchToggle.addEventListener('click', this.toggleSearch);
+
+    // Login management
+    this.loginToggle = queryOne(this.loginToggleSelector);
+    this.loginBox = queryOne(this.loginBoxSelector);
+
+    this.loginToggle.addEventListener('click', this.toggleLogin);
+  }
+
+  destroy() {
+    if (this.languageSelector) {
+      this.languageSelector.removeEventListener('click', this.toggleOverlay);
+    }
+    if (this.focusTrap) {
+      this.focusTrap.deactivate();
+    }
+
+    if (this.close) {
+      this.close.removeEventListener('click', this.toggleOverlay);
+    }
+
+    if (this.searchToggle) {
+      this.searchToggle.removeEventListener('click', this.toggleSearch);
+    }
+
+    if (this.loginToggle) {
+      this.loginToggle.removeEventListener('click', this.toggleLogin);
+    }
+  }
+
+  openOverlay() {
+    this.languageListOverlay.removeAttribute('hidden');
+  }
+
+  closeOverlay() {
+    this.languageListOverlay.setAttribute('hidden', true);
+  }
+
+  toggleOverlay(e) {
+    if (!this.languageListOverlay || !this.focusTrap) return;
+
+    e.preventDefault();
+
+    if (this.languageListOverlay.hasAttribute('hidden')) {
+      this.openOverlay();
+      this.focusTrap.activate();
+    } else {
+      this.focusTrap.deactivate();
+    }
+  }
+
+  toggleSearch(e) {
+    if (!this.searchForm) return;
+
+    e.preventDefault();
+
+    // Get current status
+    const isExpanded =
+      this.searchToggle.getAttribute('aria-expanded') === 'true';
+
+    // Close other boxes
+    if (this.loginToggle.getAttribute('aria-expanded') === 'true') {
+      this.toggleLogin(e);
+    }
+
+    // Toggle the search form
+    this.searchToggle.setAttribute('aria-expanded', !isExpanded);
+    if (!isExpanded) {
+      this.searchForm.classList.add(
+        'ecl-site-header-harmonised__search--active'
+      );
+    } else {
+      this.searchForm.classList.remove(
+        'ecl-site-header-harmonised__search--active'
+      );
+    }
+  }
+
+  toggleLogin(e) {
+    if (!this.loginBox) return;
+
+    e.preventDefault();
+
+    // Get current status
+    const isExpanded =
+      this.loginToggle.getAttribute('aria-expanded') === 'true';
+
+    // Close other boxes
+    if (this.searchToggle.getAttribute('aria-expanded') === 'true') {
+      this.toggleSearch(e);
+    }
+
+    // Toggle the login box
+    this.loginToggle.setAttribute('aria-expanded', !isExpanded);
+    if (!isExpanded) {
+      this.loginBox.classList.add(
+        'ecl-site-header-harmonised__login-box--active'
+      );
+    } else {
+      this.loginBox.classList.remove(
+        'ecl-site-header-harmonised__login-box--active'
+      );
+    }
+  }
+}
+
+export default SiteHeaderHarmonised;
