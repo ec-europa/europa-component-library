747a131d7e738f8ef2ba32ff1ec8592bac14a0a6
Romain Emery <emery.romain@gmail.com>
2019-10-10 10:58:55 +0200

    feat(site-header-core): add component - INNO-1792 (#1322)
    * init
    
    * addcomponent
    
    * fix drone
    
    * cleanup
    
    * fix print
    
    * improve code
    
    * improvement
    
    * add website
    
    * fix js
    
    * small fixes
    
    * update website

---
+++ src/systems/ec/implementations/react/components/site-header-core/stories/Index.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import StoryWrapper from '@ecl/story-wrapper';
+import demoContentEn from '@ecl/ec-specs-site-header-core/demo/data--en';
+import demoContentFr from '@ecl/ec-specs-site-header-core/demo/data--fr';
+
+import SiteHeaderCore from '../src/SiteHeaderCore';
+
+storiesOf('Components|Site Headers/Core', module)
+  .addDecorator(story => (
+    <StoryWrapper
+      afterMount={() => {
+        if (!window.ECL) return {};
+
+        const components = window.ECL.autoInit();
+        return { components };
+      }}
+      beforeUnmount={context => {
+        if (context.components) {
+          context.components.forEach(c => c.destroy());
+        }
+      }}
+    >
+      {story()}
+    </StoryWrapper>
+  ))
+  .add('default', () => (
+    <SiteHeaderCore {...demoContentEn} data-ecl-auto-init="SiteHeaderCore" />
+  ))
+  .add('translated', () => (
+    <SiteHeaderCore {...demoContentFr} data-ecl-auto-init="SiteHeaderCore" />
+  ));
