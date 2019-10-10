da9fca13eda656662f2ee759dd04cd29052ec83b
Yannick Huard <mail@yannickhuard.com>
2019-10-10 14:06:33 +0200

    more content updates

---
+++ src/website/src/website-components/Typography/List.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+import styles from './List.scss';
+
+const List = ({ size, children }) => (
+  <ul
+    className={classnames(styles.list, {
+      [styles[`list--${size}`]]: size,
+    })}
+  >
+    {children}
+  </ul>
+);
+
+List.propTypes = {
+  size: PropTypes.string,
+  children: PropTypes.node,
+};
+
+List.defaultProps = {
+  size: '',
+  children: null,
+};
+
+export default List;
