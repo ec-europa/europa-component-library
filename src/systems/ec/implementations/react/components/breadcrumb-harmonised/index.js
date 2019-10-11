c3bea6291554057f46cc63762c6f3df42e443f21
emeryro <emery.romain@gmail.com>
2019-10-11 15:15:52 +0200

    add component

---
+++ src/systems/ec/implementations/react/components/breadcrumb-harmonised/index.js
@@ -0,0 +1,6 @@
+export default from './src/BreadcrumbHarmonised';
+export { BreadcrumbHarmonised } from './src/BreadcrumbHarmonised';
+export { BreadcrumbHarmonisedItem } from './src/BreadcrumbHarmonisedItem';
+export {
+  BreadcrumbHarmonisedEllipsis,
+} from './src/BreadcrumbHarmonisedEllipsis';
