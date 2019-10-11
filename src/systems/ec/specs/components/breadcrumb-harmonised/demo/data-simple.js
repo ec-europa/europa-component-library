429f5792b841585b3c343c6fcb498a9ef7f84bcf
Romain Emery <emery.romain@gmail.com>
2019-10-11 16:54:10 +0200

    feat(breadcrumb-harmonised): add component - INNO-1817 (#1340)
    * add component
    
    * fix size

---
+++ src/systems/ec/specs/components/breadcrumb-harmonised/demo/data-simple.js
@@ -0,0 +1,9 @@
+// Simple content for demo
+module.exports = {
+  items: [
+    { label: 'Home', href: '/example' },
+    { label: 'About the European Commission', href: '/example' },
+    { label: 'News' },
+  ],
+  label: 'You are here:',
+};
