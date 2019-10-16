0f80e5fd64ff7cbe25ea71e53f9900097ee611ca
Yannick Huard <mail@yannickhuard.com>
2019-10-16 11:39:52 +0200

    feat(search-page): change template in storybook - INNO-1861 (#1348)
    * feat(search-page): change template in storybook - INNO-1861
    
    * add harmonised groups
    
    * fix demo

--- src/systems/ec/implementations/react/templates/pages/search/examples/Default.jsx
+++ src/systems/ec/implementations/react/templates/pages/search/examples/Default.jsx
@@ -1,27 +1,70 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import data from '@ecl/ec-specs-search-page/demo/data';
+import getData from '@ecl/ec-specs-search-page/demo/data';
 
-import Breadcrumb, { BreadcrumbItem } from '@ecl/ec-react-component-breadcrumb';
+// import Breadcrumb, { BreadcrumbItem } from '@ecl/ec-react-component-breadcrumb';
 import SearchPage from '../src/SearchPage';
 
-export default () => {
-  const { items, ...breadcrumbProps } = data.breadcrumb;
-  const breadcrumb = (
-    <Breadcrumb {...breadcrumbProps}>
-      {items.map(item => (
-        <BreadcrumbItem {...item} key={item.label} />
-      ))}
-    </Breadcrumb>
-  );
-  data.pageHeader.breadcrumb = breadcrumb;
+class SearchExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.components = null;
+  }
 
-  return (
-    <SearchPage
-      siteHeader={data.siteHeader}
-      pageHeader={data.pageHeader}
-      footer={data.footer}
-    />
-  );
+  componentDidMount() {
+    if (!window.ECL) return;
+    this.components = window.ECL.autoInit();
+  }
+
+  componentDidUpdate() {
+    if (!window.ECL) return;
+
+    if (this.components) {
+      this.components.forEach(c => c.destroy());
+    }
+
+    this.components = window.ECL.autoInit();
+  }
+
+  componentWillUnmount() {
+    if (!window.ECL) return;
+
+    if (this.components) {
+      this.components.forEach(c => c.destroy());
+    }
+  }
+
+  render() {
+    const { template } = this.props;
+    const data = getData(template);
+
+    /*
+    const { items, ...breadcrumbProps } = data.breadcrumb;
+    const breadcrumb = (
+      <Breadcrumb {...breadcrumbProps}>
+        {items.map(item => (
+          <BreadcrumbItem {...item} key={item.label} />
+        ))}
+      </Breadcrumb>
+    );
+    data.pageHeader.breadcrumb = breadcrumb;
+    */
+
+    return (
+      <SearchPage
+        siteHeader={data.siteHeader}
+        // pageHeader={data.pageHeader}
+        footer={data.footer}
+        template={template}
+      />
+    );
+  }
+}
+
+SearchExample.propTypes = {
+  template: PropTypes.string.isRequired,
 };
+
+export default SearchExample;
