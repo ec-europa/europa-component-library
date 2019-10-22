5a652ffe14dc452af86c2c0abb68a3327ce80d98
Romain Emery <emery.romain@gmail.com>
2019-10-22 15:49:19 +0900

    feat(page-header-core): add component - INNO-1896 (#1365)
    * add component
    
    * quick fixes

---
+++ src/website/src/pages/ec/core-template/page-header/docs/ShowcaseBreadcrumb.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import BreadcrumbCore, {
+  BreadcrumbCoreItem,
+} from '@ecl/ec-react-component-breadcrumb-core';
+import breadcrumbContent from '@ecl/ec-specs-breadcrumb-core/demo/data';
+
+const { items, ...breadcrumbProps } = breadcrumbContent;
+const breadcrumb = (
+  <BreadcrumbCore {...breadcrumbProps}>
+    {items.map(item => (
+      <BreadcrumbCoreItem {...item} key={item.label} />
+    ))}
+  </BreadcrumbCore>
+);
+
+export default breadcrumb;
