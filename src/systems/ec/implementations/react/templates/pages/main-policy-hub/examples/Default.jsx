8f37d8373685f9437dc3eb8e6ba28468759fb543
Romain Emery <emery.romain@gmail.com>
2019-11-08 18:14:32 +0900

    feat(main-policy-hub): add content type - INNO-1879 (#1384)
    * init
    
    * update content type
    
    * update content type
    
    * fix ct and update border utilities
    
    * fix sizes
    
    * update
    
    * fix sizes
    
    * small fixes

---
+++ src/systems/ec/implementations/react/templates/pages/main-policy-hub/examples/Default.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import getData from '@ecl/ec-specs-main-policy-hub-page/demo/data';
+
+import MainPolicyHubPage from '../src/MainPolicyHubPage';
+
+class MainPolicyHubExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.components = null;
+  }
+
+  componentDidMount() {
+    if (!window.ECL) return;
+    this.components = window.ECL.autoInit();
+  }
+
+  componentDidUpdate() {
+    if (!window.ECL) return;
+
+    if (this.components) {
+      this.components.forEach(c => c.destroy());
+    }
+
+    this.components = window.ECL.autoInit();
+  }
+
+  componentWillUnmount() {
+    if (!window.ECL) return;
+
+    if (this.components) {
+      this.components.forEach(c => c.destroy());
+    }
+  }
+
+  render() {
+    const { template } = this.props;
+    const data = getData(template);
+
+    return (
+      <MainPolicyHubPage
+        siteHeader={data.siteHeader}
+        footer={data.footer}
+        template={template}
+      />
+    );
+  }
+}
+
+MainPolicyHubExample.propTypes = {
+  template: PropTypes.string.isRequired,
+};
+
+export default MainPolicyHubExample;
