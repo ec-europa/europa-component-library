006e26d160cb82dbe855560e3d23565d47a0248d
emeryro <emery.romain@gmail.com>
2019-12-02 14:40:37 +0900

    add specs

--- src/systems/ec/implementations/react/components/tag/stories/Index.jsx
+++ src/systems/ec/implementations/react/components/tag/stories/Index.jsx
@@ -3,20 +3,27 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
+import demoDataLink from '@ecl/ec-specs-tag/demo/data--link';
+import demoDataButton from '@ecl/ec-specs-tag/demo/data--button';
+import demoDataRemovable from '@ecl/ec-specs-tag/demo/data--removable';
+
 import Tag from '../src/Tag';
 
 storiesOf('Components|Tag', module)
   .addDecorator(withKnobs)
   .add('as a link', () => (
-    <Tag label={text('Label', 'Link tag')} href={text('Link', '/example')} />
+    <Tag
+      label={text('Label', demoDataLink.label)}
+      href={text('Link', demoDataLink.href)}
+    />
   ))
   .add('as a button', () => (
-    <Tag label={text('Label', 'Button tag')} type="button" />
+    <Tag label={text('Label', demoDataButton.label)} type="button" />
   ))
   .add('removable', () => (
     <Tag
-      label={text('Label', 'Removable tag')}
+      label={text('Label', demoDataRemovable.label)}
       variant="removable"
-      dismissButtonLabel="Dismiss"
+      dismissButtonLabel={demoDataRemovable.dismissButtonLabel}
     />
   ));
