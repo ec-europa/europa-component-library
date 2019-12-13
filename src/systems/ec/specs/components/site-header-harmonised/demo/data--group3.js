4fc1d78a548ee7b085b19dd509916438865c4e5f
emeryro <emery.romain@gmail.com>
2019-12-13 09:49:56 +0900

    update display

--- src/systems/ec/specs/components/site-header-harmonised/demo/data--group3.js
+++ src/systems/ec/specs/components/site-header-harmonised/demo/data--group3.js
@@ -4,7 +4,7 @@ module.exports = {
     alt: 'Site logo',
     href: '/example',
     language: 'en',
-    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
+    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/placeholder.svg',
   },
   siteName: 'Site name',
 };
