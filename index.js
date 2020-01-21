eab40f737c3006db1fd28064ec0090971f4abff0
Kalin Chernev <kalin.chernev@gmail.com>
2020-01-21 09:25:02 +0100

    Add exceptions

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ const fs = require("fs");
 const path = require("path");
 const glob = require("glob");
 
-// options is optional
-glob.sync("**/*.html").forEach(file => {
-  fs.writeFileSync(
-    path.resolve(file),
-    `<!DOCTYPE html>
+const v1 = "v1.15.0";
+const root = "https://ec.europa.eu/component-library/";
+
+const createRedirect = (target = root) => {
+  return `<!DOCTYPE html>
 <meta charset="utf-8">
 <title>Redirecting ...</title>
-<meta http-equiv="refresh" content="0; URL=https://ec.europa.eu/component-library/">
-<link rel="canonical" href="https://ec.europa.eu/component-library/">
-`
+<meta http-equiv="refresh" content="0; URL=${target}">
+<link rel="canonical" href="${target}">
+`;
+};
+
+const exceptions = ["eu-component-site-header", "eu-component-footer"];
+
+glob
+  .sync(`**/!(${exceptions.join("|")}).html`)
+  .forEach(file => fs.writeFileSync(path.resolve(file), createRedirect()));
+
+glob
+  .sync(`eu/components/detail/*(${exceptions.join("|")}).html`)
+  .forEach(component =>
+    fs.writeFileSync(
+      path.resolve(component),
+      createRedirect(`${root}${v1}/${component.replace(".html", "/")}`)
+    )
   );
-});
