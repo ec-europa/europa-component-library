6c07b2192b7c898f0366e0b32d6d52166f2d2c7b
Alexis Gaillard <a@bypopcorn.com>
2020-01-28 16:32:18 +0100

    Force svg4everybody on all browser to process SVG icons for exportation - FRONT-10

--- src/systems/ec/implementations/react/storybook/.storybook/ECL.jsx
+++ src/systems/ec/implementations/react/storybook/.storybook/ECL.jsx
@@ -1,4 +1,7 @@
 import 'iframe-resizer/js/iframeResizer.contentWindow.min.js';
 import svg4everybody from 'svg4everybody/dist/svg4everybody.min';
 
-svg4everybody();
+svg4everybody({
+  nosvg: true, // shiv <svg> and <use> elements and use image fallbacks
+  polyfill: true, // polyfill <use> elements for External Content
+});
