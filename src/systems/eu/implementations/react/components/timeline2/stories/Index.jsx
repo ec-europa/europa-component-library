9326f89d14bf15d63c0f255d194acebdc8f3aca6
Kalin Chernev <kalinchernev@users.noreply.github.com>
2020-03-13 14:51:24 +0200

    feat(lib): provide update and destroy methods on ECL.autoInit()… (#1544)
    * Remove unneccessary dependency
    
    * Provide update and destroy methods in ECL.autoInit()
    
    * Rerun CI with a new commit
    
    * Remove unneccessary dependency
    
    * feat(lib): update ECL.autoInit() for eu system - FRONT-33

--- src/systems/eu/implementations/react/components/timeline2/stories/Index.jsx
+++ src/systems/eu/implementations/react/components/timeline2/stories/Index.jsx
@@ -36,8 +36,8 @@ storiesOf('Components/Timeline', module)
       afterMount={() => {
         if (!window.ECL) return {};
 
-        const components = window.ECL.autoInit();
-        return { components };
+        const autoinit = window.ECL.autoInit();
+        return { components: autoinit.components };
       }}
       beforeUnmount={context => {
         if (context.components) {
