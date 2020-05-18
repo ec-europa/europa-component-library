8415e5df1a5b27b89016261380f5f0093e2e8fe8
emeryro <emery.romain@gmail.com>
2020-05-18 11:06:49 +0200

    init select multiple

--- src/systems/ec/implementations/react/components/select/src/Select.jsx
+++ src/systems/ec/implementations/react/components/select/src/Select.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import Icon from '@ecl/ec-react-component-icon';
+import Checkbox from '@ecl/ec-react-component-checkbox';
 
 const Select = ({
   disabled,
@@ -19,6 +21,9 @@ const Select = ({
   required,
   requiredText,
   width,
+  multiple,
+  multiplePlaceholder,
+  multipleSearchText,
   className,
   ...props
 }) => {
@@ -77,6 +82,13 @@ const Select = ({
           className={classNames}
           disabled={disabled}
           required={required}
+          {...(multiple
+            ? {
+                'data-ecl-select-multiple': true,
+                'data-ecl-select-default': { multiplePlaceholder },
+                'data-ecl-select-search': { multipleSearchText },
+              }
+            : {})}
         >
           {options.map(option => (
             <option key={option.label} value={option.value}>
@@ -91,6 +103,60 @@ const Select = ({
             size="s"
             className="ecl-select__icon-shape"
           />
+        </div>
+      </div>
+
+      {/* temporary markup */}
+      <div className="ecl-select__multiple">
+        <div
+          className={classnames('ecl-select__container', {
+            [`ecl-select__container--${width}`]: width,
+          })}
+        >
+          <input
+            id="select-multiple-toggle"
+            type="text"
+            className="ecl-select ecl-select__multiple-toggle"
+            placeholder={multiplePlaceholder}
+            readOnly
+          />
+
+          <div className="ecl-select__icon">
+            <Icon
+              shape="ui--corner-arrow"
+              transform="rotate-180"
+              size="s"
+              className="ecl-select__icon-shape"
+            />
+          </div>
+        </div>
+        <div
+          className={classnames(
+            'ecl-select__container ecl-select__multiple-dropdown',
+            {
+              [`ecl-select__container--${width}`]: width,
+            }
+          )}
+        >
+          <input
+            placeholder={multipleSearchText}
+            id="select-multiple-search"
+            type="search"
+            className={classnames('ecl-text-input', {
+              [`ecl-text-input--${width}`]: width,
+            })}
+          />
+          <Checkbox id="select-multiple-all" label="Select all" />
+          <Checkbox id="select-multiple-item-1" label="Belgium" />
+          <Checkbox id="select-multiple-item-2" label="France" />
+          <Checkbox id="select-multiple-item-3" label="Luxembourg" />
+          <Checkbox id="select-multiple-item-4" label="Germany" />
+          <Checkbox id="select-multiple-item-5" label="Bulgaria" />
+          <Checkbox id="select-multiple-item-6" label="Italy" />
+          <Checkbox id="select-multiple-item-7" label="Romania" />
+          <Checkbox id="select-multiple-item-8" label="Greece" />
+          <Checkbox id="select-multiple-item-9" label="Hungary" />
+          <Checkbox id="select-multiple-item-10" label="Portugal" />
         </div>
       </div>
     </div>
@@ -117,6 +183,9 @@ Select.propTypes = {
     })
   ),
   width: PropTypes.string,
+  multiple: PropTypes.bool,
+  multiplePlaceholder: PropTypes.string,
+  multipleSearchText: PropTypes.string,
   className: PropTypes.string,
 };
 
@@ -134,6 +203,9 @@ Select.defaultProps = {
   requiredText: '',
   options: [],
   width: '',
+  multiple: false,
+  multiplePlaceholder: '',
+  multipleSearchText: '',
   className: '',
 };
 
--- src/systems/ec/implementations/react/components/select/stories/Index.jsx
+++ src/systems/ec/implementations/react/components/select/stories/Index.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 
-import demoContent from '@ecl/ec-specs-select/demo/data';
+import demoContentSingle from '@ecl/ec-specs-select/demo/data--single';
+import demoContentMultiple from '@ecl/ec-specs-select/demo/data--multiple';
 
 import Select from '../src/Select';
 
@@ -13,16 +14,17 @@ export default {
 
 export const Default = () => (
   <Select
-    id="select-id"
-    options={demoContent.options}
-    label={text('Label', demoContent.label)}
-    helperText={text('Helper text', "This is the input's helper text.")}
+    {...demoContentSingle}
+    id="select-default"
+    options={demoContentSingle.options}
+    label={text('Label', demoContentSingle.label)}
+    helperText={text('Helper text', demoContentSingle.helperText)}
     invalid={boolean('Invalid', false)}
-    invalidText={text('Invalid text', 'This is the error message')}
+    invalidText={text('Invalid text', demoContentSingle.invalidText)}
     disabled={boolean('Disabled', false)}
     required={boolean('Required', true)}
-    requiredText={text('Required text', '*')}
-    optionalText={text('Optional text', ' (optional)')}
+    requiredText={text('Required text', demoContentSingle.requiredText)}
+    optionalText={text('Optional text', demoContentSingle.optionalText)}
     width={select(
       'Width',
       {
@@ -30,13 +32,57 @@ export const Default = () => (
         medium: 'm',
         large: 'l',
       },
-      demoContent.width
+      'm'
     )}
   />
 );
 
 Default.story = {
   name: 'default',
+
+  parameters: {
+    knobs: {
+      escapeHTML: false,
+    },
+  },
+};
+
+export const Multiple = () => (
+  <Select
+    {...demoContentMultiple}
+    id="select-multiple"
+    options={demoContentMultiple.options}
+    label={text('Label', demoContentMultiple.label)}
+    helperText={text('Helper text', demoContentMultiple.helperText)}
+    invalid={boolean('Invalid', false)}
+    invalidText={text('Invalid text', demoContentMultiple.invalidText)}
+    disabled={boolean('Disabled', false)}
+    required={boolean('Required', true)}
+    requiredText={text('Required text', demoContentMultiple.requiredText)}
+    optionalText={text('Optional text', demoContentMultiple.optionalText)}
+    width={select(
+      'Width',
+      {
+        small: 's',
+        medium: 'm',
+        large: 'l',
+      },
+      'm'
+    )}
+    multiplePlaceholder={text(
+      'Placeholder (multiple)',
+      demoContentMultiple.multiplePlaceholder
+    )}
+    multipleSearch
+    text={text(
+      'Search text (multiple)',
+      demoContentMultiple.multipleSearchText
+    )}
+  />
+);
+
+Multiple.story = {
+  name: 'multiple',
 
   parameters: {
     knobs: {
