2438fc14ea2870922f7244d432374722a2b81989
Kalin Chernev <kalin.chernev@gmail.com>
2020-05-19 10:57:43 +0200

    Add virtual input text field

--- src/systems/ec/implementations/vanilla/packages/ec-component-form-select/ec-component-form-select.js
+++ src/systems/ec/implementations/vanilla/packages/ec-component-form-select/ec-component-form-select.js
@@ -25,8 +25,9 @@ export class Select {
   constructor(
     element,
     {
+      originalId = 'select-multiple',
+      copyWrapperCssSelector = 'ecl-select__multiple',
       originalDataSelector = 'data-ecl-select-multiple',
-      copyCssSelector = 'ecl-select__multiple',
       defaultPlaceholderSelector = 'data-ecl-select-default',
       searchPlaceholderSelector = 'data-ecl-select-search',
     } = {}
@@ -41,15 +42,19 @@ export class Select {
     this.element = element;
 
     // Options
+    this.originalId = originalId;
+    this.copyWrapperCssSelector = copyWrapperCssSelector;
     this.originalDataSelector = originalDataSelector;
-    this.copyCssSelector = copyCssSelector;
     this.defaultPlaceholderSelector = defaultPlaceholderSelector;
     this.searchPlaceholderSelector = searchPlaceholderSelector;
 
     // Private variables
     this.id = 0;
     this.original = null;
-    this.copy = null;
+    this.selectIcon = null;
+    this.copyWrapper = null;
+    this.copyInputWrapper = null;
+    this.copyInput = null;
     this.defaultPlaceholder = null;
     this.searchPlaceholder = null;
 
@@ -60,7 +65,7 @@ export class Select {
    * Initialise component.
    */
   init() {
-    this.id += 1;
+    this.id += 1; // Ensures unique elements in the markup even when multiple instances.
     this.defaultPlaceholder = this.element.getAttribute(
       this.defaultPlaceholderSelector
     );
@@ -69,12 +74,32 @@ export class Select {
     );
 
     this.original = queryOne(`[${this.originalDataSelector}]`);
-    this.copy = document.createElement('div');
-    this.copy.classList.add(this.copyCssSelector);
-    this.copy.setAttribute('id', `${this.copyCssSelector}-${this.id}`);
+    if (this.original.nextSibling.classList.contains('ecl-select__icon')) {
+      this.selectIcon = this.original.nextSibling;
+    }
+
+    this.copyWrapper = document.createElement('div');
+    this.copyWrapper.classList.add(this.copyWrapperCssSelector);
+    this.copyWrapper.setAttribute('id', `${this.originalId}-${this.id}`);
+
+    this.copyInputWrapper = document.createElement('div');
+    this.copyInputWrapper.classList.add(
+      'ecl-select__container',
+      'ecl-select__container--m'
+    );
+    this.copyWrapper.append(this.copyInputWrapper);
+
+    this.copyInput = document.createElement('input');
+    this.copyInput.classList.add('ecl-select', 'ecl-select__multiple-toggle');
+    this.copyInput.setAttribute('id', `select-multiple-toggle-${this.id}`);
+    this.copyInput.setAttribute('type', 'text');
+    this.copyInput.setAttribute('placeholder', this.defaultPlaceholder || '');
+    this.copyInput.setAttribute('readonly', true);
+    this.copyInputWrapper.append(this.copyInput);
+    this.copyInputWrapper.append(this.selectIcon);
 
     this.original.parentNode.parentNode.insertBefore(
-      this.copy,
+      this.copyWrapper,
       this.original.parentNode.nextSibling
     );
   }
@@ -83,8 +108,8 @@ export class Select {
    * Destroy component.
    */
   destroy() {
-    if (this.copy) {
-      this.copy.remove();
+    if (this.copyWrapper) {
+      this.copyWrapper.remove();
     }
   }
 }
