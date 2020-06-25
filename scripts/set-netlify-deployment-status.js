8d6a878f231abecf860472635e5692eb3b507d4d
Kalin Chernev <kalin.chernev@gmail.com>
2020-06-25 15:28:09 +0200

    Revert changes to custom preview deployment script

--- scripts/set-netlify-deployment-status.js
+++ scripts/set-netlify-deployment-status.js
@@ -5,41 +5,35 @@ const fetch = require('node-fetch');
 const run = async () => {
   const NETLIFY_API = 'https://api.netlify.com/api/v1';
 
-  const { NETLIFY_SITE_ID, NETLIFY_AUTH_TOKEN } = process.env;
-  const isRunningOnGithub = process.env.GITHUB_ACTIONS;
-  // GITHUB_TOKEN is provided by default on Github Actions, no need to set in manually anywwhere.
-  const token = isRunningOnGithub
-    ? process.env.GITHUB_TOKEN
-    : process.env.GH_TOKEN;
-  const buildNumber = isRunningOnGithub
-    ? process.env.GITHUB_RUN_ID
-    : process.env.DRONE_BUILD_NUMBER;
-  const repo = isRunningOnGithub
-    ? process.env.GITHUB_REPOSITORY
-    : process.env.DRONE_REPO;
-  const commitSha = isRunningOnGithub
-    ? process.env.COMMIT_SHA
-    : process.env.DRONE_COMMIT_SHA;
-  // Use Checks API if to build path to a given run
-  // @see https://developer.github.com/v3/checks/runs/
-  let targetUrl = isRunningOnGithub ? '' : process.env.DRONE_BUILD_LINK;
-  const context = isRunningOnGithub
-    ? 'github-actions/netlify-preview'
-    : 'drone/netlify-preview';
+  const {
+    GH_TOKEN,
+    DRONE_BUILD_NUMBER,
+    DRONE_REPO,
+    DRONE_COMMIT_SHA,
+    DRONE_BUILD_LINK,
+    NETLIFY_SITE_ID,
+    NETLIFY_AUTH_TOKEN,
+  } = process.env;
 
-  if (!buildNumber) {
-    console.error('Missing information about build number.');
+  if (!DRONE_BUILD_NUMBER) {
+    console.info('Missing information about build number.');
     return;
   }
 
-  if (!token) {
+  if (!GH_TOKEN) {
     console.info("Won't be able to communicate Netlify deployment to Github.");
-    console.error('Auth token is required!');
+    console.info('GH_TOKEN environment variable is required!');
     return;
   }
 
-  if (!repo || !commitSha) {
-    console.error('Missing information about repository or commit');
+  if (!DRONE_REPO || !DRONE_COMMIT_SHA) {
+    console.info(
+      'Current script depends on Drone CI 0.8 environment variables.'
+    );
+    console.info(
+      'Please see https://0-8-0.docs.drone.io/environment-reference'
+    );
+    console.log('Required: DRONE_REPO, DRONE_COMMIT_SHA');
     return;
   }
 
@@ -61,36 +55,9 @@ const run = async () => {
 
     const deployments = await deploymentsResponse.json();
 
-    const pattern = isRunningOnGithub
-      ? `Github Actions Run: ${buildNumber}`
-      : `Drone build: ${buildNumber}`;
     const currentDeployment = deployments.find(
-      deployment => deployment.title === pattern
+      deployment => deployment.title === `Drone build: ${DRONE_BUILD_NUMBER}`
     );
-
-    if (!currentDeployment) {
-      payload = {
-        context,
-        state: 'failure',
-        description: 'No deployment on Netlify matching current pull request.',
-      };
-
-      await fetch(
-        `https://api.github.com/repos/${repo}/statuses/${commitSha}`,
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Accept-Charset': 'utf-8',
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify(payload),
-        }
-      );
-
-      return;
-    }
 
     const siteDeploymentResponse = await fetch(
       `${NETLIFY_API}/sites/${NETLIFY_SITE_ID}/deploys/${currentDeployment.id}`,
@@ -107,36 +74,35 @@ const run = async () => {
 
     const siteDeployment = await siteDeploymentResponse.json();
 
-    if (siteDeployment && siteDeployment.deploy_ssl_url) {
-      targetUrl = siteDeployment.deploy_ssl_url;
-    }
-
     payload = {
-      context,
       state: 'success',
-      target_url: targetUrl,
+      target_url: siteDeployment.deploy_ssl_url,
       description: 'Preview ready!',
+      context: 'drone/netlify',
     };
   } catch (error) {
     payload = {
-      context,
       state: 'error',
-      target_url: targetUrl,
+      target_url: DRONE_BUILD_LINK,
       description: 'Could not get data about Netlify deployment.',
+      context: 'drone/netlify',
     };
   }
 
   // @see https://developer.github.com/v3/repos/statuses
-  await fetch(`https://api.github.com/repos/${repo}/statuses/${commitSha}`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-Charset': 'utf-8',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(payload),
-  });
+  await fetch(
+    `https://api.github.com/repos/${DRONE_REPO}/statuses/${DRONE_COMMIT_SHA}`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-Charset': 'utf-8',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${GH_TOKEN}`,
+      },
+      body: JSON.stringify(payload),
+    }
+  );
 
   console.log('Status on pull request successfully updated!');
 };
