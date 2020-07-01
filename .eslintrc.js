c24a7dbac7916675e28f5c6884850772019ab9b8
Kalin Chernev <kalin.chernev@gmail.com>
2020-07-01 15:15:43 +0200

    test caching and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,7 @@ module.exports = {
   env: {
     node: true,
   },
-  rules: {
-    'class-methods-use-this': 'off',
-    'no-underscore-dangle': 'off',
-    'no-param-reassign': 'off',
-    'react/jsx-fragments': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    // Temporary disable rule until https://github.com/yannickcr/eslint-plugin-react/issues/2427 is fixed
-    'react/jsx-curly-brace-presence': 'off',
-    'import/no-extraneous-dependencies': 'off',
-  },
+  rules: {},
   overrides: [
     {
       files: '**/scripts/**/*.js',
