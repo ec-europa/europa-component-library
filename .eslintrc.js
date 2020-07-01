241372c7fd0fe9bc401ab6905a0dd552c4ba8b76
Kalin Chernev <kalin.chernev@gmail.com>
2020-07-01 15:29:33 +0200

    Put back

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,16 @@ module.exports = {
   env: {
     node: true,
   },
-  rules: {},
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    'react/jsx-fragments': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    // Temporary disable rule until https://github.com/yannickcr/eslint-plugin-react/issues/2427 is fixed
+    'react/jsx-curly-brace-presence': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
   overrides: [
     {
       files: '**/scripts/**/*.js',
