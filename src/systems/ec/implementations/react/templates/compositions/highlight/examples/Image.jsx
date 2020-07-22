3fdec7e21e1d8cc0e363b6bfa37076c19b3afbff
Romain Emery <emery.romain@gmail.com>
2020-07-22 08:03:30 +0200

    feat(highlight-composition): add highlight composition - FRONT-1284 (#1627)
    Co-authored-by: Alexis Gaillard <a@bypopcorn.com>

---
+++ src/systems/ec/implementations/react/templates/compositions/highlight/examples/Image.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import demoContentImage from '@ecl/ec-specs-highlight/demo/data--image';
+import Highlight from '../src/Highlight';
+
+export default () => <Highlight {...demoContentImage} />;
