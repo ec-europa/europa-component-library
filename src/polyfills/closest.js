094319aa9f0b785da189f7d29e62d25a2d1b701a
Kalin Chernev <kalin.chernev@gmail.com>
2020-07-23 11:58:44 +0200

    feat(polyfills): enable centralized management - FRONT-1187

---
+++ src/polyfills/closest.js
@@ -0,0 +1,16 @@
+/* global Element */
+
+// Polyfill for closest (support for IE11)
+if (!Element.prototype.matches)
+  Element.prototype.matches = Element.prototype.msMatchesSelector;
+if (!Element.prototype.closest)
+  Element.prototype.closest = function poly(selector) {
+    let el = this;
+    while (el) {
+      if (el.matches(selector)) {
+        return el;
+      }
+      el = el.parentElement;
+    }
+    return null;
+  };
--- src/systems/ec/implementations/vanilla/packages/ec-component-menu/ec-component-menu.js
+++ src/systems/ec/implementations/vanilla/packages/ec-component-menu/ec-component-menu.js
@@ -4,21 +4,6 @@ import { queryOne, queryAll } from '@ecl/ec-base/helpers/dom';
 import isMobile from 'mobile-device-detect';
 import SwipeListener from 'swipe-listener';
 
-// Polyfill for closest (support for IE11)
-if (!Element.prototype.matches)
-  Element.prototype.matches = Element.prototype.msMatchesSelector;
-if (!Element.prototype.closest)
-  Element.prototype.closest = function poly(selector) {
-    let el = this;
-    while (el) {
-      if (el.matches(selector)) {
-        return el;
-      }
-      el = el.parentElement;
-    }
-    return null;
-  };
-
 /**
  * @param {HTMLElement} element DOM element for component instantiation and scope
  * @param {Object} options
