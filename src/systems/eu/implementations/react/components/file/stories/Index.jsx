9545ff50ec2e1b7829656545f1c47dab12f402a5
Romain Emery <emery.romain@gmail.com>
2020-07-29 15:31:17 +0200

    fix(file): allow array of meta - FRONT-1309 (#1642)
    * allow array of meta
    
    * fix website

--- src/systems/eu/implementations/react/components/file/stories/Index.jsx
+++ src/systems/eu/implementations/react/components/file/stories/Index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { withKnobs, text, select, array } from '@storybook/addon-knobs';
 import StoryWrapper from '@ecl/story-wrapper';
 import demoContent from '@ecl/eu-specs-file/demo/data--without-translation';
 import demoContentTranslation from '@ecl/eu-specs-file/demo/data--with-translation';
@@ -82,7 +82,9 @@ WithTranslation.story = {
 };
 
 export const Thumbnail = () => {
-  const meta = text('Meta', demoContentThumbnail.detailMeta);
+  const meta = Array.isArray(demoContentThumbnail.detailMeta)
+    ? array('Meta (comma separated)', demoContentThumbnail.detailMeta)
+    : text('Meta', demoContentThumbnail.detailMeta);
   const title = text('File title', demoContentThumbnail.title);
   const description = text('Description', demoContentThumbnail.description);
 
