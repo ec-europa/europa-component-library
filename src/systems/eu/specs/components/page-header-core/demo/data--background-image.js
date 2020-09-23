478cd2718d2a00fe53ebc4fab4ce7ac62ad8168e
Romain Emery <emery.romain@gmail.com>
2020-09-23 08:43:08 +0200

    feat(page-header): add background image variant - FRONT-1333 (#1677)
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

---
+++ src/systems/eu/specs/components/page-header-core/demo/data--background-image.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Page title',
+  description:
+    'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',
+  meta: '<span class="ecl-u-type-uppercase">Meta info</span>',
+  image: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image8.jpg',
+};
