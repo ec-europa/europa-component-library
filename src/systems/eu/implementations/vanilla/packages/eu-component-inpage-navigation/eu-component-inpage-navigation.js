761b407db72e41a2b8e2fb6e3f5f99c288b55218
Paulo Gingão <pgingao@gmail.com>
2020-09-24 15:34:54 +0100

    FRONT-1248 adding fix to EU, and getting rid of unnecessary class 'ecl-inpage-navigation__heading'

--- src/systems/eu/implementations/vanilla/packages/eu-component-inpage-navigation/eu-component-inpage-navigation.js
+++ src/systems/eu/implementations/vanilla/packages/eu-component-inpage-navigation/eu-component-inpage-navigation.js
@@ -51,6 +51,7 @@ export class InpageNavigation {
       spyClass = 'ecl-inpage-navigation__item--active',
       spyTrigger = '[data-ecl-inpage-navigation-trigger-current]',
       attachClickListener = true,
+      contentClass = 'ecl-inpage-navigation__heading--active',
     } = {}
   ) {
     // Check element
@@ -72,6 +73,7 @@ export class InpageNavigation {
     this.spyOffset = spyOffset;
     this.spyClass = spyClass;
     this.spyTrigger = spyTrigger;
+    this.contentClass = contentClass;
     this.gumshoe = null;
     this.observer = null;
     this.stickyObserver = null;
@@ -111,6 +113,7 @@ export class InpageNavigation {
   initScrollSpy() {
     this.gumshoe = new Gumshoe(this.spySelector, {
       navClass: this.spyClass,
+      contentClass: this.contentClass,
       offset: this.spyOffset,
       reflow: true,
     });
