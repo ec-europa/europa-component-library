0ccf26c7a2ab7f1bfd4f0861cfd4a85ac23869be
Paulo Gingão <pgingao@gmail.com>
2020-09-30 10:08:17 +0100

    FRONT-1335 adding meaningful titles on timeline items

--- src/systems/ec/specs/components/timeline/demo/data.js
+++ src/systems/ec/specs/components/timeline/demo/data.js
@@ -16,201 +16,204 @@ module.exports = {
     {
       id: '0',
       label: '13 September 2017',
-      title: 'Item title',
+      title: 'State of the Union speech',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">President Juncker\'s State of the Union speech</a>',
     },
     {
       id: '1',
       label: '28-29 September 2017',
-      title: 'Item title',
+      title: 'Informal Summit',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Informal Digital Summit, Tallinn</a>',
     },
     {
       id: '2',
       label: '14 November 2017',
-      title: 'Item title',
+      title: 'Opening the Leaders meeting discussion in Gothenburg, Sweden',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Strengthening European identity through education and culture: European Commission\'s contribution to the Leaders\' meeting, Gothenburg, Sweden</a>',
     },
     {
       id: '3',
       label: '17 November 2017',
-      title: 'Item title',
+      title: 'Social Summit is open',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Social Summit in Gothenburg, Sweden</a>',
     },
     {
       id: '4',
       label: '6 December 2017',
-      title: 'Item title',
+      title: 'The proposals event',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Economic and Monetary Union package of proposals</a>',
     },
     {
       id: '5',
       label: '14-15 December 2017',
-      title: 'Item title',
+      title: 'Migration issues meeting',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">EU Leaders\' meeting on migration, Brussels</a>',
     },
     {
       id: '6',
       label: '15 December 2017',
-      title: 'Item title',
+      title: 'The Euro Summmit',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Euro Summit</a>',
     },
     {
       id: '7',
       label: '6 February 2018',
+      title: 'Balkans strategy discussion',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">EU-Western Balkans Strategy</a>',
     },
     {
       id: '8',
       label: '14 February 2018',
-      title: 'Item title',
+      title: 'Institutional issues discussion meeting',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Multiannual Financial Framework</a> and <a href="/example" class="ecl-link ecl-link--standalone">institutional issues</a> - enhancing efficiency at the helm of the European Union',
     },
     {
       id: '9',
       label: '23 February 2018',
-      title: 'Item title',
+      title: 'European council conclusions',
       content:
-        '<a href="/example" class="ecl-link ecl-link--standalone">Informal European Council</a>',
+        '<a href="/example" class="ecl-link ecl-link--standalone">Informal European Council traineeship</a>',
     },
     {
       id: '10',
       label: '22-23 March 2018',
-      title: 'Item title',
+      title: 'Foreign Relations Summit',
       content:
-        '<a href="/example" class="ecl-link ecl-link--standalone">European Council</a>',
+        '<a href="/example" class="ecl-link ecl-link--standalone">European council on foreign relations</a>',
     },
     {
       id: '11',
       label: '2 May 2018',
+      title: 'Foreign Relations Summit',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Commission proposal: Long-term budget post-2020</a>',
     },
     {
       id: '12',
       label: '15 May 2018',
-      title: 'Item title',
+      title: 'The Informal Leaders meeting',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">European Commission’s contribution to the Informal Leaders\' meeting</a>, Sofia, Bulgaria (16 May 2018)',
     },
     {
       id: '13',
       label: '17 May 2018',
-      title: 'Item title',
+      title: 'The Balkans Summit',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">EU-Western Balkans Summit</a> (Sofia, Bulgaria)',
     },
     {
       id: '14',
       label: '23 May 2018',
-      title: 'Item title',
+      title: 'Adopting the euro - Open Discussion',
       content:
         'Progress by Member States in meeting the conditions for adopting the euro - <a href="/example" class="ecl-link ecl-link--standalone">reports</a> by the European Commission and ECB',
     },
     {
       id: '15',
       label: '23 May 2018',
-      title: 'Item title',
+      title: 'Opening the European Semester',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">European Semester</a>: European Commission publishes economic policy guidance for Member States',
     },
     {
       id: '16',
       label: '29 May - 14 June 2018',
-      title: 'Item title',
+      title: 'Policy Guidance for Member States Publications',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">EU budget for the future</a>: European Commission\'s legislative proposals for spending programmes',
     },
     {
       id: '17',
       label: '28-29 June 2018',
-      title: 'Item title',
+      title:
+        'Commission contributions to the European Council - Discussion meetings',
       content:
         'Commission contribution to the European Council: <a href="/example" class="ecl-link ecl-link--standalone">Economic and Monetary Union, migration, EU budget for the future</a>',
     },
     {
       id: '18',
       label: '10 July 2018',
-      title: 'Item title',
+      title: 'Regulations in the daily operations',
       content:
         'Ways to enhance subsidiarity, proportionality and better regulation in the daily operations of the EU: <a href="/example" class="ecl-link ecl-link--standalone">Report</a> by the Task Force chaired by Commission First Vice-President Timmermans',
     },
     {
       id: '19',
       label: '12 September 2018',
-      title: 'Item title',
+      title: 'Opening speech',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">President Juncker\'s State of the Union speech</a>',
     },
     {
       id: '20',
       label: '20 September 2018',
-      title: 'Item title',
+      title: 'Informal European Council',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">Informal European Council/Meeting of EU27 (Salzburg, Austria)</a>',
     },
     {
       id: '21',
       label: '18 October 2018',
-      title: 'Item title',
+      title: 'Formal European Council meetings',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">European Council meeting</a>, Brussels',
     },
     {
       id: '22',
       label: '21 November 2018',
-      title: 'Item title',
+      title: 'The Council meetings opening',
       content:
         '<a href="/example" class="ecl-link ecl-link--standalone">European Semester</a>: setting the economic and social priorities',
     },
     {
       id: '23',
       label: '28 November 2018',
-      title: 'Item title',
+      title: 'Capital Markets Union Summit',
       content:
         'Commission contribution to the European Council on <a href="/example" class="ecl-link ecl-link--standalone">Capital Markets Union and Banking Union</a>',
     },
     {
       id: '24',
       label: '04 December 2018',
-      title: 'Item title',
+      title: 'Contribution to the European Council - Regulations',
       content:
         'Commission contribution to the European Council on <a href="/example" class="ecl-link ecl-link--standalone">long-term EU budget</a> and <a href="/example" class="ecl-link ecl-link--standalone">migration</a>',
     },
     {
       id: '25',
       label: '05 December 2018',
-      title: 'Item title',
+      title: 'Contribution to the European Council - Discussion',
       content:
         'Commission contribution to the European Council on the <a href="/example" class="ecl-link ecl-link--standalone">international role of the euro</a> and the <a href="/example" class="ecl-link ecl-link--standalone">Action Plan on disinformation</a>',
     },
     {
       id: '26',
       label: '11 December 2018',
-      title: 'Item title',
+      title: 'Contribution to the European Council - Workshops',
       content:
         'Commission contribution to the European Council on the <a href="/example" class="ecl-link ecl-link--standalone">Citizens\' Dialogues</a>',
     },
     {
       id: '27',
       label: '11 December 2018',
-      title: 'Item title',
+      title: 'Contribution to the European Council - Final thoughts',
       content:
         'Commission contribution to the <a href="/example" class="ecl-link ecl-link--standalone">Euro Summit</a>',
     },
     {
       id: '28',
       label: '13-14 December 2018',
-      title: 'Item title',
+      title: 'Summit closing',
       content: 'European Council and Euro Summit',
     },
   ],
