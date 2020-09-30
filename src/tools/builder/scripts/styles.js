65bfc64380eac07f888e10f9acf51b444696b241
Kalin Chernev <kalin.chernev@gmail.com>
2020-09-30 13:11:50 +0200

    sass rendering waits before next theme generation

--- src/tools/builder/scripts/styles.js
+++ src/tools/builder/scripts/styles.js
@@ -6,57 +6,36 @@ const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const mkdirp = require('mkdirp');
 const bannerPlugin = require('postcss-banner');
-// const postcssEnvFunction = require('postcss-env-function');
 
-const render = (entry, dest, options, plugins, postcssSourceMap) => {
-  sass.render(
-    {
-      file: entry,
-      outFile: dest,
-      sourceMap: options.sourceMap === true,
-      sourceMapContents: options.sourceMap === true,
-      sourceMapEmbed: options.sourceMap === true,
-      includePaths: [
-        path.resolve(process.cwd(), 'node_modules'),
-        ...(options.includePaths || []),
-      ],
-    },
-    (sassErr, sassResult) => {
-      if (sassErr) throw sassErr;
+const render = async (entry, dest, options, plugins, postcssSourceMap) => {
+  const sassResult = sass.renderSync({
+    file: entry,
+    outFile: dest,
+    sourceMap: options.sourceMap === true,
+    sourceMapContents: options.sourceMap === true,
+    sourceMapEmbed: options.sourceMap === true,
+    includePaths: [
+      path.resolve(process.cwd(), 'node_modules'),
+      ...(options.includePaths || []),
+    ],
+  });
 
-      const postcssOperation = async () => {
-        const postcssResult = await postcss(plugins).process(sassResult.css, {
-          map: postcssSourceMap,
-          from: entry,
-          to: dest,
-        });
+  const postcssResult = await postcss(plugins).process(sassResult.css, {
+    map: postcssSourceMap,
+    from: entry,
+    to: dest,
+  });
 
-        await mkdirp(path.dirname(dest));
-        fs.writeFileSync(dest, postcssResult.css);
+  await mkdirp(path.dirname(dest));
+  fs.writeFileSync(dest, postcssResult.css);
 
-        if (postcssResult.map) {
-          fs.writeFileSync(`${dest}.map`, postcssResult.map);
-        }
-      };
-
-      postcssOperation();
-    }
-  );
+  if (postcssResult.map) {
+    fs.writeFileSync(`${dest}.map`, postcssResult.map);
+  }
 };
 
 module.exports = (entry, dest, options, themes) => {
-  const plugins = [
-    autoprefixer({ grid: 'no-autoplace' }),
-    // postcssEnvFunction({
-    //   importFrom: [
-    //     {
-    //       environmentVariables: {
-    //         '--ecl-theme': process.env.ECL_THEME || 'ec',
-    //       },
-    //     },
-    //   ],
-    // }),
-  ];
+  const plugins = [autoprefixer({ grid: 'no-autoplace' })];
 
   let postcssSourceMap = false;
   if (options.sourceMap === true) {
@@ -81,7 +60,7 @@ module.exports = (entry, dest, options, themes) => {
 
   if (themes && themes.list && Array.isArray(themes.list) && themes.module) {
     let themeDest = dest;
-    themes.list.map((theme) => {
+    themes.list.map(async (theme) => {
       /**
        * sass does not support dynamic imports and dynamic theming at the moment.
        * @see https://github.com/nex3/sass/issues/451
@@ -92,6 +71,7 @@ module.exports = (entry, dest, options, themes) => {
        */
       const marker = `${themes.module}/index.scss`;
 
+      // Ensure clean start
       if (fs.existsSync(marker)) {
         fs.unlinkSync(marker);
       }
@@ -102,7 +82,12 @@ module.exports = (entry, dest, options, themes) => {
       themeDest.splice(themeDest.length - 1, 0, theme);
       themeDest = themeDest.join('/');
 
-      render(entry, themeDest, options, plugins, postcssSourceMap);
+      await render(entry, themeDest, options, plugins, postcssSourceMap);
+
+      // Cleanup
+      if (fs.existsSync(marker)) {
+        fs.unlinkSync(marker);
+      }
       themeDest = dest; // reset splice mutation
       return themeDest;
     });
