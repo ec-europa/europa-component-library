a38df6019fd7d6fa24bea68dcb91358d02d51c08
Kalin Chernev <kalin.chernev@gmail.com>
2020-10-07 14:46:17 +0200

    remove adapter

--- src/website/src/pages/ec/components/icon/demo/icon.js
+++ src/website/src/pages/ec/components/icon/demo/icon.js
@@ -1,15 +1,14 @@
 import iconPath from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import demoContentAudio from '@ecl/ec-specs-icon/demo/data--audio';
 import template from '@ecl/ec-twig-component-icon/ecl-icon.html.twig';
-import adapter from '@ecl/ec-twig-component-icon/adapter';
 
 const applySvgPath = (data) => {
   data.icon.path = iconPath;
   return data;
 };
 
-export const iconDefault = template(applySvgPath(adapter(demoContentAudio)));
+export const iconDefault = template(applySvgPath(demoContentAudio));
 
-const demoContentAudioPrimary = applySvgPath(adapter(demoContentAudio));
+const demoContentAudioPrimary = applySvgPath(demoContentAudio);
 demoContentAudioPrimary.icon.color = 'primary';
 export const iconPrimary = template(demoContentAudioPrimary);
