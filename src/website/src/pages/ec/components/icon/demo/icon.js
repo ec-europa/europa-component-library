b685e674a70536d0b30bf37e7b8ff368bbb6662e
Kalin Chernev <kalin.chernev@gmail.com>
2020-10-07 16:02:42 +0200

    Icons

--- src/website/src/pages/ec/components/icon/demo/icon.js
+++ src/website/src/pages/ec/components/icon/demo/icon.js
@@ -1,9 +1,50 @@
+import merge from 'deepmerge';
 import demoContentAudio from '@ecl/ec-specs-icon/demo/data--audio';
 import template from '@ecl/ec-twig-component-icon/ecl-icon.html.twig';
 import correctSvgPath from '../../../../../utils/correctSvgPath';
 
-export const iconDefault = template(correctSvgPath(demoContentAudio));
+const dataDefault = correctSvgPath(demoContentAudio);
+export const iconDefault = template(dataDefault);
 
-const demoContentAudioPrimary = correctSvgPath(demoContentAudio);
-demoContentAudioPrimary.icon.color = 'primary';
-export const iconPrimary = template(demoContentAudioPrimary);
+export const iconPrimary = template(
+  merge(dataDefault, { icon: { color: 'primary' } })
+);
+export const iconXs = template(merge(dataDefault, { icon: { size: 'xs' } }));
+export const iconS = template(merge(dataDefault, { icon: { size: 's' } }));
+export const iconM = template(merge(dataDefault, { icon: { size: 'm' } }));
+export const iconL = template(merge(dataDefault, { icon: { size: 'l' } }));
+export const iconXL = template(merge(dataDefault, { icon: { size: 'xl' } }));
+export const icon2XL = template(merge(dataDefault, { icon: { size: '2xl' } }));
+export const iconFluid = template(
+  merge(dataDefault, { icon: { size: 'fluid' } })
+);
+export const iconRotate0 = template(
+  merge(dataDefault, { icon: { transform: 'rotate-0' } })
+);
+export const iconRotate90 = template(
+  merge(dataDefault, { icon: { transform: 'rotate-90' } })
+);
+export const iconRotate180 = template(
+  merge(dataDefault, { icon: { transform: 'rotate-180' } })
+);
+export const iconRotate270 = template(
+  merge(dataDefault, { icon: { transform: 'rotate-270' } })
+);
+export const iconFlipVertical = template(
+  merge(dataDefault, { icon: { transform: 'flip-vertical' } })
+);
+export const iconFlipHorizontal = template(
+  merge(dataDefault, { icon: { transform: 'flip-horizontal' } })
+);
+export const iconImage = template(
+  merge(dataDefault, {
+    extra_attributes: [
+      { name: 'role', value: 'img' },
+      { name: 'aria-hidden', value: false },
+      { name: 'title', value: 'Title' },
+      { name: 'titleId', value: 'TitleID' },
+      { name: 'desc', value: 'Description' },
+      { name: 'descId', value: 'Description ID' },
+    ],
+  })
+);
