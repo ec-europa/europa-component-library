fdb32a517b1f144dcbc048f0152e5eb23d57ca04
emeryro <emery.romain@gmail.com>
2020-10-15 14:45:09 +0200

    update tests

--- src/implementations/twig/components/ecl-component-link/link.test.js
+++ src/implementations/twig/components/ecl-component-link/link.test.js
@@ -4,7 +4,7 @@ import dataCta from '@ecl/ec-specs-link/demo/data--cta';
 import dataStandalone from '@ecl/ec-specs-link/demo/data--standalone';
 
 describe('EC - Link', () => {
-  const template = '@ecl-twig/ec-component-link/ecl-link.html.twig';
+  const template = '@ecl/ec-twig-component-link/ecl-link.html.twig';
   const render = (params) => renderTwigFileAsNode(template, params);
   const defaultIconPath = 'static/icons.svg';
 
