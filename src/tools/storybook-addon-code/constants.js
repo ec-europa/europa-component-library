d465b8085ce497eb3ee8b6253e3d91adb26c3586
planctus <davidepuntoferrante@gmail.com>
2020-10-29 13:58:50 +0200

    chore(stories): fixing html panel, removing the react storybook addon (#1725)

--- src/tools/storybook-addon-code/constants.js
+++
@@ -1,8 +0,0 @@
-export const ADDON_ID = 'storybook/ecl-code';
-export const PANEL_ID = `${ADDON_ID}/panel`;
-export const PARAM_KEY = 'ecl-code';
-
-// Events
-const TOGGLE_CODE = `${ADDON_ID}/toggle_code`;
-const ADD_CODE = `${ADDON_ID}/add_code`;
-export const EVENTS = { ADD_CODE, TOGGLE_CODE };
