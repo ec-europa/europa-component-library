2bd79872b42e3a06462c5f8330d0622a9407015f
Kalin Chernev <kalin.chernev@gmail.com>
2020-11-04 16:04:55 +0100

    website

---
+++ src/website/src/pages/ec/components/forms/select/demo/index.js
@@ -0,0 +1,6 @@
+import demoContentSingle from '@ecl/specs-component-select/demo/data--single';
+import demoContentMultiple from '@ecl/specs-component-select/demo/data--multiple';
+import template from '@ecl/twig-component-select/select.html.twig';
+
+export const selectSingle = template(demoContentSingle);
+export const selectMultiple = template(demoContentMultiple);
