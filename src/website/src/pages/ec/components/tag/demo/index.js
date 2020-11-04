af5b753fd4313897b21faa679bfa10c765511daa
emeryro <emery.romain@gmail.com>
2020-11-04 12:54:39 +0100

    fix icon

--- src/website/src/pages/ec/components/tag/demo/index.js
+++ src/website/src/pages/ec/components/tag/demo/index.js
@@ -2,7 +2,8 @@ import demoContentDisplay from '@ecl/specs-component-tag/demo/data--display';
 import demoContentLink from '@ecl/specs-component-tag/demo/data--link';
 import demoContentRemovable from '@ecl/specs-component-tag/demo/data--removable';
 import template from '@ecl/twig-component-tag/tag.html.twig';
+import { correctSvgPath } from '@ecl/website-utils';
 
 export const tagDisplay = template(demoContentDisplay);
 export const tagLink = template(demoContentLink);
-export const tagRemovable = template(demoContentRemovable);
+export const tagRemovable = template(correctSvgPath(demoContentRemovable));
