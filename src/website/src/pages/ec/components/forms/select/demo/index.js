e900b3138a2d5909e61cb1a92532b7cb7003edc8
Kalin Chernev <kalin.chernev@gmail.com>
2020-11-04 16:06:41 +0100

    svg path correction

--- src/website/src/pages/ec/components/forms/select/demo/index.js
+++ src/website/src/pages/ec/components/forms/select/demo/index.js
@@ -1,6 +1,7 @@
 import demoContentSingle from '@ecl/specs-component-select/demo/data--single';
 import demoContentMultiple from '@ecl/specs-component-select/demo/data--multiple';
 import template from '@ecl/twig-component-select/select.html.twig';
+import { correctSvgPath } from '@ecl/website-utils';
 
-export const selectSingle = template(demoContentSingle);
-export const selectMultiple = template(demoContentMultiple);
+export const selectSingle = template(correctSvgPath(demoContentSingle));
+export const selectMultiple = template(correctSvgPath(demoContentMultiple));
