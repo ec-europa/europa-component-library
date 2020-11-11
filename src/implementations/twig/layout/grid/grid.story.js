bac0020aaea3c98078ecd670e33b804203acf8bd
Kalin Chernev <kalin.chernev@gmail.com>
2020-11-11 15:01:15 +0100

    refactor website grid page

--- src/implementations/twig/layout/grid/grid.story.js
+++ src/implementations/twig/layout/grid/grid.story.js
@@ -1,11 +1,11 @@
 import { withKnobs } from '@storybook/addon-knobs';
 
-import example234612 from './examples/234612.html.twig';
-import example642 from './examples/642.html.twig';
-import example282 from './examples/282.html.twig';
-import example336 from './examples/336.html.twig';
-import offset from './examples/offset.html.twig';
-import responsive from './examples/responsive.html.twig';
+import example234612 from './examples/234612.html';
+import example642 from './examples/642.html';
+import example282 from './examples/282.html';
+import example336 from './examples/336.html';
+import offset from './examples/offset.html';
+import responsive from './examples/responsive.html';
 
 export default {
   title: 'Layout/Grid',
@@ -17,20 +17,20 @@ export default {
   },
 };
 
-export const _234612 = () => example234612();
+export const _234612 = () => example234612;
 _234612.storyName = '2-3-4-6-12';
 
-export const _642 = () => example642();
+export const _642 = () => example642;
 _642.storyName = '6-4-2';
 
-export const _282 = () => example282();
+export const _282 = () => example282;
 _282.storyName = '2-8-2';
 
-export const _336 = () => example336();
+export const _336 = () => example336;
 _336.storyName = '3-3-6';
 
-export const _offset = () => offset();
+export const _offset = () => offset;
 _offset.storyName = 'offset';
 
-export const _responsive = () => responsive();
+export const _responsive = () => responsive;
 _responsive.storyName = 'responsive';
