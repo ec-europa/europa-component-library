7e710e26a8856fee014e60b171eacd4c8a85bec7
emeryro <emery.romain@gmail.com>
2020-11-11 10:10:38 +0100

    add component

--- src/presets/dev/src/dev.js
+++ src/presets/dev/src/dev.js
@@ -4,5 +4,6 @@ export * from '@ecl/dom-utils/autoinit';
 export * from '@ecl/vanilla-component-accordion';
 export * from '@ecl/vanilla-component-expandable';
 export * from '@ecl/vanilla-component-file';
+export * from '@ecl/vanilla-component-message';
 export * from '@ecl/vanilla-component-select';
 export * from '@ecl/vanilla-component-table';
