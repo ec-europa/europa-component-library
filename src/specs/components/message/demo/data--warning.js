e1e8d1af569cfddbf877bc16901e87cb3a140811
Romain Emery <emery.romain@gmail.com>
2020-11-12 10:14:54 +0100

    feat(message): migrate component message - FRONT-1767 (#1748)
    * add component
    
    * update test
    
    * Update src/implementations/twig/components/message/README.md
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * Update src/implementations/twig/components/message/README.md
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * Update src/implementations/vanilla/components/message/README.md
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * small fixes
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

---
+++ src/specs/components/message/demo/data--warning.js
@@ -0,0 +1,23 @@
+// Simple content for demo
+module.exports = {
+  variant: 'warning',
+  icon: {
+    path: '/icons.svg',
+    type: 'notifications',
+    name: 'warning',
+    size: 'l',
+  },
+  title: 'Warning message',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan semper lorem, ac mollis lacus tincidunt eu. Duis scelerisque diam eu tempus fringilla.',
+  close: {
+    variant: 'ghost',
+    label: 'Close',
+    icon: {
+      path: '/icons.svg',
+      type: 'ui',
+      name: 'close',
+      size: 's',
+    },
+  },
+};
