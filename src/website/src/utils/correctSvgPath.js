39a4218d5a3d58c5cffab04249cdecc22e89bdf0
Alexis Gaillard <a@bypopcorn.com>
2020-11-12 10:21:38 +0100

    feat(social-media-follow): Adding component documentation - FRONT-1768

--- src/website/src/utils/correctSvgPath.js
+++ src/website/src/utils/correctSvgPath.js
@@ -1,4 +1,5 @@
 import iconPath from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
+import iconSocialPath from '@ecl/resources-ec-social-icons/dist/sprites/icons-social.svg';
 
 /**
  * Pass a specification data object and get all occurences of *.svg corrected to file loader path.
@@ -7,7 +8,11 @@ import iconPath from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
 const correctSvgPath = (data) => {
   Object.keys(data).forEach((prop) => {
     if (typeof data[prop] === 'string' && data[prop].includes('.svg')) {
-      data[prop] = iconPath;
+      if (data[prop].includes('social')) {
+        data[prop] = iconSocialPath;
+      } else {
+        data[prop] = iconPath;
+      }
     }
     if (data[prop] !== null && typeof data[prop] === 'object') {
       data[prop] = correctSvgPath(data[prop]);
