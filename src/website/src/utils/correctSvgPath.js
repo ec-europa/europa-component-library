f18e87a874b621dfc1443c8ebd95b8e95f396f40
planctus <davidepuntoferrante@gmail.com>
2020-11-13 08:14:27 +0200

    feat(social-media-share): migrate social media share - FRONT-1769 (#1754)
    * feat(social-media-share): Adding component - FRONT-1769
    
    * feat(social-media-share): Adding snapshot - front-1769
    
    * feat(social-media-share): Remove redundant code from test - front-1769
    
    * feat(social-media-share): Reordering scss file - front-1769
    
    * feat(social-media-share): Using map - front-1769
    
    * feat(social-media-share): Fixing dev dependency - front-1769
    
    * feat(social-media-share): Removing one shot variable from scss - front-1769

--- src/website/src/utils/correctSvgPath.js
+++ src/website/src/utils/correctSvgPath.js
@@ -1,5 +1,5 @@
 import iconPath from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
-
+import iconSocialPath from '@ecl/resources-ec-social-icons/dist/sprites/icons-social.svg';
 /**
  * Pass a specification data object and get all occurences of *.svg corrected to file loader path.
  * @param {Object} data
@@ -7,7 +7,7 @@ import iconPath from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
 const correctSvgPath = (data) => {
   Object.keys(data).forEach((prop) => {
     if (typeof data[prop] === 'string' && data[prop].includes('.svg')) {
-      data[prop] = iconPath;
+      data[prop] = data[prop].includes('social') ? iconSocialPath : iconPath;
     }
     if (data[prop] !== null && typeof data[prop] === 'object') {
       data[prop] = correctSvgPath(data[prop]);
