bea4e84f0d1d2dc2ec74c88ec226fc5281618c2d
Kalin Chernev <kalin.chernev@gmail.com>
2020-11-13 13:36:44 +0100

    feat: add datepicker

--- src/presets/dev/src/dev.js
+++ src/presets/dev/src/dev.js
@@ -2,6 +2,7 @@ import '@ecl/dom-utils/polyfills';
 
 export * from '@ecl/dom-utils/autoinit';
 export * from '@ecl/vanilla-component-accordion';
+export * from '@ecl/vanilla-component-datepicker';
 export * from '@ecl/vanilla-component-expandable';
 export * from '@ecl/vanilla-component-file';
 export * from '@ecl/vanilla-component-message';
