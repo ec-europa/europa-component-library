f1d8f617fa9151006e973cc1acf82855ca2157f9
planctus <davidepuntoferrante@gmail.com>
2020-11-16 21:33:17 +0200

    feat(gallery): Adding component - front-1776

---
+++ src/implementations/twig/components/gallery/gallery.test.js
@@ -0,0 +1,54 @@
+import { merge, renderTwigFileAsNode } from '@ecl/test-utils';
+
+// Import data for tests
+import demoData from '@ecl/specs-component-gallery/demo/data';
+
+describe('Gallery', () => {
+  const template = '@ecl/gallery/gallery.html.twig';
+  const render = (params) => renderTwigFileAsNode(template, params);
+
+  describe('Default', () => {
+    test('renders correctly', () => {
+      expect.assertions(1);
+      return expect(render(demoData)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(demoData, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(demoData, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+
+    test('with missing input data and debug enabled it returns the right warning messages', () => {
+      expect.assertions(1);
+
+      const dataCompliance = { ...demoData, _compliance_: true };
+      dataCompliance.overlay.download.link.label = '';
+      dataCompliance.items[2].path = '';
+      dataCompliance.overlay.close.icon = {};
+      dataCompliance.overlay.next.label = '';
+      dataCompliance.items[4].video.sources[0].src = '';
+      dataCompliance.items[4].icon = {};
+      dataCompliance.items[7].sources = [];
+
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
+  });
+});
