12b99dbe78b2c374d5d5cb31e62f8338948a8dee
Kalin Chernev <kalinchernev@users.noreply.github.com>
2020-11-18 14:25:36 +0100

    feat: migrate hero banner - FRONT-1771 (#1753)
    * chore: correct storybook iframe styles
    
    * feat: migrate hero banner
    
    * merge latest target branch
    
    * merge latest target branch
    
    * update tests
    
    * Update src/implementations/twig/components/hero-banner/README.md
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * Update src/implementations/twig/components/hero-banner/README.md
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * Update src/implementations/twig/components/hero-banner/README.md
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * Update src/implementations/twig/components/hero-banner/README.md
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * Update src/implementations/twig/components/hero-banner/hero-banner.html.twig
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * Update src/implementations/twig/components/hero-banner/hero-banner.html.twig
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    
    * update annotation
    
    * update annotation
    
    * update specs
    
    * correction
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

---
+++ src/implementations/twig/components/hero-banner/hero-banner.test.js
@@ -0,0 +1,117 @@
+import { merge, renderTwigFileAsNode } from '@ecl/test-utils';
+
+import bannerDataImage from '@ecl/specs-component-hero-banner/demo/data--image-box';
+import bannerDataImageGradient from '@ecl/specs-component-hero-banner/demo/data--image-gradient';
+import bannerDataImageShade from '@ecl/specs-component-hero-banner/demo/data--image-shade';
+import bannerDataSimplePrimary from '@ecl/specs-component-hero-banner/demo/data--simple-primary';
+import bannerDataSimpleGrey from '@ecl/specs-component-hero-banner/demo/data--simple-grey';
+import bannerDataSimpleWhite from '@ecl/specs-component-hero-banner/demo/data--simple-white';
+
+describe('Hero Banner', () => {
+  const template = '@ecl/hero-banner/hero-banner.html.twig';
+  const render = (params) => renderTwigFileAsNode(template, params);
+
+  describe('Simple', () => {
+    test(`- primary renders correctly`, () => {
+      expect.assertions(1);
+
+      return expect(render(bannerDataSimplePrimary)).resolves.toMatchSnapshot();
+    });
+
+    test(`- grey renders correctly`, () => {
+      expect.assertions(1);
+
+      return expect(render(bannerDataSimpleGrey)).resolves.toMatchSnapshot();
+    });
+
+    test(`- white renders correctly`, () => {
+      expect.assertions(1);
+
+      return expect(render(bannerDataSimpleWhite)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(bannerDataSimplePrimary, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(bannerDataSimplePrimary, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+
+    test('with validation enabled and missing input data returns the right warning message', () => {
+      expect.assertions(1);
+
+      const dataCompliance = { ...bannerDataSimplePrimary, _compliance_: true };
+      dataCompliance.title = '';
+
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
+  });
+
+  describe('image', () => {
+    test(`- text-box renders correctly`, () => {
+      expect.assertions(1);
+
+      return expect(render(bannerDataImage)).resolves.toMatchSnapshot();
+    });
+
+    test(`- gradient renders correctly`, () => {
+      expect.assertions(1);
+
+      return expect(render(bannerDataImageGradient)).resolves.toMatchSnapshot();
+    });
+
+    test(`- shade renders correctly`, () => {
+      expect.assertions(1);
+
+      return expect(render(bannerDataImageShade)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(bannerDataImage, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(bannerDataImage, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+
+    test('with validation enabled and missing input data returns the right warning message', () => {
+      expect.assertions(1);
+
+      const dataCompliance = { ...bannerDataImage, _compliance_: true };
+      dataCompliance.image = '';
+
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
+  });
+});
