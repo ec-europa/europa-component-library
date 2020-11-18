286c5cb0739c784447d0b6350db6a9f1f7e6aeff
Alexis Gaillard <a@bypopcorn.com>
2020-11-18 07:25:38 +0100

    feat(page-banner): Adding component - FRONT-1772 (#1756)
    * feat(page-banner): Adding component - FRONT-1772
    
    * feat(page-banner): Fix package issues - FRONT-1772
    
    * feat(page-banner): Update tests - FRONT-1772
    
    * feat(page-banner): Adding documentation - FRONT-1772
    
    * feat(page-banner): Update tests - FRONT-1772
    
    * feat(page-banner): Reorder presets - FRONT-1772
    
    * feat(page-banner): Review note and readme file - FRONT-1772
    
    * apply prettier rules
    
    Co-authored-by: Kalin Chernev <kalin.chernev@gmail.com>

---
+++ src/specs/components/page-banner/demo/data--simple-grey.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'EU Budget for the future',
+  baseline: 'Innovation, economy, environment and geopolitics',
+  link: {
+    link: {
+      label: 'Subscribe',
+      path: '/example',
+      aria_label: 'Subscribe',
+      icon_position: 'after',
+    },
+    icon: {
+      path: '/icons.svg',
+      type: 'ui',
+      name: 'corner-arrow',
+      size: 'xs',
+      transform: 'rotate-90',
+    },
+  },
+  type: 'grey',
+  centered: true,
+};
