8d9680526a70288fe596a361de4119a2b1fd7a60
Alexis Gaillard <a@bypopcorn.com>
2020-11-18 01:10:48 +0100

    feat(inpage-navigation): Adding documentation - FRONT-1779

---
+++ src/website/src/pages/ec/components/navigation/inpage-navigation/demo/index.js
@@ -0,0 +1,5 @@
+import demoDefault from '@ecl/specs-component-inpage-navigation/demo/data';
+import template from '@ecl/twig-component-inpage-navigation/inpage-navigation.html.twig';
+
+const inpageNavigationExample = template(demoDefault);
+export default inpageNavigationExample;
