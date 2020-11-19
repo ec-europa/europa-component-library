2d29fded33516518160c025be46fddd4c22f22bc
Alexis Gaillard <a@bypopcorn.com>
2020-11-19 17:01:05 +0100

    feat(breadcrumb): Adding components - FRONT-1778 (#1761)
    * feat(breadcrumb): Adding components + tests - FRONT-1778
    
    * feat(breadcrumb): Adding documentation - FRONT-1778
    
    * feat(breadcrumb): Updates based on reviews - FRONT-1778
    
    * feat(breadcrumb): change JSdoc path - FRONT-1778
    
    * feat(breadcrumb): Fix JSdoc api documentation - FRONT-1778
    
    * feat(breadcrumb): Fix prettier issues - FRONT-1778
    
    * feat(breadcrumb): Fix prettier issues - FRONT-1778
    
    * feat(breadcrumb): Fix code issues based on review - FRONT-1778
    
    * feat(breadcrumb): Fix doc component path - FRONT-1778
    
    * feat(breadcrumb): Fix ellipsis issues on documentation previews - FRONT-1778

--- src/presets/dev/src/dev.js
+++ src/presets/dev/src/dev.js
@@ -2,6 +2,9 @@ import '@ecl/dom-utils/polyfills';
 
 export * from '@ecl/dom-utils/autoinit';
 export * from '@ecl/vanilla-component-accordion';
+export * from '@ecl/vanilla-component-breadcrumb-core';
+export * from '@ecl/vanilla-component-breadcrumb-standardised';
+export * from '@ecl/vanilla-component-breadcrumb-harmonised';
 export * from '@ecl/vanilla-component-expandable';
 export * from '@ecl/vanilla-component-file';
 export * from '@ecl/vanilla-component-file-upload';
