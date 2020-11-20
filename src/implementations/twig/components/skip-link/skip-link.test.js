066a0f1bdf62582f412d6f66e038b2ac08b51d67
planctus <davidepuntoferrante@gmail.com>
2020-11-20 14:53:30 +0200

    feat(skip-link): Adding component - FRONT-1806 (#1768)
    * feat(skip-link): Adding component - FRONT-1806
    
    * feat(skip-link): Adding snapshot - front-1806
    
    * feat(skip-link): Using map for z-index - front-1806
    
    * feat(skip-link): Add dependency - FRONT-1806
    
    Co-authored-by: Kalin Chernev <kalin.chernev@gmail.com>

---
+++ src/implementations/twig/components/skip-link/skip-link.test.js
@@ -0,0 +1,47 @@
+import { merge, renderTwigFileAsNode } from '@ecl/test-utils';
+import specs from '@ecl/specs-component-skip-link/demo/data';
+
+describe('Skip Link', () => {
+  const template = '@ecl/skip-link/skip-link.html.twig';
+  const render = (params) => renderTwigFileAsNode(template, params);
+
+  describe('Default', () => {
+    test('renders correctly', () => {
+      expect.assertions(1);
+
+      return expect(render(specs)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(specs, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(specs, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+
+    test('With missing input data returns the right warning message', () => {
+      expect.assertions(1);
+
+      const dataCompliance = { ...specs, _compliance_: true };
+      dataCompliance.href = '';
+
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
+  });
+});
