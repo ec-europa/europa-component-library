2b3e408e3c2b2087308cefcc5714f7b618135c88
Alexis Gaillard <a@bypopcorn.com>
2020-11-20 11:49:02 +0100

    feat(inpage-navigation): Improve documentation showcase with custom markup example.

--- src/website/src/pages/ec/components/navigation/inpage-navigation/demo/index.js
+++ src/website/src/pages/ec/components/navigation/inpage-navigation/demo/index.js
@@ -1,5 +1,5 @@
-import demoDefault from '@ecl/specs-component-inpage-navigation/demo/data';
-import template from '@ecl/twig-component-inpage-navigation/inpage-navigation.html.twig';
+import { correctSvgPath } from '@ecl/website-utils';
+import markup from '../examples/default.html';
 
-const inpageNavigationExample = template(demoDefault);
+const inpageNavigationExample = correctSvgPath(markup);
 export default inpageNavigationExample;
