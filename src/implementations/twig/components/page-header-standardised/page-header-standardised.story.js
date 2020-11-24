39235c820bdae652ab18fca6711e4c1ec57093cf
Alexis Gaillard <a@bypopcorn.com>
2020-11-24 21:36:26 +0100

    feat(page-header-standardised): Adding component - FRONT-1802

---
+++ src/implementations/twig/components/page-header-standardised/page-header-standardised.story.js
@@ -0,0 +1,92 @@
+import { withKnobs, text, optionsKnob } from '@storybook/addon-knobs';
+import { withNotes } from '@ecl/storybook-addon-notes';
+import withCode from '@ecl/storybook-addon-code';
+import { getExtraKnobs, tabLabels, getComplianceKnob } from '@ecl/story-utils';
+
+import defaultSprite from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
+import demoTitleContent from '@ecl/specs-component-page-header-standardised/demo//data--title';
+import demoMetaTitleContent from '@ecl/specs-component-page-header-standardised/demo/data--meta-title';
+import demoMetaTitleDescriptionContent from '@ecl/specs-component-page-header-standardised/demo/data--meta-title-description';
+import dataBreadcrumbSimple from '@ecl/specs-component-breadcrumb/demo/data--simple';
+import pageHeaderStandardised from './page-header-standardised.html.twig';
+import notes from './README.md';
+
+const preparePageHeaderStandardised = (data, desc, meta) => {
+  data.breadcrumb = dataBreadcrumbSimple;
+  data.title = text('title', data.title, tabLabels.required);
+  if (meta) {
+    data.meta = text('meta', data.meta, tabLabels.optional);
+  }
+  if (desc) {
+    data.description = text(
+      'description',
+      data.description,
+      tabLabels.optional
+    );
+  }
+  data.breadcrumb.icon_file_path = optionsKnob(
+    'breadcrumb.icon_file_path',
+    { current: defaultSprite, 'no path': '' },
+    defaultSprite,
+    { display: 'inline-radio' },
+    tabLabels.required
+  );
+  data.breadcrumb.ellipsis_label = text(
+    'breadcrumb.ellipsis_label',
+    data.breadcrumb.ellipsis_label,
+    tabLabels.required
+  );
+  data.breadcrumb.navigation_text = text(
+    'breadcrumb.navigation_text',
+    data.breadcrumb.navigation_text,
+    tabLabels.required
+  );
+  data.breadcrumb.links.forEach((item, i) => {
+    item.label = text(
+      `data.breadcrumb.links[${i}].label`,
+      item.label,
+      tabLabels.required
+    );
+    item.path = text(
+      `data.breadcrumb.links[${i}].path`,
+      item.path,
+      tabLabels.required
+    );
+  });
+
+  getExtraKnobs(data);
+  getComplianceKnob(data);
+
+  return data;
+};
+
+export default {
+  title: 'Components/Page Headers/Page Header Standardised',
+  decorators: [withNotes, withCode, withKnobs],
+};
+
+export const Title = () =>
+  pageHeaderStandardised(preparePageHeaderStandardised(demoTitleContent));
+
+Title.storyName = 'title';
+Title.parameters = { notes: { markdown: notes, json: demoTitleContent } };
+
+export const MetaTitle = () =>
+  pageHeaderStandardised(
+    preparePageHeaderStandardised(demoMetaTitleContent, false, true)
+  );
+
+MetaTitle.storyName = 'meta-title';
+MetaTitle.parameters = {
+  notes: { markdown: notes, json: demoMetaTitleContent },
+};
+
+export const MetaTitleDescription = () =>
+  pageHeaderStandardised(
+    preparePageHeaderStandardised(demoMetaTitleDescriptionContent, true, true)
+  );
+
+MetaTitleDescription.storyName = 'meta-title-description';
+MetaTitleDescription.parameters = {
+  notes: { markdown: notes, json: demoMetaTitleDescriptionContent },
+};
