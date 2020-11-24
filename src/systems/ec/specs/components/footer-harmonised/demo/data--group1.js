906c4cde5b7e1ce62e9bbc0a9ea3d54ff0749f3d
Romain Emery <emery.romain@gmail.com>
2020-11-24 07:59:03 +0100

    update data (#1773)

--- src/systems/ec/specs/components/footer-harmonised/demo/data--group1.js
+++ src/systems/ec/specs/components/footer-harmonised/demo/data--group1.js
@@ -180,11 +180,6 @@ module.exports = {
           ariaLabel: 'Link to Legal notice',
           href: '/example',
         },
-        {
-          label: 'Brexit content disclaimer',
-          ariaLabel: 'Link to Brexit content disclaimer',
-          href: '/example',
-        },
       ],
     },
   },
