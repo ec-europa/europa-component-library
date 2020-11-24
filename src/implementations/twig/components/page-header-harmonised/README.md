6d2225395672809e419829e169c9be5ccf78d2b3
Alexis Gaillard <a@bypopcorn.com>
2020-11-24 23:25:27 +0100

    feat(page-header-harmonised): Adding component - FRONT-1803

---
+++ src/implementations/twig/components/page-header-harmonised/README.md
@@ -0,0 +1,40 @@
+# ECL-Twig Page Header Harmonised component
+
+npm package: `@ecl/twig-component-page-header-harmonised`
+
+```shell
+npm install --save @ecl/twig-component-page-header-harmonised
+```
+
+### Parameters
+
+- **"title"** (string) (default: '') Title of header
+- **"description"** (string) (default: '') Description of header
+- **"meta"** (string) (default: '') Meta of header
+- **"breadcrumb"** (associative array) (default: '') Predefined structure compatible with EC Breadcrumb
+- **"extra_classes"** (optional) (string) (default: '') Extra classes (space separated)
+- **"extra_attributes"** (optional) (array) (default: []) Extra attributes
+  - "name" (string) Attribute name, eg. 'data-test'
+  - "value" (string) Attribute value, eg: 'data-test-1'
+- **"\_compliance**"\_ (boolean) (default: false) Activates debug
+
+### Example :
+
+<!-- prettier-ignore -->
+```twig
+{% include '@ecl/page-header-harmonised/page-header-harmonised.html.twig' with {  
+  title: 'Page title',  
+  description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',  
+  meta: 'News article | 17 October 2015',  
+  breadcrumb: {  
+    links: [    
+      {  
+        label: 'Link 1',  
+        path: '/example'  
+      },  
+    ],  
+    icon_file_path: '/path-to-the-icons-file',  
+    navigation_text: 'You are here:',  
+  }  
+} %}  
+```
---
+++ src/implementations/twig/components/page-header-harmonised/page-header-harmonised.test.js
@@ -0,0 +1,117 @@
+import { merge, renderTwigFileAsNode } from '@ecl/test-utils';
+
+import demoTitleContent from '@ecl/specs-component-page-header-harmonised/demo/data--title';
+import demoMetaTitleContent from '@ecl/specs-component-page-header-harmonised/demo/data--meta-title';
+import demoMetaTitleDescriptionContent from '@ecl/specs-component-page-header-harmonised/demo/data--meta-title-description';
+import dataBreadcrumbSimple from '@ecl/specs-component-breadcrumb/demo/data';
+
+demoTitleContent.breadcrumb = dataBreadcrumbSimple;
+demoMetaTitleContent.breadcrumb = dataBreadcrumbSimple;
+demoMetaTitleDescriptionContent.breadcrumb = dataBreadcrumbSimple;
+
+describe('Page Header Harmonised', () => {
+  const template =
+    '@ecl/page-header-harmonised/page-header-harmonised.html.twig';
+  const render = (params) => renderTwigFileAsNode(template, params);
+
+  describe('Title', () => {
+    test(`- renders correctly`, () => {
+      expect.assertions(1);
+      return expect(render(demoTitleContent)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(demoTitleContent, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(demoTitleContent, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+
+    test('with missing input data and debug enabled it returns the right warning messages', () => {
+      expect.assertions(1);
+
+      const dataCompliance = { ...demoTitleContent, _compliance_: true };
+      dataCompliance.title = '';
+
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
+  });
+
+  describe('Meta-title', () => {
+    test(`- renders correctly`, () => {
+      expect.assertions(1);
+      return expect(render(demoMetaTitleContent)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(demoMetaTitleContent, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(demoMetaTitleContent, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+  });
+
+  describe('Meta-title-description', () => {
+    test(`- renders correctly`, () => {
+      expect.assertions(1);
+      return expect(
+        render(demoMetaTitleDescriptionContent)
+      ).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(demoMetaTitleDescriptionContent, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(demoMetaTitleDescriptionContent, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+  });
+});
