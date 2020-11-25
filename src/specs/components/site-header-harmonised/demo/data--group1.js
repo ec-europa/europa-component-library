a483d4c9a850e290969849f6930b9189d9ceb579
Kalin Chernev <kalinchernev@users.noreply.github.com>
2020-11-25 13:24:24 +0100

    feat: add site header harmonised - FRONT-1800 (#1781)
    Co-authored-by: Romain Emery <emery.romain@gmail.com>

---
+++ src/specs/components/site-header-harmonised/demo/data--group1.js
@@ -0,0 +1,530 @@
+module.exports = {
+  logo: {
+    title: 'European Commission',
+    alt: 'European Commission logo',
+    href: '/example',
+    language: 'en',
+    src_desktop: '/logo--en.svg',
+  },
+  group: 'group1',
+  menu: {
+    title: 'Menu',
+    close: 'Close',
+    back: 'Back',
+    items: [
+      {
+        label: 'Home',
+        path: '/example',
+      },
+      {
+        label: 'Item 2',
+        path: '/example',
+        is_current: true,
+        children: [
+          {
+            label: 'Item 2.1',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.2',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.3',
+            path: '/example',
+            is_current: true,
+          },
+          {
+            label: 'Item 2.4',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.5',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.6',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.7',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.8',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.9',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.10',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.11',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.12',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.13',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.14',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.15',
+            path: '/example',
+          },
+          {
+            label: 'Item 2.16',
+            path: '/example',
+          },
+        ],
+      },
+      {
+        label: 'Item 3',
+        path: '/example',
+        children: [
+          {
+            label: 'Item 3.1',
+            path: '/example',
+          },
+          {
+            label: 'Item 3.2',
+            path: '/example',
+          },
+          {
+            label: 'Item 3.3',
+            path: '/example',
+          },
+        ],
+      },
+      {
+        label: 'Item 4',
+        path: '/example',
+        children: [
+          {
+            label: 'Item 4.1',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.2',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.3 with a very long label going on 2 lines',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.4',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.5',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.6',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.7',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.8',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.9',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.10',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.11',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.12',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.13',
+            path: '/example',
+          },
+          {
+            label: 'Item 4.14',
+            path: '/example',
+          },
+        ],
+      },
+      {
+        label: 'Item 5',
+        path: '/example',
+        children: [
+          {
+            label: 'Item 5.1',
+            path: '/example',
+          },
+          {
+            label: 'Item 5.2',
+            path: '/example',
+          },
+          {
+            label: 'Item 5.3',
+            path: '/example',
+          },
+          {
+            label: 'Item 5.4',
+            path: '/example',
+          },
+          {
+            label: 'Item 5.5',
+            path: '/example',
+          },
+          {
+            label: 'Item 5.6',
+            path: '/example',
+          },
+          {
+            label: 'Item 5.7',
+            path: '/example',
+          },
+        ],
+      },
+      {
+        label: 'Item 6',
+        path: '/example',
+        children: [
+          {
+            label: 'Item 6.1',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.2',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.3',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.4',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.5',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.6',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.7',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.8',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.9 with a very long label',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.10',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.11',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.12',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.13',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.14',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.15',
+            path: '/example',
+          },
+          {
+            label: 'Item 6.16',
+            path: '/example',
+          },
+        ],
+      },
+      {
+        label: 'Item 7 with a long label',
+        path: '/example',
+        children: [
+          {
+            label: 'Item 7.1',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.2',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.3',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.4',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.5',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.6',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.7',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.8',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.9',
+            path: '/example',
+          },
+          {
+            label: 'Item 7.10',
+            path: '/example',
+          },
+        ],
+      },
+    ],
+    group1: true,
+    icon_path: '/icons.svg',
+    site_name: 'Site name',
+    menu_link: '/example',
+  },
+  logged: true,
+  banner_top: {
+    link: {
+      label: 'Class name',
+      path: '/example',
+    },
+  },
+  login_toggle: {
+    label_not_logged: 'Log in',
+    href_not_logged: '/example',
+    label_logged: 'Logged in',
+    href_logged: '/example',
+  },
+  login_box: {
+    id: 'login-box-id',
+    description: 'Logged in as &lt;last name&gt;, &lt;first name&gt;',
+    label: 'Log out',
+    href: '/example',
+  },
+  language_selector: {
+    href: '/example',
+    label: 'English',
+    code: 'en',
+    overlay: {
+      title: 'Select your language',
+      items: [
+        {
+          label: 'български',
+          path: '/example',
+          lang: 'bg',
+        },
+        {
+          label: 'español',
+          path: '/example',
+          lang: 'es',
+        },
+        {
+          label: 'čeština',
+          path: '/example',
+          lang: 'cs',
+        },
+        {
+          label: 'dansk',
+          path: '/example',
+          lang: 'da',
+        },
+        {
+          label: 'Deutsch',
+          path: '/example',
+          lang: 'de',
+        },
+        {
+          label: 'eesti',
+          path: '/example',
+          lang: 'et',
+        },
+        {
+          label: 'ελληνικά',
+          path: '/example',
+          lang: 'el',
+        },
+        {
+          label: 'English',
+          path: '/example',
+          active: true,
+          lang: 'en',
+        },
+        {
+          label: 'français',
+          path: '/example',
+          lang: 'fr',
+        },
+        {
+          label: 'Gaeilge',
+          path: '/example',
+          lang: 'ga',
+        },
+        {
+          label: 'hrvatski',
+          path: '/example',
+          lang: 'hr',
+        },
+        {
+          label: 'italiano',
+          path: '/example',
+          lang: 'it',
+        },
+        {
+          label: 'latviešu',
+          path: '/example',
+          lang: 'lv',
+        },
+        {
+          label: 'lietuvių',
+          path: '/example',
+          lang: 'lt',
+        },
+        {
+          label: 'magyar',
+          path: '/example',
+          lang: 'hu',
+        },
+        {
+          label: 'Malti',
+          path: '/example',
+          lang: 'mt',
+        },
+        {
+          label: 'Nederlands',
+          path: '/example',
+          lang: 'nl',
+        },
+        {
+          label: 'polski',
+          path: '/example',
+          lang: 'pl',
+        },
+        {
+          label: 'português',
+          path: '/example',
+          lang: 'pt',
+        },
+        {
+          label: 'română',
+          path: '/example',
+          lang: 'ro',
+        },
+        {
+          label: 'slovenčina',
+          path: '/example',
+          lang: 'sk',
+        },
+        {
+          label: 'slovenščina',
+          path: '/example',
+          lang: 'sl',
+        },
+        {
+          label: 'suomi',
+          path: '/example',
+          lang: 'fi',
+        },
+        {
+          label: 'svenska',
+          path: '/example',
+          lang: 'sv',
+        },
+      ],
+      close_label: 'Close',
+      non_eu_items: [
+        {
+          label: '中文',
+          path: '/example',
+          lang: 'zh',
+        },
+        {
+          label: 'Türk',
+          path: '/example',
+          lang: 'tr',
+        },
+        {
+          label: 'pусский',
+          path: '/example',
+          lang: 'ru',
+        },
+        {
+          label: 'Català',
+          path: '/example',
+          lang: 'ca',
+        },
+        {
+          label: 'عَرَبِيّ',
+          path: '/example',
+          lang: 'ar',
+        },
+      ],
+    },
+    eu_category: 'EU official languages',
+    non_eu_category: 'Non-EU languages',
+  },
+  search_toggle: {
+    label: 'Search',
+    href: '/example',
+  },
+  search_form: {
+    extra_attributes: [
+      {
+        name: 'id',
+        value: 'search-form-id',
+      },
+    ],
+    text_input: {
+      id: 'search-input-id',
+      name: 'Search',
+      label: 'Search',
+    },
+    button: {
+      label: 'Search',
+    },
+  },
+  icon_file_path: '/icons.svg',
+  menu_label: 'Menu',
+  site_name: 'Site name',
+};
