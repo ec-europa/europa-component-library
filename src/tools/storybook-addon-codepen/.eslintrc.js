93ee8970946a3413fe7ec16705d0a15c10d312fe
Kalin Chernev <kalin.chernev@gmail.com>
2020-12-01 14:07:29 +0100

    save state

---
+++ src/tools/storybook-addon-codepen/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  plugins: ['react', 'jsx-a11y'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
