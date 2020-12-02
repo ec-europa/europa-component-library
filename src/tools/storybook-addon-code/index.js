b168ab8ac25fca87ca95b7e5452fa671622d8470
Kalin Chernev <kalin.chernev@gmail.com>
2020-12-02 14:21:58 +0100

    correction

--- src/tools/storybook-addon-code/index.js
+++ src/tools/storybook-addon-code/index.js
@@ -1,8 +1,8 @@
 import addons, { makeDecorator } from '@storybook/addons';
 import { EVENTS } from './constants';
 
-export const withCoden = makeDecorator({
-  name: 'withCoden',
+export const withCode = makeDecorator({
+  name: 'withCode',
   wrapper: (getStory, context) => {
     const channel = addons.getChannel();
     const story = getStory(context);
@@ -12,4 +12,4 @@ export const withCoden = makeDecorator({
   },
 });
 
-export default withCoden;
+export default withCode;
