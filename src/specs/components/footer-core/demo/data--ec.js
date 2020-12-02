82645d43712dcfe6ccaaffd0703937f3be9609dc
emeryro <emery.romain@gmail.com>
2020-12-02 11:13:07 +0100

    update structure

--- src/specs/components/footer-core/demo/data--ec.js
+++ src/specs/components/footer-core/demo/data--ec.js
@@ -11,42 +11,194 @@ module.exports = {
         'This site is managed by the Directorate-General for Communication',
     },
     {
-      items: [
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Strategy">Strategy</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to About the European Commission">About the European Commission</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Business, Economy, Euro">Business, Economy, Euro</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Live, work, travel in the EU">Live, work, travel in the EU</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Law">Law</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Funding, Tenders">Funding, Tenders</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Research and innovation">Research and innovation</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Energy, Climate change, Environment">Energy, Climate change, Environment</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Education">Education</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Aid, Development cooperation, Fundamental rights">Aid, Development cooperation, Fundamental rights</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Food, Farming, Fisheries">Food, Farming, Fisheries</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to EU regional and urban development">EU regional and urban development</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Jobs at the European Commission">Jobs at the European Commission</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Statistics">Statistics</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to News">News</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Events">Events</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Publications">Publications</a>',
+      links: [
+        {
+          link: {
+            label: 'Strategy',
+            path: '/example',
+            aria_label: 'Link to Strategy',
+          },
+        },
+        {
+          link: {
+            label: 'About the European Commission',
+            path: '/example',
+            aria_label: 'Link About the European Commission',
+          },
+        },
+        {
+          link: {
+            label: 'Business, Economy, Euro',
+            path: '/example',
+            aria_label: 'Link to Business, Economy, Euro',
+          },
+        },
+        {
+          link: {
+            label: 'Live, work, travel in the EU',
+            path: '/example',
+            aria_label: 'Link work, travel in the EU',
+          },
+        },
+        {
+          link: {
+            label: 'Law',
+            path: '/example',
+            aria_label: 'Link to Law',
+          },
+        },
+        {
+          link: {
+            label: 'Funding, Tenders',
+            path: '/example',
+            aria_label: 'Link to Funding, Tenders',
+          },
+        },
+        {
+          link: {
+            label: 'Research and innovation',
+            path: '/example',
+            aria_label: 'Link to Research and innovation',
+          },
+        },
+        {
+          link: {
+            label: 'Energy, Climate change, Environment',
+            path: '/example',
+            aria_label: 'Link to Climate change, Environment',
+          },
+        },
+        {
+          link: {
+            label: 'Education',
+            path: '/example',
+            aria_label: 'Link to Education',
+          },
+        },
+        {
+          link: {
+            label: 'Aid, Development cooperation, Fundamental rights',
+            path: '/example',
+            aria_label:
+              'Link to Aid, Development cooperation, Fundamental rights',
+          },
+        },
+        {
+          link: {
+            label: 'Food, Farming, Fisheries',
+            path: '/example',
+            aria_label: 'Link to Food, Farming, Fisheries',
+          },
+        },
+        {
+          link: {
+            label: 'EU regional and urban development',
+            path: '/example',
+            aria_label: 'Link to EU regionaland urban development',
+          },
+        },
+        {
+          link: {
+            label: 'Jobs at the European Commission',
+            path: '/example',
+            aria_label: 'Link to Jobs at the European Commission',
+          },
+        },
+        {
+          link: {
+            label: 'Statistics',
+            path: '/example',
+            aria_label: 'Link to Statistics',
+          },
+        },
+        {
+          link: {
+            label: 'News',
+            path: '/example',
+            aria_label: 'Link to News',
+          },
+        },
+        {
+          link: {
+            label: 'Events',
+            path: '/example',
+            aria_label: 'Link to Events',
+          },
+        },
+        {
+          link: {
+            label: 'Publications',
+            path: '/example',
+            aria_label: 'Link to Publications',
+          },
+        },
       ],
       list_class_name: 'ecl-footer-core__list--columns',
-      content_before: 'More information on:',
       section_class_name: 'ecl-footer-core__section--separator',
     },
     {
-      items: [
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Contact the European Commission">Contact the European Commission</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Follow the European Commission on social media">Follow the European Commission on social media</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Resources for partners">Resources for partners</a>',
+      links: [
+        {
+          link: {
+            label: 'Contact the European Commission',
+            path: '/example',
+            aria_label: 'Link to Contact the European Commission',
+          },
+        },
+        {
+          link: {
+            label: 'Follow the European Commission on social media',
+            path: '/example',
+            aria_label:
+              'Link to Follow the European Commission on social media',
+            icon_position: 'after',
+          },
+          icon: {
+            path: '/icons.svg',
+            type: 'ui',
+            name: 'external',
+            size: 'xs',
+          },
+        },
+        {
+          link: {
+            label: 'Resources for partners',
+            path: '/example',
+            aria_label: 'Link to Resources for partners',
+          },
+        },
       ],
     },
     {
-      items: [
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Language policy">Language policy</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Cookies">Cookies</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Privacy policy">Privacy policy</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Legal notice">Legal notice</a>',
+      links: [
+        {
+          link: {
+            label: 'Language policy',
+            path: '/example',
+            aria_label: 'Link to Language policy',
+          },
+        },
+        {
+          link: {
+            label: 'Cookies',
+            path: '/example',
+            aria_label: 'Link to Cookies',
+          },
+        },
+        {
+          link: {
+            label: 'Privacy policy',
+            path: '/example',
+            aria_label: 'Link to Privacy policy',
+          },
+        },
+        {
+          link: {
+            label: 'Legal notice',
+            path: '/example',
+            aria_label: 'Link to Legal notice',
+          },
+        },
       ],
     },
   ],
--- src/specs/components/footer-core/demo/data--eu.js
+++ src/specs/components/footer-core/demo/data--eu.js
@@ -14,53 +14,220 @@ module.exports = {
       {
         title: 'Contact the EU',
         title_class_name: 'ecl-footer-core__title--separator',
-        items: [
-          'Call us <a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to 00 800 6 7 8 9 10 11">00 800 6 7 8 9 10 11</a>',
-          'Use other <a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to telephone options">telephone options</a>',
-          'Write us via our <a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to contact form">contact form</a>',
-          'Meet us at a <a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to local EU office">local EU office</a>',
+        links: [
+          {
+            label_before: 'Call us ',
+            link: {
+              label: '00 800 6 7 8 9 10 11',
+              aria_label: 'Link to 00 800 6 7 8 9 10 11',
+              path: '/example',
+            },
+          },
+          {
+            label_before: 'Use other ',
+            link: {
+              label: 'telephone options',
+              aria_label: 'Link to telephone options',
+              path: '/example',
+            },
+          },
+          {
+            label_before: 'Write us via our ',
+            link: {
+              label: 'contact form',
+              aria_label: 'Link to contact form',
+              path: '/example',
+            },
+          },
+          {
+            label_before: 'Meet us at a ',
+            link: {
+              label: 'local EU office',
+              aria_label: 'Link to local EU offices',
+              path: '/example',
+            },
+          },
         ],
       },
       {
         title: 'Social media',
         title_class_name: 'ecl-footer-core__title--separator',
-        items: [
-          'Search for <a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to EU social media channels">EU social media channels</a>',
+        links: [
+          {
+            label_before: 'Search for ',
+            link: {
+              label: 'EU social media channels',
+              aria_label: 'Link to EU social media channels',
+              path: '/example',
+            },
+          },
         ],
       },
       {
         title: 'Legal',
         title_class_name: 'ecl-footer-core__title--separator',
-        items: [
-          '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Language policy">Language policy</a>',
-          '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Privacy policy">Privacy policy</a>',
-          '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Legal notice">Legal notice</a>',
-          '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Web accessibility">Web accessibility</a>',
-          '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Cookies">Cookies</a>',
+        links: [
+          {
+            link: {
+              label: 'Language policy',
+              aria_label: 'Link to language policy',
+              path: '/example',
+            },
+          },
+          {
+            link: {
+              label: 'Privacy policy',
+              aria_label: 'Link to Privacy policy',
+              path: '/example',
+            },
+          },
+          {
+            link: {
+              label: 'Legal notice',
+              aria_label: 'Link to Legal notice',
+              path: '/example',
+            },
+          },
+          {
+            link: {
+              label: 'Web accessibility',
+              aria_label: 'Link to Web accessibility',
+              path: '/example',
+            },
+          },
+          {
+            link: {
+              label: 'Cookies',
+              aria_label: 'Link to Cookies',
+              path: '/example',
+            },
+          },
         ],
       },
     ],
     {
       title: 'EU institutions',
       title_class_name: 'ecl-footer-core__title--separator',
-      items: [
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Parliament">European Parliament</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Council">European Council</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Council of the European Union">Council of the European Union</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Commission">European Commission</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Court of Justice of the European Union">Court of Justice of the European Union</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Central Bank">European Central Bank</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Court of Auditors">European Court of Auditors</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European External Action Service">European External Action Service</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Economic and Social Committee">European Economic and Social Committee</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Commitee of the Regions">European Commitee of the Regions</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Investment Bank">European Investment Bank</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Ombudsman">European Ombudsman</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Data Protection Supervisor">European Data Protection Supervisor</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to The European Data Protection Board">The European Data Protection Board</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to European Personnel Selection Office">European Personnel Selection Office</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Publications Office of the European Union">Publications Office of the European Union</a>',
-        '<a class="ecl-link ecl-link--standalone ecl-footer-core__link" href="/example" aria-label="Link to Agencies">Agencies</a>',
+      links: [
+        {
+          link: {
+            label: 'European Parliament',
+            aria_label: 'Link to European Parliament',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Council',
+            aria_label: 'Link to European Council',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'Council of the European Union',
+            aria_label: 'Link to Council of the European Union',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Commission',
+            aria_label: 'Link to European Commission',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'Court of Justice of the European Union',
+            aria_label: 'Link to Court of Justice of the European Union',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Central Bank',
+            aria_label: 'Link to European Central Bank',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Court of Auditors',
+            aria_label: 'Link to European Court of Auditors',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European External Action Service',
+            aria_label: 'Link to European External Action Service',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Economic and Social Committee',
+            aria_label: 'Link to European Economic and social Committee',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Commitee of the Regions',
+            aria_label: 'Link to European Commitee of the Regions',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Investment Bank',
+            aria_label: 'Link to European Investment Bank',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Ombudsman',
+            aria_label: 'Link to European Ombudsman',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Data Protection Supervisor',
+            aria_label: 'Link to European Data Protection Supervisor',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'The European Data Protection Board',
+            aria_label: 'Link to The European Data Protection Board',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'European Personnel Selection Office',
+            aria_label: 'Link to European Personnel Selection Office',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'Publications Office of the European Union',
+            aria_label: 'Link to Publications office of the European Union',
+            path: '/example',
+          },
+        },
+        {
+          link: {
+            label: 'Agencies',
+            aria_label: 'Link to Agencies',
+            path: '/example',
+          },
+        },
       ],
     },
   ],
