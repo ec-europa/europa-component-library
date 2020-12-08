314f38dc18e8248764e48df2e7b866d6d7919837
planctus <davidepuntoferrante@gmail.com>
2020-12-08 12:31:20 +0200

    feat(storyobook-system-switcher): Moving it to the right - FRONT-1836

--- src/tools/storybook-addon-system-switcher/src/register.jsx
+++ src/tools/storybook-addon-system-switcher/src/register.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { addons, types } from '@storybook/addons';
 import { Switcher } from '@ecl/storybook-addon-system-switcher/src/index.js';
 
-addons.register('ecl/storybook/system-switcher', () => {
-  addons.add('ecl/storybook/system-switcher', {
+const ADDON_ID = 'ecl/storybook/system-switcher';
+const TOOL_ID = `${ADDON_ID}/tool`;
+
+addons.register(ADDON_ID, () => {
+  addons.add(TOOL_ID, {
     title: 'Switcher',
-    type: types.TOOL,
-    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story)$/)),
+    type: types.TOOLEXTRA,
     render: () => <Switcher />,
   });
 });
