bf417bf0ca79c90dc35870848fbefea0c3fc2a9e
planctus <davidepuntoferrante@gmail.com>
2020-12-11 13:36:27 +0200

    feat(controls): Small refactoring and fixes - front-1847

--- src/implementations/twig/components/accordion/accordion.story.js
+++ src/implementations/twig/components/accordion/accordion.story.js
@@ -6,39 +6,43 @@ import demoData from '@ecl/specs-component-accordion/demo/data';
 import accordion from './accordion.html.twig';
 import notes from './README.md';
 
-const argTypes = {};
-demoData.items.forEach((item, i) => {
-  argTypes[`toggle${i + 1}`] = {
-    name: `toggle ${i + 1}`,
-    type: { name: 'string', required: true },
-    defaultValue: item.toggle.label,
-    description: `Text of the accordion toggler`,
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: '' },
-      category: 'Togglers',
-    },
-    control: {
-      type: 'text',
-    },
-  };
-  argTypes[`content${i + 1}`] = {
-    name: `content ${i + 1}`,
-    type: { name: 'string', required: true },
-    defaultValue: item.content,
-    description: 'Text of the hidden content',
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: '' },
-      category: 'Hidden content',
-    },
-    control: {
-      type: 'text',
-    },
-  };
-});
+const getArgTypes = () => {
+  const argTypes = {};
+  demoData.items.forEach((item, i) => {
+    argTypes[`toggle${i + 1}`] = {
+      name: `toggle ${i + 1}`,
+      type: { name: 'string', required: true },
+      defaultValue: item.toggle.label,
+      description: `Text of the accordion toggler`,
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Toggles',
+      },
+      control: {
+        type: 'text',
+      },
+    };
+    argTypes[`content${i + 1}`] = {
+      name: `content ${i + 1}`,
+      type: { name: 'string', required: true },
+      defaultValue: item.content,
+      description: 'Text of the hidden content',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Hidden content',
+      },
+      control: {
+        type: 'text',
+      },
+    };
+  });
 
-const prepareDataWithArgs = (data, args) => {
+  return argTypes;
+};
+
+const applySpecs = (data, args) => {
   demoData.items.forEach((item, i) => {
     item.toggle.icon.path = defaultSprite;
     item.toggle.icon.type = 'general';
@@ -61,8 +65,8 @@ export default {
   title: 'Components/Accordion',
 };
 
-export const Default = (args) => accordion(prepareDataWithArgs(demoData, args));
-Default.argTypes = argTypes;
+export const Default = (args) => accordion(applySpecs(demoData, args));
+Default.argTypes = getArgTypes();
 Default.storyName = 'default';
 Default.parameters = {
   notes: {
@@ -70,7 +74,7 @@ Default.parameters = {
     json: demoData,
   },
   knobs: {
-    disabled: true,
+    disable: true,
   },
 };
 Default.decorators = [withCode, withNotes];
