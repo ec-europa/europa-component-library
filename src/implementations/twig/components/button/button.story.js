3e9a4da1a5128b6f2653e034f97a22bb23050241
Alexis Gaillard <a@bypopcorn.com>
2020-12-11 14:21:07 +0100

    feat(button): Update based on reviews - FRONT-1857

--- src/implementations/twig/components/button/button.story.js
+++ src/implementations/twig/components/button/button.story.js
@@ -18,7 +18,92 @@ uiIcons.forEach((icon) => {
   iconsList[icon] = icon;
 });
 
-const prepareControls = (data, args) => {
+const getArgTypes = (data) => {
+  const argTypes = {};
+  argTypes.label = {
+    name: 'label',
+    type: { name: 'string', required: true },
+    defaultValue: data.label,
+    description: 'The main label of the button',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Content',
+    },
+    control: {
+      type: 'text',
+    },
+  };
+  argTypes.icon_name = {
+    name: 'icon name',
+    type: { name: 'select' },
+    defaultValue: data.icon.name,
+    description: 'Button icon',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Icon',
+    },
+    control: {
+      type: 'select',
+      options: iconsList,
+    },
+  };
+  argTypes.icon_transform = {
+    name: 'icon transform',
+    type: { name: 'select' },
+    defaultValue: data.icon.transform,
+    description: 'Button icon transform',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Icon',
+    },
+    control: {
+      type: 'select',
+      options: [
+        'rotate-90',
+        'rotate-180',
+        'rotate-270',
+        'flip-horizontal',
+        'flip-vertical',
+      ],
+    },
+  };
+  argTypes.icon_position = {
+    name: 'icon position',
+    type: { name: 'inline-radio' },
+    defaultValue: data.icon_position,
+    description: 'Icon position inside the button',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: 'after' },
+      category: 'Icon',
+    },
+    control: {
+      type: 'inline-radio',
+      options: ['before', 'after'],
+    },
+  };
+  argTypes.disabled = {
+    name: 'disabled',
+    type: { name: 'boolean' },
+    defaultValue: data.disabled,
+    description: 'Disabled button',
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: false },
+      category: 'States',
+    },
+    control: {
+      type: 'boolean',
+    },
+  };
+
+  return argTypes;
+};
+
+const applySpecs = (data, args) => {
   data.label = args.label;
   data.disabled = args.disabled;
   if (args.icon_name != null) {
@@ -38,129 +123,72 @@ const prepareControls = (data, args) => {
 
 export default {
   title: 'Components/Button',
-  argTypes: {
-    label: {
-      name: 'label',
-      type: { name: 'string', required: true },
-      description: 'The main label of the button',
-      table: {
-        type: { summary: 'string' },
-        defaultValue: { summary: '' },
-        category: 'Content',
-      },
-      control: {
-        type: 'text',
-      },
-    },
-    icon_name: {
-      name: 'icon name',
-      type: { name: 'select' },
-      description: 'Button icon',
-      table: {
-        type: { summary: 'string' },
-        defaultValue: { summary: '' },
-        category: 'Icon',
-      },
-      control: {
-        type: 'select',
-        options: iconsList,
-      },
-    },
-    icon_transform: {
-      name: 'icon transform',
-      type: { name: 'select' },
-      description: 'Button icon transform',
-      table: {
-        type: { summary: 'string' },
-        defaultValue: { summary: '' },
-        category: 'Icon',
-        disable: true,
-      },
-      control: {
-        type: 'select',
-        options: [
-          'rotate-90',
-          'rotate-180',
-          'rotate-270',
-          'flip-horizontal',
-          'flip-vertical',
-        ],
-      },
-    },
-    icon_position: {
-      name: 'icon position',
-      type: { name: 'inline-radio' },
-      defaultValue: 'after',
-      description: 'Icon position inside the button',
-      table: {
-        type: { summary: 'string' },
-        defaultValue: { summary: 'after' },
-        category: 'Icon',
-      },
-      control: {
-        type: 'inline-radio',
-        options: ['before', 'after'],
-      },
-    },
-    disabled: {
-      name: 'disabled',
-      type: { name: 'boolean' },
-      defaultValue: false,
-      description: 'Disabled button',
-      table: {
-        type: { summary: 'boolean' },
-        defaultValue: { summary: false },
-        category: 'States',
-      },
-      control: {
-        type: 'boolean',
-      },
-    },
-  },
   decorators: [withCode, withNotes],
 };
 
-export const Primary = (args) => button(prepareControls(dataPrimary, args));
+export const Primary = (args) => button(applySpecs(dataPrimary, args));
 
+Primary.argTypes = getArgTypes(dataPrimary);
 Primary.args = {
   label: dataPrimary.label,
 };
+
 Primary.storyName = 'primary';
-Primary.parameters = { notes: { markdown: notes, json: dataPrimary } };
+Primary.parameters = {
+  notes: { markdown: notes, json: dataPrimary },
+  knobs: { disable: true },
+};
 
-export const Secondary = (args) => button(prepareControls(dataSecondary, args));
+export const Secondary = (args) => button(applySpecs(dataSecondary, args));
 
+Secondary.argTypes = getArgTypes(dataSecondary);
 Secondary.args = {
   label: dataSecondary.label,
 };
+
 Secondary.storyName = 'secondary';
-Secondary.parameters = { notes: { markdown: notes, json: dataSecondary } };
-
-export const CallToAction = (args) => button(prepareControls(dataCall, args));
-
-CallToAction.argTypes = {
-  icon_transform: { table: { disable: false } },
+Secondary.parameters = {
+  notes: { markdown: notes, json: dataSecondary },
+  knobs: { disable: true },
 };
+
+export const CallToAction = (args) => button(applySpecs(dataCall, args));
+
+CallToAction.argTypes = getArgTypes(dataCall);
 CallToAction.args = {
   label: dataCall.label,
   icon_name: 'corner-arrow',
   icon_transform: 'rotate-90',
 };
+
 CallToAction.storyName = 'call to action';
-CallToAction.parameters = { notes: { markdown: notes, json: dataCall } };
+CallToAction.parameters = {
+  notes: { markdown: notes, json: dataCall },
+  knobs: { disable: true },
+};
 
-export const Ghost = (args) => button(prepareControls(dataGhost, args));
+export const Ghost = (args) => button(applySpecs(dataGhost, args));
 
+Ghost.argTypes = getArgTypes(dataGhost);
 Ghost.args = {
   label: dataGhost.label,
 };
+
 Ghost.storyName = 'text';
-Ghost.parameters = { notes: { markdown: notes, json: dataGhost } };
+Ghost.parameters = {
+  notes: { markdown: notes, json: dataGhost },
+  knobs: { disable: true },
+};
 
-export const Search = (args) => button(prepareControls(dataSearch, args));
+export const Search = (args) => button(applySpecs(dataSearch, args));
 
+Search.argTypes = getArgTypes(dataSearch);
 Search.args = {
   label: dataSearch.label,
 };
+
 Search.storyName = 'search';
-Search.parameters = { notes: { markdown: notes, json: dataSearch } };
+Search.parameters = {
+  notes: { markdown: notes, json: dataSearch },
+  knobs: { disable: true },
+};
