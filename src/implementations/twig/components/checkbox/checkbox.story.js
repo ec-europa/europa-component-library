573e04a5a330f474cfba4266fdfd93c34a6bf7af
Alexis Gaillard <a@bypopcorn.com>
2020-12-16 16:55:18 +0100

    feat(checkbox): Migrate knobs to controls- FRONT- 1859

--- src/implementations/twig/components/checkbox/checkbox.story.js
+++ src/implementations/twig/components/checkbox/checkbox.story.js
@@ -1,11 +1,4 @@
-import { withKnobs, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl/storybook-addon-notes';
-import {
-  getExtraKnobs,
-  getFormGroupKnobs,
-  tabLabels,
-  getFormItemKnobs,
-} from '@ecl/story-utils';
 import withCode from '@ecl/storybook-addon-code';
 
 import defaultSprite from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
@@ -14,21 +7,121 @@ import dataDefault from '@ecl/specs-component-checkbox/demo/data--default';
 import checkboxGroup from './checkbox-group.html.twig';
 import notes from './README.md';
 
-const prepareCheckbox = (data) => {
-  getFormGroupKnobs(data);
+const getArgTypes = (data) => {
+  const argTypes = {};
+  argTypes.label = {
+    name: 'legend',
+    type: { name: 'string', required: true },
+    defaultValue: data.label,
+    description: 'The heading of the checkbox group',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Content',
+    },
+    control: {
+      type: 'text',
+    },
+  };
+  argTypes.helper_text = {
+    name: 'group helper text',
+    type: { name: 'string' },
+    defaultValue: data.helper_text,
+    description: 'Helper information to describe the checkbox button group',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Content',
+    },
+    control: {
+      type: 'text',
+    },
+  };
+  argTypes.invalid_text = {
+    name: 'error message',
+    type: { name: 'string' },
+    defaultValue: data.invalid_text,
+    description:
+      'A message informing the user if there is a problem with their entry',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Content',
+    },
+    control: {
+      type: 'text',
+    },
+  };
+  argTypes.optional_text = {
+    name: 'optional text',
+    type: { name: 'string' },
+    defaultValue: data.optional_text,
+    description: 'Indicator text stating if input fields are optional',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Content',
+    },
+    control: {
+      type: 'text',
+    },
+  };
+  argTypes.required_text = {
+    name: 'required text',
+    type: { name: 'string' },
+    defaultValue: data.required_text,
+    description: 'Indicator text stating if input fields are mandatory',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '*' },
+      category: 'Content',
+    },
+    control: {
+      type: 'text',
+    },
+  };
+  argTypes.invalid = {
+    name: 'invalid',
+    type: { name: 'boolean' },
+    defaultValue: data.invalid,
+    description: 'Make the checkbox group invalid',
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: false },
+      category: 'State',
+    },
+    control: {
+      type: 'boolean',
+    },
+  };
+  argTypes.required = {
+    name: 'required',
+    type: { name: 'boolean' },
+    defaultValue: data.required,
+    description: 'Make the checkbox group required',
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: false },
+      category: 'State',
+    },
+    control: {
+      type: 'boolean',
+    },
+  };
 
-  getFormItemKnobs(data, true);
+  return argTypes;
+};
 
-  data.items.forEach((item, i) => {
-    item.icon_path = select(
-      `items[${i}].icon_path`,
-      [defaultSprite],
-      defaultSprite,
-      tabLabels.required
-    );
-  });
-
-  getExtraKnobs(data, true);
+const prepareData = (data, args) => {
+  data.icon_path = defaultSprite;
+  data.label = args.label;
+  data.helper_text = args.helper_text;
+  data.invalid_text = args.invalid_text;
+  data.required_text = args.required_text;
+  data.optional_text = args.optional_text;
+  data.invalid_text = args.invalid_text;
+  data.required = args.required;
+  data.invalid = args.invalid;
 
   return data;
 };
@@ -37,12 +130,9 @@ export default {
   title: 'Components/Forms/Checkbox',
 };
 
-export const Default = () => {
-  const data = prepareCheckbox(dataDefault);
+export const Default = (args) => checkboxGroup(prepareData(dataDefault, args));
 
-  return checkboxGroup(data);
-};
-
-Default.storName = 'default';
+Default.storyName = 'default';
+Default.argTypes = getArgTypes(dataDefault);
 Default.parameters = { notes: { markdown: notes, json: dataDefault } };
-Default.decorators = [withKnobs, withNotes, withCode];
+Default.decorators = [withNotes, withCode];
