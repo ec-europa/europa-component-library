84f70413c77f6c5eb661120ee6c23af7f456b569
Kalin Chernev <kalin.chernev@gmail.com>
2020-12-17 18:15:08 +0100

    focus scripts

--- src/presets/ec-core/scripts/watch.js
+++ src/presets/ec-core/scripts/watch.js
@@ -2,33 +2,37 @@ const path = require('path');
 const { exec } = require('child_process');
 const bs = require('browser-sync').create();
 
-bs.watch(`${path.resolve(__dirname, '../..')}/ec-core/src/*.scss`, (event) => {
-  if (event === 'change') {
-    bs.notify(`Change in ec-core preset scss files, rebuilding styles ...`);
-    const childProcess = exec('npm run build:styles');
-    childProcess.on('data', (data) => console.log(data));
-    childProcess.on('error', (err) => console.error(err));
-    childProcess.on('exit', () => {
-      bs.notify('New styles ready');
-      bs.reload('*.css');
-    });
+bs.watch(
+  `${path.resolve(__dirname, '../..')}/(dev|ec-core)/src/*.scss`,
+  (event) => {
+    if (event === 'change') {
+      bs.notify(`Change in preset scss files, rebuilding styles ...`);
+      const childProcess = exec('npm run build:styles');
+      childProcess.on('data', (data) => console.log(data));
+      childProcess.on('error', (err) => console.error(err));
+      childProcess.on('exit', () => {
+        bs.notify('New styles ready');
+        bs.reload('*.css');
+      });
+    }
   }
-});
+);
 
-bs.watch(`${path.resolve(__dirname, '../..')}/ec-core/src/*.js`, (event) => {
-  if (event === 'change') {
-    bs.notify(
-      `Change in ec-core preset javascript files, rebuilding scripts ...`
-    );
-    const childProcess = exec('npm run build:scripts');
-    childProcess.on('data', (data) => console.log(data));
-    childProcess.on('error', (err) => console.error(err));
-    childProcess.on('exit', () => {
-      bs.notify('New scripts ready');
-      bs.reload('*.js');
-    });
+bs.watch(
+  `${path.resolve(__dirname, '../..')}/(dev|ec-core)/src/*.js`,
+  (event) => {
+    if (event === 'change') {
+      bs.notify(`Change in preset javascript files, rebuilding scripts ...`);
+      const childProcess = exec('npm run build:scripts');
+      childProcess.on('data', (data) => console.log(data));
+      childProcess.on('error', (err) => console.error(err));
+      childProcess.on('exit', () => {
+        bs.notify('New scripts ready');
+        bs.reload('ec-core.js');
+      });
+    }
   }
-});
+);
 
 bs.watch(
   `${path.resolve(__dirname, '../../..')}/implementations/vanilla/**/*.scss`,
@@ -56,24 +60,27 @@ bs.watch(
       childProcess.on('error', (err) => console.error(err));
       childProcess.on('exit', () => {
         bs.notify('New scripts ready');
-        bs.reload('*.js');
+        bs.reload('ec-core.js');
       });
     }
   }
 );
 
-bs.watch(`${path.resolve(__dirname, '../../..')}/themes/**/*.scss`, (event) => {
-  if (event === 'change') {
-    bs.notify(`Change in theme scss files, rebuilding styles ...`);
-    const childProcess = exec('npm run build:styles');
-    childProcess.on('data', (data) => console.log(data));
-    childProcess.on('error', (err) => console.error(err));
-    childProcess.on('exit', () => {
-      bs.notify('New styles ready');
-      bs.reload('*.css');
-    });
+bs.watch(
+  `${path.resolve(__dirname, '../../..')}/themes/(dev|ec-core)/**/*.scss`,
+  (event) => {
+    if (event === 'change') {
+      bs.notify(`Change in theme scss files, rebuilding styles ...`);
+      const childProcess = exec('npm run build:styles');
+      childProcess.on('data', (data) => console.log(data));
+      childProcess.on('error', (err) => console.error(err));
+      childProcess.on('exit', () => {
+        bs.notify('New styles ready');
+        bs.reload('*.css');
+      });
+    }
   }
-});
+);
 
 bs.init({
   proxy: 'localhost:6006', // ec storybook
