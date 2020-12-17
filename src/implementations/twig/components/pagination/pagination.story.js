f6aeb14da02b5a393467ff354b3e91680938d021
Alexis Gaillard <a@bypopcorn.com>
2020-12-17 17:11:00 +0100

    feat(pagination): Use correctSvgPath utils - FRONT-1855

--- src/implementations/twig/components/pagination/pagination.story.js
+++ src/implementations/twig/components/pagination/pagination.story.js
@@ -1,16 +1,10 @@
 import { withNotes } from '@ecl/storybook-addon-notes';
 import withCode from '@ecl/storybook-addon-code';
+import { correctSvgPath } from '@ecl/story-utils';
 
-import defaultSprite from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
 import specs from '@ecl/specs-component-pagination/demo/data';
 import pagination from './pagination.html.twig';
 import notes from './README.md';
-
-const prepareData = (data) => {
-  data.icon_path = defaultSprite;
-
-  return data;
-};
 
 export default {
   title: 'Components/Navigation/Pagination',
@@ -21,7 +15,7 @@ export default {
   },
 };
 
-export const Default = () => pagination(prepareData(specs));
+export const Default = () => pagination(correctSvgPath(specs));
 
 Default.storyName = 'default';
 Default.parameters = { notes: { markdown: notes, json: specs } };
