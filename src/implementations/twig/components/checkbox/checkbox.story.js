f9bac80b6e28d5636ef2fa40870f8e0a30ea3633
Alexis Gaillard <a@bypopcorn.com>
2020-12-18 01:23:39 +0100

    feat(checkbox): Add forms argTypes as utiles - FRONT-1859

--- src/implementations/twig/components/checkbox/checkbox.story.js
+++ src/implementations/twig/components/checkbox/checkbox.story.js
@@ -1,137 +1,23 @@
 import { withNotes } from '@ecl/storybook-addon-notes';
 import withCode from '@ecl/storybook-addon-code';
+import { correctSvgPath, getFormControls } from '@ecl/story-utils';
 
-import defaultSprite from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
 import dataDefault from '@ecl/specs-component-checkbox/demo/data--default';
 
 import checkboxGroup from './checkbox-group.html.twig';
 import notes from './README.md';
 
-const getArgTypes = (data) => {
-  const argTypes = {};
-  argTypes.label = {
-    name: 'legend',
-    type: { name: 'string', required: true },
-    defaultValue: data.label,
-    description: 'The heading of the checkbox group',
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: '' },
-      category: 'Content',
-    },
-    control: {
-      type: 'text',
-    },
-  };
-  argTypes.helper_text = {
-    name: 'group helper text',
-    type: { name: 'string' },
-    defaultValue: data.helper_text,
-    description: 'Helper information to describe the checkbox button group',
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: '' },
-      category: 'Content',
-    },
-    control: {
-      type: 'text',
-    },
-  };
-  argTypes.invalid_text = {
-    name: 'error message',
-    type: { name: 'string' },
-    defaultValue: data.invalid_text,
-    description:
-      'A message informing the user if there is a problem with their entry',
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: '' },
-      category: 'Content',
-    },
-    control: {
-      type: 'text',
-    },
-  };
-  argTypes.optional_text = {
-    name: 'optional text',
-    type: { name: 'string' },
-    defaultValue: data.optional_text,
-    description: 'Indicator text stating if input fields are optional',
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: '' },
-      category: 'Content',
-    },
-    control: {
-      type: 'text',
-    },
-  };
-  argTypes.required_text = {
-    name: 'required text',
-    type: { name: 'string' },
-    defaultValue: data.required_text,
-    description: 'Indicator text stating if input fields are mandatory',
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: '*' },
-      category: 'Content',
-    },
-    control: {
-      type: 'text',
-    },
-  };
-  argTypes.invalid = {
-    name: 'invalid',
-    type: { name: 'boolean' },
-    defaultValue: data.invalid,
-    description: 'Make the checkbox group invalid',
-    table: {
-      type: { summary: 'boolean' },
-      defaultValue: { summary: false },
-      category: 'State',
-    },
-    control: {
-      type: 'boolean',
-    },
-  };
-  argTypes.required = {
-    name: 'required',
-    type: { name: 'boolean' },
-    defaultValue: data.required,
-    description: 'Make the checkbox group required',
-    table: {
-      type: { summary: 'boolean' },
-      defaultValue: { summary: false },
-      category: 'State',
-    },
-    control: {
-      type: 'boolean',
-    },
-  };
-
-  return argTypes;
-};
+const getArgTypes = (data) => getFormControls(data, 'group');
 
 const prepareData = (data, args) => {
-  data.icon_path = defaultSprite;
-  data.label = args.label;
-  data.helper_text = args.helper_text;
-  data.invalid_text = args.invalid_text;
-  data.required_text = args.required_text;
-  data.optional_text = args.optional_text;
-  data.invalid_text = args.invalid_text;
-  data.required = args.required;
-  data.invalid = args.invalid;
-
-  return data;
+  correctSvgPath(data);
+  return Object.assign(data, args);
 };
 
 export default {
   title: 'Components/Forms/Checkbox',
   parameters: {
-    knobs: {
-      disable: true,
-    },
+    knobs: { disable: true },
   },
 };
 
