db97a8166ca3af72cb0894ef930589d6ce70c03e
Alexis Gaillard <a@bypopcorn.com>
2020-12-21 12:01:30 +0100

    feat(description-list): Add controls for the first item - FRONT-1868

--- src/implementations/twig/components/description-list/description-list.story.js
+++ src/implementations/twig/components/description-list/description-list.story.js
@@ -7,25 +7,59 @@ import dataDescriptionListHorizontal from '@ecl/specs-component-description-list
 import descriptionList from './description-list.html.twig';
 import notes from './README.md';
 
+const getArgTypes = (data) => {
+  return {
+    term: {
+      name: 'first item term',
+      type: { name: 'string', required: true },
+      defaultValue: data.items[0].term,
+      description: 'Test the content for the first item term',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    },
+    description: {
+      name: 'first item definition',
+      type: { name: 'string', required: true },
+      defaultValue: data.items[0].definition,
+      description: 'Test the content for the first item definition',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    },
+  };
+};
+
+const prepareData = (data, args) => {
+  return Object.assign(data, args);
+};
+
 export default {
   title: 'Components/List/Description list',
   decorators: [withNotes, withCode],
   parameters: {
     knobs: { disable: true },
-    controls: { disable: true },
   },
 };
 
-export const Vertical = () => descriptionList(dataDescriptionListDefault);
+export const Vertical = (args) =>
+  descriptionList(dataDescriptionListDefault, args);
 
 Vertical.storyName = 'vertical';
+Vertical.argTypes = getArgTypes(prepareData(dataDescriptionListDefault));
 Vertical.parameters = {
   notes: { markdown: notes, json: dataDescriptionListDefault },
 };
 
-export const Horizontal = () => descriptionList(dataDescriptionListHorizontal);
+export const Horizontal = (args) =>
+  descriptionList(dataDescriptionListHorizontal, args);
 
 Horizontal.storyName = 'horizontal';
+Horizontal.argTypes = getArgTypes(prepareData(dataDescriptionListHorizontal));
 Horizontal.parameters = {
   notes: { markdown: notes, json: dataDescriptionListHorizontal },
 };
