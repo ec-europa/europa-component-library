63ef28b129ea07953c600f7c75df1b6ca0e54dad
planctus <davidepuntoferrante@gmail.com>
2020-12-21 12:28:15 +0200

    feat(controls): Textarea, replacing knobs with controls - front-1865

--- src/implementations/twig/components/text-area/text-area.story.js
+++ src/implementations/twig/components/text-area/text-area.story.js
@@ -1,24 +1,64 @@
 import { withKnobs } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl/storybook-addon-notes';
-import { getExtraKnobs, getFormKnobs } from '@ecl/story-utils';
+import { getFormControls } from '@ecl/story-utils';
 import withCode from '@ecl/storybook-addon-code';
-import specData from '@ecl/specs-component-text-area/demo/data--default';
+import demoData from '@ecl/specs-component-text-area/demo/data--default';
+
 import textArea from './text-area.html.twig';
 import notes from './README.md';
 
-const prepareTextArea = (data) => {
-  getFormKnobs(data);
-  getExtraKnobs(data);
+const getArgTypes = (data) => {
+  return {
+    ...getFormControls(data),
+    width: {
+      type: { name: 'select' },
+      description: 'Width of the textarea',
+      defaultValue: 'm',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'm' },
+        category: 'Content',
+      },
+      control: {
+        type: 'select',
+        options: {
+          small: 's',
+          medium: 'm',
+          large: 'l',
+        },
+      },
+    },
+    rows: {
+      type: { name: 'number' },
+      description: 'Number or rows',
+      defaultValue: 4,
+      table: {
+        type: { summary: 'integer' },
+        min: 1,
+        step: 1,
+        defaultValue: { summary: '4' },
+        category: 'Content',
+      },
+    },
+  };
+};
 
-  return data;
+const prepareData = (data, args) => {
+  return Object.assign(data, args);
 };
 
 export default {
   title: 'Components/Forms/Text area',
+  parameters: {
+    knobs: {
+      disable: true,
+    },
+  },
 };
 
-export const Default = () => textArea(prepareTextArea(specData));
+export const Default = (args) => textArea(prepareData(demoData, args));
 
 Default.storyName = 'default';
-Default.parameters = { notes: { markdown: notes, json: specData } };
+Default.argTypes = getArgTypes(demoData);
+Default.parameters = { notes: { markdown: notes, json: demoData } };
 Default.decorators = [withKnobs, withNotes, withCode];
