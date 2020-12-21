c50c26546c0c1e03199ee04fbd715ee2b72950b3
Romain Emery <emery.romain@gmail.com>
2020-12-21 12:31:36 +0100

    update doc (#1843)

--- src/website/src/pages/ec/components/date-block/demo/index.js
+++ src/website/src/pages/ec/components/date-block/demo/index.js
@@ -1,7 +1,6 @@
 import demoDefault from '@ecl/specs-component-date-block/demo/data';
 import template from '@ecl/twig-component-date-block/date-block.html.twig';
 
-demoDefault.variant = 'default';
 const demoCancelled = { ...demoDefault, variant: 'cancelled' };
 const demoOngoing = { ...demoDefault, variant: 'ongoing' };
 const demoPast = { ...demoDefault, variant: 'past' };
