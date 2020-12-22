c439970e4449f8f5ce34b84bae06b0377666eea4
Alexis Gaillard <a@bypopcorn.com>
2020-12-22 14:56:17 +0100

    feat(file): Add controls - FRONT-1873 (#1851)

--- src/implementations/twig/components/file/file.story.js
+++ src/implementations/twig/components/file/file.story.js
@@ -1,9 +1,7 @@
-import { withKnobs, text, optionsKnob, array } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl/storybook-addon-notes';
-import { getExtraKnobs, tabLabels } from '@ecl/story-utils';
+import { correctSvgPath } from '@ecl/story-utils';
 import withCode from '@ecl/storybook-addon-code';
 
-import defaultSprite from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
 import dataWithTranslation from '@ecl/specs-component-file/demo/data--with-translation';
 import dataWithoutTranslation from '@ecl/specs-component-file/demo/data--without-translation';
 import dataThumbnail from '@ecl/specs-component-file/demo/data--thumbnail';
@@ -11,138 +9,147 @@ import dataThumbnail from '@ecl/specs-component-file/demo/data--thumbnail';
 import file from './file.html.twig';
 import notes from './README.md';
 
-const prepareFile = (data, variant) => {
-  data.title = text('title', data.title, tabLabels.required);
-  data.language = text('language', data.language, tabLabels.required);
-  data.meta = text('meta', data.meta, tabLabels.required);
-  if (variant) {
-    data.description = text(
-      'description',
-      data.description,
-      tabLabels.optional
-    );
-    data.image.src = text('image.src', data.image.src, tabLabels.optional);
-    data.image.alt = text('image.alt', data.image.alt, tabLabels.optional);
-    data.detail_meta = array(
-      'detail_meta (array)',
-      data.detail_meta,
-      '|',
-      tabLabels.optional
-    );
+const getArgTypes = (data) => {
+  const argTypes = {};
+  if (data.detail_meta) {
+    argTypes.detail_meta = {
+      name: 'Meta',
+      type: 'array',
+      defaultValue: data.detail_meta,
+      description: 'File metadatas (comma separated)',
+      table: {
+        type: { summary: 'array' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    };
   }
-  data.icon.path = optionsKnob(
-    'icon.path',
-    { current: defaultSprite, 'no path': '' },
-    defaultSprite,
-    { display: 'inline-radio' },
-    tabLabels.required
-  );
-  data.download.link.label = text(
-    'download.link.label',
-    data.download.link.label,
-    tabLabels.required
-  );
-  data.download.link.aria_label = text(
-    'download.link.aria_label',
-    data.download.link.aria_label,
-    tabLabels.required
-  );
-  data.download.icon.path = optionsKnob(
-    'download.icon.path',
-    { current: defaultSprite, 'no path': '' },
-    defaultSprite,
-    { display: 'inline-radio' },
-    tabLabels.required
-  );
+
+  argTypes.title = {
+    name: 'title',
+    type: { name: 'string', required: true },
+    defaultValue: data.title,
+    description: 'The heading that describes the file',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Content',
+    },
+  };
+
+  if (data.description) {
+    argTypes.description = {
+      name: 'description',
+      type: 'string',
+      defaultValue: data.description,
+      description: 'A summary that describes the content of the file',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    };
+  }
+
+  argTypes.download_label = {
+    name: 'download label',
+    type: { name: 'string', required: true },
+    defaultValue: data.download.link.label,
+    description: 'The label of the link to download the file',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: '' },
+      category: 'Content',
+    },
+  };
+
+  if (data.image) {
+    argTypes.image = {
+      name: 'image example',
+      type: 'select',
+      defaultValue: data.image.src,
+      description: 'Select different image variant to test thumbnail size',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+      control: {
+        type: 'select',
+        options: {
+          landscape:
+            'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
+          portrait:
+            'https://inno-ecl.s3.amazonaws.com/media/examples/example-image10.jpg',
+          square:
+            'https://inno-ecl.s3.amazonaws.com/media/examples/example-image-square.jpg',
+        },
+      },
+    };
+  }
 
   if (data.translation) {
-    data.translation.description = text(
-      'translation.description',
-      data.translation.description,
-      tabLabels.optional
-    );
-    data.translation.toggle.label = text(
-      'translation.toggle.label',
-      data.translation.toggle.label,
-      tabLabels.required
-    );
-    data.translation.toggle.icon.path = optionsKnob(
-      'translation.toggle.icon.path',
-      { current: defaultSprite, 'no path': '' },
-      defaultSprite,
-      { display: 'inline-radio' },
-      tabLabels.required
-    );
-
-    data.translation.items.forEach((item, i) => {
-      data.translation.items[i].title = text(
-        `data.translation.items[${i}].title`,
-        data.translation.items[i].title,
-        tabLabels.required
-      );
-      data.translation.items[i].meta = text(
-        `data.translation.items[${i}].meta`,
-        data.translation.items[i].meta,
-        tabLabels.required
-      );
-      data.translation.items[i].lang = text(
-        `data.translation.items[${i}].lang`,
-        data.translation.items[i].lang,
-        tabLabels.required
-      );
-      if (data.translation.items[i].description) {
-        data.translation.items[i].description = text(
-          `data.translation.items[${i}].description`,
-          data.translation.items[i].description,
-          tabLabels.required
-        );
-      }
-      data.translation.items[i].download.link.label = text(
-        `data.translation.items[${i}].download.link.label`,
-        data.translation.items[i].download.link.label,
-        tabLabels.required
-      );
-      data.translation.items[i].download.link.path = text(
-        `data.translation.items[${i}].download.link.path`,
-        data.translation.items[i].download.link.path,
-        tabLabels.required
-      );
-      data.translation.items[i].download.icon.path = optionsKnob(
-        `data.translation.items[${i}].download.icon.path`,
-        { current: defaultSprite, 'no path': '' },
-        defaultSprite,
-        { display: 'inline-radio' },
-        tabLabels.required
-      );
-    });
+    argTypes.toggle_label = {
+      name: 'toggle label',
+      type: 'string',
+      defaultValue: data.translation.toggle.label,
+      description:
+        'The label of the button to display the file in different languages available',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    };
   }
 
-  getExtraKnobs(data);
+  return argTypes;
+};
+
+const prepareData = (data, args) => {
+  correctSvgPath(data);
+  data.detail_meta = args.detail_meta;
+  data.title = args.title;
+  data.description = args.description;
+  data.download.link.label = args.download_label;
+  if (data.image) {
+    data.image.src = args.image;
+  }
+  if (data.translation) {
+    data.translation.toggle.label = args.toggle_label;
+  }
 
   return data;
 };
 
 export default {
   title: 'Components/File',
-  decorators: [withNotes, withCode, withKnobs],
+  decorators: [withNotes, withCode],
+  parameters: {
+    knobs: { disable: true },
+  },
 };
 
-export const WithoutTranslation = () =>
-  file(prepareFile(dataWithoutTranslation));
+export const WithoutTranslation = (args) =>
+  file(prepareData(dataWithoutTranslation, args));
 
 WithoutTranslation.storyName = 'without translation';
+WithoutTranslation.argTypes = getArgTypes(dataWithoutTranslation);
 WithoutTranslation.parameters = {
   notes: { markdown: notes, json: dataWithoutTranslation },
 };
 
-export const WithTranslation = () => file(prepareFile(dataWithTranslation));
+export const WithTranslation = (args) =>
+  file(prepareData(dataWithTranslation, args));
 
 WithTranslation.storyName = 'with translation';
+WithTranslation.argTypes = getArgTypes(dataWithTranslation);
 WithTranslation.parameters = {
   notes: { markdown: notes, json: dataWithTranslation },
 };
 
-export const Thumbnail = () => file(prepareFile(dataThumbnail, 'thumbnail'));
+export const Thumbnail = (args) => file(prepareData(dataThumbnail, args));
 
 Thumbnail.storyName = 'thumbnail';
+Thumbnail.argTypes = getArgTypes(dataThumbnail);
 Thumbnail.parameters = { notes: { markdown: notes, json: dataThumbnail } };
