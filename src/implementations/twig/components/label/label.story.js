5d05d064e1bc6a270a6739815762ffb47d68d170
emeryro <emery.romain@gmail.com>
2020-12-23 13:43:20 +0100

    add controls

--- src/implementations/twig/components/label/label.story.js
+++ src/implementations/twig/components/label/label.story.js
@@ -1,5 +1,3 @@
-import { withKnobs, text, select } from '@storybook/addon-knobs';
-import { getExtraKnobs, tabLabels } from '@ecl/story-utils';
 import { withNotes } from '@ecl/storybook-addon-notes';
 import withCode from '@ecl/storybook-addon-code';
 
@@ -7,31 +5,58 @@ import dataDefault from '@ecl/specs-component-label/demo/data';
 import label from './label.html.twig';
 import notes from './README.md';
 
-const options = {
-  'low importance': 'low',
-  'medium importance': 'medium',
-  'high importance': 'high',
-  highlight: 'highlight',
+const getArgTypes = (data) => {
+  return {
+    label: {
+      name: 'label',
+      type: { name: 'string', required: true },
+      defaultValue: data.label,
+      description: 'The content of the label',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    },
+    variant: {
+      name: 'variant',
+      type: { name: 'select' },
+      defaultValue: 'low',
+      description: 'The variant / importance of the label',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'low' },
+        category: 'Content',
+      },
+      control: {
+        type: 'select',
+        options: {
+          'low importance': 'low',
+          'medium importance': 'medium',
+          'high importance': 'high',
+          highlight: 'highlight',
+        },
+      },
+    },
+  };
 };
 
-const prepareLabel = (data) => {
-  data.label = text('label', data.label, tabLabels.required);
-  data.variant = select(
-    'variant',
-    options,
-    options['low importance'],
-    tabLabels.optional
-  );
-
-  getExtraKnobs(data);
-
-  return data;
+const prepareData = (data, args) => {
+  return Object.assign(data, args);
 };
 
-export default { title: 'Components/Label' };
+export default {
+  title: 'Components/Label',
+  decorators: [withNotes, withCode],
+  parameters: {
+    knobs: { disable: true },
+  },
+};
 
-export const Default = () => label(prepareLabel(dataDefault));
+export const Default = (args) => label(prepareData(dataDefault, args));
 
 Default.storyName = 'default';
-Default.parameters = { notes: { markdown: notes, json: dataDefault } };
-Default.decorators = [withKnobs, withNotes, withCode];
+Default.argTypes = getArgTypes(dataDefault);
+Default.parameters = {
+  notes: { markdown: notes, json: dataDefault },
+};
