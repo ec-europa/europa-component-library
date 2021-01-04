b88554e94e4246f3ad9ecf7871e4d8c167a9d0ad
Alexis Gaillard <a@bypopcorn.com>
2021-01-04 17:37:43 +0100

    Update breadcrumbs with EC and EU specs.

--- src/specs/components/breadcrumb/demo/data--eu.js
+++ src/specs/components/breadcrumb/demo/data--eu.js
@@ -4,7 +4,7 @@ module.exports = {
     { label: 'Home', path: '/example' },
     { label: 'About the European Union', path: '/example' },
     { label: 'Organisational structure', path: '/example' },
-    { label: 'How the Commission is organised', path: '/example' },
+    { label: 'How the Union is organised', path: '/example' },
     { label: 'News' },
   ],
   navigation_text: 'You are here:',
