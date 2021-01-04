91693893c7019c4df515cceb796bb2bbf0150f7d
Alexis Gaillard <a@bypopcorn.com>
2021-01-04 17:49:50 +0100

    feat(page-header-core) Add controls - FRONT-1885 (#1865)
    * feat(page-header-core) Add controls - FRONT-1885
    
    * Update argTypes.
    
    * Create EC and EU specs for breadcrumb
    
    * Update breadcrumbs with EC and EU specs.

--- src/implementations/twig/components/page-header-harmonised/page-header-harmonised.story.js
+++ src/implementations/twig/components/page-header-harmonised/page-header-harmonised.story.js
@@ -7,7 +7,7 @@ import defaultSprite from '@ecl/resources-ec-icons/dist/sprites/icons.svg';
 import demoTitleContent from '@ecl/specs-component-page-header-harmonised/demo//data--title';
 import demoMetaTitleContent from '@ecl/specs-component-page-header-harmonised/demo/data--meta-title';
 import demoMetaTitleDescriptionContent from '@ecl/specs-component-page-header-harmonised/demo/data--meta-title-description';
-import dataBreadcrumbLong from '@ecl/specs-component-breadcrumb/demo/data';
+import dataBreadcrumbLong from '@ecl/specs-component-breadcrumb/demo/data--ec';
 import pageHeaderHarmonised from './page-header-harmonised.html.twig';
 import notes from './README.md';
 
---
+++ src/specs/components/breadcrumb/demo/data-simple--eu.js
@@ -0,0 +1,11 @@
+// Simple content for demo
+module.exports = {
+  links: [
+    { label: 'Home', path: '/example' },
+    { label: 'About the European Union', path: '/example' },
+    { label: 'News' },
+  ],
+  navigation_text: 'You are here:',
+  icon_file_path: '/icons.svg',
+  ellipsis_label: 'Click to expand',
+};
