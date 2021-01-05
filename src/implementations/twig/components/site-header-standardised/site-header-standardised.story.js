7f77a8bd8c10af9d3fc71dc8cfc29894877e474c
planctus <davidepuntoferrante@gmail.com>
2021-01-05 15:37:41 +0200

    feat(site-header-standardised): Adding EU version, replacing knobs with controls - front-1890

--- src/implementations/twig/components/site-header-standardised/site-header-standardised.story.js
+++ src/implementations/twig/components/site-header-standardised/site-header-standardised.story.js
@@ -5,6 +5,8 @@ import withCode from '@ecl/storybook-addon-code';
 
 import englishBanner from '@ecl/resources-ec-logo/logo--en.svg';
 import frenchBanner from '@ecl/resources-ec-logo/logo--fr.svg';
+import euEnglishBanner from '@ecl/resources-eu-logo/standard-version/positive/en.svg';
+import euFrenchBanner from '@ecl/resources-eu-logo/standard-version/positive/fr.svg';
 import euFrenchMobileBanner from '@ecl/resources-eu-logo/condensed-version/positive/fr.svg';
 import euEnglishMobileBanner from '@ecl/resources-eu-logo/condensed-version/positive/en.svg';
 import englishData from '@ecl/specs-component-site-header-standardised/demo/data';
@@ -15,8 +17,11 @@ import notes from './README.md';
 const system = getSystem();
 const enData = { ...englishData };
 const frData = { ...frenchData };
+const menuEn = { ...enData.menu };
+const menuFr = { ...frData.menu };
+const languageSelector = { ...enData.language_selector };
 const loggedInData = { ...enData, logged: true };
-const clonedLoggedInData = { loggedInData };
+const clonedLoggedInData = { ...loggedInData };
 
 const getArgTypes = () => {
   return {
@@ -62,16 +67,21 @@ const prepareData = (data, demo, args) => {
       data = demo === 'default' ? enData : frData;
     }
   }
+
   if (!args.menu) {
     delete data.menu;
   } else if (args.menu && !data.menu) {
-    if (demo === 'logged') {
-      data = clonedLoggedInData;
-    } else {
-      data = demo === 'default' ? enData : frData;
-    }
+    data.menu = demo !== 'translated' ? menuEn : menuFr;
   }
+
+  if (!args.language_selector) {
+    delete data.language_selector;
+  } else if (args.language_selector && !data.language_selector) {
+    data.language_selector = languageSelector;
+  }
+
   correctSvgPath(data);
+
   if (system === 'ec') {
     if (demo !== 'translated') {
       data.logo.src_desktop = englishBanner;
@@ -79,23 +89,11 @@ const prepareData = (data, demo, args) => {
       data.logo.src_desktop = frenchBanner;
     }
   } else if (demo !== 'translated') {
-    if (args.menu) {
-      data.menu = dataMenuEn;
-      data.menu.site_name = '';
-    } else if (!args.menu && data.menu) {
-      delete data.menu;
-    }
-
+    delete data.banner_top;
     data.logo.src_desktop = euEnglishBanner;
     data.logo.src_mobile = euEnglishMobileBanner;
   } else {
-    if (args.menu) {
-      data.menu = dataMenuFr;
-      data.menu.site_name = '';
-    } else if (!args.menu && data.menu) {
-      delete data.menu;
-    }
-
+    delete data.banner_top;
     data.logo.src_desktop = euFrenchBanner;
     data.logo.src_mobile = euFrenchMobileBanner;
   }
@@ -106,38 +104,28 @@ const prepareData = (data, demo, args) => {
 export default {
   title: 'Components/Site Headers/Standardised',
   decorators: [withNotes, withCode],
+  parameters: {
+    knobs: { disable: true },
+  },
 };
 
-export const Default = () => {
-  button(btnLangLabel, enBtnLangHandler, tabLabels.cases);
-  button(btnLoginLabel, enBtnLoginHandler, tabLabels.cases);
-  button(btnMenuLabel, enBtnMenuHandler, tabLabels.cases);
-
-  return siteHeaderStandardised(prepareSiteHeaderStandardised(enData, 'en'));
-};
+export const Default = (args) =>
+  siteHeaderStandardised(prepareData(englishData, 'default', args));
 
 Default.storyName = 'default';
-Default.parameters = { notes: { markdown: notes, json: enData } };
+Default.argTypes = getArgTypes();
+Default.parameters = { notes: { markdown: notes, json: englishData } };
 
-export const LoggedIn = () => {
-  button(btnLangLabel, enBtnLangHandler, tabLabels.cases);
-  button(btnMenuLabel, enBtnMenuLoggedHandler, tabLabels.cases);
-
-  return siteHeaderStandardised(
-    prepareSiteHeaderStandardised(loggedInData, 'en')
-  );
-};
+export const LoggedIn = (args) =>
+  siteHeaderStandardised(prepareData(loggedInData, 'logged', args));
 
 LoggedIn.storyName = 'logged in';
+LoggedIn.argTypes = getArgTypes();
 LoggedIn.parameters = { notes: { markdown: notes, json: loggedInData } };
 
-export const Translated = () => {
-  button(btnLangLabel, frBtnLangHandler, tabLabels.cases);
-  button(btnLoginLabel, frBtnLoginHandler, tabLabels.cases);
-  button(btnMenuLabel, frBtnMenuHandler, tabLabels.cases);
-
-  return siteHeaderStandardised(prepareSiteHeaderStandardised(frData, 'fr'));
-};
+export const Translated = (args) =>
+  siteHeaderStandardised(prepareData(frenchData, 'translated', args));
 
 Translated.storyName = 'translated';
-Translated.parameters = { notes: { markdown: notes, json: frData } };
+Translated.argTypes = getArgTypes();
+Translated.parameters = { notes: { markdown: notes, json: frenchData } };
