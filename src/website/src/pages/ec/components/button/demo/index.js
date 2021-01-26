5042bd6121971fd4017fe55b1326e7cd46e8c894
Romain Emery <emery.romain@gmail.com>
2021-01-26 08:06:29 +0100

    feat(button): add EU button - FRONT-1931 (#1878)
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

--- src/website/src/pages/ec/components/button/demo/index.js
+++ src/website/src/pages/ec/components/button/demo/index.js
@@ -2,7 +2,6 @@ import demoContentPrimary from '@ecl/specs-component-button/demo/data--primary';
 import demoContentSecondary from '@ecl/specs-component-button/demo/data--secondary';
 import demoContentCall from '@ecl/specs-component-button/demo/data--call';
 import demoContentGhost from '@ecl/specs-component-button/demo/data--ghost';
-import demoContentSearch from '@ecl/specs-component-button/demo/data--search';
 import template from '@ecl/twig-component-button/button.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
@@ -10,4 +9,3 @@ export const buttonPrimary = template(demoContentPrimary);
 export const buttonSecondary = template(demoContentSecondary);
 export const buttonCall = template(correctSvgPath(demoContentCall));
 export const buttonGhost = template(demoContentGhost);
-export const buttonSearch = template(demoContentSearch);
