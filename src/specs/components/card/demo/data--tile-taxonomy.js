ff1884b43fa4ad712493f723c49db98cd8597cad
planctus <davidepuntoferrante@gmail.com>
2021-02-18 18:23:40 +0200

    feat(taxonomy): Card with description lists - front-2039

---
+++ src/specs/components/card/demo/data--tile-taxonomy.js
@@ -0,0 +1,67 @@
+// Simple content for demo
+module.exports = {
+  card: {
+    lists: [
+      {
+        variant: 'horizontal',
+        items: [
+          {
+            term: 'EU contribution',
+            definition: '1.000.000,00 euro (100% of the overall budget)',
+          },
+        ],
+      },
+      {
+        variant: 'taxonomy',
+        items: [
+          {
+            term: 'Science areas',
+            definition: [
+              {
+                label: 'Energy and transport',
+                variant: 'display',
+              },
+              {
+                label: 'Standards',
+                variant: 'display',
+              },
+            ],
+          },
+          {
+            term: 'Keywords',
+            definition: [
+              {
+                label: 'Electricity',
+                variant: 'display',
+              },
+              {
+                label: 'Electromobility',
+                variant: 'display',
+              },
+              {
+                label: 'Energy',
+                variant: 'display',
+              },
+              {
+                label: 'Energy storage',
+                variant: 'display',
+              },
+              {
+                label: 'Security',
+                variant: 'display',
+              },
+              {
+                label: 'Transport',
+                variant: 'display',
+              },
+              {
+                label: 'Low carbon',
+                variant: 'display',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
