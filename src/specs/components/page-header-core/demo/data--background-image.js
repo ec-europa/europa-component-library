af0f2ddeaae6aa543e115f2509e16ae04f3f110e
emeryro <emery.romain@gmail.com>
2021-04-22 16:15:47 +0200

    update demo

--- src/specs/components/page-header-core/demo/data--background-image.js
+++ src/specs/components/page-header-core/demo/data--background-image.js
@@ -4,10 +4,10 @@ module.exports = {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec ullamcorper mi. Morbi interdum fermentum tempus. Nam nec rhoncus risus, eget dictum elit. Vestibulum gravida tincidunt venenatis.',
   meta: ['Meta info', 'DD Month YYYY'],
   thumbnail: {
-    alt: 'European flag',
-    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
+    alt: 'Europe map',
+    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image2.jpg',
   },
   background_image_url:
-    'https://inno-ecl.s3.amazonaws.com/media/examples/example-image8.jpg',
+    'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
   overlay: 'dark',
 };
--- src/specs/components/page-header-standardised/demo/data--default.js
+++ src/specs/components/page-header-standardised/demo/data--default.js
@@ -3,8 +3,8 @@ module.exports = {
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec ullamcorper mi. Morbi interdum fermentum tempus. Nam nec rhoncus risus, eget dictum elit. Vestibulum gravida tincidunt venenatis.',
   thumbnail: {
-    alt: 'European flag',
-    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
+    alt: 'Europe map',
+    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image2.jpg',
   },
   meta: ['Meta info', 'DD Month YYYY'],
 };
