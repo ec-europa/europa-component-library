eff12c540625cd91793fe2bbc09f461a9ee15c8d
emeryro <emery.romain@gmail.com>
2021-04-22 10:37:10 +0200

    add thumbnail everywhere

--- src/specs/components/page-header-core/demo/data--background-image.js
+++ src/specs/components/page-header-core/demo/data--background-image.js
@@ -3,7 +3,11 @@ module.exports = {
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec ullamcorper mi. Morbi interdum fermentum tempus. Nam nec rhoncus risus, eget dictum elit. Vestibulum gravida tincidunt venenatis.',
   meta: ['Meta info', 'DD Month YYYY'],
+  thumbnail: {
+    alt: 'European flag',
+    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
+  },
   background_image_url:
-    'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
+    'https://inno-ecl.s3.amazonaws.com/media/examples/example-image8.jpg',
   overlay: 'dark',
 };
