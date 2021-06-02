dff82fc5a4f5721b09c6a2bc722c899f125e9dec
emeryro <emery.romain@gmail.com>
2021-06-02 09:56:08 +0200

    apply id

--- src/resources/icons-flag/scripts/config/svgo.js
+++ src/resources/icons-flag/scripts/config/svgo.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: [{ removeViewBox: false }],
+  plugins: [
+    {
+      removeViewBox: false,
+    },
+    {
+      cleanupIDs: {
+        prefix: {
+          toString() {
+            this.counter = this.counter || 0;
+            const id = `id-flag-${(this.counter += 1)}`;
+            return id;
+          },
+        },
+      },
+    },
+  ],
 };
