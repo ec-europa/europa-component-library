2f8e3ee1b263bd27b7536c55f1e0e95b25191a08
planctus <davidepuntoferrante@gmail.com>
2021-06-24 16:01:40 +0300

    chore: fixing packaging of the v3 website in production

---
+++ src/website/src/utils/correctCmsImagesPath.js
@@ -0,0 +1,19 @@
+const visit = require('unist-util-visit');
+
+module.exports = (
+  options = {
+    search: /^/,
+    replace: '/uploads',
+  }
+) => {
+  return function image() {
+    return function transformer(tree) {
+      // eslint-disable-next-line no-use-before-define
+      visit(tree, 'image', onImage);
+
+      function onImage(node) {
+        node.url = node.url.replace(options.search, options.replace);
+      }
+    };
+  };
+};
