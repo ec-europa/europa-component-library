a30a37e26c1a47a7ef494416a6e397fa82554459
emeryro <emery.romain@gmail.com>
2021-09-21 11:59:32 +0200

    update display list

--- src/implementations/twig/components/display-list/display-list.story.js
+++ src/implementations/twig/components/display-list/display-list.story.js
@@ -1,5 +1,6 @@
 import { withNotes } from '@ecl/storybook-addon-notes';
 import withCode from '@ecl/storybook-addon-code';
+import { correctSvgPath } from '@ecl/story-utils';
 
 import dataDisplayListDefault from '@ecl/specs-component-display-list/demo/data--default';
 
@@ -8,25 +9,76 @@ import notes from './README.md';
 
 const getArgs = (data) => {
   const args = {
-    term: data.items[0].term,
+    title: data.items[0].title,
+    description: data.items[0].description,
+    image: data.items[0].image.src,
   };
-  if (!Array.isArray(data.items[0].definition)) {
-    args.definition = data.items[0].definition;
-  } else {
-    args.label = data.items[0].definition[0].label;
-  }
 
   return args;
 };
 
 const getArgTypes = () => {
-  const argTypes = {};
+  const argTypes = {
+    column: {
+      name: 'number of columns',
+      type: { name: 'number' },
+      description: 'The title of the display list item',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '' },
+        category: 'Layout',
+      },
+    },
+    title: {
+      name: 'title (first item)',
+      type: { name: 'string', required: true },
+      description: 'The title of the display list item',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    },
+    description: {
+      name: 'description (first item)',
+      type: { name: 'string' },
+      description: 'The description of the display list item',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    },
+    image: {
+      name: 'image (first item)',
+      type: { name: 'string' },
+      description: 'The url of the of the display list item image',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+        category: 'Content',
+      },
+    },
+  };
 
   return argTypes;
 };
 
-const prepareData = (data) => {
+const prepareData = (data, args) => {
+  data.items[0].title = args.title;
+  data.items[0].description = args.description;
+  data.items[0].image.src = args.image;
+
   return data;
+};
+
+const renderStory = (data, args, variant) => {
+  let story = displayList(prepareData(correctSvgPath(data), args));
+  if (variant === 'horizontal') {
+    story = `<div class="ecl-container">${story}</div>`;
+  }
+
+  return story;
 };
 
 export default {
@@ -35,7 +87,7 @@ export default {
 };
 
 export const Horizontal = (args) =>
-  displayList(prepareData(dataDisplayListDefault, args));
+  renderStory(dataDisplayListDefault, args, 'horizontal');
 
 Horizontal.storyName = 'horizontal';
 Horizontal.args = getArgs(dataDisplayListDefault);
