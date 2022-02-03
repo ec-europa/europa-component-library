37cb8604a8c08ea8be03a7f87b0eecf8a33cc22c
Alexis Gaillard <a@bypopcorn.com>
2022-02-03 14:12:37 +0100

    Move html in the specs folder as a template

--- src/implementations/twig/components/file-upload/file-upload.story.js
+++ src/implementations/twig/components/file-upload/file-upload.story.js
@@ -4,6 +4,7 @@ import { getFormControls, correctSvgPath } from '@ecl/story-utils';
 
 import dataDefault from '@ecl/specs-component-file-upload/demo/data';
 import dataMulti from '@ecl/specs-component-file-upload/demo/data--multiple';
+import fileUploadList from '@ecl/specs-component-file-upload/demo/file-upload-list.html.twig';
 import fileUpload from './file-upload.html.twig';
 import notes from './README.md';
 
@@ -65,65 +66,6 @@ const prepareData = (data, args) => {
   return data;
 };
 
-const renderFilesList = (data) => `
-<ul class="ecl-file-upload__list ecl-file-upload__item--dynamic" data-ecl-file-upload-list>
-  <li class="ecl-file-upload__item">
-    <div class="ecl-file-upload__item-info">
-      <span class="ecl-file-upload__item-name">File_3.jpg</span>
-      <span class="ecl-file-upload__item-meta">(58.6 kB - jpg)</span>
-    </div>
-    <div class="ecl-file-upload__item-action">
-      <div class="ecl-file-upload__item-uploading">
-        Uploading... 
-        <div class="ecl-spinner ecl-spinner--primary ecl-spinner--small ecl-spinner--visible">
-          <svg class="ecl-spinner__loader" viewBox="25 25 50 50">
-            <circle class="ecl-spinner__circle" cx="50" cy="50" r="20" fill="none" stroke-width="4px" stroke-miterlimit="10" vector-effect="non-scaling-stroke" />
-          </svg>
-        </div>
-      </div>
-    </div>
-  </li>
-  <li class="ecl-file-upload__item ecl-file-upload__item--error">
-    <div class="ecl-file-upload__item-info">
-      <span class="ecl-file-upload__item-name">File_2.pdf</span>
-      <span class="ecl-file-upload__item-meta">(65 kB - pdf)</span>
-      <span class="ecl-file-upload__item-error-text">Error text</span>
-    </div>
-    <div class="ecl-file-upload__item-action">
-      <a href="#" class="ecl-link ecl-link--standalone ecl-link--icon ecl-link--icon-after" aria-label="Delete">
-        <span class="ecl-link__label">Delete</span>
-        <svg class="ecl-icon ecl-icon--xs ecl-link__icon ecl-icon--primary" focusable="false" aria-hidden="true">
-          <use xlink:href="${data.icon_path}#trash"></use>
-        </svg>
-      </a>
-    </div>
-  </li>
-  <li class="ecl-file-upload__item">
-    <div class="ecl-file-upload__item-info">
-      <span class="ecl-file-upload__item-name">File_1.docx</span>
-      <span class="ecl-file-upload__item-meta">(44.7 kB - docx)</span>
-    </div>
-    <div class="ecl-file-upload__item-action">
-      <a href="#" class="ecl-link ecl-link--standalone ecl-link--icon ecl-link--icon-after" aria-label="Delete">
-        <span class="ecl-link__label">Delete</span>
-        <svg class="ecl-icon ecl-icon--xs ecl-link__icon ecl-icon--primary" focusable="false" aria-hidden="true">
-          <use xlink:href="${data.icon_path}#trash"></use>
-        </svg>
-      </a>
-    </div>
-  </li>
-  <li class="ecl-file-upload__item">
-    <div class="ecl-file-upload__item-action">
-      <a href="#" class="ecl-link ecl-link--standalone ecl-link--icon ecl-link--icon-after" aria-label="Delete all">
-        <span class="ecl-link__label">Delete all</span>
-        <svg class="ecl-icon ecl-icon--xs ecl-link__icon ecl-icon--primary" focusable="false" aria-hidden="true">
-          <use xlink:href="${data.icon_path}#trash"></use>
-        </svg>
-      </a>
-    </div>
-  </li>
-</ul>`;
-
 export default {
   title: 'Components/Forms/File Upload',
   decorators: [withNotes, withCode],
@@ -143,10 +85,7 @@ Multiple.args = getArgs(dataMulti);
 Multiple.argTypes = getArgTypes(dataMulti);
 Multiple.parameters = { notes: { markdown: notes, json: dataMulti } };
 
-export const FileUploadList = () => renderFilesList(correctSvgPath(dataFiles));
+export const FileUploadList = () => fileUploadList(correctSvgPath(dataFiles));
 
 FileUploadList.storyName = 'file upload list';
-FileUploadList.parameters = {
-  notes: { disable: true },
-  controls: { disable: true },
-};
+FileUploadList.parameters = { controls: { disable: true } };
