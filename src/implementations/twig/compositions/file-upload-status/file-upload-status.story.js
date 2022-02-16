2eb41322389028c493603fcce91e27ed2bb8d7bb
Alexis Gaillard <a@bypopcorn.com>
2022-02-16 11:46:17 +0100

    Rename story to file-upload-status

--- src/implementations/twig/compositions/file-upload-status/file-upload-status.story.js
+++ src/implementations/twig/compositions/file-upload-status/file-upload-status.story.js
@@ -2,14 +2,15 @@ import { withNotes } from '@ecl/storybook-addon-notes';
 import withCode from '@ecl/storybook-addon-code';
 import { correctSvgPath } from '@ecl/story-utils';
 
-import fileUpload from './file-upload-list.html.twig';
+import fileUpload from './file-upload-status.html.twig';
+import notes from './README.md';
 
 const dataFiles = {
   icon_path: '/icons.svg',
 };
 
 export default {
-  title: 'Compositions/File upload list',
+  title: 'Compositions/File upload status',
   decorators: [withNotes, withCode],
   parameters: {
     controls: { disable: true },
@@ -19,3 +20,4 @@ export default {
 export const Default = () => fileUpload(correctSvgPath(dataFiles));
 
 Default.storyName = 'default';
+Default.parameters = { notes: { markdown: notes } };
--- src/implementations/twig/compositions/file-upload-status/file-upload-status.test.js
+++ src/implementations/twig/compositions/file-upload-status/file-upload-status.test.js
@@ -1,16 +1,16 @@
 import { renderTwigFileAsNode } from '@ecl/test-utils';
 
-describe('File upload list', () => {
-  const template = '@ecl/file-upload-list/file-upload-list.html.twig';
+describe('File upload status', () => {
+  const template = '@ecl/file-upload-status/file-upload-status.html.twig';
   const render = (params) => renderTwigFileAsNode(template, params);
 
-  test('renders correctly with languages list', () => {
+  test('renders correctly with languages status', () => {
     expect.assertions(1);
 
     return expect(render()).resolves.toMatchSnapshot();
   });
 
-  test('renders correctly without languages list', () => {
+  test('renders correctly without languages status', () => {
     expect.assertions(1);
 
     return expect(render()).resolves.toMatchSnapshot();
