9261c6ebe9de7b9a63d25aa988c6c3abf65de6de
Alexis Gaillard <a@bypopcorn.com>
2022-02-18 14:03:58 +0100

    Fix breadcrumb core negative links showcase

--- src/website/src/pages/ec/components/site-wide/page-header/demo/index.js
+++ src/website/src/pages/ec/components/site-wide/page-header/demo/index.js
@@ -5,24 +5,20 @@ import demoBreadcrumbLong from '@ecl/specs-component-breadcrumb/demo/data--ec';
 
 import template from '@ecl/twig-component-page-header/page-header.html.twig';
 
-const dataBreadcrumb = { ...demoBreadcrumbLong };
-
 // Default
 const dataDefault = { ...demoDefault };
+const dataBreadcrumb = { ...demoBreadcrumbLong };
 dataDefault.breadcrumb = dataBreadcrumb;
 
 // Core
-const prepareCoreData = (data) => {
-  data.variant = 'negative';
-  data.breadcrumb.links.forEach((item) => {
-    item.negative = true;
-  });
-  return data;
-};
+const dataCore = { ...demoDefault, variant: 'negative' };
+const dataBreadcrumbCore = { ...demoBreadcrumbLong };
+dataBreadcrumbCore.links.forEach((item) => {
+  item.negative = true;
+});
+dataCore.breadcrumb = dataBreadcrumbCore;
 
-export const pageHeaderCoreDefault = template(
-  correctSvgPath(prepareCoreData(dataDefault))
-);
+export const pageHeaderCoreDefault = template(correctSvgPath(dataCore));
 
 // Standardised & Harmonised
 export const pageHeaderDefault = template(correctSvgPath(dataDefault));
