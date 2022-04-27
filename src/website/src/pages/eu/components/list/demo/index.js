f979a4a47a94012154dc00db36dfcc84cb447cbd
emeryro <emery.romain@gmail.com>
2022-04-27 11:04:43 +0200

    fix website

--- src/website/src/pages/eu/components/list/demo/index.js
+++ src/website/src/pages/eu/components/list/demo/index.js
@@ -10,6 +10,7 @@ import demoContentDescriptionHorizontal from '@ecl/specs-component-description-l
 import templateUnordered from '@ecl/twig-component-unordered-list/unordered-list.html.twig';
 import templateOrdered from '@ecl/twig-component-ordered-list/ordered-list.html.twig';
 import templateDescription from '@ecl/twig-component-description-list/description-list.html.twig';
+import { correctSvgPath } from '@ecl/website-utils';
 
 export const listOrdered = templateOrdered(demoContentOrdered);
 export const listUnordered = templateUnordered(demoContentUnorderedText);
@@ -20,8 +21,8 @@ export const listUnorderedWithDivider = templateUnordered(
   demoContentUnorderedWithDivider
 );
 export const listDescription = templateDescription(
-  demoContentDescriptionDefault
+  correctSvgPath(demoContentDescriptionDefault)
 );
 export const listDescriptionHorizontal = templateDescription(
-  demoContentDescriptionHorizontal
+  correctSvgPath(demoContentDescriptionHorizontal)
 );
