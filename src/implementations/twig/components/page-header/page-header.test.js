75acda6f3a4f4ecc1b67d670927b0e68c1fe4af5
Alexis Gaillard <a@bypopcorn.com>
2022-05-02 13:31:13 +0200

    feat(page-header): add site-wide components - FRONT-3546 (#2337)
    Co-authored-by: Romain Emery <emery.romain@gmail.com>
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

---
+++ src/implementations/twig/components/page-header/page-header.test.js
@@ -0,0 +1,52 @@
+import { merge, renderTwigFileAsNode } from '@ecl/test-utils';
+
+import demoDefault from '@ecl/specs-component-page-header/demo/data';
+import dataBreadcrumbSimple from '@ecl/specs-component-breadcrumb/demo/data--ec';
+
+demoDefault.breadcrumb = dataBreadcrumbSimple;
+
+const demoSimple = { ...demoDefault };
+delete demoSimple.thumbnails;
+delete demoSimple.background_image_url;
+
+describe('Page Header Standardised', () => {
+  const template = '@ecl/page-header/page-header.html.twig';
+  const render = (params) => renderTwigFileAsNode(template, params);
+
+  describe('Default', () => {
+    test(`- renders correctly`, () => {
+      expect.assertions(1);
+      return expect(render(demoSimple)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(demoSimple, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(demoSimple, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+  });
+
+  describe('Background image', () => {
+    test(`- renders correctly`, () => {
+      expect.assertions(1);
+      return expect(render(demoDefault)).resolves.toMatchSnapshot();
+    });
+  });
+});
