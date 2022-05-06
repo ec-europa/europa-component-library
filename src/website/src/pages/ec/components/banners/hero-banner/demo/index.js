5303610fba99950a5ce433d913675f50e2e2e1e6
Romain Emery <emery.romain@gmail.com>
2022-05-06 16:01:11 +0200

    feat(banner): add coypright - FRONT-3616 (#2404)

--- src/website/src/pages/ec/components/banners/hero-banner/demo/index.js
+++ src/website/src/pages/ec/components/banners/hero-banner/demo/index.js
@@ -2,12 +2,11 @@ import demoContentImageBox from '@ecl/specs-component-hero-banner/demo/data--ima
 import demoContentImageShade from '@ecl/specs-component-hero-banner/demo/data--image-shade';
 import demoContentImageGradient from '@ecl/specs-component-hero-banner/demo/data--image-gradient';
 import demoContentSimplePrimary from '@ecl/specs-component-hero-banner/demo/data--simple-primary';
+
 import template from '@ecl/twig-component-hero-banner/hero-banner.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
-export const heroBannerImageBox = template(
-  correctSvgPath(correctSvgPath(demoContentImageBox))
-);
+export const heroBannerImageBox = template(correctSvgPath(demoContentImageBox));
 export const heroBannerImageShade = template(
   correctSvgPath(demoContentImageShade)
 );
