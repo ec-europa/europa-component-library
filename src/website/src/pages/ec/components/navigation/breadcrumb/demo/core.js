9297b82cf28b97e8863292ecbdb8310f1554f071
Alexis Gaillard <a@bypopcorn.com>
2022-05-10 11:53:46 +0200

    Fix website previews

--- src/website/src/pages/ec/components/navigation/breadcrumb/demo/core.js
+++ src/website/src/pages/ec/components/navigation/breadcrumb/demo/core.js
@@ -3,5 +3,8 @@ import dataLong from '@ecl/specs-component-breadcrumb/demo/data--ec';
 import template from '@ecl/twig-component-breadcrumb/breadcrumb.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
-export const breadcrumbSimpleCore = template(correctSvgPath(dataSimple));
-export const breadcrumbLongCore = template(correctSvgPath(dataLong));
+const dataCoreSimple = { ...dataSimple, variant: 'negative' };
+const dataCoreLong = { ...dataLong, variant: 'negative' };
+
+export const breadcrumbSimpleCore = template(correctSvgPath(dataCoreSimple));
+export const breadcrumbLongCore = template(correctSvgPath(dataCoreLong));
