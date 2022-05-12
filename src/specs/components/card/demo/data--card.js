18b279a4db78c3cd9a88a0900ea3205a4a023ca2
emeryro <emery.romain@gmail.com>
2022-05-12 10:58:51 +0200

    use content block

--- src/specs/components/card/demo/data--card.js
+++ src/specs/components/card/demo/data--card.js
@@ -3,46 +3,114 @@ const publicUrl = process.env.PUBLIC_URL || '';
 const exampleLink = `${publicUrl}/example`;
 
 module.exports = {
-  card: {
-    description:
-      'Transparently designing and evaluating evidence-based EU legislation, backed by citizens views.',
-    meta: ['META1', 'Meta2'],
-    title: {
-      type: 'standalone',
-      label: 'Better regulation',
-      path: exampleLink,
-      level: 1,
-    },
-    image: {
-      alt: 'card image',
-      src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
-    },
-    tags: [
-      { label: 'tag 1', path: exampleLink },
-      { label: 'tag 2', path: exampleLink },
-      { label: 'tag 3', path: exampleLink },
-    ],
-    labels: [
-      { label: 'highlighted', variant: 'highlight' },
-      { label: 'call status: upcoming', variant: 'medium' },
-    ],
-    infos: [
-      {
-        icon: {
-          name: 'calendar',
-          size: 'xs',
-          path: '/icons.svg',
-        },
-        label: '2018/10/22',
-      },
-      {
-        icon: {
-          name: 'location',
-          size: 'xs',
-          path: '/icons.svg',
-        },
-        label: 'Luxembourg',
-      },
-    ],
+  description:
+    'Transparently designing and evaluating evidence-based EU legislation, backed by citizens views.',
+  primary_meta: ['META1', 'Meta2'],
+  title: {
+    type: 'standalone',
+    label: 'Better regulation',
+    path: exampleLink,
+    level: 1,
   },
+  image: {
+    alt: 'card image',
+    src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
+  },
+  tags: [
+    {
+      tag: { label: 'tag 1', path: exampleLink },
+    },
+    {
+      tag: { label: 'tag 2', path: exampleLink },
+    },
+    {
+      tag: { label: 'tag 3', path: exampleLink },
+    },
+  ],
+  labels: [
+    { label: 'highlighted', variant: 'highlight' },
+    { label: 'call status: upcoming', variant: 'medium' },
+  ],
+  secondary_meta: [
+    {
+      icon: {
+        name: 'calendar',
+        size: 'xs',
+        path: '/icons.svg',
+      },
+      label: '2018/10/22',
+    },
+    {
+      icon: {
+        name: 'location',
+        size: 'xs',
+        path: '/icons.svg',
+      },
+      label: 'Luxembourg',
+    },
+  ],
+  lists: [
+    {
+      items: [
+        {
+          term: 'Standard text',
+          definition:
+            'Lorem ipsum dolor sit amet, <a href="#" class="ecl-link">consectetur adipiscing elit</a>. Suspendisse ut sapien condimentum, aliquet turpis sit amet, finibus purus. Donec porttitor iaculis felis ut dapibus. Sed blandit, massa ac suscipit facilisis',
+        },
+        {
+          term: 'Standalone links',
+          type: 'link',
+          definition: [
+            {
+              link: {
+                label: 'Lorem ipsum dolor sit amet',
+                path: exampleLink,
+                icon_position: 'before',
+              },
+              icon: {
+                name: 'copy',
+                path: '/icons.svg',
+                size: 's',
+              },
+            },
+            {
+              link: {
+                label: 'Lorem ipsum dolor sit amet',
+                path: exampleLink,
+                icon_position: 'before',
+              },
+              icon: {
+                name: 'download',
+                path: '/icons.svg',
+                size: 's',
+              },
+            },
+          ],
+        },
+        {
+          term: 'Links inline',
+          type: 'link-inline',
+          definition: [
+            {
+              link: {
+                label: 'Lorem ipsum dolor sit amet',
+                path: exampleLink,
+              },
+            },
+            {
+              link: {
+                label: 'Lorem ipsum dolor sit amet',
+                path: exampleLink,
+              },
+            },
+          ],
+        },
+        {
+          term: 'Taxonomy list',
+          type: 'taxonomy',
+          definition: ['Taxonomy item 1', 'Taxonomy item 2', 'Taxonomy item 3'],
+        },
+      ],
+    },
+  ],
 };
