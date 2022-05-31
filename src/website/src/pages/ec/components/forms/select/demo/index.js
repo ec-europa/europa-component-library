dc1c456464cb8b1cee711d5beff52fa070451298
Alexis Gaillard <a@bypopcorn.com>
2022-05-31 15:41:26 +0200

    Fix website forms specs path

--- src/website/src/pages/ec/components/forms/select/demo/index.js
+++ src/website/src/pages/ec/components/forms/select/demo/index.js
@@ -1,5 +1,5 @@
-import demoContentSingle from '@ecl/specs-component-select/demo/data-single--ec';
-import demoContentMultiple from '@ecl/specs-component-select/demo/data-multiple--ec';
+import demoContentSingle from '@ecl/specs-component-select/demo/data-single';
+import demoContentMultiple from '@ecl/specs-component-select/demo/data-multiple';
 import template from '@ecl/twig-component-select/select.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
