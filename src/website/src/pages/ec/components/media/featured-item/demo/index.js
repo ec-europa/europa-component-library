4cb2bb6b7bdbda8dfdbceb51d568f2878229e646
Alexis Gaillard <a@bypopcorn.com>
2022-06-01 16:26:52 +0200

    Add correctSvgPath for the demos

--- src/website/src/pages/ec/components/media/featured-item/demo/index.js
+++ src/website/src/pages/ec/components/media/featured-item/demo/index.js
@@ -1,14 +1,21 @@
 import demoContent from '@ecl/specs-component-featured-item/demo/data';
 import demoContentExtended from '@ecl/specs-component-featured-item/demo/data--extended';
 import template from '@ecl/twig-component-featured-item/featured-item.html.twig';
+import { correctSvgPath } from '@ecl/website-utils';
 
-export const featuredItem = template(demoContent);
-export const featuredItemRightAlignement = template({
-  ...demoContent,
-  position: 'right',
-});
-export const featuredItemExtended = template(demoContentExtended);
-export const featuredItemExtendedRightAlignement = template({
-  ...demoContent,
-  position: 'right',
-});
+export const featuredItem = template(correctSvgPath(demoContent));
+export const featuredItemRightAlignement = template(
+  correctSvgPath({
+    ...demoContent,
+    position: 'right',
+  })
+);
+export const featuredItemExtended = template(
+  correctSvgPath(demoContentExtended)
+);
+export const featuredItemExtendedRightAlignement = template(
+  correctSvgPath({
+    ...demoContent,
+    position: 'right',
+  })
+);
