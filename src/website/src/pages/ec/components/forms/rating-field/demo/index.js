d1c6c80ece497788bd702a4117e5bf1d372c75b9
emeryro <emery.romain@gmail.com>
2022-11-28 15:29:09 +0100

    fix website

--- src/website/src/pages/ec/components/forms/rating-field/demo/index.js
+++ src/website/src/pages/ec/components/forms/rating-field/demo/index.js
@@ -3,18 +3,22 @@ import template from '@ecl/twig-component-rating-field/rating-field.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
 const demoContentSelected = JSON.parse(JSON.stringify(demoContentDefault));
+demoContentSelected.label = '';
+demoContentSelected.helper_text = '';
 demoContentSelected.items[2].checked = true;
 
 export const ratingFieldGroupDefault = template(
   correctSvgPath({
     ...demoContentDefault,
     id: 'rating-default',
+    name: 'rating-default',
   })
 );
 export const ratingFieldGroupInvalid = template(
   correctSvgPath({
     ...demoContentDefault,
     id: 'rating-invalid',
+    name: 'rating-invalid',
     invalid: true,
   })
 );
@@ -22,6 +26,7 @@ export const ratingFieldGroupOptional = template(
   correctSvgPath({
     ...demoContentDefault,
     id: 'rating-optional',
+    name: 'rating-optional',
     required: false,
   })
 );
@@ -29,6 +34,7 @@ export const ratingFieldGroupDisabled = template(
   correctSvgPath({
     ...demoContentSelected,
     id: 'rating-disabled',
+    name: 'rating-disabled',
     disabled: true,
   })
 );
