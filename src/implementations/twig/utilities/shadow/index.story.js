f43b9c296b5202718d9b8ef05607a54abb180ab5
emeryro <emery.romain@gmail.com>
2023-01-17 07:53:06 +0100

    add inner and negative shadow

--- src/implementations/twig/utilities/shadow/index.story.js
+++ src/implementations/twig/utilities/shadow/index.story.js
@@ -2,15 +2,38 @@ import withCode from '@ecl/storybook-addon-code';
 import classnames from 'classnames';
 import { styled } from '@ecl/dom-utils';
 
+const styleContainer = {
+  alignItems: 'center',
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '2rem 0',
+};
+const styleContainerNegative = {
+  alignItems: 'center',
+  backgroundColor: '#808080',
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '2rem 0',
+};
 const styleBox = {
-  backgroundColor: '#d9d9d9',
+  border: '1px solid #000',
   height: '5rem',
-  margin: '2rem auto',
   width: '10rem',
+};
+const styleInner = {
+  backgroundColor: '#ebebeb',
+  height: '100%',
+  width: '100%',
+};
+const styleInnerNegative = {
+  backgroundColor: '#404040',
+  height: '100%',
+  width: '100%',
 };
 
 const getArgs = () => ({
   shadow: 'ecl-u-shadow-1',
+  shadowInner: 'ecl-u-shadow-none',
 });
 
 const getArgTypes = () => ({
@@ -23,6 +46,9 @@ const getArgTypes = () => ({
       'ecl-u-shadow-1',
       'ecl-u-shadow-2',
       'ecl-u-shadow-3',
+      'ecl-u-shadow-negative-1',
+      'ecl-u-shadow-negative-2',
+      'ecl-u-shadow-negative-3',
     ],
     control: {
       labels: {
@@ -30,6 +56,34 @@ const getArgTypes = () => ({
         'ecl-u-shadow-1': 'elevation 1',
         'ecl-u-shadow-2': 'elevation 2',
         'ecl-u-shadow-3': 'elevation 3',
+        'ecl-u-shadow-negative-1': 'negative, elevation 1',
+        'ecl-u-shadow-negative-2': 'negative, elevation 2',
+        'ecl-u-shadow-negative-3': 'negative, elevation 3',
+      },
+    },
+    table: {
+      type: 'string',
+      defaultValue: { summary: '' },
+    },
+  },
+  shadowInner: {
+    name: 'inner shadow',
+    type: 'select',
+    description: 'Possible inner shadow',
+    options: [
+      'ecl-u-shadow-none',
+      'ecl-u-shadow-inner-1',
+      'ecl-u-shadow-inner-2',
+      'ecl-u-shadow-negative-inner-1',
+      'ecl-u-shadow-negative-inner-2',
+    ],
+    control: {
+      labels: {
+        'ecl-u-shadow-none': 'none',
+        'ecl-u-shadow-inner-1': 'elevation 1',
+        'ecl-u-shadow-inner-2': 'elevation 2',
+        'ecl-u-shadow-negative-inner-1': 'negative, elevation 1',
+        'ecl-u-shadow-negative-inner-2': 'negative, elevation 2',
       },
     },
     table: {
@@ -44,8 +98,22 @@ export default {
   decorators: [withCode],
 };
 
-export const Custom = (args) =>
-  `<div style="${styled(styleBox)}" class="${classnames(args.shadow)}" />`;
+export const Custom = (args) => {
+  const container = args.shadow.includes('negative')
+    ? styled(styleContainerNegative)
+    : styled(styleContainer);
+  const inner = args.shadowInner.includes('negative')
+    ? styled(styleInnerNegative)
+    : styled(styleInner);
+
+  return `
+  <div style="${container}">
+    <div style="${styled(styleBox)}" class="${classnames(args.shadow)}">
+      <div style="${inner}" class="${classnames(args.shadowInner)}" />
+    </div>
+  </div>
+  `;
+};
 
 Custom.storyName = 'custom';
 Custom.args = getArgs();
