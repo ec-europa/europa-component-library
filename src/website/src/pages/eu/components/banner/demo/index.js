a62dd32eef78e6143526c0eb229942c4810f090a
Romain Emery <emery.romain@gmail.com>
2023-01-25 11:24:42 +0100

    feat(banner): add unified banner - FONT-3829 (#2728)
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

---
+++ src/website/src/pages/eu/components/banner/demo/index.js
@@ -0,0 +1,16 @@
+import demoContentImageBox from '@ecl/specs-component-banner/demo/data--image-box';
+import demoContentImageShade from '@ecl/specs-component-banner/demo/data--image-shade';
+import demoContentImageGradient from '@ecl/specs-component-banner/demo/data--image-gradient';
+import demoContentPrimary from '@ecl/specs-component-banner/demo/data--primary';
+
+import template from '@ecl/twig-component-banner/banner.html.twig';
+import { correctSvgPath } from '@ecl/website-utils';
+
+export const bannerImageBox = template(
+  correctSvgPath(correctSvgPath(demoContentImageBox))
+);
+export const bannerImageShade = template(correctSvgPath(demoContentImageShade));
+export const bannerImageGradient = template(
+  correctSvgPath(demoContentImageGradient)
+);
+export const bannerPrimary = template(correctSvgPath(demoContentPrimary));
