01210d1b81ad18de6ca3026926f6bcbb1f565ee3
planctus <davidepuntoferrante@gmail.com>
2023-02-13 15:56:42 +0200

    chore(icons): Fixing gradients in social sprite, replacing instagram icon (chrome not working)

--- src/resources/icons-social-media/scripts/write-sprite.js
+++ src/resources/icons-social-media/scripts/write-sprite.js
@@ -2,6 +2,81 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const path = require('path');
 const SVGSpriter = require('svg-sprite');
+const { DOMParser } = require('xmldom');
+
+const defs = new DOMParser().parseFromString('<defs></defs>');
+let count = 0;
+
+/**
+ * Escape regex characters in given string
+ * @param {string} str
+ * @return {string}
+ */
+function regexEscape(str) {
+  // eslint-disable-next-line no-useless-escape
+  return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
+/**
+ * Updates urls in given SVG from array of [oldId, newId].
+ * @param {string} svg
+ * @param {Array} idsToReplace
+ * @return {string}
+ */
+function updateUrls(svg, idsToReplace) {
+  for (let i = 0; i < idsToReplace.length; i += 1) {
+    const str = `url(#${idsToReplace[i][0]})`;
+    svg = svg.replace(
+      new RegExp(regexEscape(str), 'g'),
+      `url(#${idsToReplace[i][1]})`
+    );
+  }
+
+  return svg;
+}
+
+/**
+ * Extracts specific gradient defined by tag from given shape.
+ * @param {SVGShape} shape
+ * @param {string} tag
+ * @return {Array}
+ */
+function extractGradients(shape, tag) {
+  const idsToReplace = [];
+
+  const gradients = shape.dom.getElementsByTagName(tag);
+  while (gradients.length > 0) {
+    // Add gradient to defs block
+    defs.documentElement.appendChild(gradients[0]);
+
+    // Give gradient new ID
+    const id = gradients[0].getAttribute('id');
+    // eslint-disable-next-line no-plusplus
+    const newId = `g${++count}`;
+    gradients[0].setAttribute('id', newId);
+
+    idsToReplace.push([id, newId]);
+  }
+
+  return idsToReplace;
+}
+
+/**
+ * Extracts gradient from the sprite and replaces their ids to prevent duplicates.
+ * @param {SVGShape} shape
+ * @param {SVGSpriter} spriter
+ * @param {Function} callback
+ */
+function gradientsExtraction(shape, spriter, callback) {
+  const idsToReplace = [].concat(
+    extractGradients(shape, 'linearGradient'),
+    extractGradients(shape, 'radialGradient')
+  );
+
+  shape.setSVG(updateUrls(shape.getSVG(), idsToReplace));
+
+  callback(null);
+}
 
 const writeSprite = ({ cwd, files, dest, outputFile }) => {
   const spriter = new SVGSpriter({
@@ -14,9 +89,20 @@ const writeSprite = ({ cwd, files, dest, outputFile }) => {
         },
       },
       dimension: { attributes: true },
-      transform: [], // no transform, SVGs have already been optimized
+      // https://github.com/svg-sprite/svg-sprite/issues/74
+      transform: [gradientsExtraction],
     },
-    svg: { dimensionAttributes: true },
+    svg: {
+      dimensionAttributes: true,
+      transform: [
+        function modeDefs(svg) {
+          return svg.replace(
+            '<symbol ',
+            `${defs.firstChild.toString()}<symbol `
+          );
+        },
+      ],
+    },
     mode: {
       symbol: {
         dest: '',
