a95e781368637c9940d1c0ff8ee2abe5d0ef829a
Romain Emery <emery.romain@gmail.com>
2023-03-06 12:35:52 +0100

    feat(picture): add picture component - FRONT-3868 (#2789)

---
+++ src/implementations/twig/components/picture/picture.test.js
@@ -0,0 +1,53 @@
+import {
+  merge,
+  renderTwigFileAsNode,
+  renderTwigFileAsHtml,
+} from '@ecl/test-utils';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
+import data from '@ecl/specs-component-picture/demo/data';
+
+expect.extend(toHaveNoViolations);
+
+describe('Picture', () => {
+  const template = '@ecl/picture/picture.html.twig';
+  const render = (params) => renderTwigFileAsNode(template, params);
+
+  describe('Default', () => {
+    test('renders correctly', () => {
+      expect.assertions(1);
+
+      return expect(render(data)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const optionsWithExtraClasses = merge(data, {
+        extra_classes: 'custom-class custom-class--picture',
+        extra_image_classes: 'custom-class custom-class--image',
+      });
+
+      return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(data, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+
+    test(`passes the accessibility tests`, async () => {
+      expect(
+        await axe(renderTwigFileAsHtml(template, data, true))
+      ).toHaveNoViolations();
+    });
+  });
+});
