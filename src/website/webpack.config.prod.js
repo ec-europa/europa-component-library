eee42da747f426ba52216844792931ee7564e94d
planctus <davidepuntoferrante@gmail.com>
2023-03-18 15:16:02 +0200

    chore(webpack-5): Restoring pages, restoring svgs in dist/media, using esmodules for file loader

--- src/website/webpack.config.prod.js
+++ src/website/webpack.config.prod.js
@@ -241,8 +241,18 @@ module.exports = {
             ],
           },
           {
-            exclude: [/(^|\.(js|jsx|ts|tsx|html|json))$/],
-            type: 'asset/resource',
+            // Exclude `js` files to keep "css" loader working as it injects
+            // its runtime that would otherwise processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            use: {
+              loader: 'file-loader',
+              options: {
+                name: 'dist/media/[name].[hash:8].[ext]',
+                esModule: true,
+              },
+            },
           },
         ],
       },
