bdb2af0cf650ef3c17b35cc876270f4342203bb6
planctus <davidepuntoferrante@gmail.com>
2023-04-13 15:18:41 +0300

    feat(gallery): Adding full width option

--- src/implementations/twig/components/gallery/gallery.story.js
+++ src/implementations/twig/components/gallery/gallery.story.js
@@ -16,12 +16,15 @@ export default {
 
 const prepareData = (data, args) => Object.assign(correctPaths(data), args);
 
-export const Default = (args) => gallery(prepareData(specs, args));
+export const Default = (args) =>
+  `<div class="ecl-container">${gallery(prepareData(specs, args))}</div>`;
 
 Default.storyName = 'default';
 Default.parameters = { notes: { markdown: notes, json: specs } };
 Default.args = {
   expandable: true,
+  full_width: false,
+  visible_items: 8,
 };
 Default.argTypes = {
   expandable: {
@@ -30,6 +33,30 @@ Default.argTypes = {
     table: {
       type: { summary: 'boolean' },
       defaultValue: { summary: 'true' },
+    },
+  },
+  full_width: {
+    name: 'full width',
+    control: { type: 'boolean' },
+    description: 'full width gallery',
+    table: {
+      type: { summary: 'boolean' },
+      defaultValue: { summary: 'false' },
+    },
+  },
+  visible_items: {
+    name: 'limit the number of visible items',
+    control: {
+      type: 'range',
+      step: 1,
+      min: 0,
+      max: 11,
+    },
+    description:
+      'Only relevant when the gallery is set to be expandable (0 means no limit)',
+    table: {
+      type: { summary: 'integer' },
+      defaultValue: { summary: '8' },
     },
   },
 };
