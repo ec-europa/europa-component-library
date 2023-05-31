55bbba448a4cd68cd2cf7ee2e8e5a1fdd1c3602c
emeryro <emery.romain@gmail.com>
2023-05-31 10:10:11 +0200

    update test

--- src/implementations/twig/components/button/button.story.js
+++ src/implementations/twig/components/button/button.story.js
@@ -147,8 +147,6 @@ export const CallToAction = (args) => button(prepareData(dataCall, args));
 
 CallToAction.args = {
   label: dataCall.label,
-  icon_name: 'corner-arrow',
-  icon_transform: 'rotate-90',
 };
 CallToAction.storyName = 'call to action';
 CallToAction.parameters = {
