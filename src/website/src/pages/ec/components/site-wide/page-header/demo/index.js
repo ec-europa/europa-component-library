e9f6a7434fb3a4c35df84ee9340502fd4182a49d
emeryro <emery.romain@gmail.com>
2023-07-11 09:17:34 +0200

    fix website

--- src/website/src/pages/ec/components/site-wide/page-header/demo/index.js
+++ src/website/src/pages/ec/components/site-wide/page-header/demo/index.js
@@ -1,7 +1,7 @@
 import { correctSvgPath } from '@ecl/website-utils';
 
 import demoDefault from '@ecl/specs-component-page-header/demo/data';
-import demoBreadcrumbLong from '@ecl/specs-component-breadcrumb/demo/data--ec';
+import demoBreadcrumbLong from '@ecl/specs-component-breadcrumb/demo/data--long';
 
 import template from '@ecl/twig-component-page-header/page-header.html.twig';
 
