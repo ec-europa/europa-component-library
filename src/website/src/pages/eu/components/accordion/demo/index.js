680f1603498c99e37449a3aafbb4f7bb54d43eac
planctus <davidepuntoferrante@gmail.com>
2023-07-31 10:59:26 +0300

    feat(accordion): Restoring icon size for EU in the website - FRONT-4030

--- src/website/src/pages/eu/components/accordion/demo/index.js
+++ src/website/src/pages/eu/components/accordion/demo/index.js
@@ -1,5 +1,6 @@
 import demoContent from '@ecl/specs-component-accordion/demo/data';
 import template from '@ecl/twig-component-accordion/accordion.html.twig';
 
+demoContent.icon.size = 'm';
 const accordion = template(demoContent);
 export default accordion;
