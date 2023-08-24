e5d7c8129eccfe150dd7204d515b41224d73ce61
planctus <davidepuntoferrante@gmail.com>
2023-08-24 15:04:23 +0300

    feat(date-block): Using single date format in the demo - FRONT-4060

--- src/specs/components/date-block/demo/data.js
+++ src/specs/components/date-block/demo/data.js
@@ -1,7 +1,7 @@
 module.exports = {
-  date_time: '2023-06-01',
-  day: '01-02',
-  month: 'Sept-Jun',
-  month_full: 'September / June',
-  year: '2023',
+  date_time: '2019-09-26',
+  day: '26',
+  month: 'Sep',
+  month_full: 'September',
+  year: '2019',
 };
