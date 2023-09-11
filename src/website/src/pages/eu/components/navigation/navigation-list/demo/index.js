4fcd579be187ac2280e606928401616d0b679496
planctus <davidepuntoferrante@gmail.com>
2023-09-11 12:00:09 +0300

    chore(navigation-list): Adding border in website showcase - FRONT-4058

--- src/website/src/pages/eu/components/navigation/navigation-list/demo/index.js
+++ src/website/src/pages/eu/components/navigation/navigation-list/demo/index.js
@@ -3,4 +3,8 @@ import demo from '@ecl/specs-component-navigation-list/demo/data';
 import template from '@ecl/twig-component-navigation-list/navigation-list.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
+demo.items.forEach((item) => {
+  item.border = true;
+});
+
 export const navigationList = template(correctSvgPath(demo));
