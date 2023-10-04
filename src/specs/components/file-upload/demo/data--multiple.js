e729fcb93aaf2034e92453de1483daca8b7ebfb7
emeryro <emery.romain@gmail.com>
2023-10-04 08:14:31 +0200

    apply feedback

--- src/specs/components/file-upload/demo/data--multiple.js
+++ src/specs/components/file-upload/demo/data--multiple.js
@@ -2,7 +2,7 @@
 module.exports = {
   label: 'Label',
   helper_text:
-    "This is the input's helper text.<div class='ecl-u-mt-xs ecl-u-type-color-grey-75'>Only <strong>txt doc docx pdf odt rtf</strong> files. Maximum size is <strong>5 MB</strong>.<br>Encrypted documents and those containing macros are not accepted.</div>",
+    'Only <strong>txt doc docx pdf odt rtf</strong> files. Maximum size is <strong>5 MB</strong>.<br>Encrypted documents and those containing macros are not accepted.',
   invalid_text: 'This is the error message',
   invalid_icon: {
     path: '/icons.svg',
