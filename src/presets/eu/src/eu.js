2572b98c91f08cb184abfbdc34cc3df00cad8afb
Romain Emery <emery.romain@gmail.com>
2023-11-08 14:37:27 +0100

    feat(theme): remove theme "dev" - FRONT-4103 (#3076)

--- src/presets/eu/src/eu.js
+++ src/presets/eu/src/eu.js
@@ -1,3 +1,28 @@
-// Export components
+import '@ecl/dom-utils/polyfills';
 
-export * from '@ecl/preset-dev/src/dev';
+export * from '@ecl/dom-utils/autoinit';
+export * from '@ecl/vanilla-component-accordion';
+export * from '@ecl/vanilla-component-banner';
+export * from '@ecl/vanilla-component-carousel';
+export * from '@ecl/vanilla-component-category-filter';
+export * from '@ecl/vanilla-component-datepicker';
+export * from '@ecl/vanilla-component-breadcrumb';
+export * from '@ecl/vanilla-component-content-block';
+export * from '@ecl/vanilla-component-description-list';
+export * from '@ecl/vanilla-component-expandable';
+export * from '@ecl/vanilla-component-file';
+export * from '@ecl/vanilla-component-file-upload';
+export * from '@ecl/vanilla-component-gallery';
+export * from '@ecl/vanilla-component-inpage-navigation';
+export * from '@ecl/vanilla-component-media-container';
+export * from '@ecl/vanilla-component-menu';
+export * from '@ecl/vanilla-component-modal';
+export * from '@ecl/vanilla-component-news-ticker';
+export * from '@ecl/vanilla-component-notification';
+export * from '@ecl/vanilla-component-popover';
+export * from '@ecl/vanilla-component-range';
+export * from '@ecl/vanilla-component-select';
+export * from '@ecl/vanilla-component-site-header';
+export * from '@ecl/vanilla-component-table';
+export * from '@ecl/vanilla-component-tabs';
+export * from '@ecl/vanilla-component-timeline';
--- src/themes/dev/README.md
+++
@@ -1,1 +0,0 @@
-# ECL - Dev theme
