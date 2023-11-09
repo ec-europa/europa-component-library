ef0679a5f592689674fd8db8069e390e886cad18
emeryro <emery.romain@gmail.com>
2023-11-09 15:47:49 +0100

    add variant

--- src/website/src/pages/eu/components/button/demo/index.js
+++ src/website/src/pages/eu/components/button/demo/index.js
@@ -2,6 +2,8 @@ import demoContentPrimary from '@ecl/specs-component-button/demo/data--primary';
 import demoContentSecondary from '@ecl/specs-component-button/demo/data--secondary';
 import demoContentCall from '@ecl/specs-component-button/demo/data--call';
 import demoContentGhost from '@ecl/specs-component-button/demo/data--ghost';
+import demoContentGhostInverted from '@ecl/specs-component-button/demo/data--ghost-inverted';
+import demoContentTertiary from '@ecl/specs-component-button/demo/data--tertiary';
 import template from '@ecl/twig-component-button/button.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
@@ -18,6 +20,8 @@ const demoContentIcon = {
 
 export const buttonPrimary = template(demoContentPrimary);
 export const buttonSecondary = template(demoContentSecondary);
+export const buttonTertiary = template(demoContentTertiary);
 export const buttonCall = template(demoContentCall);
 export const buttonGhost = template(demoContentGhost);
+export const buttonGhostInverted = template(demoContentGhostInverted);
 export const buttonIconOnly = template(correctSvgPath(demoContentIcon));
