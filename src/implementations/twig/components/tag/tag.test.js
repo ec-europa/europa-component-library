3fc19010b44f3e98ed3ab9139f5ff802bc575fb6
emeryro <emery.romain@gmail.com>
2023-11-28 15:44:30 +0100

    add test

--- src/implementations/twig/components/tag/tag.test.js
+++ src/implementations/twig/components/tag/tag.test.js
@@ -7,6 +7,7 @@ import { axe, toHaveNoViolations } from 'jest-axe';
 
 import dataLink from '@ecl/specs-component-tag/demo/data--link';
 import dataRemovable from '@ecl/specs-component-tag/demo/data--removable';
+import dataSet from '@ecl/specs-component-tag/demo/data--set';
 
 expect.extend(toHaveNoViolations);
 
@@ -69,10 +70,23 @@ describe('Tag', () => {
       return expect(render(dataRemovable)).resolves.toMatchSnapshot();
     });
 
+    test(`passes the accessibility tests`, async () => {
+      expect(
+        await axe(await renderTwigFileAsHtml(template, dataRemovable, true)),
+      ).toHaveNoViolations();
+    });
+  });
+
+  describe('Set', () => {
+    test('renders correctly', () => {
+      expect.assertions(1);
+      return expect(render(dataSet)).resolves.toMatchSnapshot();
+    });
+
     test('renders correctly with extra class names', () => {
       expect.assertions(1);
 
-      const optionsWithExtraClasses = merge(dataRemovable, {
+      const optionsWithExtraClasses = merge(dataSet, {
         extra_classes: 'custom-class custom-class--test',
       });
 
@@ -82,7 +96,7 @@ describe('Tag', () => {
     test('renders correctly with extra attributes', () => {
       expect.assertions(1);
 
-      const optionsWithExtraClasses = merge(dataRemovable, {
+      const optionsWithExtraClasses = merge(dataSet, {
         extra_attributes: [
           { name: 'data-test', value: 'data-test-value' },
           { name: 'data-test-1', value: 'data-test-value-1' },
@@ -94,7 +108,7 @@ describe('Tag', () => {
 
     test(`passes the accessibility tests`, async () => {
       expect(
-        await axe(await renderTwigFileAsHtml(template, dataRemovable, true)),
+        await axe(await renderTwigFileAsHtml(template, dataSet, true)),
       ).toHaveNoViolations();
     });
   });
