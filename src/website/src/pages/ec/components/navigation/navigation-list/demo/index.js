849c9075c784dcd6e8758699fe89d83d406af3f6
planctus <davidepuntoferrante@gmail.com>
2024-01-11 15:53:11 +0200

    feat(navigation-list): EU alignment and website showcase - FRONT-4197

--- src/website/src/pages/ec/components/navigation/navigation-list/demo/index.js
+++ src/website/src/pages/ec/components/navigation/navigation-list/demo/index.js
@@ -1,6 +1,8 @@
 import demo from '@ecl/specs-component-navigation-list/demo/data';
+import demoIcon from '@ecl/specs-component-navigation-list/demo/data-icon';
 
 import template from '@ecl/twig-component-navigation-list/navigation-list.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
 export const navigationList = template(correctSvgPath(demo));
+export const navigationListIcon = template(correctSvgPath(demoIcon));
