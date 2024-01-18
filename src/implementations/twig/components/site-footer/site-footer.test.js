c61afcd696ebed1afbdbc7fa5c01cf1972d2b794
emeryro <emery.romain@gmail.com>
2024-01-18 15:26:30 +0100

    add icon, update test

--- src/implementations/twig/components/site-footer/site-footer.test.js
+++ src/implementations/twig/components/site-footer/site-footer.test.js
@@ -9,14 +9,15 @@ import dataCoreEC from '@ecl/specs-component-site-footer/demo/data-core--ec';
 import dataStandardisedEC from '@ecl/specs-component-site-footer/demo/data-standardised--ec';
 import dataHarmonisedEC from '@ecl/specs-component-site-footer/demo/data-harmonised--ec';
 import dataCoreEU from '@ecl/specs-component-site-footer/demo/data-core--eu';
+import dataHarmonisedEU from '@ecl/specs-component-site-footer/demo/data-harmonised--eu';
 
 expect.extend(toHaveNoViolations);
 
-describe('Site Footer Core EC', () => {
+describe('Site Footer', () => {
   const template = '@ecl/site-footer/site-footer.html.twig';
   const render = (params) => renderTwigFileAsNode(template, params);
 
-  describe('default', () => {
+  describe('Core EC', () => {
     const options = dataCoreEC;
 
     test('renders correctly', () => {
@@ -53,41 +54,13 @@ describe('Site Footer Core EC', () => {
       ).toHaveNoViolations();
     });
   });
-});
 
-describe('Site Footer Standardised EC', () => {
-  const template = '@ecl/site-footer/site-footer.html.twig';
-  const render = (params) => renderTwigFileAsNode(template, params);
-
-  describe('default', () => {
+  describe('Standardised EC', () => {
     const options = dataStandardisedEC;
 
     test('renders correctly', () => {
       expect.assertions(1);
       return expect(render(options)).resolves.toMatchSnapshot();
-    });
-
-    test('renders correctly with extra class names', () => {
-      expect.assertions(1);
-
-      const withExtraClasses = merge(options, {
-        extra_classes: 'custom-class custom-class--test',
-      });
-
-      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
-    });
-
-    test('renders correctly with extra attributes', () => {
-      expect.assertions(1);
-
-      const withExtraAttributes = merge(options, {
-        extra_attributes: [
-          { name: 'data-test', value: 'data-test-value' },
-          { name: 'data-test-1', value: 'data-test-value-1' },
-        ],
-      });
-
-      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
     });
 
     test(`passes the accessibility tests`, async () => {
@@ -98,41 +71,13 @@ describe('Site Footer Standardised EC', () => {
       ).toHaveNoViolations();
     });
   });
-});
 
-describe('Site Footer Harmonised EC', () => {
-  const template = '@ecl/site-footer/site-footer.html.twig';
-  const render = (params) => renderTwigFileAsNode(template, params);
-
-  describe('default', () => {
+  describe('Harmonised EC', () => {
     const options = dataHarmonisedEC;
 
     test('renders correctly', () => {
       expect.assertions(1);
       return expect(render(options)).resolves.toMatchSnapshot();
-    });
-
-    test('renders correctly with extra class names', () => {
-      expect.assertions(1);
-
-      const withExtraClasses = merge(options, {
-        extra_classes: 'custom-class custom-class--test',
-      });
-
-      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
-    });
-
-    test('renders correctly with extra attributes', () => {
-      expect.assertions(1);
-
-      const withExtraAttributes = merge(options, {
-        extra_attributes: [
-          { name: 'data-test', value: 'data-test-value' },
-          { name: 'data-test-1', value: 'data-test-value-1' },
-        ],
-      });
-
-      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
     });
 
     test(`passes the accessibility tests`, async () => {
@@ -141,13 +86,8 @@ describe('Site Footer Harmonised EC', () => {
       ).toHaveNoViolations();
     });
   });
-});
 
-describe('Site Footer Core EU', () => {
-  const template = '@ecl/site-footer/site-footer.html.twig';
-  const render = (params) => renderTwigFileAsNode(template, params);
-
-  describe('default', () => {
+  describe('Core EU', () => {
     const options = dataCoreEU;
 
     test('renders correctly', () => {
@@ -155,32 +95,24 @@ describe('Site Footer Core EU', () => {
       return expect(render(options)).resolves.toMatchSnapshot();
     });
 
-    test('renders correctly with extra class names', () => {
-      expect.assertions(1);
-
-      const withExtraClasses = merge(options, {
-        extra_classes: 'custom-class custom-class--test',
-      });
-
-      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    test(`passes the accessibility tests`, async () => {
+      expect(
+        await axe(await renderTwigFileAsHtml(template, dataCoreEU, true)),
+      ).toHaveNoViolations();
     });
+  });
 
-    test('renders correctly with extra attributes', () => {
+  describe('Harmonised EU', () => {
+    const options = dataHarmonisedEU;
+
+    test('renders correctly', () => {
       expect.assertions(1);
-
-      const withExtraAttributes = merge(options, {
-        extra_attributes: [
-          { name: 'data-test', value: 'data-test-value' },
-          { name: 'data-test-1', value: 'data-test-value-1' },
-        ],
-      });
-
-      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+      return expect(render(options)).resolves.toMatchSnapshot();
     });
 
     test(`passes the accessibility tests`, async () => {
       expect(
-        await axe(await renderTwigFileAsHtml(template, dataCoreEU, true)),
+        await axe(await renderTwigFileAsHtml(template, dataHarmonisedEU, true)),
       ).toHaveNoViolations();
     });
   });
