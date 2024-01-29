2a0556cd29e45a6dde98063d72c858a0c8de5e59
planctus <davidepuntoferrante@gmail.com>
2024-01-29 14:50:32 +0200

    fix(select): not preventing default when using enter or space in a default select - FRONT-4230 (#3205)

--- src/implementations/vanilla/components/select/select.js
+++ src/implementations/vanilla/components/select/select.js
@@ -925,11 +925,17 @@ export class Select {
 
       case ' ':
       case 'Enter':
-      case 'ArrowDown':
-        e.preventDefault();
         this.handleToggle(e);
         if (this.multiple) {
+          e.preventDefault();
           this.search.focus();
+        }
+        break;
+
+      case 'ArrowDown':
+        if (this.multiple) {
+          e.preventDefault();
+          this.handleToggle(e);
         }
         break;
 
