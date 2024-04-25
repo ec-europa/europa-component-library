0787bb21653212f09ded54dfb9043bc54abf5c6a
planctus <davidepuntoferrante@gmail.com>
2024-04-25 16:54:32 +0300

    feat(slider): Still on the renaming - FRONT-4252

--- src/implementations/twig/components/slider/slider.story.js
+++ src/implementations/twig/components/slider/slider.story.js
@@ -2,7 +2,7 @@ import withCode from '@ecl/storybook-addon-code';
 import { withNotes } from '@ecl/storybook-addon-notes';
 
 import slider from './slider.html';
-import sliderTags from './slider.fluid.html';
+import sliderFluid from './slider.fluid.html';
 import notes from './README.md';
 import notesFluid from './README-fluid.md';
 
@@ -21,7 +21,7 @@ export const Default = () => slider;
 Default.storyName = 'default';
 Default.parameters = { notes: { markdown: notes } };
 
-export const Tags = () => sliderTags;
+export const Fluid = () => sliderFluid;
 
-Tags.storyName = 'fluid';
-Tags.parameters = { notes: { markdown: notesFluid } };
+Fluid.storyName = 'fluid';
+Fluid.parameters = { notes: { markdown: notesFluid } };
