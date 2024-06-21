d9c414ed46bcbc0c3e3bceebdebd5d04b881dda4
planctus <davidepuntoferrante@gmail.com>
2024-06-21 09:59:56 +0300

    feat(lists): Using the same data for the variants - FRONT-4417

--- src/specs/components/unordered-list/demo/data--with-divider.js
+++ src/specs/components/unordered-list/demo/data--with-divider.js
@@ -5,7 +5,7 @@ module.exports = {
   variant: 'divider',
   items: [
     {
-      label: `<a class="ecl-link" href="${exampleLink}">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>`,
+      label: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
       nested: [
         {
           label: `<a class="ecl-link" href="${exampleLink}">Duis commodo nibh eget magna</a>`,
@@ -16,19 +16,19 @@ module.exports = {
       ],
     },
     {
-      label: `<a class="ecl-link" href="${exampleLink}">Nam dignissim condimentum pulvinar. Nullam volutpat tortor vel turpis iaculis feugiat. Vivamus eget turpis a est lacinia blandit</a>`,
+      label: `Nam dignissim condimentum pulvinar. Nullam volutpat tortor vel turpis iaculis feugiat. Vivamus eget turpis a est lacinia blandit`,
     },
     {
-      label: `<a class="ecl-link" href="${exampleLink}">Vestibulum sed accumsan ipsum</a>`,
+      label: `Vestibulum sed accumsan ipsum`,
       nested: [
         {
           label: `<a class="ecl-link" href="${exampleLink}">Morbi vitae tortor accumsan</a>`,
         },
         {
-          label: `<a class="ecl-link" href="${exampleLink}">Nulla facilisi</a>`,
+          label: `Nulla facilisi`,
         },
         {
-          label: `<a class="ecl-link" href="${exampleLink}">Phasellus in metus et libero scelerisque sagittis sollicitudin at lectus</a>`,
+          label: `Phasellus in metus et libero scelerisque sagittis sollicitudin at lectus`,
         },
       ],
     },
