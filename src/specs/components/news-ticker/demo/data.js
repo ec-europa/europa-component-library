cfae8cd1313267d909198a0115fb6d04983a255b
planctus <davidepuntoferrante@gmail.com>
2024-06-24 17:50:46 +0300

    feat(news-ticker): Each item with its own icon, increased height in intermediate screens - FRONT-4463

--- src/specs/components/news-ticker/demo/data.js
+++ src/specs/components/news-ticker/demo/data.js
@@ -15,32 +15,50 @@ module.exports = {
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et finibus mi. Sed hendrerit velit id ex ornare interdum. Duis suscipit tortor vitae mi feugiat suscipit. Morbi consequat tortor magna, ac malesuada nunc placerat non. Vivamus pulvinar augue tristique lectus lobortis iaculis.',
       link: exampleLink,
+      icon: {
+        name: 'euro',
+      },
     },
     {
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et finibus mi. Sed hendrerit velit id ex ornare interdum. Duis suscipit tortor vitae mi feugiat suscipit. Morbi consequat tortor magna, ac malesuada nunc placerat non. Vivamus pulvinar augue tristique lectus lobortis iaculis. Nam condimentum aliquam sem, a bibendum mauris faucibus id. In euismod semper ex et tincidunt. Donec eu augue posuere, feugiat enim ac, faucibus tortor.',
       link: exampleLink,
       external: true,
+      icon: {
+        name: 'euro',
+      },
     },
     {
       content:
         'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
       link: exampleLink,
+      icon: {
+        name: 'euro',
+      },
     },
     {
       content:
         'Excepteur sint occaecat cupidatat officia deserunt mollit anim id est laborum',
       link: exampleLink,
+      icon: {
+        name: 'euro',
+      },
     },
     {
       content:
         'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium',
       link: exampleLink,
+      icon: {
+        name: 'euro',
+      },
     },
     {
       content:
         'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia consequuntur magni dolores',
       link: exampleLink,
+      icon: {
+        name: 'euro',
+      },
     },
   ],
 };
