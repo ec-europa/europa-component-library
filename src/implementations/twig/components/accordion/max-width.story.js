892d819d4fcb95bab3c77779338ebbe380d36763
planctus <davidepuntoferrante@gmail.com>
2024-07-08 14:52:33 +0300

    chore(max-width): Poc for custom max-width - FRONT-4485

---
+++ src/implementations/twig/components/accordion/max-width.story.js
@@ -0,0 +1,23 @@
+export default {
+  title: 'Components/Max-width',
+};
+
+export const Default = () =>
+  `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non efficitur dui. Pellentesque hendrerit finibus massa, sed tristique ex. Donec a lacus faucibus, cursus est non, interdum metus. Mauris rutrum lobortis urna, sit amet gravida nulla porttitor et. Sed suscipit, nulla id eleifend rutrum, leo tortor consectetur ipsum, vitae aliquam est nulla quis libero. Praesent vitae nibh vel justo dignissim ultrices. Integer efficitur suscipit urna eu consectetur. Proin egestas, sapien ut euismod gravida, tortor turpis ultricies massa, at sollicitudin augue nisl pulvinar dui. Praesent hendrerit odio finibus tortor mattis, in mollis magna commodo. Etiam imperdiet lorem leo, a sollicitudin justo viverra in. Fusce interdum hendrerit enim vel efficitur. Sed ultrices tincidunt metus nec bibendum. In sed maximus nisl, feugiat malesuada turpis. Morbi quis est tincidunt, dignissim sem non, dignissim mi. Vestibulum sit amet feugiat orci, ut mollis arcu. Nulla vitae purus ut neque ornare pulvinar.</p>`;
+Default.storyName = 'default';
+
+export const Custom =
+  () => `<div style="--max-w: 400px"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Donec non efficitur dui. Pellentesque hendrerit finibus massa, sed tristique ex. 
+    Donec a lacus faucibus, cursus est non, interdum metus. 
+    Mauris rutrum lobortis urna, sit amet gravida nulla porttitor et.
+    Sed suscipit, nulla id eleifend rutrum, leo tortor consectetur ipsum, vitae aliquam est nulla quis libero.
+    Praesent vitae nibh vel justo dignissim ultrices.
+    Integer efficitur suscipit urna eu consectetur.
+    Proin egestas, sapien ut euismod gravida, tortor turpis ultricies massa, at sollicitudin augue nisl pulvinar dui.
+    Praesent hendrerit odio finibus tortor mattis, in mollis magna commodo.
+    Etiam imperdiet lorem leo, a sollicitudin justo viverra in.
+    Fusce interdum hendrerit enim vel efficitur. Sed ultrices tincidunt metus nec bibendum.
+    In sed maximus nisl, feugiat malesuada turpis. Morbi quis est tincidunt, dignissim sem non, dignissim mi.
+    Vestibulum sit amet feugiat orci, ut mollis arcu. Nulla vitae purus ut neque ornare pulvinar.</p></div>`;
+Custom.storyName = 'custom';
