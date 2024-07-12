188c9f01d055635e755a531f93f210d11eeca032
Romain Emery <emery.romain@gmail.com>
2024-07-12 10:12:39 +0200

    feat(modal): update styles - FRONT-4453 (#3431)

--- src/website/src/pages/ec/components/modal/demo/index.js
+++ src/website/src/pages/ec/components/modal/demo/index.js
@@ -1,4 +1,4 @@
-import data from '@ecl/specs-component-modal/demo/data-ec';
+import data from '@ecl/specs-component-modal/demo/data';
 import template from '@ecl/twig-component-modal/modal.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
