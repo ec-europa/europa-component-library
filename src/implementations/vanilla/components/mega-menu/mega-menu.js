15366d9c051c9d706c88b9107bfeb4b675014a61
planctus <davidepuntoferrante@gmail.com>
2024-07-16 10:48:17 +0300

    feat(mega-menu): Not moving the focus when opening the menu - FRONT-4518 (#3488)
    * feat(mega-menu): Not moving the focus when opening the menu - FRONT-4518
    
    * feat(mega-menu): Focus on back in mobile when opening - FRONT-4518
    
    ---------
    
    Co-authored-by: Romain Emery <emery.romain@gmail.com>

--- src/implementations/vanilla/components/mega-menu/mega-menu.js
+++ src/implementations/vanilla/components/mega-menu/mega-menu.js
@@ -1198,20 +1198,6 @@ export class MegaMenu {
               item.setAttribute('aria-expanded', 'true');
               itemLink.setAttribute('aria-expanded', 'true');
               itemLink.setAttribute('aria-current', 'true');
-              const infoLink = queryOne('.ecl-mega-menu__info-link', item);
-              // Focus on the info link, if present
-              if (infoLink) {
-                infoLink.focus();
-              } else {
-                // Otherwise focus on the first element in the sub-list
-                const firstItem = queryOne(
-                  '.ecl-mega-menu__subitem:first-child .ecl-mega-menu__sublink',
-                  item,
-                );
-                if (firstItem) {
-                  firstItem.focus();
-                }
-              }
               this.backItemLevel1 = item;
             } else {
               item.setAttribute('aria-expanded', 'false');
@@ -1224,6 +1210,10 @@ export class MegaMenu {
             }
           }
         });
+
+        if (!this.isDesktop && this.back) {
+          this.back.focus();
+        }
 
         this.openPanel = {
           num: 1,
