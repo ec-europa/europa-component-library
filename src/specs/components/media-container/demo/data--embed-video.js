a576e74edd0440075e6b16543d09d46ddadeed10
emeryro <emery.romain@gmail.com>
2024-07-25 13:25:31 +0200

    remove hardcoded

--- src/specs/components/media-container/demo/data--embed-video.js
+++ src/specs/components/media-container/demo/data--embed-video.js
@@ -4,5 +4,5 @@ module.exports = {
   description:
     'The European Commission has put forward ambitious yet realistic proposals for a modern EU budget. It is time for an EU budget that reflects rapid developments in innovation, the economy, the environment and geopolitics, amongst others.',
   embedded_media:
-    '<iframe title="New digital strategy - Video player" width="350" height="197" src="https://www.youtube.com/embed/fgi-GSCB6ho" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>',
+    '<iframe width="350" height="197" src="https://www.youtube.com/embed/fgi-GSCB6ho" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>',
 };
