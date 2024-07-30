f526b267ed1433345a047812ec0bfcc565e71fc0
planctus <davidepuntoferrante@gmail.com>
2024-07-30 13:01:22 +0300

    feat(banner): Description as a link, some fixes - FRONT-4353 (#3522)
    * feat(banner): Description as a link, some fixes - FRONT-4353
    
    * feat(banner): removing unused arg - FRONT-4353
    
    * feat(banner): Aligning markup with the demo in the website pages - FRONT-4353

--- src/website/src/pages/ec/components/banner/demo/index.js
+++ src/website/src/pages/ec/components/banner/demo/index.js
@@ -4,5 +4,8 @@ import demoContentVideo from '@ecl/specs-component-banner/demo/data--video';
 import template from '@ecl/twig-component-banner/banner.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
+demoContentImage.description = demoContentImage.description.link.label;
+demoContentVideo.description = demoContentVideo.description.link.label;
+
 export const bannerImage = template(correctSvgPath(demoContentImage));
 export const bannerVideo = template(correctSvgPath(demoContentVideo));
