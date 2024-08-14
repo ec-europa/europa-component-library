b70c1322c67f213635f14a663cf83e6053cdfe5f
planctus <davidepuntoferrante@gmail.com>
2024-08-14 10:32:17 +0300

    feat(mega-menu): wax-on was-off - FRONT-4572

--- src/specs/components/mega-menu/demo/data.js
+++ src/specs/components/mega-menu/demo/data.js
@@ -48,9 +48,6 @@ module.exports = {
           sublink_id: 'item-2-1-id',
           see_all: true,
           see_all_label: 'View all',
-          see_all_attributes: [
-            { name: 'aria-label', value: 'View all sub-pages of this section' },
-          ],
           featured: {
             picture: {
               img: {
@@ -142,9 +139,6 @@ module.exports = {
           path: exampleLink,
           see_all: true,
           see_all_label: 'See all items',
-          see_all_attributes: [
-            { name: 'aria-label', value: 'View all sub-pages of this section' },
-          ],
           sublink_id: 'research-and-innovation-id',
           featured: {
             picture: {
