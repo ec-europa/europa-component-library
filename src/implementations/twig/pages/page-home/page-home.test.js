852872f04d856e7b5897252c4856ffde1d98287e
emeryro <emery.romain@gmail.com>
2024-09-03 12:30:08 +0200

    pages

--- src/implementations/twig/pages/page-home/page-home.test.js
+++ src/implementations/twig/pages/page-home/page-home.test.js
@@ -1,21 +1,22 @@
 import { renderTwigFileAsNode, renderTwigFileAsHtml } from '@ecl/test-utils';
 import { axe, toHaveNoViolations } from 'jest-axe';
+import dataHome from '@ecl/specs-page-home/demo/data';
 
 expect.extend(toHaveNoViolations);
 
-describe('Page example - Inner', () => {
-  const template = '@ecl/inner/inner.html.twig';
+describe('Page example - Home', () => {
+  const template = '@ecl/page-home/page-home.html.twig';
   const render = (params) => renderTwigFileAsNode(template, params);
 
   test('renders correctly', () => {
     expect.assertions(1);
 
-    return expect(render()).resolves.toMatchSnapshot();
+    return expect(render(dataHome)).resolves.toMatchSnapshot();
   });
 
   test(`passes the accessibility tests`, async () => {
     expect(
-      await axe(await renderTwigFileAsHtml(template, {}, true)),
+      await axe(await renderTwigFileAsHtml(template, dataHome)),
     ).toHaveNoViolations();
-  });
+  }, 20000);
 });
--- src/implementations/twig/pages/page-inner/page-inner.test.js
+++ src/implementations/twig/pages/page-inner/page-inner.test.js
@@ -1,21 +1,22 @@
 import { renderTwigFileAsNode, renderTwigFileAsHtml } from '@ecl/test-utils';
 import { axe, toHaveNoViolations } from 'jest-axe';
+import dataInner from '@ecl/specs-page-inner/demo/data';
 
 expect.extend(toHaveNoViolations);
 
-describe('Page example - Home', () => {
-  const template = '@ecl/home/home.html.twig';
+describe('Page example - Inner', () => {
+  const template = '@ecl/page-inner/page-inner.html.twig';
   const render = (params) => renderTwigFileAsNode(template, params);
 
   test('renders correctly', () => {
     expect.assertions(1);
 
-    return expect(render()).resolves.toMatchSnapshot();
+    return expect(render(dataInner)).resolves.toMatchSnapshot();
   });
 
   test(`passes the accessibility tests`, async () => {
     expect(
-      await axe(await renderTwigFileAsHtml(template, {}, true)),
+      await axe(await renderTwigFileAsHtml(template, dataInner)),
     ).toHaveNoViolations();
-  });
+  }, 20000);
 });
