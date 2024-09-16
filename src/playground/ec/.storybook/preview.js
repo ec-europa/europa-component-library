f7cf00b42d87184c83479713fbd4563e9cda2ebd
planctus <davidepuntoferrante@gmail.com>
2024-09-16 14:05:41 +0300

    feat(easy-what): Adding another optional css, named easy-to-read - FRONT-4598

--- src/playground/ec/.storybook/preview.js
+++ src/playground/ec/.storybook/preview.js
@@ -57,6 +57,12 @@ export const parameters = {
       hideCode: true,
     },
     {
+      id: 'ecl-ec-easy-to-read',
+      code: `<link rel="stylesheet" type="text/css" href="./styles/optional/ecl-ec-easy-to-read.css" />`,
+      picked: false,
+      hideCode: true,
+    },
+    {
       id: 'ecl-ec-print',
       code: `<link rel="stylesheet" type="text/css" href="./styles/ecl-ec-print.css" />`,
       picked: false,
--- src/playground/eu/.storybook/preview.js
+++ src/playground/eu/.storybook/preview.js
@@ -51,6 +51,12 @@ export const parameters = {
       hideCode: true,
     },
     {
+      id: 'ecl-ec-easy-to-read',
+      code: `<link rel="stylesheet" type="text/css" href="./styles/optional/ecl-eu-easy-to-read.css" />`,
+      picked: false,
+      hideCode: true,
+    },
+    {
       id: 'ecl-rtl',
       code: `<link rel="stylesheet" type="text/css" href="./styles/optional/ecl-rtl.css" />`,
       picked: false,
