a471aad1b2978cb15f2154bcdde80a1b621fe22d
planctus <davidepuntoferrante@gmail.com>
2024-11-26 11:04:27 +0200

    fix(showcase): Including menu markup in the showcase examples - FRONT-4689

--- src/website/src/pages/eu/components/navigation/mega-menu/demo/index.js
+++ src/website/src/pages/eu/components/navigation/mega-menu/demo/index.js
@@ -1,8 +1,11 @@
 import dataFull from '@ecl/specs-component-site-header/demo/data--eu';
+import dataMegaMenu from '@ecl/specs-component-mega-menu/demo/data';
 import template from '@ecl/twig-component-site-header/site-header.html.twig';
 import { correctSvgPath } from '@ecl/website-utils';
 
-const dataCore = JSON.parse(JSON.stringify(dataFull));
+const dataWithMenu = { ...dataFull, mega_menu: dataMegaMenu };
+
+const dataCore = JSON.parse(JSON.stringify(dataWithMenu));
 delete dataCore.login_box;
 delete dataCore.site_name;
 delete dataCore.banner_top;
