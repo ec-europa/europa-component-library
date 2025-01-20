70febe401eacef8071d42a19a43cd3071651a997
planctus <davidepuntoferrante@gmail.com>
2025-01-20 10:25:05 +0200

    feat(v5): Merge components packages from P to R - FRONT-4720 (#3777)
    * chore(v5): Merging page-header packages - FRONT-4720
    
    * chore(v5): Merging pagination packages - FRONT-4720
    
    * chore(v5): Merging picture packages - FRONt-4720
    
    * chore(v5): Merging popover packages - FRONT-4720
    
    * chore(v5): Merging radio packages - FRONT-4720
    
    * chore(v5): Merging range packages - FRONT-4720
    
    * chore(v5): Merging rating field packages - FRONT-4720
    
    * chore(v5): Removing wrongly committed files - FRONT-4720
    
    * chore(v5): Fixes - FRONT-4720

--- src/components/form-group/form-group.test.js
+++ src/components/form-group/form-group.test.js
@@ -8,12 +8,12 @@ import { axe, toHaveNoViolations } from 'jest-axe';
 // Import data for tests
 import dataText from '@ecl/specs-component-text-input/demo/data';
 import dataSingle from '@ecl/select/demo/data-single';
-import dataDefault from '@ecl/specs-component-radio/demo/data--default';
+import dataDefault from '@ecl/radio/demo/data--default';
 import dataTextarea from '@ecl/specs-component-text-area/demo/data';
 import dataDatepicker from '@ecl/datepicker/demo/data';
 import dataCheckbox from '@ecl/checkbox/demo/data';
-import dataRange from '@ecl/specs-component-range/demo/data';
-import dataRatingField from '@ecl/specs-component-rating-field/demo/data';
+import dataRange from '@ecl/range/demo/data';
+import dataRatingField from '@ecl/rating-field/demo/data';
 import dataFile from '@ecl/file-upload/demo/data';
 
 expect.extend(toHaveNoViolations);
---
+++ src/components/pagination/pagination.story.js
@@ -0,0 +1,25 @@
+import { withNotes } from '@ecl/storybook-addon-notes';
+import withCode from '@ecl/storybook-addon-code';
+import { correctPaths } from '@ecl/story-utils';
+
+import specs from './demo/data';
+import pagination from './pagination.html.twig';
+import notes from './README.md';
+
+export default {
+  title: 'Components/Navigation/Pagination',
+  decorators: [withNotes, withCode],
+  parameters: {
+    controls: { disable: true },
+    parameters: { layout: 'fullscreen' },
+  },
+};
+
+export const Default = (_, { loaded: { component } }) => component;
+
+Default.render = async () => {
+  const renderedPagination = await pagination(correctPaths(specs));
+  return renderedPagination;
+};
+Default.storyName = 'default';
+Default.parameters = { notes: { markdown: notes, json: specs } };
