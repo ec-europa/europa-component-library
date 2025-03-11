04b8e4ef52fab306efcd7cb8e6dc4dba0f1ed31d
planctus <davidepuntoferrante@gmail.com>
2025-03-11 16:40:05 +0200

    chore(vike): fixing resizing of iframes in api pages

--- src/website/scripts/inject-mdx-listener.js
+++ src/website/scripts/inject-mdx-listener.js
@@ -52,7 +52,7 @@ for (const file of mdxFiles) {
 
               let button = document.createElement("button");
               button.id = "eclShowcaseShowMore";
-  
+
               if (code.nextSibling.id === 'eclShowcaseShowMore') {
                 return;
               }
@@ -81,4 +81,25 @@ for (const file of mdxFiles) {
 
   content = content.replace('</body>', `${listenerScript}</body>`);
   await writeFile(filePath, content);
+
+  const apiFiles = glob.sync('**/api/*.html', { cwd: mdxPagesDir });
+  for (const api of apiFiles) {
+    const apiFilePath = join(mdxPagesDir, api);
+    let apiContent = await readFile(apiFilePath, 'utf8');
+    // Skip if already injected
+    if (content.includes('api-iframe-resize')) continue;
+
+    const resizeScript = `
+          <script id="api-iframe-resize">
+            window.addEventListener("load", function () {
+              iframeResize({
+                license: 'GPLv3',
+                checkOrigin: false,
+              }, 'iframe');
+            });
+          </script>`;
+
+    apiContent = apiContent.replace('</body>', `${resizeScript}</body>`);
+    await writeFile(apiFilePath, apiContent);
+  }
 }
