1e2e6e29dc73eea2ce9f658209009ffc30aab408
emeryro <emery.romain@gmail.com>
2025-03-18 09:56:47 +0100

    update website

--- src/website/src/website-components/Icon/IconCard.jsx
+++ src/website/src/website-components/Icon/IconCard.jsx
@@ -3,22 +3,26 @@ import PropTypes from 'prop-types';
 
 import styles from './IconCard.scss';
 
-function IconCard({ name, set }) {
+function IconCard({ name, label, set }) {
   const cardClass = styles.card;
   let family = '';
   if (set === 'flag' || set === 'flag-non-members') family = '-flag';
   if (set === 'social-media') family = '-networks';
 
+  let iconLabel = name;
+  if (label !== '') iconLabel = `${name}\n(${label})`;
+
   return (
     <li className={cardClass}>
       <span className={`wt-icon${family}--${name} ${styles.icon}`} />
-      <div className={styles.title}>{name}</div>
+      <div className={styles.title}>{iconLabel}</div>
     </li>
   );
 }
 
 IconCard.propTypes = {
   name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
   set: PropTypes.string,
 };
 
