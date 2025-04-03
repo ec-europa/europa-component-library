c941b7ee5915460eeffae5827f540c21c201f3fa
Dumitru Tiberiu-Andrei <Tiberiu-Andrei.DUMITRU@ext.ec.europa.eu>
2025-04-03 11:18:52 +0300

    feat(social-media): Removed social-media-share and color variants from social media follow. - FRONT-4853 (#3925)

--- src/website/src/pages/ec/components/social-media-share/demo/index.js
+++
@@ -1,8 +0,0 @@
-import demoDefault from '@ecl/social-media-share/demo/data';
-import template from '@ecl/social-media-share/social-media-share.html.twig';
-import { correctSvgPath } from '@ecl/website-utils';
-
-export const socialMediaHorizontal = template(correctSvgPath(demoDefault));
-export const socialMediaVertical = template(
-  correctSvgPath({ ...demoDefault, variant: 'vertical' }),
-);
--- src/website/src/pages/eu/components/social-media-share/index.md
+++
@@ -1,8 +0,0 @@
----
-title: Social Media Share
-defaultTab: usage
-status: ready
-playground:
-  system: eu
-  path: /story/components-social-media-share--horizontal
----
