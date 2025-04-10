193203b856f9657b7ce6bc629c72dcb9c79b3f55
planctus <davidepuntoferrante@gmail.com>
2025-04-10 14:02:02 +0300

    chore(website): cssresources in root, only one react-router-dom version

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,8 +2,11 @@ module.exports = {
   hooks: {
     readPackage(pkg) {
       if (pkg.name === "@storybook/addon-cssresources") {
-        // pkg.dependencies = {};
-        // pkg.peerDependencies = {};
+        pkg.dependencies = { 
+          "global": "^4.4.0",
+          "regenerator-runtime": "^0.13.7",
+        };
+        pkg.peerDependencies = {};
       }
      
       return pkg;
