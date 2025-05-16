c940a216bf0cca994dc9f81927321dc2cff8991b
Romain Emery <emery.romain@gmail.com>
2025-05-16 11:04:06 +0200

    feat(color): update highlight color (#3985)

--- src/website/src/website-components/Color/Palette.jsx
+++ src/website/src/website-components/Color/Palette.jsx
@@ -1,32 +1,51 @@
-import React, { PureComponent, createRef } from 'react';
-import classnames from 'classnames';
+import React, {
+  useLayoutEffect,
+  useRef,
+  useState,
+  createContext,
+  useContext,
+} from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import styles from './Palette.module.scss';
 
-class Palette extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.paletteRef = createRef();
-  }
+const PaletteContext = createContext(null);
+export const usePaletteRef = () => useContext(PaletteContext);
 
-  render() {
-    const { children, mode } = this.props;
+export const Palette = ({ children, mode }) => {
+  const olRef = useRef(null);
 
-    return (
+  const [bgColor, setBgColor] = useState(null);
+
+  useLayoutEffect(() => {
+    const node = olRef.current;
+    if (!node) return;
+
+    const computed = getComputedStyle(node);
+    setBgColor(computed.getPropertyValue('background-color'));
+  }, []);
+
+  return (
+    <PaletteContext.Provider value={olRef}>
       <ol
-        ref={this.paletteRef}
-        className={classnames(styles.palette, {
+        ref={olRef}
+        className={classNames(styles.palette, {
           [styles[`mode--${mode}`]]: mode,
         })}
       >
         {React.Children.map(children, (child) =>
-          React.cloneElement(child, { parentRef: this.paletteRef }),
+          React.isValidElement(child)
+            ? React.cloneElement(child, {
+                paletteRef: olRef,
+                paletteBg: bgColor,
+              })
+            : child,
         )}
       </ol>
-    );
-  }
-}
+    </PaletteContext.Provider>
+  );
+};
 
 Palette.propTypes = {
   children: PropTypes.node.isRequired,
