fc4a6e5cac5f667bae636e7b0a9d48a8c5ebf64f
Romain Emery <emery.romain@gmail.com>
2025-05-20 09:14:27 +0200

    update container (#3984)

--- src/website/src/components/SimplePage/SimplePage.jsx
+++ src/website/src/components/SimplePage/SimplePage.jsx
@@ -8,7 +8,7 @@ import Container from '../Grid/Container';
 const HomePage = React.memo(({ children }) => (
   <main id="main-content" tabIndex="-1">
     <ScrollToTopOnMount />
-    <Container spacing="pv-2xl">{children}</Container>
+    <Container spacing="pv-6xl">{children}</Container>
   </main>
 ));
 
