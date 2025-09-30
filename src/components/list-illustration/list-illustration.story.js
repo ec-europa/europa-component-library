6ebe07c4bcbf5b6a994f1819fd0832bb2bafad61
planctus <davidepuntoferrante@gmail.com>
2025-09-30 15:09:19 +0300

    feat(list-illustration): Style iteration, adding divider in number list - FRONT-5094

--- src/components/list-illustration/list-illustration.story.js
+++ src/components/list-illustration/list-illustration.story.js
@@ -361,7 +361,9 @@ const getVariantArgs = (data) => {
   if (getSystem() === 'ec') {
     args.color_mode = 'default';
   }
+  args.title = data.items[0].title;
   args.description = data.items[0].description;
+  args.divider = false;
 
   return args;
 };
@@ -371,6 +373,12 @@ const getVariantArgTypes = () => {
     ...getColorModeControls(),
     description: {
       control: { type: 'text' },
+    },
+    title: {
+      control: { type: 'text' },
+    },
+    divider: {
+      control: { type: 'boolean' },
     },
   };
 };
@@ -577,7 +585,6 @@ IconList.storyName = 'icon list';
 IconList.args = {
   ...getVariantArgs(dataListIllustrationIconList),
   icon: 'check-bold',
-  divider: false,
   column: 1,
 };
 IconList.argTypes = {
@@ -587,9 +594,6 @@ IconList.argTypes = {
       type: 'select',
     },
     options: ['check-bold', 'arrow-right-bold', 'close-bold'],
-  },
-  divider: {
-    control: { type: 'boolean' },
   },
   column: {
     name: 'number of columns',
