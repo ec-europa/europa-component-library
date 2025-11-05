ca832ef569bd77387d83f80d9eca42913a5230bc
planctus <davidepuntoferrante@gmail.com>
2025-11-05 14:46:21 +0200

    feat(tabs): Fixing test - FRONT-5144

--- src/components/tabs/demo/data.js
+++ src/components/tabs/demo/data.js
@@ -4,19 +4,20 @@ const exampleLink = `${publicUrl}/example`;
 
 module.exports = {
   items: [
-    { label: 'Tab label', path: exampleLink },
-    { label: 'Tab label', path: exampleLink },
-    { label: 'Tab label', path: exampleLink },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-1' },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-2' },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-3' },
     {
       label: 'Tab label',
       path: exampleLink,
       is_current: true,
+      id: 'tabs-4',
     },
-    { label: 'Tab label', path: exampleLink },
-    { label: 'Tab label', path: exampleLink },
-    { label: 'Tab label', path: exampleLink },
-    { label: 'Tab label', path: exampleLink },
-    { label: 'Tab label', path: exampleLink },
-    { label: 'Tab label', path: exampleLink },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-5' },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-6' },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-7' },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-8' },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-9' },
+    { label: 'Tab label', path: exampleLink, id: 'tabs-10' },
   ],
 };
