20254bf437b92f4c8220310903c6b8878641997b
emeryro <emery.romain@gmail.com>
2025-11-19 09:22:59 +0100

    horizontal meta

--- src/components/content-item/demo/data--default.js
+++ src/components/content-item/demo/data--default.js
@@ -34,6 +34,13 @@ module.exports = {
       },
       label: 'Luxembourg',
     },
+    {
+      icon: {
+        name: 'clock',
+        size: 'xs',
+      },
+      label: '1 hour',
+    },
   ],
   lists: [
     {
