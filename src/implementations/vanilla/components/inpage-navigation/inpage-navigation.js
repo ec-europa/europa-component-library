cb07eab5c9f6229c60d2ade0630a7adc9cd51d55
claurennt <c.benedetti1989@gmail.com>
2025-12-03 11:08:17 +0100

    fix(Inpage-navigation): focus heading programamtically when relative anchor is selected

--- src/implementations/vanilla/components/inpage-navigation/inpage-navigation.js
+++ src/implementations/vanilla/components/inpage-navigation/inpage-navigation.js
@@ -516,6 +516,11 @@ export class InpageNavigation {
     // Untrap focus
     this.focusTrap.deactivate();
 
+    if (heading) {
+      heading.setAttribute('tabindex', '-1');
+      heading.focus();
+    }
+
     const eventData = { target: heading || href, e };
     this.trigger('onClick', eventData);
   }
