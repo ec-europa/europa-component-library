6915255ac6825cd8e7899cbbe8559b3f7239d9c8
emeryro <emery.romain@gmail.com>
2026-04-08 09:46:37 +0200

    add micro title

--- src/components/featured-item/demo/data.js
+++ src/components/featured-item/demo/data.js
@@ -4,7 +4,8 @@ const exampleLink = `${publicUrl}/example`;
 module.exports = {
   id: 'featured-item-example',
   title:
-    'Title: Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+    'Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+  micro_title: 'about',
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   link: {
