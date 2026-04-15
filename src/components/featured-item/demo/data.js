d2c0c491bc235f9867f42beb42c0651f21f3d206
emeryro <emery.romain@gmail.com>
2026-04-15 11:41:22 +0200

    spacing, data

--- src/components/featured-item/demo/data.js
+++ src/components/featured-item/demo/data.js
@@ -27,8 +27,7 @@ module.exports = {
         src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
       },
     },
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elite tempored incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipiscing',
+    description: 'Lorem ipsum dolor amet consectetur adipiscing.',
     credit: '@Copyright',
   },
 };
