50f0c1085483d6cf1f14fe53ab6a515c722b341d
Romain Emery <emery.romain@gmail.com>
2026-04-23 10:35:46 +0200

    feat(featured-item): add micro title - FRONT-5281 (#4360)
    * add micro title
    
    * story
    
    * story
    
    * fix content
    
    * spacing, data
    
    * update snap
    
    * apply feedback
    
    * apply feedback
    
    * chromatic
    
    ---------
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

--- src/components/featured-item/demo/data.js
+++ src/components/featured-item/demo/data.js
@@ -4,7 +4,8 @@ const exampleLink = `${publicUrl}/example`;
 module.exports = {
   id: 'featured-item-example',
   title:
-    'Title: Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+    'Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+  micro_title: 'about',
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   link: {
@@ -26,8 +27,7 @@ module.exports = {
         src: 'https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg',
       },
     },
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elite tempored incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipiscing',
+    description: 'Lorem ipsum dolor amet consectetur adipiscing.',
     credit: '@Copyright',
   },
 };
