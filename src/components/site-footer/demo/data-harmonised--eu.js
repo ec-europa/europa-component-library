6794a2bb316f68f9a8c6a7b1f3d954ea2be42d0c
emeryro <emery.romain@gmail.com>
2026-05-20 15:31:08 +0200

    update demo

--- src/components/site-footer/demo/data-harmonised--eu.js
+++ src/components/site-footer/demo/data-harmonised--eu.js
@@ -61,7 +61,43 @@ module.exports = {
           links: [
             {
               link: {
-                label: 'Social 1',
+                label: 'Mastodon',
+                path: exampleLink,
+                icon_position: 'before',
+              },
+              icon: {
+                name: 'mastodon',
+                size: 'xs',
+                family: 'networks',
+              },
+            },
+            {
+              link: {
+                label: 'LinkedIn',
+                path: exampleLink,
+                icon_position: 'before',
+              },
+              icon: {
+                name: 'linkedin',
+                size: 'xs',
+                family: 'networks',
+              },
+            },
+            {
+              link: {
+                label: 'Bluesky',
+                path: exampleLink,
+                icon_position: 'before',
+              },
+              icon: {
+                name: 'bluesky',
+                size: 'xs',
+                family: 'networks',
+              },
+            },
+            {
+              link: {
+                label: 'Facebook',
                 path: exampleLink,
                 icon_position: 'before',
               },
@@ -71,26 +107,28 @@ module.exports = {
                 family: 'networks',
               },
             },
+
             {
               link: {
-                label: 'Social 2',
+                label: 'YouTube',
                 path: exampleLink,
                 icon_position: 'before',
               },
               icon: {
-                name: 'twitter',
+                name: 'youtube',
                 size: 'xs',
                 family: 'networks',
               },
             },
+
             {
               link: {
-                label: 'Social 3',
+                label: 'Other',
                 path: exampleLink,
                 icon_position: 'before',
               },
               icon: {
-                name: 'mastodon',
+                name: 'chain',
                 size: 'xs',
                 family: 'networks',
               },
