946375d213ee640f23011f1ed4d82173aa5d04cb
emeryro <emery.romain@gmail.com>
2026-06-02 10:04:45 +0200

    link color

--- src/components/featured-item/demo/data.js
+++ src/components/featured-item/demo/data.js
@@ -7,7 +7,7 @@ module.exports = {
     'Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
   micro_title: 'about',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, <a class="ecl-link" href="#">sed do eiusmod tempor</a> incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   link: {
     link: {
       type: 'standalone',
