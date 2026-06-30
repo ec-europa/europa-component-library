e44da619e42502b71f6ae016de895f963f069a16
emeryro <emery.romain@gmail.com>
2026-06-30 09:38:41 +0200

    put code in list

--- src/components/unordered-list/demo/data--link.js
+++ src/components/unordered-list/demo/data--link.js
@@ -18,7 +18,7 @@ module.exports = {
       label: `<a class="ecl-link ecl-link--standalone" href="${exampleLink}">Nam dignissim condimentum pulvinar. Nullam volutpat tortor vel turpis iaculis feugiat. Vivamus eget turpis a est lacinia blandit</a>`,
     },
     {
-      label: `<a class="ecl-link" href="${exampleLink}">Vestibulum sed accumsan ipsum</a>`,
+      label: `<a class="ecl-link ecl-link--standalone" href="${exampleLink}">Vestibulum sed accumsan ipsum</a>`,
       nested: [
         {
           label: `<a class="ecl-link" href="${exampleLink}">Morbi vitae tortor accumsan</a>`,
